86771c1349ac9fbf2dc03f8d031c8eeeaccff490
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-01 23:02:24 +0000

    614a35d feat(compiler): read and write `.ngsummary.json` files

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-rc.0-82c81cd
+ * @license Angular v2.3.0-rc.0-614a35d
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3489,7 +3489,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-rc.0-82c81cd');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-rc.0-614a35d');
 
     exports.VERSION = VERSION;
     exports.PlatformLocation = PlatformLocation;
