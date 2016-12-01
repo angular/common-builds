e5d1d78e9b1255a271be7c8e77be94ef1093c962
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-01 00:00:25 +0000

    c066281 docs(changelog): add changelog for 2.3.0-rc.0

--- index.js
+++ index.js
@@ -10,9 +10,14 @@
  * @description
  * Entry point for all public APIs of the common package.
  */
-export * from './src/location';
+export { PlatformLocation, LocationStrategy, APP_BASE_HREF, HashLocationStrategy, PathLocationStrategy, Location } from './src/location';
 export { NgLocalization } from './src/localization';
 export { CommonModule } from './src/common_module';
 export { NgClass, NgFor, NgIf, NgPlural, NgPluralCase, NgStyle, NgSwitch, NgSwitchCase, NgSwitchDefault, NgTemplateOutlet } from './src/directives/index';
 export { AsyncPipe, DatePipe, I18nPluralPipe, I18nSelectPipe, JsonPipe, LowerCasePipe, CurrencyPipe, DecimalPipe, PercentPipe, SlicePipe, UpperCasePipe } from './src/pipes/index';
+import { Version } from '@angular/core';
+/**
+ * @stable
+ */
+export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-c066281');
 //# sourceMappingURL=index.js.map
