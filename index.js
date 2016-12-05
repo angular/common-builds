6976fa1d10cc26eea128a382604db9204051f441
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-05 18:37:54 +0000

    b8c839b docs(cheatsheet): correct HostBinding decorator example (#13206)

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-d2e5198');
+export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-b8c839b');
 //# sourceMappingURL=index.js.map
