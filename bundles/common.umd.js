da25e575b84dcc786cb4a602829f2e1b360b67c9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-06 18:08:52 +0000

    93556a5 fix(language-service): avoid throwing for invalid class declarations (#13257)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-rc.0-5614c4f
+ * @license Angular v2.3.0-rc.0-93556a5
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3478,7 +3478,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-rc.0-5614c4f');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-rc.0-93556a5');
 
     exports.VERSION = VERSION;
     exports.PlatformLocation = PlatformLocation;
