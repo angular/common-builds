1a93ab03acb9f6b1dc6eba2eeeb4e07bc760b223
Victor Berchet <victor@suumit.com>
2016-12-08 19:37:25 +0000

    9591a08 fix(router): Use T type in Resolve interface (#13242)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-65965c2
+ * @license Angular v2.3.0-9591a08
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3478,7 +3478,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-65965c2');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-9591a08');
 
     exports.NgLocalization = NgLocalization;
     exports.CommonModule = CommonModule;
