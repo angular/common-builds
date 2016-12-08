d553e64c0d10c5e1ab0e7502be7489687ad2bcc4
Victor Berchet <victor@suumit.com>
2016-12-08 19:37:15 +0000

    65965c2 docs(changelog): fix a typo (#13298)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-13b41bd
+ * @license Angular v2.3.0-65965c2
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3478,7 +3478,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-13b41bd');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-65965c2');
 
     exports.NgLocalization = NgLocalization;
     exports.CommonModule = CommonModule;
