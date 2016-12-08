867481d49ac3504d1267b19d310ea4c5af0084da
GitHub <noreply@github.com>
2016-12-08 22:01:36 +0000

    1b54788 build(tslint): enable no-inner-declarations (#13316)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-9591a08
+ * @license Angular v2.3.0-1b54788
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3478,7 +3478,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-9591a08');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-1b54788');
 
     exports.NgLocalization = NgLocalization;
     exports.CommonModule = CommonModule;
