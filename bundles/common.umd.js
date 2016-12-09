6d9554578ba772ecfea61d42b400a2a417b656d6
Victor Berchet <victor@suumit.com>
2016-12-09 02:06:09 +0000

    4e3d58a Revert "fix(compiler): fix transpiled ES5 code (#13322)"

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-61d7c1e
+ * @license Angular v2.3.0-4e3d58a
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3496,7 +3496,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-61d7c1e');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-4e3d58a');
 
     exports.NgLocalization = NgLocalization;
     exports.CommonModule = CommonModule;
