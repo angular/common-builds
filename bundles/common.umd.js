e4b346f9fee3e0f887f4d4932c3b8209af7a6411
Victor Berchet <victor@suumit.com>
2016-12-09 23:52:15 +0000

    08ff2e5 fix(http): check response body text against undefined (#13017)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-90c2235
+ * @license Angular v2.3.0-08ff2e5
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3619,7 +3619,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-90c2235');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-08ff2e5');
 
     exports.NgLocalization = NgLocalization;
     exports.CommonModule = CommonModule;
