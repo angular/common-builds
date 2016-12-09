5053f57a60126230d2411b9ac5f1a9965e6888b7
Victor Berchet <victor@suumit.com>
2016-12-09 23:53:46 +0000

    8a8c532 fix(dom_adapter): remove logError from logGroup (#12925)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-08ff2e5
+ * @license Angular v2.3.0-8a8c532
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3619,7 +3619,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-08ff2e5');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-8a8c532');
 
     exports.NgLocalization = NgLocalization;
     exports.CommonModule = CommonModule;
