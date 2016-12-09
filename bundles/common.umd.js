9de01c99a45fc6e099b5c003642adca99b9a5d59
GitHub <noreply@github.com>
2016-12-09 19:35:19 +0000

    4022173 fix(compiler): fix PR 13322 (#13331)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-c8baf51
+ * @license Angular v2.3.0-4022173
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3619,7 +3619,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-c8baf51');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-4022173');
 
     exports.NgLocalization = NgLocalization;
     exports.CommonModule = CommonModule;
