f71852f44e18a9fda9a85cbd4aa23cb166950f29
Victor Berchet <victor@suumit.com>
2016-12-13 20:46:19 +0000

    d17e690 test(upgrade): fix failing test in browsers which do not support RAF

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-714f2af
+ * @license Angular v2.3.0-d17e690
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3598,7 +3598,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-714f2af');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-d17e690');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
