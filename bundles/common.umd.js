bc9cbad81b1a6d37372f111c67b1334470620368
Victor Berchet <victor@suumit.com>
2016-12-14 01:47:39 +0000

    d62d893 fix(compiler): generated CSS files suffixed with ngstyle. (#13353)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-7256d0e
+ * @license Angular v2.3.0-d62d893
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3598,7 +3598,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-7256d0e');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-d62d893');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
