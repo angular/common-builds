a0e1408141084176a4eb6302a7d7370e946df267
Victor Berchet <victor@suumit.com>
2016-12-14 22:45:26 +0000

    aa3769b fix(compiler): resolver should merge host bindings and listeners (#13474)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-d4ddb60
+ * @license Angular v2.3.0-aa3769b
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3601,7 +3601,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-d4ddb60');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-aa3769b');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
