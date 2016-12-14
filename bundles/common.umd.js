590c23e7ed81b42604e507f88c84ae73f7c7fd20
Victor Berchet <victor@suumit.com>
2016-12-14 17:07:07 +0000

    821b8f0 fix(forms): ensure select[multiple] retains selections

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-7b0a867
+ * @license Angular v2.3.0-821b8f0
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3598,7 +3598,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-7b0a867');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-821b8f0');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
