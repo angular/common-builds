4b399d6cd43a10dfae3cbde14507fef50245a678
Victor Berchet <victor@suumit.com>
2016-12-14 21:18:39 +0000

    d4ddb60 refactor: format & lint

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-5031adc
+ * @license Angular v2.3.0-d4ddb60
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3601,7 +3601,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-5031adc');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-d4ddb60');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
