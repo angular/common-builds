ebf0c68859028572e08f13ca416b5018d59876b5
Victor Berchet <victor@suumit.com>
2016-12-14 01:47:05 +0000

    7256d0e chore(internal API): introduce an internal API for ngtools. (#13415)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-f5f1d5f
+ * @license Angular v2.3.0-7256d0e
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3598,7 +3598,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-f5f1d5f');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-7256d0e');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
