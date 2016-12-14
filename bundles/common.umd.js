befd7a20ac47a2dfb4ab7f3923a8f97416c15515
Victor Berchet <victor@suumit.com>
2016-12-14 22:49:26 +0000

    aa40366 fix(compiler): fix simplify a reference without a name

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-aa3769b
+ * @license Angular v2.3.0-aa40366
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3601,7 +3601,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-aa3769b');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-aa40366');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
