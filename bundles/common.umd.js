fb44ff54020db22702ed2ba449a0020a584508a0
Victor Berchet <victor@suumit.com>
2016-12-14 16:48:02 +0000

    7b0a867 fix (forms): clear selected options when model is not an array (#12519)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-3edca4d
+ * @license Angular v2.3.0-7b0a867
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3598,7 +3598,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-3edca4d');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-7b0a867');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
