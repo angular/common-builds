646bfe6fab70977b9616487c13951bd400d4d231
GitHub <noreply@github.com>
2016-12-14 00:38:46 +0000

    42203ea fix(core): detectChanges() doesn't work on detached instance

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-a8d2375
+ * @license Angular v2.3.0-42203ea
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3598,7 +3598,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-a8d2375');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-42203ea');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
