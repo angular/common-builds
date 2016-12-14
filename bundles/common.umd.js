819ccbe6822af18a629b857ead7c6d2541477f56
Victor Berchet <victor@suumit.com>
2016-12-14 23:21:37 +0000

    169ed82 feat(testing): add overrideTemplate method (#13372)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-fd8e15b
+ * @license Angular v2.3.0-169ed82
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3601,7 +3601,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-fd8e15b');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-169ed82');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
