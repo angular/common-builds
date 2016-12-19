4440f0fd7315bc4457775ce3d632e4b518a5b8d3
GitHub <noreply@github.com>
2016-12-19 23:41:28 +0000

    e49c7fa refactor(compiler-cli): support extracting the mesage bundle without writing a file (#13580)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-6b65fc1
+ * @license Angular v4.0.0-beta.0-e49c7fa
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3612,7 +3612,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.0-6b65fc1');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.0-e49c7fa');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
