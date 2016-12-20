b9fac1fa9565febdefda02b76959524740f171cb
Chuck Jazdzewski <chuckj@google.com>
2016-12-20 00:39:49 +0000

    171a9bd feat: update to rxjs@5.0.1 and unpin the rxjs peerDeps via ^5.0.1 (#13572)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-e49c7fa
+ * @license Angular v4.0.0-beta.0-171a9bd
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3612,7 +3612,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.0-e49c7fa');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.0-171a9bd');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
