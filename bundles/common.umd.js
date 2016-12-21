046f5a721290e6b1d6d53c385b12b51b2adb4feb
Chuck Jazdzewski <chuckj@google.com>
2016-12-21 02:03:36 +0000

    c0f750a fix(compiler): ignore @import in comments (#13368)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-bcd37f5
+ * @license Angular v4.0.0-beta.0-c0f750a
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3630,7 +3630,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.0-bcd37f5');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.0-c0f750a');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
