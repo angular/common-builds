67837b3c1d6dabca5f6eae2fe93f6d6113229281
Chuck Jazdzewski <chuckj@google.com>
2016-12-21 00:12:06 +0000

    9b8488f build: fix publish-build-artifacts branch detection (#13599)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-1ee574c
+ * @license Angular v4.0.0-beta.0-9b8488f
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3612,7 +3612,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.0-1ee574c');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.0-9b8488f');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
