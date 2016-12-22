07eeaa12f172d973a936cea1df7970a74e848108
Hans <hans@hansl.ca>
2016-12-22 20:49:31 +0000

    67380d4 fix(testing): improve misleading error message when don't call compileComponents (#13543)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-e7ece6c
+ * @license Angular v4.0.0-beta.1-67380d4
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3630,7 +3630,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.1-e7ece6c');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.1-67380d4');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
