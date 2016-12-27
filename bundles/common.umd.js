baf3bd1d7bb8323f489466150e29026a9174260a
Hans Larsen <hans@hansl.ca>
2016-12-27 23:46:46 +0000

    0ac8e10 test(i18n): add extraction to integration specs

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-881eb89
+ * @license Angular v4.0.0-beta.1-0ac8e10
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3630,7 +3630,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.1-881eb89');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.1-0ac8e10');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
