5479b4358fff68b20b5c1666937aa61c7dafd259
Hans <hans@hansl.ca>
2016-12-27 22:22:08 +0000

    69fa3bb feat(router): add an extra argument to CanDeactivate interface (#13560)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-445ed43
+ * @license Angular v4.0.0-beta.1-69fa3bb
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3630,7 +3630,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.1-445ed43');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.1-69fa3bb');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
