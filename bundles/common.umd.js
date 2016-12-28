7caa1c57eac9ec439cec5e7ad13ef41c33c86c6b
Hans <hans@hansl.ca>
2016-12-28 00:41:28 +0000

    7c21064 fix(compiler): query `<template>` elements before their children. (#13677)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-07e0fce
+ * @license Angular v4.0.0-beta.1-7c21064
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3630,7 +3630,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.1-07e0fce');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.1-7c21064');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
