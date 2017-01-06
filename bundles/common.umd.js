a8adff27cbf99286070960d80da19c1769dc2a2a
Igor Minar <igor@angularjs.org>
2017-01-06 02:08:05 +0000

    1c85e99 chore(tsc-wrapped): bump version number to 4.0.0-beta.2

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-8063b0d
+ * @license Angular v4.0.0-beta.2-1c85e99
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3636,7 +3636,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.1-8063b0d');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.2-1c85e99');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
