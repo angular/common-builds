04f4c3f8b1a207fdd3fdb227427fa356528bb5cf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-19 20:21:18 +0000

    b049217 chore(docs): add missing comments (#14003)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.3-1200cf2
+ * @license Angular v4.0.0-beta.4-b049217
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3826,7 +3826,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.3-1200cf2');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.4-b049217');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
