7c1a7228340ab8ff006054fe0379b0db309dd814
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-20 22:40:29 +0000

    0589f93 Fixed documentation reference to canActivate in canDeactivate (#14018)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.4-2f87eb5
+ * @license Angular v4.0.0-beta.4-0589f93
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3826,7 +3826,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.4-2f87eb5');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.4-0589f93');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
