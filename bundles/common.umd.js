f0492417943a5549a94fd29040a0aafe215ca7f4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-23 19:40:43 +0000

    d3a3a8e fix(core): fix not declared variable in view engine (#14045)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.4-dff6ee3
+ * @license Angular v4.0.0-beta.4-d3a3a8e
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3826,7 +3826,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.4-dff6ee3');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.4-d3a3a8e');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
