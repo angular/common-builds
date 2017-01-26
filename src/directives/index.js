64936c4ec918e4ede2713e96d8d8c236cebc142b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-26 00:11:10 +0000

    c4e7c08 docs(changelog): add changelog for 4.0.0-beta.5

---
+++ src/directives/index.js
@@ -0,0 +1,34 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { NgClass } from './ng_class';
+import { NgComponentOutlet } from './ng_component_outlet';
+import { NgFor } from './ng_for';
+import { NgIf } from './ng_if';
+import { NgPlural, NgPluralCase } from './ng_plural';
+import { NgStyle } from './ng_style';
+import { NgSwitch, NgSwitchCase, NgSwitchDefault } from './ng_switch';
+import { NgTemplateOutlet } from './ng_template_outlet';
+export { NgClass, NgComponentOutlet, NgFor, NgIf, NgPlural, NgPluralCase, NgStyle, NgSwitch, NgSwitchCase, NgSwitchDefault, NgTemplateOutlet };
+/**
+ * A collection of Angular directives that are likely to be used in each and every Angular
+ * application.
+ */
+export var /** @type {?} */ COMMON_DIRECTIVES = [
+    NgClass,
+    NgComponentOutlet,
+    NgFor,
+    NgIf,
+    NgTemplateOutlet,
+    NgStyle,
+    NgSwitch,
+    NgSwitchCase,
+    NgSwitchDefault,
+    NgPlural,
+    NgPluralCase,
+];
+//# sourceMappingURL=index.js.map
