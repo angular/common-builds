575dd2a09138858f8049ea761249a3f2e41a3594
Miško Hevery <misko@hevery.com>
2017-01-27 21:21:10 +0000

    da41a95 docs: branding fixes (#14132)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-d339d8b
+ * @license Angular v4.0.0-beta.5-da41a95
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3857,7 +3857,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.5-d339d8b');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.5-da41a95');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
