b2157fb01faeba93625a4e64c1bff0d13463a5c4
Miško Hevery <misko@hevery.com>
2017-01-30 23:14:02 +0000

    104c157 build: fix red travis: fetch more github history (#14193)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-bc1320d
+ * @license Angular v4.0.0-beta.5-104c157
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3857,7 +3857,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.5-bc1320d');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.5-104c157');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
