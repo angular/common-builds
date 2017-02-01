8fe3aea9ddeb7316d0836df5e89d297f2e795846
Miško Hevery <misko@hevery.com>
2017-02-01 01:42:01 +0000

    e0e5e78 fix: lint errors to make circle-ci green

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-8270bec
+ * @license Angular v4.0.0-beta.5-e0e5e78
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3858,7 +3858,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.5-8270bec');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.5-e0e5e78');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
