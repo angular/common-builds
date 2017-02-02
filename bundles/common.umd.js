86edb718232d5cf57fa738dee37c5679e3c3ffa5
Miško Hevery <misko@hevery.com>
2017-02-02 05:53:26 +0000

    69a4bb0 doc: standardize deprecation of `preserveQueryParams` (4.0 only) (#14236)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-bc20e8a
+ * @license Angular v4.0.0-beta.5-69a4bb0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3981,7 +3981,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.5-bc20e8a');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.5-69a4bb0');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
