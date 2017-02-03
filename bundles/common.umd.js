ee4a97b0fd58cc3f12f1dcf5033d933c936d1cdd
Igor Minar <iminar@google.com>
2017-02-03 07:46:20 +0000

    a909643 feat(aio): delete unused kruft

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.6-559cf9d
+ * @license Angular v4.0.0-beta.6-a909643
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3981,7 +3981,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.6-559cf9d');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.6-a909643');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
