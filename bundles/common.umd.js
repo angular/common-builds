5c2a4335ae3fda46a01b163732a20069d113ea6c
Igor Minar <iminar@google.com>
2017-02-06 21:39:34 +0000

    80b66ed docs(zone): fix whitespace around backtick code

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.6-41db177
+ * @license Angular v4.0.0-beta.6-80b66ed
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3981,7 +3981,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.6-41db177');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.6-80b66ed');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
