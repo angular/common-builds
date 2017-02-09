625e895b3d9e40f4f275ce13c9a965bbc136fb2f
Misko Hevery <misko@hevery.com>
2017-02-09 04:56:00 +0000

    e90661a docs(changelog): add changelog for 2.4.7

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.6-059085b
+ * @license Angular v2.4.7-e90661a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3634,7 +3634,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.6-059085b');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.7-e90661a');
 
     exports.NgLocalization = NgLocalization;
     exports.CommonModule = CommonModule;
