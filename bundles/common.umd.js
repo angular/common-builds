f6759d62f09dd8e53c693848cfcf364522dc6886
Miško Hevery <misko@hevery.com>
2017-02-09 23:38:57 +0000

    44bb337 fix(compiler): disable non-components as an entry component (#14335)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-ac6f8e3
+ * @license Angular v4.0.0-beta.7-44bb337
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3877,7 +3877,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-ac6f8e3');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-44bb337');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
