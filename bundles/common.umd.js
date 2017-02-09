b41946590b065aadad223190cfe2e01c21a266c6
Miško Hevery <misko@hevery.com>
2017-02-09 17:27:31 +0000

    d1feb47 build(integration): fix tests (#14371)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.6-24af51a
+ * @license Angular v4.0.0-beta.6-d1feb47
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3880,7 +3880,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.6-24af51a');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.6-d1feb47');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
