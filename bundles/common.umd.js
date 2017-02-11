75211a6ced67ab244dc4bc8271e88462084214be
Misko Hevery <misko@hevery.com>
2017-02-11 05:15:54 +0000

    d3f174a ci: fix validate-commit-messages

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-8dd16bb
+ * @license Angular v4.0.0-beta.7-d3f174a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3877,7 +3877,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-8dd16bb');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-d3f174a');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
