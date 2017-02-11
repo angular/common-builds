43d25e179ed3ac9c4fb32fd4c83c489da024f036
GitHub <noreply@github.com>
2017-02-11 06:37:13 +0000

    2ca93e1 doc: update Node.js and npm version requirements

---
+++ src/location/hash_location_strategy.js
@@ -0,0 +1,133 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { Inject, Injectable, Optional } from '@angular/core/index';
+import { isPresent } from '../facade/lang';
+import { Location } from './location';
+import { APP_BASE_HREF, LocationStrategy } from './location_strategy';
+import { PlatformLocation } from './platform_location';
+/**
+ * \@whatItDoes Use URL hash for storing application location data.
+ * \@description
+ * `HashLocationStrategy` is a {\@link LocationStrategy} used to configure the
+ * {\@link Location} service to represent its state in the
+ * [hash fragment](https://en.wikipedia.org/wiki/Uniform_Resource_Locator#Syntax)
+ * of the browser's URL.
+ *
+ * For instance, if you call `location.go('/foo')`, the browser's URL will become
+ * `example.com#/foo`.
+ *
+ * ### Example
+ *
+ * {\@example common/location/ts/hash_location_component.ts region='LocationComponent'}
+ *
+ * \@stable
+ */
+export class HashLocationStrategy extends LocationStrategy {
+    /**
+     * @param {?} _platformLocation
+     * @param {?=} _baseHref
+     */
+    constructor(_platformLocation, _baseHref) {
+        super();
+        this._platformLocation = _platformLocation;
+        this._baseHref = '';
+        if (isPresent(_baseHref)) {
+            this._baseHref = _baseHref;
+        }
+    }
+    /**
+     * @param {?} fn
+     * @return {?}
+     */
+    onPopState(fn) {
+        this._platformLocation.onPopState(fn);
+        this._platformLocation.onHashChange(fn);
+    }
+    /**
+     * @return {?}
+     */
+    getBaseHref() { return this._baseHref; }
+    /**
+     * @param {?=} includeHash
+     * @return {?}
+     */
+    path(includeHash = false) {
+        // the hash value is always prefixed with a `#`
+        // and if it is empty then it will stay empty
+        let /** @type {?} */ path = this._platformLocation.hash;
+        if (!isPresent(path))
+            path = '#';
+        return path.length > 0 ? path.substring(1) : path;
+    }
+    /**
+     * @param {?} internal
+     * @return {?}
+     */
+    prepareExternalUrl(internal) {
+        const /** @type {?} */ url = Location.joinWithSlash(this._baseHref, internal);
+        return url.length > 0 ? ('#' + url) : url;
+    }
+    /**
+     * @param {?} state
+     * @param {?} title
+     * @param {?} path
+     * @param {?} queryParams
+     * @return {?}
+     */
+    pushState(state, title, path, queryParams) {
+        let /** @type {?} */ url = this.prepareExternalUrl(path + Location.normalizeQueryParams(queryParams));
+        if (url.length == 0) {
+            url = this._platformLocation.pathname;
+        }
+        this._platformLocation.pushState(state, title, url);
+    }
+    /**
+     * @param {?} state
+     * @param {?} title
+     * @param {?} path
+     * @param {?} queryParams
+     * @return {?}
+     */
+    replaceState(state, title, path, queryParams) {
+        let /** @type {?} */ url = this.prepareExternalUrl(path + Location.normalizeQueryParams(queryParams));
+        if (url.length == 0) {
+            url = this._platformLocation.pathname;
+        }
+        this._platformLocation.replaceState(state, title, url);
+    }
+    /**
+     * @return {?}
+     */
+    forward() { this._platformLocation.forward(); }
+    /**
+     * @return {?}
+     */
+    back() { this._platformLocation.back(); }
+}
+HashLocationStrategy.decorators = [
+    { type: Injectable },
+];
+/** @nocollapse */
+HashLocationStrategy.ctorParameters = () => [
+    { type: PlatformLocation, },
+    { type: undefined, decorators: [{ type: Optional }, { type: Inject, args: [APP_BASE_HREF,] },] },
+];
+function HashLocationStrategy_tsickle_Closure_declarations() {
+    /** @type {?} */
+    HashLocationStrategy.decorators;
+    /**
+     * @nocollapse
+     * @type {?}
+     */
+    HashLocationStrategy.ctorParameters;
+    /** @type {?} */
+    HashLocationStrategy.prototype._baseHref;
+    /** @type {?} */
+    HashLocationStrategy.prototype._platformLocation;
+}
+//# sourceMappingURL=hash_location_strategy.js.map
---
+++ src/location/path_location_strategy.js
@@ -0,0 +1,136 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { Inject, Injectable, Optional } from '@angular/core/index';
+import { isBlank } from '../facade/lang';
+import { Location } from './location';
+import { APP_BASE_HREF, LocationStrategy } from './location_strategy';
+import { PlatformLocation } from './platform_location';
+/**
+ * \@whatItDoes Use URL for storing application location data.
+ * \@description
+ * `PathLocationStrategy` is a {\@link LocationStrategy} used to configure the
+ * {\@link Location} service to represent its state in the
+ * [path](https://en.wikipedia.org/wiki/Uniform_Resource_Locator#Syntax) of the
+ * browser's URL.
+ *
+ * If you're using `PathLocationStrategy`, you must provide a {\@link APP_BASE_HREF}
+ * or add a base element to the document. This URL prefix that will be preserved
+ * when generating and recognizing URLs.
+ *
+ * For instance, if you provide an `APP_BASE_HREF` of `'/my/app'` and call
+ * `location.go('/foo')`, the browser's URL will become
+ * `example.com/my/app/foo`.
+ *
+ * Similarly, if you add `<base href='/my/app'/>` to the document and call
+ * `location.go('/foo')`, the browser's URL will become
+ * `example.com/my/app/foo`.
+ *
+ * ### Example
+ *
+ * {\@example common/location/ts/path_location_component.ts region='LocationComponent'}
+ *
+ * \@stable
+ */
+export class PathLocationStrategy extends LocationStrategy {
+    /**
+     * @param {?} _platformLocation
+     * @param {?=} href
+     */
+    constructor(_platformLocation, href) {
+        super();
+        this._platformLocation = _platformLocation;
+        if (isBlank(href)) {
+            href = this._platformLocation.getBaseHrefFromDOM();
+        }
+        if (isBlank(href)) {
+            throw new Error(`No base href set. Please provide a value for the APP_BASE_HREF token or add a base element to the document.`);
+        }
+        this._baseHref = href;
+    }
+    /**
+     * @param {?} fn
+     * @return {?}
+     */
+    onPopState(fn) {
+        this._platformLocation.onPopState(fn);
+        this._platformLocation.onHashChange(fn);
+    }
+    /**
+     * @return {?}
+     */
+    getBaseHref() { return this._baseHref; }
+    /**
+     * @param {?} internal
+     * @return {?}
+     */
+    prepareExternalUrl(internal) {
+        return Location.joinWithSlash(this._baseHref, internal);
+    }
+    /**
+     * @param {?=} includeHash
+     * @return {?}
+     */
+    path(includeHash = false) {
+        const /** @type {?} */ pathname = this._platformLocation.pathname +
+            Location.normalizeQueryParams(this._platformLocation.search);
+        const /** @type {?} */ hash = this._platformLocation.hash;
+        return hash && includeHash ? `${pathname}${hash}` : pathname;
+    }
+    /**
+     * @param {?} state
+     * @param {?} title
+     * @param {?} url
+     * @param {?} queryParams
+     * @return {?}
+     */
+    pushState(state, title, url, queryParams) {
+        const /** @type {?} */ externalUrl = this.prepareExternalUrl(url + Location.normalizeQueryParams(queryParams));
+        this._platformLocation.pushState(state, title, externalUrl);
+    }
+    /**
+     * @param {?} state
+     * @param {?} title
+     * @param {?} url
+     * @param {?} queryParams
+     * @return {?}
+     */
+    replaceState(state, title, url, queryParams) {
+        const /** @type {?} */ externalUrl = this.prepareExternalUrl(url + Location.normalizeQueryParams(queryParams));
+        this._platformLocation.replaceState(state, title, externalUrl);
+    }
+    /**
+     * @return {?}
+     */
+    forward() { this._platformLocation.forward(); }
+    /**
+     * @return {?}
+     */
+    back() { this._platformLocation.back(); }
+}
+PathLocationStrategy.decorators = [
+    { type: Injectable },
+];
+/** @nocollapse */
+PathLocationStrategy.ctorParameters = () => [
+    { type: PlatformLocation, },
+    { type: undefined, decorators: [{ type: Optional }, { type: Inject, args: [APP_BASE_HREF,] },] },
+];
+function PathLocationStrategy_tsickle_Closure_declarations() {
+    /** @type {?} */
+    PathLocationStrategy.decorators;
+    /**
+     * @nocollapse
+     * @type {?}
+     */
+    PathLocationStrategy.ctorParameters;
+    /** @type {?} */
+    PathLocationStrategy.prototype._baseHref;
+    /** @type {?} */
+    PathLocationStrategy.prototype._platformLocation;
+}
+//# sourceMappingURL=path_location_strategy.js.map
