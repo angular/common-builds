b5a0c69f9a972ee3213f00bd2230171235e3f3a2
GitHub <noreply@github.com>
2017-02-15 16:56:45 +0000

    59b2b65 build: update .pullapprove

---
+++ testing/location_mock.js
@@ -0,0 +1,101 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { EventEmitter, Injectable } from '@angular/core';
+/**
+ * A spy for {@link Location} that allows tests to fire simulated location events.
+ *
+ * @experimental
+ */
+export class SpyLocation {
+    constructor() {
+        this.urlChanges = [];
+        this._history = [new LocationState('', '')];
+        this._historyIndex = 0;
+        /** @internal */
+        this._subject = new EventEmitter();
+        /** @internal */
+        this._baseHref = '';
+        /** @internal */
+        this._platformStrategy = null;
+    }
+    setInitialPath(url) { this._history[this._historyIndex].path = url; }
+    setBaseHref(url) { this._baseHref = url; }
+    path() { return this._history[this._historyIndex].path; }
+    isCurrentPathEqualTo(path, query = '') {
+        const givenPath = path.endsWith('/') ? path.substring(0, path.length - 1) : path;
+        const currPath = this.path().endsWith('/') ? this.path().substring(0, this.path().length - 1) : this.path();
+        return currPath == givenPath + (query.length > 0 ? ('?' + query) : '');
+    }
+    simulateUrlPop(pathname) { this._subject.emit({ 'url': pathname, 'pop': true }); }
+    simulateHashChange(pathname) {
+        // Because we don't prevent the native event, the browser will independently update the path
+        this.setInitialPath(pathname);
+        this.urlChanges.push('hash: ' + pathname);
+        this._subject.emit({ 'url': pathname, 'pop': true, 'type': 'hashchange' });
+    }
+    prepareExternalUrl(url) {
+        if (url.length > 0 && !url.startsWith('/')) {
+            url = '/' + url;
+        }
+        return this._baseHref + url;
+    }
+    go(path, query = '') {
+        path = this.prepareExternalUrl(path);
+        if (this._historyIndex > 0) {
+            this._history.splice(this._historyIndex + 1);
+        }
+        this._history.push(new LocationState(path, query));
+        this._historyIndex = this._history.length - 1;
+        const locationState = this._history[this._historyIndex - 1];
+        if (locationState.path == path && locationState.query == query) {
+            return;
+        }
+        const url = path + (query.length > 0 ? ('?' + query) : '');
+        this.urlChanges.push(url);
+        this._subject.emit({ 'url': url, 'pop': false });
+    }
+    replaceState(path, query = '') {
+        path = this.prepareExternalUrl(path);
+        const history = this._history[this._historyIndex];
+        if (history.path == path && history.query == query) {
+            return;
+        }
+        history.path = path;
+        history.query = query;
+        const url = path + (query.length > 0 ? ('?' + query) : '');
+        this.urlChanges.push('replace: ' + url);
+    }
+    forward() {
+        if (this._historyIndex < (this._history.length - 1)) {
+            this._historyIndex++;
+            this._subject.emit({ 'url': this.path(), 'pop': true });
+        }
+    }
+    back() {
+        if (this._historyIndex > 0) {
+            this._historyIndex--;
+            this._subject.emit({ 'url': this.path(), 'pop': true });
+        }
+    }
+    subscribe(onNext, onThrow = null, onReturn = null) {
+        return this._subject.subscribe({ next: onNext, error: onThrow, complete: onReturn });
+    }
+    normalize(url) { return null; }
+}
+SpyLocation.decorators = [
+    { type: Injectable },
+];
+/** @nocollapse */
+SpyLocation.ctorParameters = () => [];
+class LocationState {
+    constructor(path, query) {
+        this.path = path;
+        this.query = query;
+    }
+}
+//# sourceMappingURL=location_mock.js.map
