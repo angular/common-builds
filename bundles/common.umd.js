698b3e7fc3d46da4167f6c79449ac30c937f6848
Igor Minar <iminar@google.com>
2017-02-15 05:22:18 +0000

    bb9c7ae feat: RendererV2 integration (#14469)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-b4d444a
+ * @license Angular v4.0.0-beta.7-bb9c7ae
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3877,7 +3877,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-b4d444a');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-bb9c7ae');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
