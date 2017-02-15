941829da49eb0bfd6f319ad09bdd8fce0212bd97
Igor Minar <iminar@google.com>
2017-02-15 00:34:09 +0000

    b4d444a feat(platform-server): add API to render Module and ModuleFactory to string (#14381)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-269cf42
+ * @license Angular v4.0.0-beta.7-b4d444a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3877,7 +3877,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-269cf42');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-b4d444a');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
