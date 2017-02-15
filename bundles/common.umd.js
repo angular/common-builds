d7a4f594b2b4c609d8152d37460a97b1726f3725
Igor Minar <iminar@google.com>
2017-02-15 16:58:30 +0000

    4e7752a feat(compiler): integrate compiler with view engine (#14487)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-e9ba7aa
+ * @license Angular v4.0.0-beta.7-4e7752a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3877,7 +3877,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-e9ba7aa');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-4e7752a');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
