4638046cab4996ae4d47fc4854dfad53322be97d
Igor Minar <iminar@google.com>
2017-02-17 01:31:28 +0000

    74ce121 feat(aio): temporarily include ‘content’ folder among production assets (#14548)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-b1a79fd
+ * @license Angular v4.0.0-beta.7-74ce121
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3896,7 +3896,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-b1a79fd');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-74ce121');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
