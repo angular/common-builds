8fb6db5a74d7a7db90e2b585492da10d7d66c900
Igor Minar <igor@angularjs.org>
2017-02-18 23:37:25 +0000

    1bdf706 release: cut v4.0.0-beta.8

---
+++ src/common_module.d.ts
@@ -0,0 +1,14 @@
+/**
+ * The module that includes all the basic Angular directives like {@link NgIf}, {@link NgForOf}, ...
+ *
+ * @stable
+ */
+export declare class CommonModule {
+}
+/**
+ * A module to contain deprecated directives.
+ *
+ * @deprecated
+ */
+export declare class DeprecatedCommonModule {
+}
