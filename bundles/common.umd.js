4f3301314e9f4bf94af0353418d6678f07fbc0c1
GitHub <noreply@github.com>
2017-02-21 00:39:48 +0000

    d6a58f9 feat(core): make `new Inject()` optional for deps specified as `InjectionToken` (#14486)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-5f3c844
+ * @license Angular v4.0.0-beta.8-d6a58f9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3896,7 +3896,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.8-5f3c844');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.8-d6a58f9');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
