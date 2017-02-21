089ff0dff26caec077391edbbca59b6ca2eb8b3d
Victor Berchet <victor@suumit.com>
2017-02-21 02:20:47 +0000

    c2e0f71 feat(router): add an option to rerun guards and resolvers when query changes

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-fbe4b76
+ * @license Angular v4.0.0-beta.8-c2e0f71
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3896,7 +3896,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.8-fbe4b76');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.8-c2e0f71');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
