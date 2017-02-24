0f811ad3a975bd35a4049df50c0eca20e557fb25
Igor Minar <iminar@google.com>
2017-02-24 02:24:40 +0000

    ee747f7 refactor(core): change abstract classes for interfaces (#12324)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -3619,7 +3619,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-a23634d');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-ee747f7');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
