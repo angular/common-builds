405e3935b308130ca7f75f8b847830c6d947d579
Igor Minar <iminar@google.com>
2017-02-24 07:15:32 +0000

    2da3844 build(language-service): support deep import bundling of language services (#14689)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -3619,7 +3619,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-41da599');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-2da3844');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
