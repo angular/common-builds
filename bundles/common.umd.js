59233e6146adad3a3735c3cbbedbfd9dfb965099
Igor Minar <igor@angularjs.org>
2017-02-25 02:28:51 +0000

    c7d1b36 docs: update release schedule due to bad 4.0.0-rc.0 release

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -3619,7 +3619,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-2602b03');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-c7d1b36');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
