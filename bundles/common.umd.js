d3d8b8fd803505f061822ee03da4ede3721ed366
Igor Minar <iminar@google.com>
2017-02-27 23:12:05 +0000

    5caab71 refactor: make identifiers for generated code small to improve dev size

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -3619,7 +3619,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-f356041');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-5caab71');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
