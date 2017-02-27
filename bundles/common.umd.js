614676a9caff549ea67344f9d7b46bffb07e53c6
Igor Minar <iminar@google.com>
2017-02-27 19:48:07 +0000

    f356041 docs: update version in changelog to reflect RC.1 (not 0) (#14740)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -3619,7 +3619,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-77682a3');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-f356041');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
