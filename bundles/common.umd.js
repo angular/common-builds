eedc0359ae38c5aa6ba1c531335e2ca59cf9b84e
Igor Minar <iminar@google.com>
2017-02-28 07:28:31 +0000

    126fda2 perf: delete pre-view-engine core, compiler, platform-browser, etc code (#14788)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -3619,7 +3619,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-e58cb7b');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-126fda2');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
