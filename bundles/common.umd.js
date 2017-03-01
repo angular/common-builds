e32a8c6757b64142fb79d7b5046d2fa0b983d2d9
Igor Minar <iminar@google.com>
2017-03-01 23:48:04 +0000

    dd49901 ci: add NGBUILDS_IO_KEY to .travis.yaml for ngbuilds.io integration

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -3619,7 +3619,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-a6996a9');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-dd49901');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
