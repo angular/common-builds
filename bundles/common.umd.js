1c3557e5654f5bf99c0c4111fba28588c811ac98
Igor Minar <iminar@google.com>
2017-03-01 16:23:02 +0000

    bc9e1de docs: changelog - fix else example (#14766)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -3619,7 +3619,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-fc9e6b2');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-bc9e1de');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
