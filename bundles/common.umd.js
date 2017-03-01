bec381dfe83cacb0bf3d5051169e30c97c3d3557
Igor Minar <iminar@google.com>
2017-03-01 19:38:24 +0000

    9402df9 test(platform-server): enable meta tag and title service specs for server (#14574)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -3619,7 +3619,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-47bdc2b');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-9402df9');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
