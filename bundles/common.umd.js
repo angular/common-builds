a730d761de8c277f28b773bf965573543749124f
Igor Minar <iminar@google.com>
2017-03-01 20:37:11 +0000

    79fc1e3 fix(compiler): apply element bindings before host bindings (#14823)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -3619,7 +3619,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-9402df9');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-79fc1e3');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
