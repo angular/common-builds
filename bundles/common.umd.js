18d83612f8306ba4b7bbb4986d7d76892a2c431d
Igor Minar <iminar@google.com>
2017-03-02 08:41:22 +0000

    207298c build(aio): generate marketing docs JSON files

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -3619,7 +3619,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-a24e652');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-207298c');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
