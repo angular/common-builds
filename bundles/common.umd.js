b275660216bccb46468ca28ec65ae6e82db30a81
Igor Minar <igor@angularjs.org>
2017-03-02 07:34:47 +0000

    80fe41a release: cut v2.4.9

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.8-de36f8a
+ * @license Angular v2.4.9-80fe41a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3634,7 +3634,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.8-de36f8a');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.9-80fe41a');
 
     exports.NgLocalization = NgLocalization;
     exports.CommonModule = CommonModule;
