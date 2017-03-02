a889572f079b5aecb2c16d57cc9ce27c26b0b074
Igor Minar <iminar@google.com>
2017-03-02 08:17:34 +0000

    e6e8123 build: update to yarn 0.21.3 (#14805)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -3619,7 +3619,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-49aa508');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-e6e8123');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
