ceda767cd7b4fef3bba90376fd726907d91ebad8
Chuck Jazdzewski <chuckj@google.com>
2017-03-08 02:43:38 +0000

    174d4c8 ci(aio): do not deploy PR if preconditions not met

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -3536,7 +3536,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-ad3b44a');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-174d4c8');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
