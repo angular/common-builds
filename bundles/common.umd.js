b6aafc9f62435213a68fcc6ee4accbb94dff69c5
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 20:13:03 +0000

    893652a fix(aio): upgrade to zone.js 0.7.8 (#15099)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-6559425
+ * @license Angular v4.0.0-rc.3-893652a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3682,7 +3682,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-6559425');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-893652a');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
