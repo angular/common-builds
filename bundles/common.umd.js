70962d9bbba573b47a7a88dc3ae8b8d1eca8b9ef
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 20:39:53 +0000

    018e5c9 fix(platform-server): fix get/set title in parse5 adapter (#14965)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-e7dab7e
+ * @license Angular v4.0.0-rc.3-018e5c9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3682,7 +3682,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-e7dab7e');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-018e5c9');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
