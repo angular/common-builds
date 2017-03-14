bca2355cc8315433c1848d89173e286f1e71ff83
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 21:51:34 +0000

    2c5a671 fix: don't instantiate providers with ngOnDestroy eagerly. (#15070)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-0aad270
+ * @license Angular v4.0.0-rc.3-2c5a671
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3682,7 +3682,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-0aad270');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-2c5a671');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
