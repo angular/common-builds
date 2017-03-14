771849769e4b42501e46d6ce140e8c086edbc420
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 00:10:56 +0000

    8850098 build(aio): ensure API docs are generated in the correct place (#15103)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-e6c81d2
+ * @license Angular v4.0.0-rc.3-8850098
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3682,7 +3682,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-e6c81d2');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-8850098');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
