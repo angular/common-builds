4f33cbc4b1ec6b02c609bfe03d7bfdaff078c9fb
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 22:57:07 +0000

    778f7d6 fix(forms): remove equalsTo validator (#15050)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-2c5a671
+ * @license Angular v4.0.0-rc.3-778f7d6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3682,7 +3682,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-2c5a671');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-778f7d6');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
