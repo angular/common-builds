2fc2f05bcfc1f114d539ba100b112333adfda0a7
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-14 14:09:13 +0000

    2f250c1 refactor(aio): move window:resize handler into component

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-d4205bb
+ * @license Angular v4.0.0-rc.3-2f250c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3682,7 +3682,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-d4205bb');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-2f250c1');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
