df55e2ce5a038c8503d6c9fec6d6a7db60da18eb
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 00:27:22 +0000

    3b1956b fix(compiler): warning prints "WARNING" instead of "ERROR" (#15125)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-ff60c04
+ * @license Angular v4.0.0-rc.3-3b1956b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3679,7 +3679,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-ff60c04');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-3b1956b');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
