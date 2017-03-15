377596e2f7e4b39cc066921e92f64b623b19357b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-15 22:42:10 +0000

    a9b1880 feat(aio): top bar typography, sidenav size

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-ec548ad
+ * @license Angular v4.0.0-rc.3-a9b1880
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3696,7 +3696,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-ec548ad');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-a9b1880');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
