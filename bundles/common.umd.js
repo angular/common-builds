db2c9357521eec60f0d6892dd78dff621fc3181d
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 00:28:33 +0000

    959a03a fix(compiler): fix utf8encode, move to sharted utils, add tests (#15076)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-3b1956b
+ * @license Angular v4.0.0-rc.3-959a03a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3679,7 +3679,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-3b1956b');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-959a03a');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
