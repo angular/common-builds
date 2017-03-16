00ce080ad19fbdced981fff26f149f702a506e42
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 20:13:52 +0000

    992aa17 build: add source maps into NPM distribution (#15159)

--- @angular/common/testing.js
+++ @angular/common/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-a4076c7
+ * @license Angular v4.0.0-rc.3-992aa17
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -165,3 +165,4 @@ class _MockPopStateEvent {
 }
 
 export { SpyLocation, MockLocationStrategy };
+//# sourceMappingURL=testing.js.map
--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-a4076c7
+ * @license Angular v4.0.0-rc.3-992aa17
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3756,7 +3756,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-a4076c7');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-992aa17');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
@@ -3807,3 +3807,4 @@
     exports.ɵb = COMMON_PIPES;
 
 }));
+//# sourceMappingURL=common.umd.js.map
