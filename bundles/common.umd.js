2d123c1486b92c4b0aae5ff264455fb306790bce
Chuck Jazdzewski <chuckj@google.com>
2017-03-17 02:50:35 +0000

    6b57928 docs: add changelog for 2.4.10

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.9-b0ae464
+ * @license Angular v2.4.10-6b57928
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3639,7 +3639,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.9-b0ae464');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.10-6b57928');
 
     exports.NgLocalization = NgLocalization;
     exports.CommonModule = CommonModule;
