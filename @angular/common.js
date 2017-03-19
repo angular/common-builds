29dbad5b9dae789a59100f50f4abbd7db76e7b4d
Miško Hevery <misko@hevery.com>
2017-03-19 18:21:27 +0000

    f634c62 test: add systemjs+umd integration test (#14196)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-80075af
+ * @license Angular v4.0.0-rc.5-f634c62
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3808,7 +3808,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-80075af');
+const VERSION = new Version('4.0.0-rc.5-f634c62');
 
 /**
  * @license
