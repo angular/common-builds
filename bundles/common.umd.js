729ed9c6d14156d095b95a5b324a405902b3962d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-20 15:03:03 +0000

    60c8368 build(aio): fix generated github links

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-6d4f5ce
+ * @license Angular v4.0.0-rc.5-60c8368
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3750,7 +3750,7 @@
     /**
      * \@stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-rc.5-6d4f5ce');
+    var VERSION = new _angular_core.Version('4.0.0-rc.5-60c8368');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
