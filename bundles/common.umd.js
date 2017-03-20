8ae23e230198aafdbd8ffcfc90a2ed6627784e99
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-20 08:12:51 +0000

    20f3b55 feat(aio): expand nav menu branch of select item on page load

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-d0bc83c
+ * @license Angular v4.0.0-rc.5-20f3b55
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3750,7 +3750,7 @@
     /**
      * \@stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-rc.5-d0bc83c');
+    var VERSION = new _angular_core.Version('4.0.0-rc.5-20f3b55');
 
     exports.NgLocaleLocalization = NgLocaleLocalization;
     exports.NgLocalization = NgLocalization;
