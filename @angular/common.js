e04273a564f65c2f93f9c9a68804e0e19d3b0a42
Miško Hevery <misko@hevery.com>
2017-03-22 03:47:05 +0000

    1bcbcfd revert: build(aio): implement prerendering (#15346)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-5efc860
+ * @license Angular v4.0.0-rc.5-1bcbcfd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3808,7 +3808,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-5efc860');
+const VERSION = new Version('4.0.0-rc.5-1bcbcfd');
 
 /**
  * @license
