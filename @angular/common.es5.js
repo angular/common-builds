b0f358112fe2953dc2a8bec788469723c2322b00
Igor Minar <iminar@google.com>
2017-03-22 22:40:50 +0000

    ea49a95 fix(upgrade): component injectors should not link the module injector tree (#15385)

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.5-a50d79d
+ * @license Angular v4.0.0-rc.5-ea49a95
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3954,7 +3954,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.0.0-rc.5-a50d79d');
+var VERSION = new Version('4.0.0-rc.5-ea49a95');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
