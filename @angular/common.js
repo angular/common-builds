a4f8341143b193ea768a805f920c29e1b69e9e95
GitHub <noreply@github.com>
2017-03-23 06:05:18 +0000

    da700d1 docs: move markdown docs from root dir to docs/ and remove obsolete files (#15410)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-de87c47
+ * @license Angular v4.0.0-rc.5-da700d1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3808,7 +3808,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-de87c47');
+const VERSION = new Version('4.0.0-rc.5-da700d1');
 
 /**
  * @license
