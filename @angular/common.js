f1f51f8ca128be0d23ac193e7df587719ac20426
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-27 11:41:44 +0000

    837ed78 feat(aio): add code-example and code-tabs

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-52ea193
+ * @license Angular v4.0.0-837ed78
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3808,7 +3808,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-52ea193');
+const VERSION = new Version('4.0.0-837ed78');
 
 /**
  * @license
