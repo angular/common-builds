4672da12aea6cfb211ef1d0ef6db27bb0eb5675a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-27 08:37:19 +0000

    a6fd22c docs(aio): fix typo

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-1616cae
+ * @license Angular v4.0.0-a6fd22c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3808,7 +3808,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-1616cae');
+const VERSION = new Version('4.0.0-a6fd22c');
 
 /**
  * @license
