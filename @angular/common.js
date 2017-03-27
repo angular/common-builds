f0f7a8410ae01bc680877dd6955141a955a44967
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-27 09:30:30 +0000

    52ea193 build(aio): left align code regions

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-228238e
+ * @license Angular v4.0.0-52ea193
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3808,7 +3808,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-228238e');
+const VERSION = new Version('4.0.0-52ea193');
 
 /**
  * @license
