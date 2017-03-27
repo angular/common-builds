5062ae98802ef10c564bfe30ffc6c2b20cc7e5a8
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-27 13:53:33 +0000

    6605dd1 docs(aio): correct spelling in overview

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-837ed78
+ * @license Angular v4.0.0-6605dd1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3808,7 +3808,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-837ed78');
+const VERSION = new Version('4.0.0-6605dd1');
 
 /**
  * @license
