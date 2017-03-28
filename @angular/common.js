cf11366a161360acdefb3c0a805a78ebecddec5a
Victor Berchet <victor@suumit.com>
2017-03-28 20:50:52 +0000

    aa16ccd fix(core): check for undefined on normalizeDebugBindingValue (#15503)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-d438b88
+ * @license Angular v4.0.0-aa16ccd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3808,7 +3808,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-d438b88');
+const VERSION = new Version('4.0.0-aa16ccd');
 
 /**
  * @license
