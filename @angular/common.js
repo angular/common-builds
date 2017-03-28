98a74455cce632640b1f8fdecee22d0c6c6eecab
Victor Berchet <victor@suumit.com>
2017-03-28 17:08:52 +0000

    8e03f65 refactor(router): improve flatten fn

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-b7fa5de
+ * @license Angular v4.0.0-8e03f65
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3808,7 +3808,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-b7fa5de');
+const VERSION = new Version('4.0.0-8e03f65');
 
 /**
  * @license
