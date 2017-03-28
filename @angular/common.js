6f4cab5eb853577b885a715692e43e526776912b
Victor Berchet <victor@suumit.com>
2017-03-28 16:58:44 +0000

    d05aa70 docs(core): fix typo and example in InjectionToken doc (#15449)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-eac99c1
+ * @license Angular v4.0.0-d05aa70
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3808,7 +3808,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-eac99c1');
+const VERSION = new Version('4.0.0-d05aa70');
 
 /**
  * @license
