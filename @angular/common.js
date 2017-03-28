73a3e22e4c948c85dcdd327578d8ed74fc8f0007
Victor Berchet <victor@suumit.com>
2017-03-28 20:49:25 +0000

    6269d28 fix(language-service): improve performance of `updateModuleAnalysis()` (#15543)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-a5c972a
+ * @license Angular v4.0.0-6269d28
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3808,7 +3808,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-a5c972a');
+const VERSION = new Version('4.0.0-6269d28');
 
 /**
  * @license
