e21e2ef62e5f7e8b9729dcc5dd7455c259f9f6d8
GitHub <noreply@github.com>
2017-03-29 17:01:02 +0000

    d58a242 refactor(router): cleanup & simplification (#15436)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-910c0d9
+ * @license Angular v4.0.0-d58a242
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3808,7 +3808,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-910c0d9');
+const VERSION = new Version('4.0.0-d58a242');
 
 /**
  * @license
