61e93e32a163d3450c15d8ff8c04089649c1ee59
Victor Berchet <victor@suumit.com>
2017-03-29 17:34:08 +0000

    23bf348 refactor(router): cleanup & simplification (#15436)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-95afaf4
+ * @license Angular v4.0.0-23bf348
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3808,7 +3808,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-95afaf4');
+const VERSION = new Version('4.0.0-23bf348');
 
 /**
  * @license
