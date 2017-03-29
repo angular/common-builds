6b52127d181a4e6f08f77d7537674f0cf7149387
Victor Berchet <victor@suumit.com>
2017-03-29 22:27:43 +0000

    19cb503 docs(http): remove deprecated stuff and outdated plunkrs (#15598)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-9f2acf5
+ * @license Angular v4.0.0-19cb503
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3808,7 +3808,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-9f2acf5');
+const VERSION = new Version('4.0.0-19cb503');
 
 /**
  * @license
