036eb204590d7105991d8ff55aaed42a18fdd0a8
Victor Berchet <victor@suumit.com>
2017-03-29 17:36:30 +0000

    331b9f6 fix(language-service): don't require `reflect-metadata` module to be provided (#15569)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-d58a242
+ * @license Angular v4.0.0-331b9f6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3808,7 +3808,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-d58a242');
+const VERSION = new Version('4.0.0-331b9f6');
 
 /**
  * @license
