cfcc9470b9e7dbb5d586d79e6b203549d37ccd89
Victor Berchet <victor@suumit.com>
2017-03-29 17:46:39 +0000

    9c77a7c fix(compiler): throw when a component defines both template and templateUrl (#15572)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-331b9f6
+ * @license Angular v4.0.0-9c77a7c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3808,7 +3808,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-331b9f6');
+const VERSION = new Version('4.0.0-9c77a7c');
 
 /**
  * @license
