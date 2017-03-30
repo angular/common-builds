d312cb8f49526feac054533736e6e87a87828716
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-03-30 19:51:31 +0000

    9e883f5 feat(aio): improve ProgressiveWepApp-ability (#15628)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-43a9619
+ * @license Angular v4.1.0-beta.0-9e883f5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3808,7 +3808,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-43a9619');
+const VERSION = new Version('4.1.0-beta.0-9e883f5');
 
 /**
  * @license
