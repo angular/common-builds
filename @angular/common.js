cb44560e68c90691134276bbb02d6e26dba43875
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-03-31 18:52:35 +0000

    5c38012 fix(aio): contributor.html typos

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-46f8a6d
+ * @license Angular v4.1.0-beta.0-5c38012
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3808,7 +3808,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-46f8a6d');
+const VERSION = new Version('4.1.0-beta.0-5c38012');
 
 /**
  * @license
