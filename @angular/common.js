80e094565a4db8237b843a15316e9f0408ca251f
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-02 21:25:16 +0000

    1e623a3 feat(aio): add original style for feature page

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-928e254
+ * @license Angular v4.1.0-beta.0-1e623a3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3808,7 +3808,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-928e254');
+const VERSION = new Version('4.1.0-beta.0-1e623a3');
 
 /**
  * @license
