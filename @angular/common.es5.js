968c9b524d5d31941e300e6ca11ec5773c7944e6
Hans <hans@hansl.ca>
2017-04-10 22:50:58 +0000

    83527fd test(router): test preloading when a module is already loaded

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.0-96aa236
+ * @license Angular v4.1.0-beta.0-83527fd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3941,7 +3941,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.0-96aa236');
+var VERSION = new Version('4.1.0-beta.0-83527fd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
