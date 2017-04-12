1440a02c0ba27facd494059d4f7ef4d92f773acc
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-12 20:17:56 +0000

    0614754 docs(aio): final migration from old site

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.0-8f1359d
+ * @license Angular v4.1.0-beta.0-0614754
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3941,7 +3941,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.0-8f1359d');
+var VERSION = new Version('4.1.0-beta.0-0614754');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
