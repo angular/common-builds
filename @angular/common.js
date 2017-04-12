6f2115d91f0c2e0c12934bb3a3d859e48860f084
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-12 13:48:00 +0000

    2798c1b feat(aio): code block styles with header

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-38d75d4
+ * @license Angular v4.1.0-beta.0-2798c1b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3812,7 +3812,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-38d75d4');
+const VERSION = new Version('4.1.0-beta.0-2798c1b');
 
 /**
  * @license
