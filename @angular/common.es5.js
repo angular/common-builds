96eda91fa70c41c9db141c65262d1443b339fe9f
Tobias Bosch <tbosch1009@gmail.com>
2017-04-14 00:28:49 +0000

    6a2e08d fix(forms): Update types for TypeScript nullability support (#15859)

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-fdb3f26
+ * @license Angular v4.1.0-beta.1-6a2e08d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3941,7 +3941,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.1-fdb3f26');
+var VERSION = new Version('4.1.0-beta.1-6a2e08d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
