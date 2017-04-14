99db901c0926c3939664f3a69eb446c4099d0b9b
Matias Niemelä <matias@yearofmoo.com>
2017-04-14 18:45:00 +0000

    6f3710e feat(animations): wip: introduce a wave of new animation features

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-c664486
+ * @license Angular v4.1.0-beta.1-6f3710e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3812,7 +3812,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-c664486');
+const VERSION = new Version('4.1.0-beta.1-6f3710e');
 
 /**
  * @license
