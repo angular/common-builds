d6da255f7e2cb8112a0351d8c156c0d741631e97
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 12:53:36 +0000

    46b0c7a docs(aio): fix links to quickstart on homepage

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-b6a0098
+ * @license Angular v4.1.0-beta.1-46b0c7a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3812,7 +3812,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-b6a0098');
+const VERSION = new Version('4.1.0-beta.1-46b0c7a');
 
 /**
  * @license
