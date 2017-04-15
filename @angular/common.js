2dacc8294765600ddd4a1a19d55ea17fd78a46b3
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:54:21 +0000

    7d986ae feat(aio): smaller typography, code header pad

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-a2bdcc9
+ * @license Angular v4.1.0-beta.1-7d986ae
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3812,7 +3812,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-a2bdcc9');
+const VERSION = new Version('4.1.0-beta.1-7d986ae');
 
 /**
  * @license
