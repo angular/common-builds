0b47c2440b24c58ff2e65e0275c80bec94baa3d0
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:41:04 +0000

    49d97e1 ci(aio): verify that the Lighthouse PWA score remains above a threshold

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-c0b1bbe
+ * @license Angular v4.1.0-beta.1-49d97e1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3812,7 +3812,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-c0b1bbe');
+const VERSION = new Version('4.1.0-beta.1-49d97e1');
 
 /**
  * @license
