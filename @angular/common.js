c3d3363cd19ff5f36e1718818cecfc645a9f03c9
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:35:30 +0000

    dfc81c3 feat(aio): sort search results by area/title

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-6649743
+ * @license Angular v4.1.0-beta.1-dfc81c3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3812,7 +3812,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-6649743');
+const VERSION = new Version('4.1.0-beta.1-dfc81c3');
 
 /**
  * @license
