125983be3f2cf771dee00a5ca03b6c9484b8351b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-16 21:32:56 +0000

    abecf3e build(aio): move the `transforms` folder into the `tools` folder

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-7c418a7
+ * @license Angular v4.1.0-beta.1-abecf3e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3812,7 +3812,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-7c418a7');
+const VERSION = new Version('4.1.0-beta.1-abecf3e');
 
 /**
  * @license
