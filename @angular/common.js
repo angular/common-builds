0eb70b569e9a7395ce593d416794706b378b9329
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-16 10:48:40 +0000

    11b54f6 feat(aio): edit page link styling and placement

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-4a3c66f
+ * @license Angular v4.1.0-beta.1-11b54f6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3812,7 +3812,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-4a3c66f');
+const VERSION = new Version('4.1.0-beta.1-11b54f6');
 
 /**
  * @license
