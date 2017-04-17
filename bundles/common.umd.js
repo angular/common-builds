86601dba168b239fa728478a2623132daf06d7a0
Tobias Bosch <tbosch1009@gmail.com>
2017-04-17 21:51:58 +0000

    38a7e0d fix(compiler): ignore calls to unresolved symbols in metadata

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-a77b126
+ * @license Angular v4.1.0-beta.1-38a7e0d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-a77b126
+ * @license Angular v4.1.0-beta.1-38a7e0d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3951,7 +3951,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.1-a77b126');
+var VERSION = new _angular_core.Version('4.1.0-beta.1-38a7e0d');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
