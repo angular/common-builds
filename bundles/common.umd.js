9072be86e8b8cd82071e5ce230d95c35d4c1fd33
Alex Eagle <alexeagle@google.com>
2017-04-18 22:51:59 +0000

    683e657 fix(compiler): suppress another closure warning

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-636858d
+ * @license Angular v4.1.0-beta.1-683e657
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-636858d
+ * @license Angular v4.1.0-beta.1-683e657
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3951,7 +3951,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.1-636858d');
+var VERSION = new _angular_core.Version('4.1.0-beta.1-683e657');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
