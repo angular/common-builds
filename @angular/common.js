302bea9eaed83343c45f7e8c79b5bdfcbec3a357
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 09:37:40 +0000

    8fa4453 docs(aio): Testing Guide: Second matcher parameter

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-c2fca3d
+ * @license Angular v4.1.0-beta.1-8fa4453
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3812,7 +3812,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-c2fca3d');
+const VERSION = new Version('4.1.0-beta.1-8fa4453');
 
 /**
  * @license
