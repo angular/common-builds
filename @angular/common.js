8c8b7e91cd7b54b515e73164d165d51c8e0f86c7
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 09:47:59 +0000

    9d40ab9 docs(aio): rename ToH to match the guide name

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-691e86c
+ * @license Angular v4.1.0-beta.1-9d40ab9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3812,7 +3812,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-691e86c');
+const VERSION = new Version('4.1.0-beta.1-9d40ab9');
 
 /**
  * @license
