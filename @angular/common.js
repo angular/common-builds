242a52ac23c35fb0fdbed90b0f9c212aca05b4c2
Miško Hevery <misko@hevery.com>
2017-04-21 20:55:50 +0000

    7a8bd99 docs(aio): correct some minor typos in the form validation cookbook (#16068)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-7520ddc
+ * @license Angular v4.1.0-rc.0-7a8bd99
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3812,7 +3812,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-7520ddc');
+const VERSION = new Version('4.1.0-rc.0-7a8bd99');
 
 /**
  * @license
