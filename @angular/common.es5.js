c7ce954513f4b3903a9d4618929c9469353a49fd
Miško Hevery <misko@hevery.com>
2017-04-21 03:28:33 +0000

    5461182 docs(aio): make glossary links work again (#16201)

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-cbe95f3
+ * @license Angular v4.1.0-beta.1-5461182
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3941,7 +3941,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.1-cbe95f3');
+var VERSION = new Version('4.1.0-beta.1-5461182');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
