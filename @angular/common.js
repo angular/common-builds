717832c6355957895e51e4769f4b5743e81d7598
Miško Hevery <misko@hevery.com>
2017-04-24 17:14:00 +0000

    feda017 docs(aio): Fix decorator JS feature explanation (#16214)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-2991221
+ * @license Angular v4.1.0-rc.0-feda017
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3812,7 +3812,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-2991221');
+const VERSION = new Version('4.1.0-rc.0-feda017');
 
 /**
  * @license
