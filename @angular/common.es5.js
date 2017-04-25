e1646d039834536a9e67a544ade6a43a5c1ab22d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-25 06:54:13 +0000

    e7c37d7 feat(aio): copy code snackbar and design updates

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-rc.0-f5aaa55
+ * @license Angular v4.1.0-rc.0-e7c37d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3941,7 +3941,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-rc.0-f5aaa55');
+var VERSION = new Version('4.1.0-rc.0-e7c37d7');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
