5c8e996507c115d3d4dcd8d55c5b2af959aca81c
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-26 09:08:29 +0000

    35a2dfc fix(aio): resource nav

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-d8e2829
+ * @license Angular v4.1.0-rc.0-35a2dfc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3812,7 +3812,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-d8e2829');
+const VERSION = new Version('4.1.0-rc.0-35a2dfc');
 
 /**
  * @license
