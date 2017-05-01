7cc07665608b4e7a95ef994c2cc0d0d6e2836d8c
Matias Niemelä <matias@yearofmoo.com>
2017-05-01 23:14:49 +0000

    d32bc5d feat(aio): add tooltip to nav node if none specified

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-efaf502
+ * @license Angular v4.1.0-d32bc5d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3809,7 +3809,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-efaf502');
+const VERSION = new Version('4.1.0-d32bc5d');
 
 /**
  * @license
