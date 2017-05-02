ec22fd7d0a692c21d9bc5f119136910bf3d3cfe2
Matias Niemelä <matias@yearofmoo.com>
2017-05-02 22:32:15 +0000

    21c96a5 feat(aio): sidenav styling extended

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-1c8772a
+ * @license Angular v4.1.0-21c96a5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3809,7 +3809,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-1c8772a');
+const VERSION = new Version('4.1.0-21c96a5');
 
 /**
  * @license
