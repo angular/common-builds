a9711758ee04554a476c03ce9a3eae28c3b6dc4e
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 20:57:58 +0000

    5b96fb9 build(aio): rename `src/content` to `src/generated`

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-1cfb263
+ * @license Angular v4.1.0-5b96fb9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3805,7 +3805,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-1cfb263');
+const VERSION = new Version('4.1.0-5b96fb9');
 
 /**
  * @license
