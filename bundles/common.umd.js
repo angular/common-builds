ea0eeca70408a68d8ebf8b07fcf969c1ca2c2569
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 20:59:06 +0000

    464701a docs(aio): revert removal of tutorial examples

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-5b96fb9
+ * @license Angular v4.1.0-464701a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-5b96fb9
+ * @license Angular v4.1.0-464701a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3944,7 +3944,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.1.0-5b96fb9');
+var VERSION = new _angular_core.Version('4.1.0-464701a');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
