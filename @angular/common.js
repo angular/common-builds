db97edfd88a8c0d15535c75cc4a67d31edbe08c3
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 16:53:55 +0000

    9684d78 build: update `concurrently` to latest version

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-71f5b73
+ * @license Angular v4.1.0-9684d78
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3805,7 +3805,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-71f5b73');
+const VERSION = new Version('4.1.0-9684d78');
 
 /**
  * @license
