f468352bcf7691463991f63869f7a8918b9f53dd
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 02:51:05 +0000

    919ff12 fix(aio): cheatsheet table layout fix

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-6748aea
+ * @license Angular v4.1.0-919ff12
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3805,7 +3805,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-6748aea');
+const VERSION = new Version('4.1.0-919ff12');
 
 /**
  * @license
