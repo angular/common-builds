3b639cdfa00cf327234dd2caf4afd69cb71dc2ca
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 23:09:16 +0000

    9449eff docs(aio): improve tutorial next-steps prose/links

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-cd28df6
+ * @license Angular v4.2.0-beta.0-9449eff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3805,7 +3805,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-cd28df6');
+const VERSION = new Version('4.2.0-beta.0-9449eff');
 
 /**
  * @license
