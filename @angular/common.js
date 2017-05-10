d26249b9fdcc3f012d87d34c34d8b7257b7a6504
Jason Aden <jasonaden@google.com>
2017-05-10 23:44:28 +0000

    162dffb docs: add changelog for 4.2.0-beta.1

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-e242e20
+ * @license Angular v4.2.0-beta.1-162dffb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3805,7 +3805,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-e242e20');
+const VERSION = new Version('4.2.0-beta.1-162dffb');
 
 /**
  * @license
