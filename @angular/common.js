8533629f2c4dc360be48d58576548ed767858881
Jason Aden <jasonaden@google.com>
2017-05-10 00:57:43 +0000

    e46a65f build: update key for pushing to PACKAGE-builds repos (#16667)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.1-5f6d0f2
+ * @license Angular v4.1.1-e46a65f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3805,7 +3805,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.1-5f6d0f2');
+const VERSION = new Version('4.1.1-e46a65f');
 
 /**
  * @license
