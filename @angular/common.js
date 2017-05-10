4bfb116a464ceb75be7d260e7eafe029ab5e028c
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-10 07:16:13 +0000

    dc7d242 feat(aio): update code style

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-412ab3f
+ * @license Angular v4.2.0-beta.0-dc7d242
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3805,7 +3805,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-412ab3f');
+const VERSION = new Version('4.2.0-beta.0-dc7d242');
 
 /**
  * @license
