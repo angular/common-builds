dfa602f28dd3a607fbc15f2abde940725fc94098
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-11 17:33:34 +0000

    b9521b5 feat(compiler): support a non-null postfix assert (#16672)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-2eca6e6
+ * @license Angular v4.2.0-beta.1-b9521b5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3805,7 +3805,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-2eca6e6');
+const VERSION = new Version('4.2.0-beta.1-b9521b5');
 
 /**
  * @license
