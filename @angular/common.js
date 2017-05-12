bd3822418fedb6e8f3b08f2f7bf1c2704594e373
Igor Minar <iminar@google.com>
2017-05-12 19:36:32 +0000

    0626464 build: use subshells when changing directories

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-21d213d
+ * @license Angular v4.2.0-beta.1-0626464
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3805,7 +3805,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-21d213d');
+const VERSION = new Version('4.2.0-beta.1-0626464');
 
 /**
  * @license
