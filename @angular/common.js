ab092bd7c3fc1cf4bc7c43dc3ecc8af43f588fd0
Igor Minar <iminar@google.com>
2017-05-17 04:23:31 +0000

    593fe5e build(aio): enable HTTP/2 on the preview server (#16826)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-54a6e4f
+ * @license Angular v4.2.0-beta.1-593fe5e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3805,7 +3805,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-54a6e4f');
+const VERSION = new Version('4.2.0-beta.1-593fe5e');
 
 /**
  * @license
