eb7f1ce071b6408a8cc153201b70b9936aaf469d
Matias Niemelä <matias@yearofmoo.com>
2017-05-18 19:14:21 +0000

    86b7bd9 revert: refactor(router): cleanup, simplification

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-6531806
+ * @license Angular v4.2.0-beta.1-86b7bd9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3939,7 +3939,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-6531806');
+var VERSION = new Version('4.2.0-beta.1-86b7bd9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
