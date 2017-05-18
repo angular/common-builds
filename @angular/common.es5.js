64bcd862b7063f273a82c516af6e9de919a0b53f
Peter Bacon Darwin <pete@bacondarwin.com>
2017-05-18 14:08:48 +0000

    0e13a59 fix(aio): add small delay before autoscrolling

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-3361a7b
+ * @license Angular v4.2.0-beta.1-0e13a59
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3939,7 +3939,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-3361a7b');
+var VERSION = new Version('4.2.0-beta.1-0e13a59');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
