bff4c2fdce35ce41f2fa1a76ed38b6ad6f3f2ba7
GitHub <noreply@github.com>
2017-05-22 18:36:40 +0000

    6e41add feat(tsc-wrapped): support template literals in metadata collection (#16880)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-11c10b2
+ * @license Angular v4.2.0-rc.0-6e41add
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3805,7 +3805,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.0-11c10b2');
+const VERSION = new Version('4.2.0-rc.0-6e41add');
 
 /**
  * @license
