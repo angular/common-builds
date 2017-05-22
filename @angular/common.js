0b96c1b3d683978e4e7e4b6d393cef5626b0316e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-22 13:08:29 +0000

    ce18fdb fix(aio): use the current version in the version picker

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-670771d
+ * @license Angular v4.2.0-rc.0-ce18fdb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3805,7 +3805,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.0-670771d');
+const VERSION = new Version('4.2.0-rc.0-ce18fdb');
 
 /**
  * @license
