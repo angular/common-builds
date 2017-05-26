569d01500b9fb25e8bfd211e74371423021a5751
Igor Minar <igor@angularjs.org>
2017-05-26 08:45:27 +0000

    9e7e178 build(aio): add google webmaster tools verification file

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-db3113b
+ * @license Angular v4.2.0-rc.0-9e7e178
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.0-db3113b
+ * @license Angular v4.2.0-rc.0-9e7e178
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3965,7 +3965,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.0-db3113b');
+var VERSION = new _angular_core.Version('4.2.0-rc.0-9e7e178');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
