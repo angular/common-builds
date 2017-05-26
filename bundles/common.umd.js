5eb6382797908841dab16cf552d3fc074afbaf1f
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-26 13:42:37 +0000

    6949510 feat(aio): provide noop service worker file

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-9e7e178
+ * @license Angular v4.2.0-rc.0-6949510
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.0-9e7e178
+ * @license Angular v4.2.0-rc.0-6949510
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3965,7 +3965,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.0-9e7e178');
+var VERSION = new _angular_core.Version('4.2.0-rc.0-6949510');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
