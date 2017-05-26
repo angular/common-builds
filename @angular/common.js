646d0499ad3ce15205b4a8506ef86e345f08f127
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-26 05:18:33 +0000

    0c691af build(aio): start using tsc importHelpers option for the app

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-5aa53d7
+ * @license Angular v4.2.0-rc.0-0c691af
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3805,7 +3805,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.0-5aa53d7');
+const VERSION = new Version('4.2.0-rc.0-0c691af');
 
 /**
  * @license
