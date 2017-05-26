eed90079c4e0aa69e841cc52235df54aad627485
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 20:58:13 +0000

    d108479 fix(animations): use a lightweight renderer for non-animation components (#17003)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-cd5bc64
+ * @license Angular v4.2.0-rc.0-d108479
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.0-cd5bc64
+ * @license Angular v4.2.0-rc.0-d108479
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3965,7 +3965,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.0-cd5bc64');
+var VERSION = new _angular_core.Version('4.2.0-rc.0-d108479');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
