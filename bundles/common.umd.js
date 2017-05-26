4c4b25e0568632676fa4bd3983fc316dda891315
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 16:31:32 +0000

    7ffb75f refactor(compiler): change `bundle` to `flatModuleIndex` in the code (#17028)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-c0981b8
+ * @license Angular v4.2.0-rc.0-7ffb75f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.0-c0981b8
+ * @license Angular v4.2.0-rc.0-7ffb75f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3965,7 +3965,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.0-c0981b8');
+var VERSION = new _angular_core.Version('4.2.0-rc.0-7ffb75f');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
