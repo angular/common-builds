9435694ca84a2bc4a87ab95cae351a05c3613af3
GitHub <noreply@github.com>
2017-05-27 19:04:02 +0000

    6220b49 build(aio): upgrade to ngo with top-level NewExpression and CallExpression prefixing (#17063)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-aa683a7
+ * @license Angular v4.2.0-rc.1-6220b49
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3805,7 +3805,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.1-aa683a7');
+const VERSION = new Version('4.2.0-rc.1-6220b49');
 
 /**
  * @license
