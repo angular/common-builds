37afe269ebae0097c85a63642408880b042b3a1f
Victor Berchet <victor@suumit.com>
2017-05-30 22:33:29 +0000

    b056adc fix(aio): fix SideNav items alignment (#17106)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-0d894a1
+ * @license Angular v4.2.0-rc.1-b056adc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3772,7 +3772,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.1-0d894a1');
+const VERSION = new Version('4.2.0-rc.1-b056adc');
 
 /**
  * @license
--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-0d894a1
+ * @license Angular v4.2.0-rc.1-b056adc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.1-0d894a1
+ * @license Angular v4.2.0-rc.1-b056adc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3932,7 +3932,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.1-0d894a1');
+var VERSION = new _angular_core.Version('4.2.0-rc.1-b056adc');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
