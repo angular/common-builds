405ad1c7dcc07ced4e140e994ae9727eddbfb772
GitHub <noreply@github.com>
2017-05-31 11:05:14 +0000

    1f9a3dd fix(aio): fix typo in web worker check (#17133)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-11505fa
+ * @license Angular v4.2.0-rc.1-1f9a3dd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3772,7 +3772,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.1-11505fa');
+const VERSION = new Version('4.2.0-rc.1-1f9a3dd');
 
 /**
  * @license
--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-11505fa
+ * @license Angular v4.2.0-rc.1-1f9a3dd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.1-11505fa
+ * @license Angular v4.2.0-rc.1-1f9a3dd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3932,7 +3932,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.1-11505fa');
+var VERSION = new _angular_core.Version('4.2.0-rc.1-1f9a3dd');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
