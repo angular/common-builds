d3002cfdf0002fbed8abfb56f13d7111315f5bd4
GitHub <noreply@github.com>
2017-05-31 21:53:48 +0000

    1408357 docs: add details about the WeakMap polyfill for 4.x in the changelog (#17146)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-1f9a3dd
+ * @license Angular v4.2.0-rc.1-1408357
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3772,7 +3772,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.1-1f9a3dd');
+const VERSION = new Version('4.2.0-rc.1-1408357');
 
 /**
  * @license
