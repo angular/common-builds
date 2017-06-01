056a2ecc777a2d6b1faafd0a043fc9f01178634b
Victor Berchet <victor@suumit.com>
2017-06-01 18:57:53 +0000

    ad6a57e perf(animations): do not place enterId values on elements for querying purposes (#17150)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-230255f
+ * @license Angular v4.2.0-rc.1-ad6a57e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.1-230255f
+ * @license Angular v4.2.0-rc.1-ad6a57e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3939,7 +3939,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.1-230255f');
+var VERSION = new _angular_core.Version('4.2.0-rc.1-ad6a57e');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
