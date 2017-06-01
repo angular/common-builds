d5428de97b3f4110f447555dedc045ff61efb3ca
Victor Berchet <victor@suumit.com>
2017-06-01 17:33:39 +0000

    230255f feat(compiler-cli): produce template diagnostics error messages (#17125)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-1338995
+ * @license Angular v4.2.0-rc.1-230255f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.1-1338995
+ * @license Angular v4.2.0-rc.1-230255f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3939,7 +3939,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.1-1338995');
+var VERSION = new _angular_core.Version('4.2.0-rc.1-230255f');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
