307914a50eacfda0d4a7b6ce5e1936e27b1f0e3d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-07 19:57:53 +0000

    cba2b3c docs(aio): add “nougat” to compat chart

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-bb46f54
+ * @license Angular v4.2.0-rc.2-cba2b3c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3779,7 +3779,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.2-bb46f54');
+const VERSION = new Version('4.2.0-rc.2-cba2b3c');
 
 /**
  * @license
--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-bb46f54
+ * @license Angular v4.2.0-rc.2-cba2b3c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.2-bb46f54
+ * @license Angular v4.2.0-rc.2-cba2b3c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3939,7 +3939,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.2-bb46f54');
+var VERSION = new _angular_core.Version('4.2.0-rc.2-cba2b3c');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
