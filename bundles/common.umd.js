40d16469d682b340d22e475ed4a4d721ef0bcaa4
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-07 13:11:21 +0000

    4759975 fix(aio): adjust SideNav top to avoid extra whitespace

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-078a4b0
+ * @license Angular v4.2.0-rc.2-4759975
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.2-078a4b0
+ * @license Angular v4.2.0-rc.2-4759975
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3939,7 +3939,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.2-078a4b0');
+var VERSION = new _angular_core.Version('4.2.0-rc.2-4759975');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
