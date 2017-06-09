36398a173762aad118b51ae573101ce3d5160521
Igor Minar <iminar@google.com>
2017-06-09 21:42:54 +0000

    65d49d5 fix(aio): temporarily remove link to source from the API pages (#17371)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-33df13a
+ * @license Angular v4.2.0-65d49d5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-33df13a
+ * @license Angular v4.2.0-65d49d5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3939,7 +3939,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-33df13a');
+var VERSION = new _angular_core.Version('4.2.0-65d49d5');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
