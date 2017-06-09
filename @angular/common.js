dee7633fe2464668491b42358f6e4be790087fe1
Igor Minar <iminar@google.com>
2017-06-09 22:09:29 +0000

    d56b7ed docs(aio): reorganise the sidenav menu (#16934)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-c2d31fb
+ * @license Angular v4.2.0-d56b7ed
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3779,7 +3779,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-c2d31fb');
+const VERSION = new Version('4.2.0-d56b7ed');
 
 /**
  * @license
