b76ec5158b2a6323f45cfc83c15d901bb33151b8
Igor Minar <iminar@google.com>
2017-06-09 20:55:54 +0000

    3a99af2 docs(packaging): update homepage in @angular/tsc-wrapped (#17352)

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-503fd1f
+ * @license Angular v4.2.0-3a99af2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3904,7 +3904,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-503fd1f');
+var VERSION = new Version('4.2.0-3a99af2');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-503fd1f
+ * @license Angular v4.2.0-3a99af2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-503fd1f
+ * @license Angular v4.2.0-3a99af2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3939,7 +3939,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-503fd1f');
+var VERSION = new _angular_core.Version('4.2.0-3a99af2');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
