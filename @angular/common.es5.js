2dcd0cbddf08bac5425e8c3f1e18782b8f98a948
Igor Minar <iminar@google.com>
2017-06-09 20:50:23 +0000

    503fd1f docs(aio): fix image width on the homepage

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-ce18c68
+ * @license Angular v4.2.0-503fd1f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3904,7 +3904,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-ce18c68');
+var VERSION = new Version('4.2.0-503fd1f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
