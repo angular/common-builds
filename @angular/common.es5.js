4cb824725193313e5fd23cbd8fb537cca2f22a02
Igor Minar <iminar@google.com>
2017-06-10 01:48:07 +0000

    afbc2b0 build(aio): upgrade purify to deal with messed up comment in animations code

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.1-15090a8
+ * @license Angular v4.2.1-afbc2b0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3904,7 +3904,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.1-15090a8');
+var VERSION = new Version('4.2.1-afbc2b0');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
