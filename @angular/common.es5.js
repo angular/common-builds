942a97192e669fe3eb1d7e2f8b50dd02ffeae79a
GitHub <noreply@github.com>
2017-06-10 01:51:46 +0000

    c8c0cee build(bazel): fix the typescript path in the BUILD file (#17316)

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.1-afbc2b0
+ * @license Angular v4.2.1-c8c0cee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3904,7 +3904,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.1-afbc2b0');
+var VERSION = new Version('4.2.1-c8c0cee');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-afbc2b0
+ * @license Angular v4.2.1-c8c0cee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3779,7 +3779,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.1-afbc2b0');
+const VERSION = new Version('4.2.1-c8c0cee');
 
 /**
  * @license
