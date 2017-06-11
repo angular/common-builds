260c5d0bb7b20a08225ca04834d6f57fd9df2fc6
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-11 14:25:29 +0000

    e543272 fix(aio): remove gap between sidenav menus

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-df7774c
+ * @license Angular v4.2.1-e543272
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3779,7 +3779,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.1-df7774c');
+const VERSION = new Version('4.2.1-e543272');
 
 /**
  * @license
