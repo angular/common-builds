3232250966f1ac112a032a473ae580a148667587
Igor Minar <igor@angularjs.org>
2017-06-12 23:49:40 +0000

    5e79449 build(aio): make deploy-to-firebase.sh executable

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-0268818
+ * @license Angular v4.2.2-5e79449
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.1-0268818
+ * @license Angular v4.2.2-5e79449
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3939,7 +3939,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.1-0268818');
+var VERSION = new _angular_core.Version('4.2.2-5e79449');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
