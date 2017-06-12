fb5a71765f6403b7f455045aa29820e2bb3ac04e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-12 23:10:02 +0000

    7a2d2ef docs: add changelog for 4.2.2

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-6ca4692
+ * @license Angular v4.2.2-7a2d2ef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3779,7 +3779,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.1-6ca4692');
+const VERSION = new Version('4.2.2-7a2d2ef');
 
 /**
  * @license
