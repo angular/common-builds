61333252115e5150ccd2b64623450d5504f4eaa5
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-13 12:09:56 +0000

    541c9a9 docs(aio): i18n guide - updates for v4

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-12452d4
+ * @license Angular v4.2.2-541c9a9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3779,7 +3779,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-12452d4');
+const VERSION = new Version('4.2.2-541c9a9');
 
 /**
  * @license
