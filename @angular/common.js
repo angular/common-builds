54b12476ba5e51e6db1cd6b8cff03444425acbeb
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-13 07:12:18 +0000

    d647db2 fix(aio): ensure that API filter page can display 3 columns in wide view

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-0658e1d
+ * @license Angular v4.2.2-d647db2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3779,7 +3779,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-0658e1d');
+const VERSION = new Version('4.2.2-d647db2');
 
 /**
  * @license
