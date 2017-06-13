cb5481c0344056300e6d3bf25ddeeb6b394004c9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-13 21:06:16 +0000

    fa81c8e ci: use npm_install for bazel

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-98308cd
+ * @license Angular v4.2.2-fa81c8e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-98308cd
+ * @license Angular v4.2.2-fa81c8e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3939,7 +3939,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-98308cd');
+var VERSION = new _angular_core.Version('4.2.2-fa81c8e');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
