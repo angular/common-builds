b2ad7e5b17374e9f325d480009df76e72ad6247e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-13 11:30:52 +0000

    12452d4 build(aio): increase docs integration test timeouts

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-709a3f6
+ * @license Angular v4.2.2-12452d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3904,7 +3904,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-709a3f6');
+var VERSION = new Version('4.2.2-12452d4');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
