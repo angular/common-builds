b335f2403500a350bc0e498cc64f379c7acfc0f2
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-14 14:35:20 +0000

    607fb1f fix(aio): always cover the whole footer with its background

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-38fc2a0
+ * @license Angular v4.2.2-607fb1f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-38fc2a0
+ * @license Angular v4.2.2-607fb1f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3939,7 +3939,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-38fc2a0');
+var VERSION = new _angular_core.Version('4.2.2-607fb1f');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
