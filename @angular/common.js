707d488b7942eb5106b2fcdac773db70da271e8f
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-14 11:36:24 +0000

    38fc2a0 fix(aio): fix trackBy demo in template-syntax article

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-0c07f8c
+ * @license Angular v4.2.2-38fc2a0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3779,7 +3779,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-0c07f8c');
+const VERSION = new Version('4.2.2-38fc2a0');
 
 /**
  * @license
