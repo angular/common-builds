236ed2783c76dab79a55e592a658e58569cd3b05
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-15 07:20:41 +0000

    53f57d7 feat(aio): add iphone pwa features

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-2b1de07
+ * @license Angular v4.2.2-53f57d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-2b1de07
+ * @license Angular v4.2.2-53f57d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3939,7 +3939,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-2b1de07');
+var VERSION = new _angular_core.Version('4.2.2-53f57d7');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
