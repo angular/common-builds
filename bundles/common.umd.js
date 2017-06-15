14b819c10e38e55bdd604e94045244009cd54015
Hans <hans@hansl.ca>
2017-06-15 22:10:52 +0000

    e096a85 fix(animations): remove duplicate license header

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-53f57d7
+ * @license Angular v4.2.2-e096a85
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-53f57d7
+ * @license Angular v4.2.2-e096a85
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3939,7 +3939,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-53f57d7');
+var VERSION = new _angular_core.Version('4.2.2-e096a85');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
