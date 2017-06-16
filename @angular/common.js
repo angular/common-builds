b79a1fbc986a0d27952f88786f111bf3fd8c6fa7
Hans <hans@hansl.ca>
2017-06-16 21:06:27 +0000

    3093c55 build(aio): update @angular/service-worker to 1.0.0-beta.16

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-5ac3919
+ * @license Angular v4.2.2-3093c55
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3779,7 +3779,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-5ac3919');
+const VERSION = new Version('4.2.2-3093c55');
 
 /**
  * @license
--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-5ac3919
+ * @license Angular v4.2.2-3093c55
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-5ac3919
+ * @license Angular v4.2.2-3093c55
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3939,7 +3939,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-5ac3919');
+var VERSION = new _angular_core.Version('4.2.2-3093c55');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
