869a72162fe711c6311899c9cbccfee1f81f463f
Peter Bacon Darwin <pete@bacondarwin.com>
2017-06-16 07:27:23 +0000

    9c7a84d docs(aio): re-add biography entry for devversion

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-dbc6a4c
+ * @license Angular v4.2.2-9c7a84d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-dbc6a4c
+ * @license Angular v4.2.2-9c7a84d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3939,7 +3939,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-dbc6a4c');
+var VERSION = new _angular_core.Version('4.2.2-9c7a84d');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
