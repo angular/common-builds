d7fbf9830d6d4ef6ccbe17fcf5d784b57141dd39
Hans <hans@hansl.ca>
2017-06-16 19:39:51 +0000

    5ac3919 feat(core): update zone.js to 0.8.12

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-fe126cb
+ * @license Angular v4.2.2-5ac3919
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3779,7 +3779,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-fe126cb');
+const VERSION = new Version('4.2.2-5ac3919');
 
 /**
  * @license
