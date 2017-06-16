5d0fa29865db7665f7fbe43055531310907a0288
Peter Bacon Darwin <pete@bacondarwin.com>
2017-06-16 07:23:19 +0000

    dbc6a4c build(aio): do not fail if `check-env` for the main angular project fails

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-b192dd5
+ * @license Angular v4.2.2-dbc6a4c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3904,7 +3904,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-b192dd5');
+var VERSION = new Version('4.2.2-dbc6a4c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-b192dd5
+ * @license Angular v4.2.2-dbc6a4c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3779,7 +3779,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-b192dd5');
+const VERSION = new Version('4.2.2-dbc6a4c');
 
 /**
  * @license
