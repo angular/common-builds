be8b7336ccb5e90d04bde23d43ff6e3deff3af80
Hans <hans@hansl.ca>
2017-06-19 18:04:13 +0000

    4f37f86 fix(aio): switch from `innerText` to `textContent` to support older browsers

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-3093c55
+ * @license Angular v4.2.2-4f37f86
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3779,7 +3779,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-3093c55');
+const VERSION = new Version('4.2.2-4f37f86');
 
 /**
  * @license
