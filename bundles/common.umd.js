d1acdba9a364e357a169c6b65205a7f6a60dbdbc
Hans <hans@hansl.ca>
2017-06-20 21:54:09 +0000

    46ddf50 fix(tsc-wrapped): skip collecting metadata for default functions

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-8c89cc4
+ * @license Angular v4.2.2-46ddf50
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-8c89cc4
+ * @license Angular v4.2.2-46ddf50
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3940,7 +3940,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-8c89cc4');
+var VERSION = new _angular_core.Version('4.2.2-46ddf50');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
