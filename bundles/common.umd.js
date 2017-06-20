fb74cfa9ffebda524d58d06e6ce84f3a547d8555
Hans <hans@hansl.ca>
2017-06-20 16:33:07 +0000

    3e685f9 fix(forms): roll back breaking change with min/max directives

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-1c6a252
+ * @license Angular v4.2.2-3e685f9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-1c6a252
+ * @license Angular v4.2.2-3e685f9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3939,7 +3939,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-1c6a252');
+var VERSION = new _angular_core.Version('4.2.2-3e685f9');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
