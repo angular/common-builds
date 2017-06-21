0278016bef1f2d0a6f68d2c108142abd5fdc6f66
Hans <hans@hansl.ca>
2017-06-21 21:23:56 +0000

    97fd248 fix(aio): fix patch script on windows

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-bb804dd
+ * @license Angular v4.2.2-97fd248
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-bb804dd
+ * @license Angular v4.2.2-97fd248
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3940,7 +3940,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-bb804dd');
+var VERSION = new _angular_core.Version('4.2.2-97fd248');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
