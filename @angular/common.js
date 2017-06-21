4dc700ea99949bc6246789f482f7ae0af770df8c
Hans Larsen <hans@hansl.ca>
2017-06-21 23:52:23 +0000

    c112232 fix(compiler): avoid emitting self importing factories

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-3634832
+ * @license Angular v4.2.3-c112232
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3780,7 +3780,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.3-3634832');
+const VERSION = new Version('4.2.3-c112232');
 
 /**
  * @license
