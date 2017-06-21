85734ee2beb189cb229d0d12ef8c42175a1c6bfb
Hans <hans@hansl.ca>
2017-06-21 21:22:33 +0000

    bb804dd feat(aio): select contributor group with URL “about?group=gde”

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-1d93cf2
+ * @license Angular v4.2.2-bb804dd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-1d93cf2
+ * @license Angular v4.2.2-bb804dd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3940,7 +3940,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-1d93cf2');
+var VERSION = new _angular_core.Version('4.2.2-bb804dd');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
