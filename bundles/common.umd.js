98c24309b4325c2072123930abd50bad1d89030d
Hans <hans@hansl.ca>
2017-06-21 23:38:15 +0000

    4352dd2 fix(compiler): avoid emitting self importing factories

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-34cc3f2
+ * @license Angular v4.2.2-4352dd2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-34cc3f2
+ * @license Angular v4.2.2-4352dd2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3940,7 +3940,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-34cc3f2');
+var VERSION = new _angular_core.Version('4.2.2-4352dd2');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
