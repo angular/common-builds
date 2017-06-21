a4acfce2868b83ebdc8a6f807108c921801dcbee
Hans <hans@hansl.ca>
2017-06-21 18:56:23 +0000

    0034bb2 fix(aio): restore component-styles/exclude hidden children

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-d168528
+ * @license Angular v4.2.2-0034bb2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-d168528
+ * @license Angular v4.2.2-0034bb2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3940,7 +3940,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-d168528');
+var VERSION = new _angular_core.Version('4.2.2-0034bb2');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
