c8d30593c1f269d4cf6df70246b8de5f3d8a2084
Hans Larsen <hans@hansl.ca>
2017-06-22 00:43:26 +0000

    8a09015 docs: add changelog for 4.2.4

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-d203136
+ * @license Angular v4.2.4-8a09015
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3780,7 +3780,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.3-d203136');
+const VERSION = new Version('4.2.4-8a09015');
 
 /**
  * @license
