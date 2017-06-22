73776acc4b1ae024529d7dba6595976a740ef7dc
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:40:15 +0000

    5dd5bfd docs(aio): remove "_" from private property name

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-1a947e4
+ * @license Angular v4.3.0-beta.0-5dd5bfd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-1a947e4
+ * @license Angular v4.3.0-beta.0-5dd5bfd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3940,7 +3940,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.0-1a947e4');
+var VERSION = new _angular_core.Version('4.3.0-beta.0-5dd5bfd');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
