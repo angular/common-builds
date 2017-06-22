c4fd44f13bf7be6bbd5dafa0f69af59b3426f1f1
Hans Larsen <hans@hansl.ca>
2017-06-22 00:48:07 +0000

    1fc0d05 docs: add changelog for 4.3.0-beta.0

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-02a38d3
+ * @license Angular v4.3.0-beta.0-1fc0d05
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-02a38d3
+ * @license Angular v4.3.0-beta.0-1fc0d05
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3940,7 +3940,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-02a38d3');
+var VERSION = new _angular_core.Version('4.3.0-beta.0-1fc0d05');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
