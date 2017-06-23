7a3c00361ce00e998caceee01850403e5f11e354
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 19:19:07 +0000

    87206e1 fix(aio): preserve newlines when copying code

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-494a0d0
+ * @license Angular v4.3.0-beta.0-87206e1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-494a0d0
+ * @license Angular v4.3.0-beta.0-87206e1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3940,7 +3940,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.0-494a0d0');
+var VERSION = new _angular_core.Version('4.3.0-beta.0-87206e1');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
