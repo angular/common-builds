09b65b9ad3cc8d71255c29a4297182573c77238b
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 23:38:52 +0000

    3097083 feat(compiler-cli): new compiler api and command-line using TypeScript transformers

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.0-3165fd3
+ * @license Angular v4.3.0-beta.0-3097083
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3905,7 +3905,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.0-3165fd3');
+var VERSION = new Version('4.3.0-beta.0-3097083');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
