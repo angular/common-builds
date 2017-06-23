811e16126517543a68f6aa4e50592e43deee113c
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 20:08:59 +0000

    ffbdf74 docs(aio): cleanup rollup-config script

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.4-9344c86
+ * @license Angular v4.2.4-ffbdf74
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3780,7 +3780,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.4-9344c86');
+const VERSION = new Version('4.2.4-ffbdf74');
 
 /**
  * @license
