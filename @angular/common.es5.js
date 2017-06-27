bfb516ac8dbe2025197c83afcc72dcbe8ffd65c4
Matias Niemelä <matias@yearofmoo.com>
2017-06-27 20:53:59 +0000

    7205d7a fix(aio): build `scripts-js` before creating a new docker image for the preview server

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.4-ffbdf74
+ * @license Angular v4.2.4-7205d7a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3905,7 +3905,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.4-ffbdf74');
+var VERSION = new Version('4.2.4-7205d7a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
