4ff26460dabf23078b2c34b7d651d777262d3324
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-01 14:06:18 +0000

    c460757 docs(aio): Add resources contributor info

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.5-1a476b8
+ * @license Angular v4.2.5-c460757
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3780,7 +3780,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.5-1a476b8');
+const VERSION = new Version('4.2.5-c460757');
 
 /**
  * @license
