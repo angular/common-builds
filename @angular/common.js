4c913540e243ec7b8ace826eba5625a243252fdd
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-03 07:55:21 +0000

    dc88e0a docs(aio): update quickstart to latest cli

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-fa34ed8
+ * @license Angular v4.3.0-beta.1-dc88e0a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3780,7 +3780,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-fa34ed8');
+const VERSION = new Version('4.3.0-beta.1-dc88e0a');
 
 /**
  * @license
