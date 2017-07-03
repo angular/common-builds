d85acc0a0d378eab76b8d76ab64d6d5af6357247
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-03 07:56:06 +0000

    ed45830 docs(aio): update quickstart to latest cli

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.5-37b2000
+ * @license Angular v4.2.5-ed45830
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.5-37b2000
+ * @license Angular v4.2.5-ed45830
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3940,7 +3940,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.5-37b2000');
+var VERSION = new _angular_core.Version('4.2.5-ed45830');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
