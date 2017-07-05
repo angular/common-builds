b6187aa00b59d96b45ccd1ddc319d0fff1951f9d
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-05 22:31:23 +0000

    b51697c ci: add Chuck to the owners of core (#17892)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-92d7ecf
+ * @license Angular v4.3.0-beta.1-b51697c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3780,7 +3780,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-92d7ecf');
+const VERSION = new Version('4.3.0-beta.1-b51697c');
 
 /**
  * @license
