3ad0499301ddf44671b24a67e3f6a3964e8ec6f4
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-05 21:50:52 +0000

    9d15d85 docs: README - use correctly encoded link for npm badge (#17707)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-9263da5
+ * @license Angular v4.3.0-beta.1-9d15d85
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3780,7 +3780,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-9263da5');
+const VERSION = new Version('4.3.0-beta.1-9d15d85');
 
 /**
  * @license
