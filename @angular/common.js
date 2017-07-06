03085024836d47c05f38ca2e955d59e3d8d42c01
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-06 14:16:21 +0000

    71ee0c5 ci(aio): raise polyfill payload limits

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-578bdeb
+ * @license Angular v4.3.0-beta.1-71ee0c5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3780,7 +3780,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-578bdeb');
+const VERSION = new Version('4.3.0-beta.1-71ee0c5');
 
 /**
  * @license
