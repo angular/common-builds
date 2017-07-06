c67e97e32bf5dd9ee26f72c020df046c21dfd26b
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-06 19:31:55 +0000

    c69fff1 fix(core): fix re-insertions in the iterable differ (#17891)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-70981c6
+ * @license Angular v4.3.0-beta.1-c69fff1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3780,7 +3780,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-70981c6');
+const VERSION = new Version('4.3.0-beta.1-c69fff1');
 
 /**
  * @license
