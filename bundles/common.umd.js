f5e3ac5fbeea4f79188b76001be55fe2ff2abb03
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 19:19:20 +0000

    72747e5 ci: disable sauce-connect logging for realz (#17995)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-504500d
+ * @license Angular v4.3.0-beta.1-72747e5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.1-504500d
+ * @license Angular v4.3.0-beta.1-72747e5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3940,7 +3940,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.1-504500d');
+var VERSION = new _angular_core.Version('4.3.0-beta.1-72747e5');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
