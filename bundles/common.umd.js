e052633db66e7eca218ac9e831100b42f6ee8f95
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 23:47:18 +0000

    c1474f3 feat(compiler-cli): add parameters to ngc main needed by bazel rules (#17885)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-0ede642
+ * @license Angular v4.3.0-beta.1-c1474f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.1-0ede642
+ * @license Angular v4.3.0-beta.1-c1474f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3956,7 +3956,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.1-0ede642');
+var VERSION = new _angular_core.Version('4.3.0-beta.1-c1474f3');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
