dc68a8577cd1921034c4e565b7c2db3e0b695f81
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 23:51:50 +0000

    abbac4b feat(aio): use new ngo (#17977)

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-671a175
+ * @license Angular v4.3.0-beta.1-abbac4b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3921,7 +3921,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-671a175');
+var VERSION = new Version('4.3.0-beta.1-abbac4b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-671a175
+ * @license Angular v4.3.0-beta.1-abbac4b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3797,7 +3797,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-671a175');
+const VERSION = new Version('4.3.0-beta.1-abbac4b');
 
 /**
  * @license
