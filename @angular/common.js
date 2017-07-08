2d9a742659cd6292b2a73917db1a13ab5e7d0246
Jason Aden <jasonaden@google.com>
2017-07-08 05:34:41 +0000

    cc611c9 docs: add changelog for 4.3.0-rc.0

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-05a33d5
+ * @license Angular v4.3.0-rc.0-cc611c9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3797,7 +3797,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-05a33d5');
+const VERSION = new Version('4.3.0-rc.0-cc611c9');
 
 /**
  * @license
