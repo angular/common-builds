e90de5c87f72a45a78c64dd99ab223ce2693f0aa
Jason Aden <jasonaden@google.com>
2017-07-08 05:46:25 +0000

    cc611c9 docs: add changelog for 4.3.0-rc.0

---
+++ http/src/module.d.ts
@@ -0,0 +1,40 @@
+import { HttpBackend, HttpHandler } from './backend';
+import { HttpInterceptor } from './interceptor';
+/**
+ * Constructs an `HttpHandler` that applies a bunch of `HttpInterceptor`s
+ * to a request before passing it to the given `HttpBackend`.
+ *
+ * Meant to be used as a factory function within `HttpClientModule`.
+ *
+ * @experimental
+ */
+export declare function interceptingHandler(backend: HttpBackend, interceptors?: HttpInterceptor[] | null): HttpHandler;
+/**
+ * Factory function that determines where to store JSONP callbacks.
+ *
+ * Ordinarily JSONP callbacks are stored on the `window` object, but this may not exist
+ * in test environments. In that case, callbacks are stored on an anonymous object instead.
+ *
+ * @experimental
+ */
+export declare function jsonpCallbackContext(): Object;
+/**
+ * `NgModule` which provides the `HttpClient` and associated services.
+ *
+ * Interceptors can be added to the chain behind `HttpClient` by binding them
+ * to the multiprovider for `HTTP_INTERCEPTORS`.
+ *
+ * @experimental
+ */
+export declare class HttpClientModule {
+}
+/**
+ * `NgModule` which enables JSONP support in `HttpClient`.
+ *
+ * Without this module, Jsonp requests will reach the backend
+ * with method JSONP, where they'll be rejected.
+ *
+ * @experimental
+ */
+export declare class HttpClientJsonpModule {
+}
