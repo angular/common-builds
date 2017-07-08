7ffd909bd703d67b0de98e6793487ba311a18a72
Jason Aden <jasonaden@google.com>
2017-07-08 05:15:23 +0000

    b69f0fa docs: add changelog for 4.2.6

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.5-8b8b4cb
+ * @license Angular v4.2.6-b69f0fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3797,7 +3797,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.5-8b8b4cb');
+const VERSION = new Version('4.2.6-b69f0fa');
 
 /**
  * @license
