8d337af70d179cfc148f03629f6f3e1289649084
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-10 13:13:51 +0000

    bd4ec5c revert:build(aio): upgrade to dgeni-packages v0.20.0-rc.1

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.6-406dd45
+ * @license Angular v4.2.6-bd4ec5c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3921,7 +3921,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.6-406dd45');
+var VERSION = new Version('4.2.6-bd4ec5c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
