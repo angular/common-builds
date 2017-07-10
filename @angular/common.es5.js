bfd3229f9ad86504120035b41f9a6fa0a67ee5b4
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-10 11:53:07 +0000

    b3d9036 fix(aio): add new common/http package into API list

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-rc.0-cb76091
+ * @license Angular v4.3.0-rc.0-b3d9036
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3921,7 +3921,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-rc.0-cb76091');
+var VERSION = new Version('4.3.0-rc.0-b3d9036');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-cb76091
+ * @license Angular v4.3.0-rc.0-b3d9036
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-rc.0-cb76091
+ * @license Angular v4.3.0-rc.0-b3d9036
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3956,7 +3956,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-rc.0-cb76091');
+var VERSION = new _angular_core.Version('4.3.0-rc.0-b3d9036');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
