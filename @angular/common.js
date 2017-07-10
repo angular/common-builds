fc04650a2d21d062d6899813ff9d85d442810be8
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-10 12:11:09 +0000

    406dd45 feat(aio): add API endpoint for notifying about PR updates

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.6-274c229
+ * @license Angular v4.2.6-406dd45
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3797,7 +3797,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.6-274c229');
+const VERSION = new Version('4.2.6-406dd45');
 
 /**
  * @license
