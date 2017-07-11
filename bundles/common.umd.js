b4c8726316a6b01a3b647f874d27f7a0590e4436
Igor Minar <iminar@google.com>
2017-07-11 20:12:40 +0000

    bbf2133 docs: update ECMAScript info (#17801)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-4300439
+ * @license Angular v4.3.0-rc.0-bbf2133
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-rc.0-4300439
+ * @license Angular v4.3.0-rc.0-bbf2133
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3956,7 +3956,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-rc.0-4300439');
+var VERSION = new _angular_core.Version('4.3.0-rc.0-bbf2133');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
