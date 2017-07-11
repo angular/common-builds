45811c08c438dc89dad2a39c0f8ebfccd507e29e
Igor Minar <iminar@google.com>
2017-07-11 22:34:43 +0000

    e110a80 docs(aio): change Angular Module to NgModule (#16964)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-26b9492
+ * @license Angular v4.3.0-rc.0-e110a80
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3797,7 +3797,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-26b9492');
+const VERSION = new Version('4.3.0-rc.0-e110a80');
 
 /**
  * @license
