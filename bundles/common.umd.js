4a22da8b5c83896372333f658d2aa6f6bcc50280
Igor Minar <iminar@google.com>
2017-07-11 20:01:48 +0000

    ec14679 docs: add @angular/animations to list (#17488)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-1b1a6ba
+ * @license Angular v4.3.0-rc.0-ec14679
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-rc.0-1b1a6ba
+ * @license Angular v4.3.0-rc.0-ec14679
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3956,7 +3956,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-rc.0-1b1a6ba');
+var VERSION = new _angular_core.Version('4.3.0-rc.0-ec14679');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
