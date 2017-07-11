39ead08570f29dce882c3520f0fa699f514cbbc3
Igor Minar <iminar@google.com>
2017-07-11 19:21:34 +0000

    b9525ec docs: Unbreak README Formatting (#18043)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-d9b03be
+ * @license Angular v4.3.0-rc.0-b9525ec
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-rc.0-d9b03be
+ * @license Angular v4.3.0-rc.0-b9525ec
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3956,7 +3956,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-rc.0-d9b03be');
+var VERSION = new _angular_core.Version('4.3.0-rc.0-b9525ec');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
