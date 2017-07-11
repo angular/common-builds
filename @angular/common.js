afe61ec2a744a50baa203d0bbe7540d1e176cc31
Igor Minar <iminar@google.com>
2017-07-11 20:35:57 +0000

    20127c1 docs: fix typos and grammar (#17818)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-a50d935
+ * @license Angular v4.3.0-rc.0-20127c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3797,7 +3797,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-a50d935');
+const VERSION = new Version('4.3.0-rc.0-20127c1');
 
 /**
  * @license
