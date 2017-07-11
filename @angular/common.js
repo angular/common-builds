9bda1c316f3c33de519c77f459f05d76924431cb
Igor Minar <iminar@google.com>
2017-07-11 19:17:02 +0000

    d9b03be test: git ignore e2e test folders (#17984)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-b6aad07
+ * @license Angular v4.3.0-rc.0-d9b03be
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3797,7 +3797,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-b6aad07');
+const VERSION = new Version('4.3.0-rc.0-d9b03be');
 
 /**
  * @license
