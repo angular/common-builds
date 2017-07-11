68f974a63c564d7fb316b1236787b9adc69ef06d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-11 14:29:13 +0000

    e28f097 ci: add aio/content to aio group

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-e8bbf86
+ * @license Angular v4.3.0-rc.0-e28f097
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3797,7 +3797,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-e8bbf86');
+const VERSION = new Version('4.3.0-rc.0-e28f097');
 
 /**
  * @license
