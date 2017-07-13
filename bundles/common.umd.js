768b42acad11d0a643b71bce3a263e3c47c46596
Igor Minar <igor@angularjs.org>
2017-07-13 22:53:00 +0000

    72143e8 revert: ci(aio): exclude changes in aio/content folder

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-45ffe54
+ * @license Angular v4.3.0-rc.0-72143e8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-rc.0-45ffe54
+ * @license Angular v4.3.0-rc.0-72143e8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3956,7 +3956,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-rc.0-45ffe54');
+var VERSION = new _angular_core.Version('4.3.0-rc.0-72143e8');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
