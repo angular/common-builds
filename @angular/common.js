a3c0d78a82ebaac9f44c6bea190257177f525e4f
Igor Minar <iminar@google.com>
2017-07-13 23:34:21 +0000

    ddb766e feat(compiler): do not evaluate metadata expressions that can use references (#18001)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-72143e8
+ * @license Angular v4.3.0-rc.0-ddb766e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3797,7 +3797,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-72143e8');
+const VERSION = new Version('4.3.0-rc.0-ddb766e');
 
 /**
  * @license
