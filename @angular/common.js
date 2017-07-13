7819af834d6c03485a4d192251e4ee40abef44f5
Igor Minar <iminar@google.com>
2017-07-13 19:34:50 +0000

    ffda3e4 build(aio): simplify the primary overload's anchor

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-a7ea008
+ * @license Angular v4.3.0-rc.0-ffda3e4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3797,7 +3797,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-a7ea008');
+const VERSION = new Version('4.3.0-rc.0-ffda3e4');
 
 /**
  * @license
