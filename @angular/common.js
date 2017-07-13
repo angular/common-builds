85b3d364fd5fe1015f1a3b010246d6bd3b2dd4d3
Igor Minar <iminar@google.com>
2017-07-13 22:48:27 +0000

    bc1ea8c docs: rewrite 'Server Communication' doc guide to use HttpClient (#18079)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-1bf7ba8
+ * @license Angular v4.3.0-rc.0-bc1ea8c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3797,7 +3797,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-1bf7ba8');
+const VERSION = new Version('4.3.0-rc.0-bc1ea8c');
 
 /**
  * @license
