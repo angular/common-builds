9c3e1750cf02ec6bffa3e21a9d36b41dfe80b47f
Igor Minar <igor@angularjs.org>
2017-07-14 06:08:29 +0000

    7955cac build(aio): turn on debugging of the payload.sh script to debug ci failures

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-rc.0-dd04f09
+ * @license Angular v4.3.0-rc.0-7955cac
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3943,7 +3943,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-rc.0-dd04f09');
+var VERSION = new Version('4.3.0-rc.0-7955cac');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
