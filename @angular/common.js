202c05043aa12e4c58eca6717227c11c1807ec42
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-15 08:25:34 +0000

    6e2ddcc build(aio): render type parameters of API function exports

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-55742e4
+ * @license Angular v4.3.0-6e2ddcc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3819,7 +3819,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-55742e4');
+const VERSION = new Version('4.3.0-6e2ddcc');
 
 /**
  * @license
