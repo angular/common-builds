8250ee121fe5c5c551d27ea3b14eb55464cacc92
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-15 15:12:37 +0000

    969ce9d fix(aio): remove title from callout

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-34834a9
+ * @license Angular v4.3.0-969ce9d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3943,7 +3943,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-34834a9');
+var VERSION = new Version('4.3.0-969ce9d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-34834a9
+ * @license Angular v4.3.0-969ce9d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3819,7 +3819,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-34834a9');
+const VERSION = new Version('4.3.0-969ce9d');
 
 /**
  * @license
