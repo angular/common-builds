647e610921592977a867d24063a8e793977bd1e6
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-15 08:09:37 +0000

    0bf0c35 build(aio): render type parameters of API function exports

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-97e6901
+ * @license Angular v4.3.0-0bf0c35
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3943,7 +3943,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-97e6901');
+var VERSION = new Version('4.3.0-0bf0c35');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
