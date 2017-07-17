3d195764f83fa51af25feaec1628ecfffa44e172
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-17 21:43:30 +0000

    1390afe docs: fix HttpClient sample

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-969ce9d
+ * @license Angular v4.3.0-1390afe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3943,7 +3943,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-969ce9d');
+var VERSION = new Version('4.3.0-1390afe');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
