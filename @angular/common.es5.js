900dcea8499e7ff3e6de585af498098084389dfb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:39:59 +0000

    5c62e30 fix(common): send flushed body as error instead of null

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-256bc8a
+ * @license Angular v4.3.0-5c62e30
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3943,7 +3943,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-256bc8a');
+var VERSION = new Version('4.3.0-5c62e30');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
