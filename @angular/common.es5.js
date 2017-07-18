f29b899da3630478e42be1d5ce00bfe7248c1420
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 17:36:28 +0000

    ad3029e fix(router): should run resolvers for the same route concurrently

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-2a2fe11
+ * @license Angular v4.3.0-ad3029e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3943,7 +3943,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-2a2fe11');
+var VERSION = new Version('4.3.0-ad3029e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
