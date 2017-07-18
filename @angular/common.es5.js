8a21ee2fa4ad84d3c383aef681cb8b70c53ebd84
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 23:53:49 +0000

    5344be5 fix(animations): make sure @.disabled works in non-animation components

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-23146c9
+ * @license Angular v4.3.0-5344be5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3943,7 +3943,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-23146c9');
+var VERSION = new Version('4.3.0-5344be5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-23146c9
+ * @license Angular v4.3.0-5344be5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3819,7 +3819,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-23146c9');
+const VERSION = new Version('4.3.0-5344be5');
 
 /**
  * @license
