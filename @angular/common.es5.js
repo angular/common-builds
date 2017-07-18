c196793e870351cec673b7055a1092f82a1ea913
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:16:47 +0000

    e03adb9 docs(platform-server): add doc string for PlatformOptions

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-b399cb2
+ * @license Angular v4.3.0-e03adb9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3943,7 +3943,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-b399cb2');
+var VERSION = new Version('4.3.0-e03adb9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
