30dbb9f7ccc6bbcd29ec7a5ec4119604d63445e9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 17:47:19 +0000

    eb6fb5f fix(router): should navigate to the same url when config changes

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-ad3029e
+ * @license Angular v4.3.0-eb6fb5f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3943,7 +3943,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-ad3029e');
+var VERSION = new Version('4.3.0-eb6fb5f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
