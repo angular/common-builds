e8e000bba5f53ea4c8ecca1059ad7679621b814a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:37:04 +0000

    17b7bc3 fix(common): send flushed body as error instead of null

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-37c626e
+ * @license Angular v4.3.0-17b7bc3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3819,7 +3819,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-37c626e');
+const VERSION = new Version('4.3.0-17b7bc3');
 
 /**
  * @license
