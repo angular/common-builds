a9ff2aefca08fe685e82de55d7fe7b708a60703f
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-18 11:12:32 +0000

    c453b7b build(aio): fail doc-gen if referenced images are missing

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-3f331b5
+ * @license Angular v4.3.0-c453b7b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3819,7 +3819,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-3f331b5');
+const VERSION = new Version('4.3.0-c453b7b');
 
 /**
  * @license
--- bundles/common-http-testing.umd.js
+++ bundles/common-http-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-3f331b5
+ * @license Angular v4.3.0-c453b7b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_common_http,_angular_core,rxjs_Observable) { 'use strict';
 
 /**
- * @license Angular v4.3.0-3f331b5
+ * @license Angular v4.3.0-c453b7b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
