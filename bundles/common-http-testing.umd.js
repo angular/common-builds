449e5675c8a52d1af4496df28efccadb6f727903
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:34:30 +0000

    256bc8a docs(http): Make name of injected HttpTestingController consistent

--- bundles/common-http-testing.umd.js
+++ bundles/common-http-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-59c23c7
+ * @license Angular v4.3.0-256bc8a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_common_http,_angular_core,rxjs_Observable) { 'use strict';
 
 /**
- * @license Angular v4.3.0-59c23c7
+ * @license Angular v4.3.0-256bc8a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-59c23c7
+ * @license Angular v4.3.0-256bc8a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-59c23c7
+ * @license Angular v4.3.0-256bc8a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3978,7 +3978,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-59c23c7');
+var VERSION = new _angular_core.Version('4.3.0-256bc8a');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
