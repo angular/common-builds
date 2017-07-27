1ace0e96ab93002974f58454a601dcfcfbb6e5e5
Miško Hevery <misko@hevery.com>
2017-07-27 21:23:52 +0000

    8de2ace docs: add changelog for 4.3.2

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.1-12b8e1a
+ * @license Angular v4.3.2-8de2ace
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.1-12b8e1a
+ * @license Angular v4.3.2-8de2ace
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3978,7 +3978,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.1-12b8e1a');
+var VERSION = new _angular_core.Version('4.3.2-8de2ace');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
