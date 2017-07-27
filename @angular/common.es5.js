b7c11b222e4925dd8b58c88a2a917be4efa47829
Miško Hevery <misko@hevery.com>
2017-07-27 18:32:20 +0000

    2245748 test(compiler): fix xliff2 integration test for the 4.3 branch (#18363)

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.1-bcea196
+ * @license Angular v4.3.1-2245748
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3943,7 +3943,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.1-bcea196');
+var VERSION = new Version('4.3.1-2245748');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
