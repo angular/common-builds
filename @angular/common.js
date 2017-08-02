5ca879b5fc9004ceaa7c220d9358caceefd51146
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-08-02 20:42:53 +0000

    24db1ed docs: add changelog for 4.3.3

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.2-da8bb1b
+ * @license Angular v4.3.3-24db1ed
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3819,7 +3819,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.2-da8bb1b');
+const VERSION = new Version('4.3.3-24db1ed');
 
 /**
  * @license
