9c181dfe25b3f14154f4a145799ed5264d3dbbd2
Victor Berchet <victor@suumit.com>
2017-08-02 22:50:15 +0000

    dbc5c58 docs: improve github labels by introducing "PR target" labels (#18436)

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.3-baf4ce0
+ * @license Angular v4.3.3-dbc5c58
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3943,7 +3943,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.3-baf4ce0');
+var VERSION = new Version('4.3.3-dbc5c58');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
