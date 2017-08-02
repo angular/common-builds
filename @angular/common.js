0d9a5fa3d90fd5124e585e1a6e39cf5961b0d556
GitHub <noreply@github.com>
2017-08-02 22:42:48 +0000

    baf4ce0 build: enable TSLint on the packages folder (#18459)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-24db1ed
+ * @license Angular v4.3.3-baf4ce0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3819,7 +3819,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.3-24db1ed');
+const VERSION = new Version('4.3.3-baf4ce0');
 
 /**
  * @license
