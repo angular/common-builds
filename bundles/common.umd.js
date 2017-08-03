c8db565ba644fae157e7f68d22043d80f90221dc
Victor Berchet <victor@suumit.com>
2017-08-03 01:04:41 +0000

    81cb5bc docs(router): fix typo (#18479)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-1640d2a
+ * @license Angular v5.0.0-beta.2-81cb5bc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.2-1640d2a
+ * @license Angular v5.0.0-beta.2-81cb5bc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3951,7 +3951,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.2-1640d2a');
+var VERSION = new _angular_core.Version('5.0.0-beta.2-81cb5bc');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
