7bc74adb947a295cbc509c4c36ea5df94d676611
Victor Berchet <victor@suumit.com>
2017-08-10 19:00:22 +0000

    06e479f docs: add changelog for 4.3.4

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-77fa3c3
+ * @license Angular v4.3.4-06e479f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.3-77fa3c3
+ * @license Angular v4.3.4-06e479f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3978,7 +3978,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.3-77fa3c3');
+var VERSION = new _angular_core.Version('4.3.4-06e479f');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
