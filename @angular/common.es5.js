5055203a01829526475914e9e1e165735f008ea7
Hans <hans@hansl.ca>
2017-08-14 19:38:58 +0000

    972538b fix(core): forbid destroyed views to be inserted or moved in VC

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.4-d7be4f1
+ * @license Angular v4.3.4-972538b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3943,7 +3943,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.4-d7be4f1');
+var VERSION = new Version('4.3.4-972538b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
