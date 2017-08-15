fddf00a0540991c54285c1fec1b396c59d23cbca
Hans Larsen <hans@hansl.ca>
2017-08-15 23:21:31 +0000

    259fc91 docs(core): correct code examples for ChangeDetectorRef

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.4-a618d6e
+ * @license Angular v4.3.4-259fc91
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3943,7 +3943,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.4-a618d6e');
+var VERSION = new Version('4.3.4-259fc91');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
