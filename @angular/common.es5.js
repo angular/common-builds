bf7c47a08290b02e405a670a2814a480fe9be316
Hans Larsen <hans@hansl.ca>
2017-08-16 18:14:13 +0000

    7e94405 docs: add changelog for 4.3.5

--- @angular/common.es5.js
+++ @angular/common.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.4-a1624f2
+ * @license Angular v4.3.5-7e94405
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3943,7 +3943,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.4-a1624f2');
+var VERSION = new Version('4.3.5-7e94405');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
