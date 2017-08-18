3086207e132813a52c77c48ecb2c0ae848e56bbc
Miško Hevery <misko@hevery.com>
2017-08-18 06:42:03 +0000

    a56468c refactor(platform-webworker): remove deprecated `PRIMITIVE` (#18761)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-56a5b02
+ * @license Angular v5.0.0-beta.4-a56468c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3863,7 +3863,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.4-56a5b02');
+const VERSION = new Version('5.0.0-beta.4-a56468c');
 
 /**
  * @fileoverview added by tsickle
