27d577254fb6ecb92cd9d1551912be6b9dfb6454
Misko Hevery <misko@hevery.com>
2017-08-24 17:04:50 +0000

    1c2f3d4 build: fix build by pinning angular-cli to specific version (#18865)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-3db84c8
+ * @license Angular v5.0.0-beta.4-1c2f3d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5654,7 +5654,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.4-3db84c8');
+const VERSION = new Version('5.0.0-beta.4-1c2f3d4');
 
 /**
  * @fileoverview added by tsickle
