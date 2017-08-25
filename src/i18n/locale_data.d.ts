cc5e713d3dc36547edb50572384be0d55fc6e59c
Misko Hevery <misko@hevery.com>
2017-08-25 16:20:22 +0000

    3d2ee6c build: fix build; missing firebase-tools

---
+++ src/i18n/locale_data.d.ts
@@ -0,0 +1,22 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+/** @experimental */
+export declare enum Plural {
+    Zero = 0,
+    One = 1,
+    Two = 2,
+    Few = 3,
+    Many = 4,
+    Other = 5,
+}
+/**
+ * @experimental i18n support is experimental.
+ */
+export declare const LOCALE_DATA: {
+    [localeId: string]: any;
+};
