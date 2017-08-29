229937e87224c344bb96de8b52a75d531088afdf
Miško Hevery <misko@hevery.com>
2017-08-29 20:23:34 +0000

    24faaf4 docs: add changelog for 5.0.0-beta.5

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-90bd984
+ * @license Angular v5.0.0-beta.5-24faaf4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.5-90bd984
+ * @license Angular v5.0.0-beta.5-24faaf4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5564,7 +5564,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.5-90bd984');
+var VERSION = new _angular_core.Version('5.0.0-beta.5-24faaf4');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
