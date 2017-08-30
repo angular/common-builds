d8f493b31d7fbfd3e182e9d7ed6fdf47ac49c947
GitHub <noreply@github.com>
2017-08-30 09:28:07 +0000

    3725535 style(changelog): improving readability

---
+++ common.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './public_api';
+export { COMMON_DIRECTIVES as ɵc } from './src/directives/index';
+export { LocaleDataIndex as ɵa } from './src/i18n/locale_data';
+export { findLocaleData as ɵb } from './src/i18n/locale_data_api';
+export { COMMON_DEPRECATED_I18N_PIPES as ɵe } from './src/pipes/deprecated/index';
+export { COMMON_PIPES as ɵd } from './src/pipes/index';
