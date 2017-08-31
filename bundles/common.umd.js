cab3e7422e05311be0eaef20b0dff6d12f102cea
Jason Aden <jasonaden@google.com>
2017-08-31 01:22:01 +0000

    2f2d5f3 feat(platform-server): provide a DOM implementation on the server

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-30d53a8
+ * @license Angular v5.0.0-beta.5-2f2d5f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.5-30d53a8
+ * @license Angular v5.0.0-beta.5-2f2d5f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5945,7 +5945,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.5-30d53a8');
+var VERSION = new _angular_core.Version('5.0.0-beta.5-2f2d5f3');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
