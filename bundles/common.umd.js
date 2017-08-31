518e5f465dead2c9dcbf8797c8f98e6217b5cfb5
Jason Aden <jasonaden@google.com>
2017-08-31 18:52:20 +0000

    57abe8d refactor(common): export locale data from closure-locale (#18973)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-cf7d47d
+ * @license Angular v5.0.0-beta.5-57abe8d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.5-cf7d47d
+ * @license Angular v5.0.0-beta.5-57abe8d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5945,7 +5945,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.5-cf7d47d');
+var VERSION = new _angular_core.Version('5.0.0-beta.5-57abe8d');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
