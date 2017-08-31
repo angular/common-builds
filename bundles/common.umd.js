d7a2079527734deab7582612b1548a52822c1cfa
Jason Aden <jasonaden@google.com>
2017-08-31 01:15:44 +0000

    497e017 fix(compiler): normalize the locale name (#18963)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-a203a95
+ * @license Angular v4.3.6-497e017
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.6-a203a95
+ * @license Angular v4.3.6-497e017
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4002,7 +4002,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.6-a203a95');
+var VERSION = new _angular_core.Version('4.3.6-497e017');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
