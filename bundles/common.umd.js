74b439b5d797f9a0347bb9e88f3ae7c4ed1c7414
Jason Aden <jasonaden@google.com>
2017-08-31 01:40:38 +0000

    ba7d70e build: fix changelog to remove AIO line items (#18956)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-497e017
+ * @license Angular v4.3.6-ba7d70e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.6-497e017
+ * @license Angular v4.3.6-ba7d70e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4002,7 +4002,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.6-497e017');
+var VERSION = new _angular_core.Version('4.3.6-ba7d70e');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
