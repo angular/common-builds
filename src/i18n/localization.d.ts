97d71a63b5d5fd5a21900ba6f837f2ee3f77c368
Olivier Combe <olivier.combe@gmail.com>
2017-08-31 09:30:15 +0000

    e1dc9bf style(changelog): improving readability

--- src/i18n/localization.d.ts
+++ src/i18n/localization.d.ts
@@ -1,4 +1,16 @@
 /**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { InjectionToken } from '@angular/core';
+/**
+ * @deprecated from v5
+ */
+export declare const USE_V4_PLURALS: InjectionToken<boolean>;
+/**
  * @experimental
  */
 export declare abstract class NgLocalization {
@@ -11,6 +23,7 @@ export declare abstract class NgLocalization {
  */
 export declare class NgLocaleLocalization extends NgLocalization {
     protected locale: string;
-    constructor(locale: string);
+    protected useV4Plurals: boolean;
+    constructor(locale: string, useV4Plurals?: boolean);
     getPluralCategory(value: any, locale?: string): string;
 }
