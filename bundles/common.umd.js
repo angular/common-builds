b701dac42a1cb7b15d68810e2355b6ec2dd8113b
Jason Aden <jasonaden@google.com>
2017-09-01 17:53:37 +0000

    1cc3fe2 fix(animations): do not leak DOM nodes/styling for host triggered animations (#18853)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-ba7d70e
+ * @license Angular v4.3.6-1cc3fe2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.6-ba7d70e
+ * @license Angular v4.3.6-1cc3fe2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4002,7 +4002,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.6-ba7d70e');
+var VERSION = new _angular_core.Version('4.3.6-1cc3fe2');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
