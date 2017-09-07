7d190a18b4a62af82817384160ee724d8e89f577
Matias Niemelä <matias@yearofmoo.com>
2017-09-07 20:17:43 +0000

    b6833d1 fix(upgrade): remove code setting id attribute.

--- esm5/index.js
+++ esm5/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-3c480e4
+ * @license Angular v5.0.0-beta.6-b6833d1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5997,7 +5997,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.6-3c480e4');
+var VERSION = new Version('5.0.0-beta.6-b6833d1');
 
 /**
  * @fileoverview added by tsickle
