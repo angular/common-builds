1a5bbf365b4c3189d8f13bee9a7c39a3202a48ec
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 23:20:43 +0000

    2373186 docs: update combined changelog for 4.4 stable

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-b6431c6
+ * @license Angular v5.0.0-beta.7-2373186
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-beta.7-b6431c6
+ * @license Angular v5.0.0-beta.7-2373186
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6486,7 +6486,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.7-b6431c6');
+var VERSION = new _angular_core.Version('5.0.0-beta.7-2373186');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
--- esm15/index.js
+++ esm15/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-b6431c6
+ * @license Angular v5.0.0-beta.7-2373186
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5904,7 +5904,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-b6431c6');
+const VERSION = new Version('5.0.0-beta.7-2373186');
 
 /**
  * @fileoverview added by tsickle
