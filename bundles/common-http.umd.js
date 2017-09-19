1238a60b2581e1ee965caebd09ca63a5afea4434
Igor Minar <igor@angularjs.org>
2017-09-19 04:34:54 +0000

    b6b18c1 fix(tsc-wrapped): deduplicate metadata only when the module is the same (#19249)

--- bundles/common-http.umd.js
+++ bundles/common-http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-2373186
+ * @license Angular v5.0.0-beta.7-b6b18c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,rxjs_observable_of,rxjs_operator_concatMap,rxjs_operator_filter,rxjs_operator_map,tslib,_angular_common,rxjs_Observable) { 'use strict';
 
 /**
- * @license Angular v5.0.0-beta.7-2373186
+ * @license Angular v5.0.0-beta.7-b6b18c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
