dc2db27af31e19bfcd6058bcb37e4401a80c3e6d
Igor Minar <igor@angularjs.org>
2017-09-20 23:40:20 +0000

    2c4107c fix(router): fix activation events toString and docs (#19147)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-b14c2d1
+ * @license Angular v5.0.0-beta.7-2c4107c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6444,7 +6444,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-b14c2d1');
+var VERSION = new Version('5.0.0-beta.7-2c4107c');
 
 /**
  * @fileoverview added by tsickle
