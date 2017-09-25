7e75540b59b9b885fa165f1500f056d6c7eb1db5
Victor Berchet <victor@suumit.com>
2017-09-25 20:11:28 +0000

    97e02c2 build(aio): render class/interface "descendants" in API docs (#19343)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-4767902
+ * @license Angular v5.0.0-beta.7-97e02c2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6445,7 +6445,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-4767902');
+var VERSION = new Version('5.0.0-beta.7-97e02c2');
 
 /**
  * @fileoverview added by tsickle
