55ae5957c3f13a2c4a87e3a6e91d298b9db123b0
Victor Berchet <victor@suumit.com>
2017-09-25 19:24:12 +0000

    68078fd build(aio): run the upload server as a non-previleged user

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-4ae546b
+ * @license Angular v5.0.0-beta.7-68078fd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-beta.7-4ae546b
+ * @license Angular v5.0.0-beta.7-68078fd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6487,7 +6487,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.7-4ae546b');
+var VERSION = new _angular_core.Version('5.0.0-beta.7-68078fd');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
