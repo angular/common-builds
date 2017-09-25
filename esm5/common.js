ed852ad15f66345a22fdda3fbf37d11dfc01dd84
Victor Berchet <victor@suumit.com>
2017-09-25 20:54:01 +0000

    13613d4 fix(compiler): skip &nbsp; when trimming / removing whitespaces (#19310)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-97e02c2
+ * @license Angular v5.0.0-beta.7-13613d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6445,7 +6445,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-97e02c2');
+var VERSION = new Version('5.0.0-beta.7-13613d4');
 
 /**
  * @fileoverview added by tsickle
