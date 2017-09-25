c791cc99ea8df017f70741b1687e3d572d013caf
Victor Berchet <victor@suumit.com>
2017-09-25 19:20:36 +0000

    adb0b76 build(aio): add metadata aliases for directives, components and pipes (#19317)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-0f5c70d
+ * @license Angular v5.0.0-beta.7-adb0b76
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-beta.7-0f5c70d
+ * @license Angular v5.0.0-beta.7-adb0b76
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6487,7 +6487,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.7-0f5c70d');
+var VERSION = new _angular_core.Version('5.0.0-beta.7-adb0b76');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
