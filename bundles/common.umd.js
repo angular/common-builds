6a64ccaf736516d56fdf1fd0a551be70a410e48a
Victor Berchet <victor@suumit.com>
2017-09-26 22:22:20 +0000

    8a0e458 fix(http): introduce named type for HttpParams options (#19360)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-b21a1d1
+ * @license Angular v5.0.0-beta.7-8a0e458
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-beta.7-b21a1d1
+ * @license Angular v5.0.0-beta.7-8a0e458
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6487,7 +6487,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.7-b21a1d1');
+var VERSION = new _angular_core.Version('5.0.0-beta.7-8a0e458');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
