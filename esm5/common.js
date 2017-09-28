359cc00d662926ab84fdac583530d67239b18444
Victor Berchet <victor@suumit.com>
2017-09-28 19:42:46 +0000

    bed8ac7 docs: add changelog for 4.4.4

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-a9d32a3
+ * @license Angular v5.0.0-beta.7-bed8ac7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6445,7 +6445,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-a9d32a3');
+var VERSION = new Version('5.0.0-beta.7-bed8ac7');
 
 /**
  * @fileoverview added by tsickle
