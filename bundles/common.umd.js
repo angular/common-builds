37027a923456716d8ca760dbc0a238f0d9eaa46a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-03 15:27:40 +0000

    7cecaf2 docs(aio): add ngVikings to the events page

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-eef7d8a
+ * @license Angular v5.0.0-rc.0-7cecaf2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.0-eef7d8a
+ * @license Angular v5.0.0-rc.0-7cecaf2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6487,7 +6487,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.0-eef7d8a');
+var VERSION = new _angular_core.Version('5.0.0-rc.0-7cecaf2');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
