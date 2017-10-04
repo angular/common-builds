e12289a3319c63562c920787f4dbe6068ada86a5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 22:32:54 +0000

    f98eb35 docs(aio): document Custom Async validators

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-95f3b1d
+ * @license Angular v4.4.4-f98eb35
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3840,7 +3840,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.4-95f3b1d');
+const VERSION = new Version('4.4.4-f98eb35');
 
 /**
  * @license
