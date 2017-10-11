9445da8f9736b02855eab04fca321663c4d192fb
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 20:53:14 +0000

    3232125 build(aio): remove unnecessary `--` from yarn commands (#19565)

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-f89ac92
+ * @license Angular v4.4.4-3232125
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3840,7 +3840,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.4-f89ac92');
+const VERSION = new Version('4.4.4-3232125');
 
 /**
  * @license
