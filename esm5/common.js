10078bf91beb0339241089196928ebf2f92e8e0e
Chuck Jazdzewski <chuckj@google.com>
2017-10-12 21:23:25 +0000

    522ec9a docs: add changelog for 5.0.0-rc.2

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-94a925a
+ * @license Angular v5.0.0-rc.2-522ec9a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6445,7 +6445,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.1-94a925a');
+var VERSION = new Version('5.0.0-rc.2-522ec9a');
 
 /**
  * @fileoverview added by tsickle
