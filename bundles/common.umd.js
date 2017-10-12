70c2555e2ca827d0d5fbeb10a25e128678d797f6
Chuck Jazdzewski <chuckj@google.com>
2017-10-12 17:54:46 +0000

    6b7cead Revert "Revert "perf(compiler): skip type check and emit in bazel in some cases. (#19646)""

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-055b802
+ * @license Angular v5.0.0-rc.1-6b7cead
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.1-055b802
+ * @license Angular v5.0.0-rc.1-6b7cead
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6487,7 +6487,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.1-055b802');
+var VERSION = new _angular_core.Version('5.0.0-rc.1-6b7cead');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
