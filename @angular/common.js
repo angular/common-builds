40e9566771b2e213dbe5926b2df04331929785a2
Chuck Jazdzewski <chuckj@google.com>
2017-10-12 20:27:32 +0000

    912068e docs: add changelog for 4.4.5

--- @angular/common.js
+++ @angular/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-f27f6e4
+ * @license Angular v4.4.5-912068e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3840,7 +3840,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.4-f27f6e4');
+const VERSION = new Version('4.4.5-912068e');
 
 /**
  * @license
