42dde67584a366fdf1f92c74e0010b158e60bded
Tobias Bosch <tbosch1009@gmail.com>
2017-10-13 16:40:04 +0000

    2e45267 ci: add service-worker to pullapprove (#19642) PR Close #19642

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-e81d1fc
+ * @license Angular v5.0.0-rc.2-2e45267
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.2-e81d1fc
+ * @license Angular v5.0.0-rc.2-2e45267
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6487,7 +6487,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.2-e81d1fc');
+var VERSION = new _angular_core.Version('5.0.0-rc.2-2e45267');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
