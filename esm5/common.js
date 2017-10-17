c70c430ba02a41e946022e885af8abb1485d07a8
Tobias Bosch <tbosch1009@gmail.com>
2017-10-17 20:38:22 +0000

    507290d build: fix broken path for animations in .pullapprove (#19453)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-15a8429
+ * @license Angular v5.0.0-rc.2-507290d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6445,7 +6445,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.2-15a8429');
+var VERSION = new Version('5.0.0-rc.2-507290d');
 
 /**
  * @fileoverview added by tsickle
