9583d3c235e4d520044da5be9fcb3e44b3999a14
Tobias Bosch <tbosch1009@gmail.com>
2017-10-18 17:47:38 +0000

    3342a82 fix(aio): make tests less flaky (#19784) PR Close #19784

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-630c19f
+ * @license Angular v4.4.5-3342a82
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.5-630c19f
+ * @license Angular v4.4.5-3342a82
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4002,7 +4002,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.4.5-630c19f');
+var VERSION = new _angular_core.Version('4.4.5-3342a82');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
