358c22746d421026873c6e0e6cf105911745983d
Tobias Bosch <tbosch1009@gmail.com>
2017-10-18 04:16:40 +0000

    5a9ed2d fix(animations): always fire inner trigger callbacks even if blocked by parent animations (#19753)

--- bundles/common-http.umd.js
+++ bundles/common-http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-01e4aa5
+ * @license Angular v5.0.0-rc.2-5a9ed2d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,rxjs_observable_of,rxjs_operator_concatMap,rxjs_operator_filter,rxjs_operator_map,tslib,_angular_common,rxjs_Observable) { 'use strict';
 
 /**
- * @license Angular v5.0.0-rc.2-01e4aa5
+ * @license Angular v5.0.0-rc.2-5a9ed2d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-01e4aa5
+ * @license Angular v5.0.0-rc.2-5a9ed2d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5905,7 +5905,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.2-01e4aa5');
+const VERSION = new Version('5.0.0-rc.2-5a9ed2d');
 
 /**
  * @fileoverview added by tsickle
