1efba1b3f1c5d569abd0d76c2be374db520847d8
Tobias Bosch <tbosch1009@gmail.com>
2017-10-18 20:35:56 +0000

    269f5ac fix(common): attempt to JSON.parse errors for JSON responses (#19773)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-3342a82
+ * @license Angular v4.4.5-269f5ac
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.5-3342a82
+ * @license Angular v4.4.5-269f5ac
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4002,7 +4002,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.4.5-3342a82');
+var VERSION = new _angular_core.Version('4.4.5-269f5ac');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
