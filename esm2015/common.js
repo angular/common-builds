181a77c838f4ae081e57340a7466926b3abbc80b
Tobias Bosch <tbosch1009@gmail.com>
2017-10-18 17:20:44 +0000

    b0c7ea8 Revert "fix(router): RouterLinkActive should update its state right after checking the children (#19449)"

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-5a9ed2d
+ * @license Angular v5.0.0-rc.2-b0c7ea8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5905,7 +5905,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.2-5a9ed2d');
+const VERSION = new Version('5.0.0-rc.2-b0c7ea8');
 
 /**
  * @fileoverview added by tsickle
