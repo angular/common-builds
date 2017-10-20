ac04131f36014e2e2c9b24669741f84329cb7e61
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-20 17:02:48 +0000

    14380ff build: add warning about changing ngtools_api2

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-441e01c
+ * @license Angular v5.0.0-rc.3-14380ff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5905,7 +5905,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.3-441e01c');
+const VERSION = new Version('5.0.0-rc.3-14380ff');
 
 /**
  * @fileoverview added by tsickle
