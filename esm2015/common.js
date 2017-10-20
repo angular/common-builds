badb561ea018eca23a259a99a021af12d0d109a7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-20 17:40:36 +0000

    910735d build: fix `yarn install` command (`--freeze-lockfile` --> `--frozen-lockfile`)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-820bb7b
+ * @license Angular v5.0.0-rc.3-910735d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5905,7 +5905,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.3-820bb7b');
+const VERSION = new Version('5.0.0-rc.3-910735d');
 
 /**
  * @fileoverview added by tsickle
