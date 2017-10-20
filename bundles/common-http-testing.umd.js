13b3108f7eeeeea342969d7061c4fd47d9425f49
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-20 17:01:29 +0000

    fc86352 build(aio): use http push for navigation.json

--- bundles/common-http-testing.umd.js
+++ bundles/common-http-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-230b98d
+ * @license Angular v5.0.0-rc.3-fc86352
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_common_http,_angular_core,rxjs_Observable) { 'use strict';
 
 /**
- * @license Angular v5.0.0-rc.3-230b98d
+ * @license Angular v5.0.0-rc.3-fc86352
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
