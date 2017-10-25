ac6ae070a5666229e99dda05e5e4c5faf68827d7
Matias Niemelä <matias@yearofmoo.com>
2017-10-25 20:10:27 +0000

    680bcf7 build: update to rxjs@5.5.2 (#19931)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.5-ef08330
+ * @license Angular v5.0.0-rc.5-680bcf7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5905,7 +5905,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.5-ef08330');
+const VERSION = new Version('5.0.0-rc.5-680bcf7');
 
 /**
  * @fileoverview added by tsickle
