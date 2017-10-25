64cf4173a6166909339f15075aebffb419e466c2
Matias Niemelä <matias@yearofmoo.com>
2017-10-25 22:15:19 +0000

    703fcda docs: add changelog for 5.0.0-rc.6

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.5-fde5f2f
+ * @license Angular v5.0.0-rc.6-703fcda
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.5-fde5f2f
+ * @license Angular v5.0.0-rc.6-703fcda
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6487,7 +6487,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.5-fde5f2f');
+var VERSION = new _angular_core.Version('5.0.0-rc.6-703fcda');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
