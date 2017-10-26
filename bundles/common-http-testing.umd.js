24e150f3683f0cd7be2ea0b2663291c131d5e451
Matias Niemelä <matias@yearofmoo.com>
2017-10-26 22:44:52 +0000

    eca822b fix(service-worker): fix improper call of Observable.merge (#19962)

--- bundles/common-http-testing.umd.js
+++ bundles/common-http-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.6-17142a7
+ * @license Angular v5.0.0-rc.6-eca822b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_common_http,_angular_core,rxjs_Observable) { 'use strict';
 
 /**
- * @license Angular v5.0.0-rc.6-17142a7
+ * @license Angular v5.0.0-rc.6-eca822b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
