3023057f9783b6d52cab8fc3760a2e4df01a8124
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 03:26:53 +0000

    de24d54 fix(compiler): report errors properly in G3 in certain conditions (#20041)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-c295aee
+ * @license Angular v5.0.0-rc.8-de24d54
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.8-c295aee
+ * @license Angular v5.0.0-rc.8-de24d54
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6487,7 +6487,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.8-c295aee');
+var VERSION = new _angular_core.Version('5.0.0-rc.8-de24d54');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
