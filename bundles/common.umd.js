229c4479d8dd1a29add731337ae3e5b110fb7a3d
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 03:30:57 +0000

    812786f fix: add missing globals from each rollup configuration (#20028)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-de24d54
+ * @license Angular v5.0.0-rc.8-812786f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.8-de24d54
+ * @license Angular v5.0.0-rc.8-812786f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6487,7 +6487,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.8-de24d54');
+var VERSION = new _angular_core.Version('5.0.0-rc.8-812786f');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
