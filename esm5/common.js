cb39e8465226bd7ca65d64c694667f80329522d6
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 03:49:47 +0000

    f1248b6 refactor: make all rollup config ES5 compatible (#20028)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-812786f
+ * @license Angular v5.0.0-rc.8-f1248b6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6445,7 +6445,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.8-812786f');
+var VERSION = new Version('5.0.0-rc.8-f1248b6');
 
 /**
  * @fileoverview added by tsickle
