5c88c9a4c09dd13bec3b626eeec8def319c41268
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 00:51:48 +0000

    04eb80c fix(compiler): always use relative paths to refer to generated code

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-308fc8e
+ * @license Angular v5.0.0-rc.8-04eb80c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6445,7 +6445,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.8-308fc8e');
+var VERSION = new Version('5.0.0-rc.8-04eb80c');
 
 /**
  * @fileoverview added by tsickle
