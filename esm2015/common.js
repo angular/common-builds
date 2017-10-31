8d3b2fe64b71132975ac0d0061ae47aeedf132ec
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 01:41:45 +0000

    e3a16ed fix(compiler): reexport less symbols in `.ngfactory.ts` files (#19884)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-fd37f3f
+ * @license Angular v5.0.0-rc.8-e3a16ed
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5905,7 +5905,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.8-fd37f3f');
+const VERSION = new Version('5.0.0-rc.8-e3a16ed');
 
 /**
  * @fileoverview added by tsickle
--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-fd37f3f
+ * @license Angular v5.0.0-rc.8-e3a16ed
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6445,7 +6445,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.8-fd37f3f');
+var VERSION = new Version('5.0.0-rc.8-e3a16ed');
 
 /**
  * @fileoverview added by tsickle
