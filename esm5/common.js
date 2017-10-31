78571392ccbc4db32a7b513e734ed5472b1401d4
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 04:07:08 +0000

    3bdbb18 build: update rollup lint rule from bad merge (#20047)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-13f8648
+ * @license Angular v5.0.0-rc.8-3bdbb18
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6445,7 +6445,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.8-13f8648');
+var VERSION = new Version('5.0.0-rc.8-3bdbb18');
 
 /**
  * @fileoverview added by tsickle
