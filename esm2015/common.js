9825add97d20ae705cea3a1731731feaca638d09
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 21:29:14 +0000

    e1fb65c docs(packaging): fix typo

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.9-5225fdb
+ * @license Angular v5.0.0-e1fb65c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5905,7 +5905,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.9-5225fdb');
+const VERSION = new Version('5.0.0-e1fb65c');
 
 /**
  * @fileoverview added by tsickle
--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.9-5225fdb
+ * @license Angular v5.0.0-e1fb65c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6445,7 +6445,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.9-5225fdb');
+var VERSION = new Version('5.0.0-e1fb65c');
 
 /**
  * @fileoverview added by tsickle
