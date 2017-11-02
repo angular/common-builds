fea4c8357f10ec7d32b90217a81772158c39a7ae
Victor Berchet <victor@suumit.com>
2017-11-02 23:27:56 +0000

    25843fe build(aio): avoid building before running the local PWA tests

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-37a740b
+ * @license Angular v5.0.0-25843fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6445,7 +6445,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-37a740b');
+var VERSION = new Version('5.0.0-25843fe');
 
 /**
  * @fileoverview added by tsickle
