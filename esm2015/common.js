44b8226a97d8b8f913a1bc80860fe97d1087f752
Igor Minar <igor@angular.io>
2017-11-02 00:25:50 +0000

    3db7112 docs(aio): fix linting for universal (#20086)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-2ea76cc
+ * @license Angular v5.1.0-beta.0-3db7112
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5905,7 +5905,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-2ea76cc');
+const VERSION = new Version('5.1.0-beta.0-3db7112');
 
 /**
  * @fileoverview added by tsickle
