b2e7d98539b95941196c73929cedde53dafbe83c
Victor Berchet <victor@suumit.com>
2017-11-03 19:30:57 +0000

    1d19d61 fix(platform-browser): support Symbols in custom jasmineToString() method (#19794)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-03f080b
+ * @license Angular v5.1.0-beta.0-1d19d61
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5905,7 +5905,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-03f080b');
+const VERSION = new Version('5.1.0-beta.0-1d19d61');
 
 /**
  * @fileoverview added by tsickle
