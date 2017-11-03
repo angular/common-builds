5e725263714300762fdeca5db5489169b6b65bf7
Victor Berchet <victor@suumit.com>
2017-11-03 22:40:06 +0000

    dbec3ca docs(aio): update pipes for CLI (#19553)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-f7c9b94
+ * @license Angular v5.1.0-beta.0-dbec3ca
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.0-f7c9b94
+ * @license Angular v5.1.0-beta.0-dbec3ca
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6487,7 +6487,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-f7c9b94');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-dbec3ca');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
