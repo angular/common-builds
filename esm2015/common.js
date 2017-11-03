08f25776e919400df31b7f2687d25953c82970be
Victor Berchet <victor@suumit.com>
2017-11-03 19:10:52 +0000

    26f8299 fix(compiler): don't overwrite missingTranslation's value in JIT (#19952)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-c7fd172
+ * @license Angular v5.1.0-beta.0-26f8299
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5905,7 +5905,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-c7fd172');
+const VERSION = new Version('5.1.0-beta.0-26f8299');
 
 /**
  * @fileoverview added by tsickle
