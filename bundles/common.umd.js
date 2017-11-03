48b5561f7263e7874a5d30d249a11753f3e59b17
Victor Berchet <victor@suumit.com>
2017-11-03 23:13:49 +0000

    3997d97 revert: feat(elements): implement `@angular/elements` #19469 (#20152)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-200d92d
+ * @license Angular v5.1.0-beta.0-3997d97
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.0-200d92d
+ * @license Angular v5.1.0-beta.0-3997d97
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6487,7 +6487,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-200d92d');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-3997d97');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
