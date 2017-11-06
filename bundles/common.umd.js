f9492877a259771a7f33fb85467c3a805d31f63f
Victor Berchet <victor@suumit.com>
2017-11-06 18:42:08 +0000

    6b74883 build(aio): upgrade to @anglar/core@5.0.0 and cli@1.5.0

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-049c896
+ * @license Angular v5.1.0-beta.0-6b74883
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.0-049c896
+ * @license Angular v5.1.0-beta.0-6b74883
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6487,7 +6487,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-049c896');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-6b74883');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-049c896
+ * @license Angular v5.1.0-beta.0-6b74883
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6445,7 +6445,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-049c896');
+var VERSION = new Version('5.1.0-beta.0-6b74883');
 
 /**
  * @fileoverview added by tsickle
