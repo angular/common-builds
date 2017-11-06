ce5ab0e6117da9de16d5f8b8a518c14afe781173
Victor Berchet <victor@suumit.com>
2017-11-06 18:23:07 +0000

    8b50ed0 docs(aio): fixed case-typo for code (#20196)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-3997d97
+ * @license Angular v5.1.0-beta.0-8b50ed0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5905,7 +5905,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-3997d97');
+const VERSION = new Version('5.1.0-beta.0-8b50ed0');
 
 /**
  * @fileoverview added by tsickle
