ce7be509d6b2739aa8e4bd1dd7c7c737a383370a
Miško Hevery <misko@hevery.com>
2017-11-16 22:38:16 +0000

    4064cbe docs: add changelog for 5.1.0-beta.1

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-dcfffbf
+ * @license Angular v5.1.0-beta.1-4064cbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.0-dcfffbf
+ * @license Angular v5.1.0-beta.1-4064cbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6524,7 +6524,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-dcfffbf');
+var VERSION = new _angular_core.Version('5.1.0-beta.1-4064cbe');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-dcfffbf
+ * @license Angular v5.1.0-beta.1-4064cbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6482,7 +6482,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-dcfffbf');
+var VERSION = new Version('5.1.0-beta.1-4064cbe');
 
 /**
  * @fileoverview added by tsickle
