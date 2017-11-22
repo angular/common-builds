58adb3b9ecd01a66c43469bb6fd708752554d4c5
Miško Hevery <misko@hevery.com>
2017-11-22 04:44:43 +0000

    7cf5e95 ci: update pullapprove (#20540)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-2a9d2ba
+ * @license Angular v5.1.0-beta.1-7cf5e95
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6482,7 +6482,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.1-2a9d2ba');
+var VERSION = new Version('5.1.0-beta.1-7cf5e95');
 
 /**
  * @fileoverview added by tsickle
