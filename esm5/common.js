07e684ff4b140795f117db2c15526e5e6b06395b
Miško Hevery <misko@hevery.com>
2017-11-27 20:02:56 +0000

    a53a040 build(aio): prevent comments in code from leaking into doc-gen code snippets (#20607)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-330bb2a
+ * @license Angular v5.1.0-beta.2-a53a040
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6482,7 +6482,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.2-330bb2a');
+var VERSION = new Version('5.1.0-beta.2-a53a040');
 
 /**
  * @fileoverview added by tsickle
