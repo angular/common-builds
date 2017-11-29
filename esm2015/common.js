b5a4034573214f43060545f81d69d4126cd4432e
Miško Hevery <misko@hevery.com>
2017-11-29 04:44:39 +0000

    c2b3792 fix(animations): ensure multi-level leave animations work (#19455)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-8bb42df
+ * @license Angular v5.1.0-beta.2-c2b3792
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5947,7 +5947,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.2-8bb42df');
+const VERSION = new Version('5.1.0-beta.2-c2b3792');
 
 /**
  * @fileoverview added by tsickle
