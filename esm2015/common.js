c27bb12a7546ecc418996fc2fe60eb4c8b85f49c
Miško Hevery <misko@hevery.com>
2017-11-30 18:24:11 +0000

    47addd1 revert: docs(aio): add service worker guide content and update nav (#20021) (#20716)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-ba6af2a
+ * @license Angular v5.1.0-beta.2-47addd1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5947,7 +5947,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.2-ba6af2a');
+const VERSION = new Version('5.1.0-beta.2-47addd1');
 
 /**
  * @fileoverview added by tsickle
