fa7d06d853f4a593045c336709b8cf661086a03e
Misko Hevery <misko@hevery.com>
2017-11-30 04:46:53 +0000

    7a2550a styl: broken build due to missing new lines

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-b9e4d62
+ * @license Angular v5.1.0-beta.2-7a2550a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5947,7 +5947,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.2-b9e4d62');
+const VERSION = new Version('5.1.0-beta.2-7a2550a');
 
 /**
  * @fileoverview added by tsickle
--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-b9e4d62
+ * @license Angular v5.1.0-beta.2-7a2550a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6487,7 +6487,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.2-b9e4d62');
+var VERSION = new Version('5.1.0-beta.2-7a2550a');
 
 /**
  * @fileoverview added by tsickle
