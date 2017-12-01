c80b993f3c238fcf31f629b2d1a85e8dc8059b0c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-01 22:40:57 +0000

    b9a91a5 fix(service-worker): don't crash if SW not supported

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.0-60a3081
+ * @license Angular v5.1.0-rc.0-b9a91a5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6487,7 +6487,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-rc.0-60a3081');
+var VERSION = new Version('5.1.0-rc.0-b9a91a5');
 
 /**
  * @fileoverview added by tsickle
