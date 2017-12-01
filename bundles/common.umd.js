ec8aaa0f0d356cd67b3ce8df4a70a6a3fe5cfe46
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-01 23:10:06 +0000

    d507057 docs: add changelog for 5.0.5

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.0-f582620
+ * @license Angular v5.1.0-rc.0-d507057
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-rc.0-f582620
+ * @license Angular v5.1.0-rc.0-d507057
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6529,7 +6529,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-rc.0-f582620');
+var VERSION = new _angular_core.Version('5.1.0-rc.0-d507057');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.0-f582620
+ * @license Angular v5.1.0-rc.0-d507057
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6487,7 +6487,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-rc.0-f582620');
+var VERSION = new Version('5.1.0-rc.0-d507057');
 
 /**
  * @fileoverview added by tsickle
