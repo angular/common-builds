7138accf41474d553b663bd0b7a39f55671ed9be
Igor Minar <igor@angular.io>
2017-12-06 20:59:21 +0000

    c2dbc55 release: cut the 5.1.0 release

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.1-b78ada1
+ * @license Angular v5.1.0-c2dbc55
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5947,7 +5947,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-rc.1-b78ada1');
+const VERSION = new Version('5.1.0-c2dbc55');
 
 /**
  * @fileoverview added by tsickle
