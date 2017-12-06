6a47eac16a8fd4ad54ab824e9cbdb79ab93b7543
Igor Minar <igor@angularjs.org>
2017-12-06 15:05:20 +0000

    be99449 docs(core): add docs for i18n tokens (#17920)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.1-6911a25
+ * @license Angular v5.1.0-rc.1-be99449
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6487,7 +6487,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-rc.1-6911a25');
+var VERSION = new Version('5.1.0-rc.1-be99449');
 
 /**
  * @fileoverview added by tsickle
