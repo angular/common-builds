8db2446fecd648cc354cfa37c0eb863d9d417d53
Igor Minar <igor@angularjs.org>
2017-12-06 06:20:19 +0000

    9efb5da docs(aio): Fix typo (#20732)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-860ef8c
+ * @license Angular v5.0.5-9efb5da
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6445,7 +6445,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.5-860ef8c');
+var VERSION = new Version('5.0.5-9efb5da');
 
 /**
  * @fileoverview added by tsickle
