91f9dbf23eaf33c29179adf92686c2a20252380d
Igor Minar <igor@angularjs.org>
2017-12-06 15:15:10 +0000

    c91c0d4 fix(service-worker): esm2015 points to wrong path (#20800)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-9efb5da
+ * @license Angular v5.0.5-c91c0d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6445,7 +6445,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.5-9efb5da');
+var VERSION = new Version('5.0.5-c91c0d4');
 
 /**
  * @fileoverview added by tsickle
