4db158cc58d03576e9a0af99674a39a184f7e66f
Igor Minar <igor@angularjs.org>
2017-12-06 16:44:42 +0000

    b78ada1 fix(animations): ensure DOM is cleaned up after multiple @trigger leave animations finish (#20740)

--- bundles/common-http-testing.umd.js
+++ bundles/common-http-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.1-be99449
+ * @license Angular v5.1.0-rc.1-b78ada1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_common_http,_angular_core,rxjs_Observable) { 'use strict';
 
 /**
- * @license Angular v5.1.0-rc.1-be99449
+ * @license Angular v5.1.0-rc.1-b78ada1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
