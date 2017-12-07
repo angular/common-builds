8e85db214b9412e41f278c6e3931eee0e84a0d62
Jason Aden <jasonaden@google.com>
2017-12-07 21:48:31 +0000

    9dd60a5 build: upgrade bazel rules to latest (#20768)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-6727336
+ * @license Angular v5.1.0-9dd60a5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5947,7 +5947,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-6727336');
+const VERSION = new Version('5.1.0-9dd60a5');
 
 /**
  * @fileoverview added by tsickle
--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-6727336
+ * @license Angular v5.1.0-9dd60a5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6487,7 +6487,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-6727336');
+var VERSION = new Version('5.1.0-9dd60a5');
 
 /**
  * @fileoverview added by tsickle
