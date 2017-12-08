ddb119711da7ff3c67a30ecbd9bb58127ae44411
Jason Aden <jasonaden@google.com>
2017-12-08 18:21:56 +0000

    e6a2805 fix(animations): properly recover and cleanup DOM when CD failures occur (#20719)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-baeec4d
+ * @license Angular v5.1.0-e6a2805
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6487,7 +6487,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-baeec4d');
+var VERSION = new Version('5.1.0-e6a2805');
 
 /**
  * @fileoverview added by tsickle
