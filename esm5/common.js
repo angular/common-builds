983a376779388b8387ae552bbb861f0f9d667491
Jason Aden <jasonaden@google.com>
2017-12-08 18:35:27 +0000

    affa54d build(aio): upgrade to latest `@angular/cli` (#18428)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-53b9de3
+ * @license Angular v5.1.0-affa54d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6487,7 +6487,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-53b9de3');
+var VERSION = new Version('5.1.0-affa54d');
 
 /**
  * @fileoverview added by tsickle
