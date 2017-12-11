2e0495881ff1b48d04f1ea68ad434ce3151691d6
Jason Aden <jasonaden@google.com>
2017-12-11 23:59:03 +0000

    70cd124 feat(compiler): add a pseudo $any() function to disable type checking (#20876)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-7363b3d
+ * @license Angular v5.1.0-70cd124
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6487,7 +6487,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-7363b3d');
+var VERSION = new Version('5.1.0-70cd124');
 
 /**
  * @fileoverview added by tsickle
