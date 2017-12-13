7791ecc9972638fe4b95a0bb63230fc6876345a6
Jason Aden <jasonaden@google.com>
2017-12-13 17:59:20 +0000

    b3eb1db build: update node version number in .nvmrc (#20832)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-8c52088
+ * @license Angular v5.1.0-b3eb1db
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6487,7 +6487,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-8c52088');
+var VERSION = new Version('5.1.0-b3eb1db');
 
 /**
  * @fileoverview added by tsickle
