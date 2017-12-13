8aba08c0cf13baab36b408371e37e76443149946
Jason Aden <jasonaden@google.com>
2017-12-13 18:02:15 +0000

    d6da798 fix(compiler): support referencing enums in namespaces (#20947)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-be9a737
+ * @license Angular v5.1.0-d6da798
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5947,7 +5947,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-be9a737');
+const VERSION = new Version('5.1.0-d6da798');
 
 /**
  * @fileoverview added by tsickle
--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-be9a737
+ * @license Angular v5.1.0-d6da798
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6487,7 +6487,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-be9a737');
+var VERSION = new Version('5.1.0-d6da798');
 
 /**
  * @fileoverview added by tsickle
