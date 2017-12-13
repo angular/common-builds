788b1487589fa28f1a41a7998547e5ad461d758a
GitHub <noreply@github.com>
2017-12-13 22:17:39 +0000

    57e0535 Merge branch 'master' into alexeagle-patch-7

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-7fd441e
+ * @license Angular v5.2.0-beta.0-57e0535
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5947,7 +5947,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-7fd441e');
+const VERSION = new Version('5.2.0-beta.0-57e0535');
 
 /**
  * @fileoverview added by tsickle
