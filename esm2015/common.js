dfba5a3872f1681737ec0ed27f9b9ef21e3b021f
Jason Aden <jasonaden@google.com>
2017-12-13 19:56:14 +0000

    7671417 docs: add changelog for 5.1.1

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-b3eb1db
+ * @license Angular v5.1.0-7671417
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5947,7 +5947,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-b3eb1db');
+const VERSION = new Version('5.1.0-7671417');
 
 /**
  * @fileoverview added by tsickle
