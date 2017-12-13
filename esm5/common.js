7669e1030ed33154074c1813604826e44c00dbe5
Alex Eagle <alexeagle@google.com>
2017-12-13 23:20:59 +0000

    541a391 ci: parallelize bazel build and test

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-d3dbb9d
+ * @license Angular v5.2.0-beta.0-541a391
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6487,7 +6487,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-beta.0-d3dbb9d');
+var VERSION = new Version('5.2.0-beta.0-541a391');
 
 /**
  * @fileoverview added by tsickle
