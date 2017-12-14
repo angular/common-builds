6deb818aaaede30e4b5e2953d5b21c7feb662f47
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-14 16:56:58 +0000

    2686219 ci: sync CircleCI config from master to 5.0.x

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-eb57bc3
+ * @license Angular v5.0.5-2686219
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6445,7 +6445,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.5-eb57bc3');
+var VERSION = new Version('5.0.5-2686219');
 
 /**
  * @fileoverview added by tsickle
