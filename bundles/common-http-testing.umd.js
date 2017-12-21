a23689a18828b66a9f3d8b35915ecf2d0e4ce47b
Igor Minar <igor@angularjs.org>
2017-12-21 22:23:51 +0000

    4337768 build: fix pullapprove (#21140)

--- bundles/common-http-testing.umd.js
+++ bundles/common-http-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-dc53248
+ * @license Angular v5.1.2-4337768
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_common_http,_angular_core,rxjs_Observable) { 'use strict';
 
 /**
- * @license Angular v5.1.2-dc53248
+ * @license Angular v5.1.2-4337768
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-dc53248
+ * @license Angular v5.1.2-4337768
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.2-dc53248
+ * @license Angular v5.1.2-4337768
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6536,7 +6536,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.2-dc53248');
+var VERSION = new _angular_core.Version('5.1.2-4337768');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
