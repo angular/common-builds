a1a6ee5aed1a96737de3d1e9b61ed78725cdf5c2
Igor Minar <igor@angularjs.org>
2017-12-22 04:29:57 +0000

    bc66d27 docs(service-worker): fix word wrap (#21114)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.1-27fc458
+ * @license Angular v5.2.0-beta.1-bc66d27
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5954,7 +5954,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-beta.1-27fc458');
+const VERSION = new Version('5.2.0-beta.1-bc66d27');
 
 /**
  * @fileoverview added by tsickle
