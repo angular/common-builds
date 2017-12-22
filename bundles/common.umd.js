8ffa57bd98be2238a79a9a5501d389a6e6506742
Igor Minar <igor@angularjs.org>
2017-12-22 04:32:13 +0000

    0aa6341 build: make umd.min.js source map paths relative (#21147)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-41abcc3
+ * @license Angular v5.1.2-0aa6341
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.2-41abcc3
+ * @license Angular v5.1.2-0aa6341
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6536,7 +6536,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.2-41abcc3');
+var VERSION = new _angular_core.Version('5.1.2-0aa6341');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-41abcc3
+ * @license Angular v5.1.2-0aa6341
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5954,7 +5954,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.2-41abcc3');
+const VERSION = new Version('5.1.2-0aa6341');
 
 /**
  * @fileoverview added by tsickle
