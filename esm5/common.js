7b8f031b497a3afd57805990de97b984ba249b40
Igor Minar <igor@angular.io>
2017-12-23 05:58:13 +0000

    12702b2 docs(changelog): fix typo in 5.1.1 (#21007)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-5de91fe
+ * @license Angular v5.1.2-12702b2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6532,7 +6532,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.2-5de91fe');
+var VERSION = new Version('5.1.2-12702b2');
 
 /**
  * @fileoverview added by tsickle
