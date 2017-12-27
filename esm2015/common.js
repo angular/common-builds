bd2cfc20d2f91057e76483f98df28ee13459bce9
Igor Minar <igor@angular.io>
2017-12-27 21:35:01 +0000

    66043e0 docs(aio): Sort in the api type dropdown (#21030) (#21176)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-12702b2
+ * @license Angular v5.1.2-66043e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5992,7 +5992,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.2-12702b2');
+const VERSION = new Version('5.1.2-66043e0');
 
 /**
  * @fileoverview added by tsickle
--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-12702b2
+ * @license Angular v5.1.2-66043e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6532,7 +6532,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.2-12702b2');
+var VERSION = new Version('5.1.2-66043e0');
 
 /**
  * @fileoverview added by tsickle
