efd48c6b2924faa4c51f7cbdb1eae6b692228f7d
Miško Hevery <misko@hevery.com>
2017-12-28 00:02:31 +0000

    9f538a6 build: add karma tests for render3 (#21188)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.1-d8abf70
+ * @license Angular v5.2.0-beta.1-9f538a6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5977,7 +5977,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-beta.1-d8abf70');
+const VERSION = new Version('5.2.0-beta.1-9f538a6');
 
 /**
  * @fileoverview added by tsickle
