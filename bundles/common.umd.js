85fc508265f3f205a2be51a1fa4ca84f0f5c1d50
Misko Hevery <misko@hevery.com>
2018-01-03 22:31:32 +0000

    00308fc build: update rules_typescript in integration test

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-a7a7bd3
+ * @license Angular v5.1.2-00308fc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.2-a7a7bd3
+ * @license Angular v5.1.2-00308fc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6574,7 +6574,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.2-a7a7bd3');
+var VERSION = new _angular_core.Version('5.1.2-00308fc');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
