8f4fdb0cbc58d47942692a979935bf54e280a1c0
Kara Erickson <karakara@google.com>
2018-01-04 22:59:57 +0000

    d79a0ba docs(aio): add missing imports to interceptor example (#21259)

--- bundles/common-http-testing.umd.js
+++ bundles/common-http-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-b7307c9
+ * @license Angular v5.1.3-d79a0ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_common_http,_angular_core,rxjs_Observable) { 'use strict';
 
 /**
- * @license Angular v5.1.3-b7307c9
+ * @license Angular v5.1.3-d79a0ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
