4de327b2ceda8ae7aadf02cdfbd91c4b95b0b69c
Misko Hevery <misko@hevery.com>
2018-01-04 00:21:03 +0000

    d138b38 docs: add changelog for 5.1.3

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-00308fc
+ * @license Angular v5.1.3-d138b38
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.2-00308fc
+ * @license Angular v5.1.3-d138b38
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6574,7 +6574,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.2-00308fc');
+var VERSION = new _angular_core.Version('5.1.3-d138b38');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-00308fc
+ * @license Angular v5.1.3-d138b38
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6532,7 +6532,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.2-00308fc');
+var VERSION = new Version('5.1.3-d138b38');
 
 /**
  * @fileoverview added by tsickle
