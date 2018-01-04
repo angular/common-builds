6858ed64061c6fb33f0d9f331e53ef5a585c5c4d
Kara Erickson <karakara@google.com>
2018-01-04 23:36:46 +0000

    b5934fc docs(router): fix missing export in docs for UrlMatcher (#21095)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,6 +1,6 @@
 /**
- * @license Angular v5.2.0-rc.0-dcc3eba
- * (c) 2010-2017 Google, Inc. https://angular.io/
+ * @license Angular v5.2.0-rc.0-b5934fc
+ * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 import { Attribute, ChangeDetectorRef, ComponentFactoryResolver, Directive, ElementRef, EventEmitter, Host, Inject, Injectable, InjectionToken, Input, IterableDiffers, KeyValueDiffers, LOCALE_ID, NgModule, NgModuleRef, Optional, Pipe, Renderer2, TemplateRef, Version, ViewContainerRef, WrappedValue, isDevMode, ɵisListLikeIterable, ɵisObservable, ɵisPromise, ɵstringify } from '@angular/core';
@@ -5977,7 +5977,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-rc.0-dcc3eba');
+const VERSION = new Version('5.2.0-rc.0-b5934fc');
 
 /**
  * @fileoverview added by tsickle
