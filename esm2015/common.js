c0cfea3446926473bb3aff429bed3715622914e5
Kara Erickson <karakara@google.com>
2018-01-04 22:39:00 +0000

    cc0b0a4 fix(aio): do not redirect pages on "next" deployment (#21027)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-b9d8ac5
+ * @license Angular v5.1.3-cc0b0a4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5992,7 +5992,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.3-b9d8ac5');
+const VERSION = new Version('5.1.3-cc0b0a4');
 
 /**
  * @fileoverview added by tsickle
