fa7327f8a4fa7709a3eef232bcf9305cddf24ca6
Kara Erickson <karakara@google.com>
2018-01-05 20:24:20 +0000

    6cd4b1b build(aio): add `@nodoc` alias to the `@internal` tag (#21024)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-24df08e
+ * @license Angular v5.2.0-rc.0-6cd4b1b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-rc.0-24df08e
+ * @license Angular v5.2.0-rc.0-6cd4b1b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6559,7 +6559,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-rc.0-24df08e');
+var VERSION = new _angular_core.Version('5.2.0-rc.0-6cd4b1b');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-24df08e
+ * @license Angular v5.2.0-rc.0-6cd4b1b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6517,7 +6517,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-rc.0-24df08e');
+var VERSION = new Version('5.2.0-rc.0-6cd4b1b');
 
 /**
  * @fileoverview added by tsickle
