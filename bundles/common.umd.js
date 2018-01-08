a067a58b664fa4965fac720ee784c151de483a52
Kara Erickson <karakara@google.com>
2018-01-08 20:30:32 +0000

    4f7c13d build(core): reduce payload limit back to normal (#21395)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-4947454
+ * @license Angular v5.1.3-4f7c13d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.3-4947454
+ * @license Angular v5.1.3-4f7c13d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6577,7 +6577,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.3-4947454');
+var VERSION = new _angular_core.Version('5.1.3-4f7c13d');
 
 exports.NgLocaleLocalization = NgLocaleLocalization;
 exports.NgLocalization = NgLocalization;
--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-4947454
+ * @license Angular v5.1.3-4f7c13d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5995,7 +5995,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.3-4947454');
+const VERSION = new Version('5.1.3-4f7c13d');
 
 /**
  * @fileoverview added by tsickle
