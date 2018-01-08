b45c9097f7b7a864800ac7b17b7ce7fea71335e8
Kara Erickson <karakara@google.com>
2018-01-08 21:29:11 +0000

    6040ee3 fix(benchpress): forward compat with selenium_webdriver 3.6.0 (#21399)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-74dbf7b
+ * @license Angular v5.2.0-rc.0-6040ee3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5980,7 +5980,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-rc.0-74dbf7b');
+const VERSION = new Version('5.2.0-rc.0-6040ee3');
 
 /**
  * @fileoverview added by tsickle
