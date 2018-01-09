26f42db058a2d76b132095bb57b4826524552366
Kara Erickson <karakara@google.com>
2018-01-09 19:20:31 +0000

    80a5b91 build(aio): upgrade to zone.js@0.8.19 (#21349)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-9728dce
+ * @license Angular v5.2.0-rc.0-80a5b91
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6520,7 +6520,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-rc.0-9728dce');
+var VERSION = new Version('5.2.0-rc.0-80a5b91');
 
 /**
  * @fileoverview added by tsickle
