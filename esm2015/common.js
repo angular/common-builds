d5df8d3651b432ee710fce7081f6ac2aba740931
Kara Erickson <karakara@google.com>
2018-01-09 00:29:18 +0000

    a73b0b6 docs(aio): fix punctuation for clarity (#21325) PR Close #21325

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-fda3815
+ * @license Angular v5.1.3-a73b0b6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5995,7 +5995,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.3-fda3815');
+const VERSION = new Version('5.1.3-a73b0b6');
 
 /**
  * @fileoverview added by tsickle
