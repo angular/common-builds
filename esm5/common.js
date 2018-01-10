591d9c7e780e130c02b86192a0ee60fa9be991fb
Alex Eagle <alexeagle@google.com>
2018-01-10 21:03:13 +0000

    0de219a docs(bazel): document an installation issue (#21401)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-ff6047c
+ * @license Angular v5.2.0-0de219a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6520,7 +6520,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-ff6047c');
+var VERSION = new Version('5.2.0-0de219a');
 
 /**
  * @fileoverview added by tsickle
