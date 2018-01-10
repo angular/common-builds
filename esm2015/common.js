eea123a5e279e3876a4e015efa3d0290566136fb
Alex Eagle <alexeagle@google.com>
2018-01-10 04:34:09 +0000

    a0166bd docs(bazel): document an installation issue

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-0d1611f
+ * @license Angular v5.2.0-a0166bd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5980,7 +5980,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-rc.0-0d1611f');
+const VERSION = new Version('5.2.0-a0166bd');
 
 /**
  * @fileoverview added by tsickle
