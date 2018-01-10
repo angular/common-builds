e234671ad8c961ca721489cce45bc4c1de8c80a7
Alex Eagle <alexeagle@google.com>
2018-01-10 20:18:46 +0000

    ef956a2 build: add bazel rulse for benchmarks (#21436)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-fc3e7e0
+ * @license Angular v5.2.0-ef956a2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6520,7 +6520,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-fc3e7e0');
+var VERSION = new Version('5.2.0-ef956a2');
 
 /**
  * @fileoverview added by tsickle
