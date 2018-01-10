05e8ccebae29231bf2c731b65ff1ba24ab13f97c
Kara Erickson <karakara@google.com>
2018-01-10 01:37:24 +0000

    04dd971 docs: add changelog for 5.2.0

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-3db91ff
+ * @license Angular v5.2.0-04dd971
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6520,7 +6520,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-rc.0-3db91ff');
+var VERSION = new Version('5.2.0-04dd971');
 
 /**
  * @fileoverview added by tsickle
