75237f6068065933d845ae9256901805ec124fa0
Alex Eagle <alexeagle@google.com>
2018-01-10 21:29:59 +0000

    7493b8a build: upgrade yarn to 1.3.2 (#21406)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-c4f02e2
+ * @license Angular v5.2.0-7493b8a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-c4f02e2
+ * @license Angular v5.2.0-7493b8a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6562,7 +6562,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-c4f02e2');
+var VERSION = new _angular_core.Version('5.2.0-7493b8a');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
