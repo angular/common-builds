9e84330b18e9cb54ce837695ae97e0f7989fd8c9
Alex Eagle <alexeagle@google.com>
2018-01-10 21:44:14 +0000

    e3e2fc0 fix(compiler): cache external reference resolution (#21359)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-7493b8a
+ * @license Angular v5.2.0-e3e2fc0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-7493b8a
+ * @license Angular v5.2.0-e3e2fc0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6562,7 +6562,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-7493b8a');
+var VERSION = new _angular_core.Version('5.2.0-e3e2fc0');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
