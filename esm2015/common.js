f3195a061f878c94b007b7d19e32896eb4dc37b2
Alex Eagle <alexeagle@google.com>
2018-01-11 00:32:29 +0000

    6be9c04 refactor(core): split up interface files in render3 (#21433)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-a33ff2c
+ * @license Angular v5.2.0-6be9c04
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5980,7 +5980,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-a33ff2c');
+const VERSION = new Version('5.2.0-6be9c04');
 
 /**
  * @fileoverview added by tsickle
