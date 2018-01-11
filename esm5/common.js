0af1b4c34178dd0b332d1b30ac82c9fb3d7c2885
Alex Eagle <alexeagle@google.com>
2018-01-11 01:29:11 +0000

    7f3d0bb build: update polyfill size (#21461)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-3db02d2
+ * @license Angular v5.2.0-7f3d0bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6520,7 +6520,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-3db02d2');
+var VERSION = new Version('5.2.0-7f3d0bb');
 
 /**
  * @fileoverview added by tsickle
