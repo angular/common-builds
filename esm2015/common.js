f93e3b1dfc6fa3d79f6d754801d6ee816526621e
Alex Eagle <alexeagle@google.com>
2018-01-11 15:20:52 +0000

    5eaaac3 refactor(ivy): remove `type` from `DirectiveDef` (#21374)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-c0080d7
+ * @license Angular v5.2.0-5eaaac3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5980,7 +5980,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-c0080d7');
+const VERSION = new Version('5.2.0-5eaaac3');
 
 /**
  * @fileoverview added by tsickle
