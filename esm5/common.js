098df61cb86f712738e40dc320040d069c994a73
Alex Eagle <alexeagle@google.com>
2018-01-12 23:03:28 +0000

    43e1520 fix(language-service): Clear caches when program changes (#21337)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-2d44a2a
+ * @license Angular v5.2.0-43e1520
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6520,7 +6520,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-2d44a2a');
+var VERSION = new Version('5.2.0-43e1520');
 
 /**
  * @fileoverview added by tsickle
