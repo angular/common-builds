5e26679d9a1220e5c0c29c0d9640e43a2c24b1ac
Alex Eagle <alexeagle@google.com>
2018-01-12 20:27:29 +0000

    f23896f docs: fix release schedule date (#21474)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-0e59d18
+ * @license Angular v5.2.0-f23896f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5980,7 +5980,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-0e59d18');
+const VERSION = new Version('5.2.0-f23896f');
 
 /**
  * @fileoverview added by tsickle
