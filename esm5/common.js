d54d86c8d050fb251cb79e5489e430e44c40082a
Alex Eagle <alexeagle@google.com>
2018-01-12 22:55:24 +0000

    c5c6d84 style(aio): enforce strict TypeScript checks (#21342)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-246de65
+ * @license Angular v5.2.0-c5c6d84
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6520,7 +6520,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-246de65');
+var VERSION = new Version('5.2.0-c5c6d84');
 
 /**
  * @fileoverview added by tsickle
