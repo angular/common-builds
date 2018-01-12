76842206f8b2990db882f9dce768a9041a345fc4
Alex Eagle <alexeagle@google.com>
2018-01-12 23:16:53 +0000

    a4cbe35 ci: disable integration/cli-hello-world test (#21492)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-cc9419d
+ * @license Angular v5.2.0-a4cbe35
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-cc9419d
+ * @license Angular v5.2.0-a4cbe35
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6562,7 +6562,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-cc9419d');
+var VERSION = new _angular_core.Version('5.2.0-a4cbe35');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
