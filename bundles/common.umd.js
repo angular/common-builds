a56384c82903ec126c69cd9c365523fc75287bc9
Alex Eagle <alexeagle@google.com>
2018-01-12 18:48:26 +0000

    63478d7 ci: add angular robot config file (#21489)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-4f31492
+ * @license Angular v5.2.0-63478d7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-4f31492
+ * @license Angular v5.2.0-63478d7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6562,7 +6562,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-4f31492');
+var VERSION = new _angular_core.Version('5.2.0-63478d7');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
