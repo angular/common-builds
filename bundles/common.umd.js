72f8d13c17737e0c129d391402fa3ce4c5f6159e
Alex Eagle <alexeagle@google.com>
2018-01-17 16:50:21 +0000

    b773a4a docs(aio): change `df-question` to `app-question` (#21438)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-55f15c5
+ * @license Angular v5.2.0-b773a4a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-55f15c5
+ * @license Angular v5.2.0-b773a4a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6562,7 +6562,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-55f15c5');
+var VERSION = new _angular_core.Version('5.2.0-b773a4a');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
