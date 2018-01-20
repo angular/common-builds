92059c60e2f90d3d4bd4d68ea5c4b7e8f0d7478e
Misko Hevery <misko@hevery.com>
2018-01-20 05:00:36 +0000

    d7dbdc5 docs: fix stray div and reformat paragraph (#21676)

--- bundles/common-http-testing.umd.js
+++ bundles/common-http-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-554129d
+ * @license Angular v5.2.1-d7dbdc5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_common_http,_angular_core,rxjs_Observable) { 'use strict';
 
 /**
- * @license Angular v5.2.1-554129d
+ * @license Angular v5.2.1-d7dbdc5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
