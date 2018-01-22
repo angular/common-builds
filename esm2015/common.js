8417ab30ede06eb0beb4ce2f9b34368c260b3a24
Miško Hevery <misko@hevery.com>
2018-01-22 23:54:15 +0000

    4b68fdc build: Update to latest rules_typescript. (#21675)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-c12ea3a
+ * @license Angular v5.2.1-4b68fdc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5985,7 +5985,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.1-c12ea3a');
+const VERSION = new Version('5.2.1-4b68fdc');
 
 /**
  * @fileoverview added by tsickle
