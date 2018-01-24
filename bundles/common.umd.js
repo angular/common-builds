471fea44e29d2d7de28786b5cdeac8b1b3c450ff
Miško Hevery <misko@hevery.com>
2018-01-24 00:59:33 +0000

    ed670a3 docs: update ICU select messages to use male/female (#21713)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-52970c0
+ * @license Angular v5.2.1-ed670a3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.1-52970c0
+ * @license Angular v5.2.1-ed670a3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6567,7 +6567,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.1-52970c0');
+var VERSION = new _angular_core.Version('5.2.1-ed670a3');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-52970c0
+ * @license Angular v5.2.1-ed670a3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6525,7 +6525,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.1-52970c0');
+var VERSION = new Version('5.2.1-ed670a3');
 
 /**
  * @fileoverview added by tsickle
