3a9fa947039d49f8c907717a083aeadab14a6022
Miško Hevery <misko@hevery.com>
2018-01-26 19:03:20 +0000

    ba045e8 docs: add notes on email used for CLA (#21754)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-35a0721
+ * @license Angular v5.2.2-ba045e8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6525,7 +6525,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.2-35a0721');
+var VERSION = new Version('5.2.2-ba045e8');
 
 /**
  * @fileoverview added by tsickle
