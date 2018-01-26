33985f8a291bcfb6203a71cdc695b61e77177005
Miško Hevery <misko@hevery.com>
2018-01-26 18:48:21 +0000

    67806a7 fix(aio): close SideNav on non-sidenav doc on wide screen (#21538)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-87e06d7
+ * @license Angular v5.2.2-67806a7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6525,7 +6525,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.2-87e06d7');
+var VERSION = new Version('5.2.2-67806a7');
 
 /**
  * @fileoverview added by tsickle
