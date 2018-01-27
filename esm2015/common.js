c9093d3d2fa3f831fd3cfac4af5e9986dd5caa7e
Misko Hevery <misko@hevery.com>
2018-01-27 19:09:36 +0000

    6245637 fix(ivy): correct query read logic after merges (#21749)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-ab69f12
+ * @license Angular v6.0.0-beta.1-6245637
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5985,7 +5985,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.1-ab69f12');
+const VERSION = new Version('6.0.0-beta.1-6245637');
 
 /**
  * @fileoverview added by tsickle
