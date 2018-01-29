62b5a801536d1e5e5aeb8c709e9bee6d28410add
Jason Aden <jasonaden@google.com>
2018-01-29 20:16:19 +0000

    3f5ead3 fix(aio): missing plural s in preserveWhiteSpaces example (#21854)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-a89e709
+ * @license Angular v5.2.2-3f5ead3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.2-a89e709
+ * @license Angular v5.2.2-3f5ead3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6567,7 +6567,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.2-a89e709');
+var VERSION = new _angular_core.Version('5.2.2-3f5ead3');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-a89e709
+ * @license Angular v5.2.2-3f5ead3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6525,7 +6525,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.2-a89e709');
+var VERSION = new Version('5.2.2-3f5ead3');
 
 /**
  * @fileoverview added by tsickle
