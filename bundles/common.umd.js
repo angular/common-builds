5d9e99bdca4ddb09cf87730adf5a229376f249e5
Jason Aden <jasonaden@google.com>
2018-01-31 21:31:31 +0000

    579bed1 docs: add changelog for 5.2.3

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-2aa460b
+ * @license Angular v5.2.3-579bed1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.2-2aa460b
+ * @license Angular v5.2.3-579bed1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6567,7 +6567,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.2-2aa460b');
+var VERSION = new _angular_core.Version('5.2.3-579bed1');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
