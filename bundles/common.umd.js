969816bc930de660ed99591bb09bae6d55a6629b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-01 16:52:13 +0000

    72df747 docs(aio): add missing closing `<code-examle>` tag (#21771)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-579bed1
+ * @license Angular v5.2.3-72df747
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.3-579bed1
+ * @license Angular v5.2.3-72df747
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6567,7 +6567,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.3-579bed1');
+var VERSION = new _angular_core.Version('5.2.3-72df747');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
