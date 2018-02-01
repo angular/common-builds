fb0a651cf223b4eed1f23dd578a16d015eb25880
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-01 16:53:05 +0000

    c4fb696 fix(common): don't convert null to a string when flushing a mock request (#21417)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-72df747
+ * @license Angular v5.2.3-c4fb696
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.3-72df747
+ * @license Angular v5.2.3-c4fb696
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6567,7 +6567,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.3-72df747');
+var VERSION = new _angular_core.Version('5.2.3-c4fb696');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-72df747
+ * @license Angular v5.2.3-c4fb696
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6525,7 +6525,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.3-72df747');
+var VERSION = new Version('5.2.3-c4fb696');
 
 /**
  * @fileoverview added by tsickle
