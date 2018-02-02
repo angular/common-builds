8fefd4d8d7fa760c147964afbbf61b9d34e16d0f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:55:57 +0000

    102d06b docs: consistency fix in describing a custom tag (#21747)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-aa9ba7f
+ * @license Angular v5.2.3-102d06b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.3-aa9ba7f
+ * @license Angular v5.2.3-102d06b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6567,7 +6567,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.3-aa9ba7f');
+var VERSION = new _angular_core.Version('5.2.3-102d06b');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
