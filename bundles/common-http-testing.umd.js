5b27f79e98f04634a4bd009112ab770ec614f341
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 21:59:51 +0000

    664f7fa build(aio): add API static members to search index (#21988)

--- bundles/common-http-testing.umd.js
+++ bundles/common-http-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-973607f
+ * @license Angular v5.2.3-664f7fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_common_http,_angular_core,rxjs_Observable) { 'use strict';
 
 /**
- * @license Angular v5.2.3-973607f
+ * @license Angular v5.2.3-664f7fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
