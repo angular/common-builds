65107cabdf813e759044c5599eabd3cd7544e0e3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 22:17:00 +0000

    f0396f1 docs(aio): fix swap value (#20905)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-adb1d62
+ * @license Angular v5.2.3-f0396f1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6525,7 +6525,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.3-adb1d62');
+var VERSION = new Version('5.2.3-f0396f1');
 
 /**
  * @fileoverview added by tsickle
