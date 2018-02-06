dff47feb2e3eee1919e73ec83e23ac77e9c4d0dc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-06 16:21:20 +0000

    c6645e7 fix(core): fix proper propagation of subscriptions in EventEmitter (#22016)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-f0396f1
+ * @license Angular v5.2.3-c6645e7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.3-f0396f1
+ * @license Angular v5.2.3-c6645e7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6567,7 +6567,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.3-f0396f1');
+var VERSION = new _angular_core.Version('5.2.3-c6645e7');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
