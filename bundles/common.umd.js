f796f37f8ea64ad11a04a4be6f2f0695f0ccdf11
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-07 19:41:13 +0000

    530b824 docs: add changelog for 5.2.4

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-c6645e7
+ * @license Angular v5.2.4-530b824
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.3-c6645e7
+ * @license Angular v5.2.4-530b824
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6567,7 +6567,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.3-c6645e7');
+var VERSION = new _angular_core.Version('5.2.4-530b824');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-c6645e7
+ * @license Angular v5.2.4-530b824
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6525,7 +6525,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.3-c6645e7');
+var VERSION = new Version('5.2.4-530b824');
 
 /**
  * @fileoverview added by tsickle
