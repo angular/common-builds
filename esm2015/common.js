881485eccece8e09027acaad65dcd2309f03ef37
Miško Hevery <misko@hevery.com>
2018-02-07 20:48:17 +0000

    140e7c0 fix(forms): make Validators.email support optional controls (#20869)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-941e88f
+ * @license Angular v6.0.0-beta.3-140e7c0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5988,7 +5988,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-941e88f');
+const VERSION = new Version('6.0.0-beta.3-140e7c0');
 
 /**
  * @fileoverview added by tsickle
