3d393fce9a96ba21faee82f0ed8c7543b1da46b9
Miško Hevery <misko@hevery.com>
2018-02-07 21:40:36 +0000

    47b73fd fix(core): ensure initial value of QueryList length (#21980) (#21982)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-07769e5
+ * @license Angular v5.2.4-47b73fd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.4-07769e5
+ * @license Angular v5.2.4-47b73fd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6567,7 +6567,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.4-07769e5');
+var VERSION = new _angular_core.Version('5.2.4-47b73fd');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
