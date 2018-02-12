29cb11e627e7ac75645835d8782fa0e08f7d5093
Miško Hevery <misko@hevery.com>
2018-02-12 18:24:32 +0000

    92a5876 refactor(router): move activation to private method (#22033)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-92d7060
+ * @license Angular v6.0.0-beta.3-92a5876
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.3-92d7060
+ * @license Angular v6.0.0-beta.3-92a5876
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6682,7 +6682,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.3-92d7060');
+var VERSION = new _angular_core.Version('6.0.0-beta.3-92a5876');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
