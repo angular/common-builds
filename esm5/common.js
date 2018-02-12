e7dc3fbb7eb27c789b3cb42f0c5b08df6f7e7089
Miško Hevery <misko@hevery.com>
2018-02-12 18:33:30 +0000

    a616dd6 fix(aio): remove broken span closing tag (#22146)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-d57fd0b
+ * @license Angular v5.2.4-a616dd6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6525,7 +6525,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.4-d57fd0b');
+var VERSION = new Version('5.2.4-a616dd6');
 
 /**
  * @fileoverview added by tsickle
