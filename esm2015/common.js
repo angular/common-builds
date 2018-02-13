7407067050a7e7d9149e9f5f9f3188f352c8845c
Miško Hevery <misko@hevery.com>
2018-02-13 19:03:19 +0000

    4f19491 ci: remove conditional clause for bazel install (#22170)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-2de0d4c
+ * @license Angular v6.0.0-beta.3-4f19491
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6095,7 +6095,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-2de0d4c');
+const VERSION = new Version('6.0.0-beta.3-4f19491');
 
 /**
  * @fileoverview added by tsickle
