610550ed4e7b54a95664462ced699f699efba326
Matias Niemelä <matias@yearofmoo.com>
2018-02-14 22:48:15 +0000

    0b8b06e build: comment-out chromium version checking code temporarily (#22232)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-106b435
+ * @license Angular v5.2.5-0b8b06e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5985,7 +5985,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.5-106b435');
+const VERSION = new Version('5.2.5-0b8b06e');
 
 /**
  * @fileoverview added by tsickle
