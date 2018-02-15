478ba36b5383737014615e52ae57353dce11fea5
Matias Niemelä <matias@yearofmoo.com>
2018-02-15 18:01:56 +0000

    0aa9b46 Revert "build: allow bazel build ... (#22168)"

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-0b68312
+ * @license Angular v6.0.0-beta.4-0aa9b46
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.4-0b68312
+ * @license Angular v6.0.0-beta.4-0aa9b46
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6682,7 +6682,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.4-0b68312');
+var VERSION = new _angular_core.Version('6.0.0-beta.4-0aa9b46');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
