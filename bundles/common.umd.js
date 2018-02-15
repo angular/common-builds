cea2582f8b8ec368020912cbffb286ec5a5599a9
Victor Berchet <victor@suumit.com>
2018-02-15 21:36:28 +0000

    03d93c9 Revert: "build: merge-pr new checks that all requested changes have been addressed (#21817)"

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-0aa9b46
+ * @license Angular v6.0.0-beta.4-03d93c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.4-0aa9b46
+ * @license Angular v6.0.0-beta.4-03d93c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6682,7 +6682,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.4-0aa9b46');
+var VERSION = new _angular_core.Version('6.0.0-beta.4-03d93c9');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
