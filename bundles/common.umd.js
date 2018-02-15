4f2b42a9ae66e16d26bc034d6e4879eda0da87d6
Matias Niemelä <matias@yearofmoo.com>
2018-02-15 00:26:17 +0000

    1e3e0fa fix(aio): improve printing styles (#19651)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-0c88d5d
+ * @license Angular v5.2.5-1e3e0fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.5-0c88d5d
+ * @license Angular v5.2.5-1e3e0fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6567,7 +6567,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.5-0c88d5d');
+var VERSION = new _angular_core.Version('5.2.5-1e3e0fa');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
