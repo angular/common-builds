568c515892ce3294002e346c0ced044d74578d0c
Matias Niemelä <matias@yearofmoo.com>
2018-02-15 04:19:41 +0000

    831592c Revert "style: fix typos boostrap to bootstrap (#21917)"

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-b54ad05
+ * @license Angular v6.0.0-beta.4-831592c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6095,7 +6095,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-b54ad05');
+const VERSION = new Version('6.0.0-beta.4-831592c');
 
 /**
  * @fileoverview added by tsickle
