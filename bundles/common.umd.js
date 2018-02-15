eb78ab133b24bd4b89ab46fab4c6865c0123a3bb
Matias Niemelä <matias@yearofmoo.com>
2018-02-15 23:13:15 +0000

    f628797 Revert "refactor(ivy): assertion (#22189)"

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-ba9cd5b
+ * @license Angular v6.0.0-beta.4-f628797
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.4-ba9cd5b
+ * @license Angular v6.0.0-beta.4-f628797
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6682,7 +6682,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.4-ba9cd5b');
+var VERSION = new _angular_core.Version('6.0.0-beta.4-f628797');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
