e94e8840588c48a98d94f677f6aa54ce1a66e81f
Victor Berchet <victor@suumit.com>
2018-02-17 02:25:22 +0000

    861250b docs: fix changelog errors (#22228)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-81c1e0a
+ * @license Angular v5.2.5-861250b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6525,7 +6525,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.5-81c1e0a');
+var VERSION = new Version('5.2.5-861250b');
 
 /**
  * @fileoverview added by tsickle
