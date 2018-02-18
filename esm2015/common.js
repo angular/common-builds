4bbb26695ccf3cc0530170f37df3afb5dffe6ad4
Victor Berchet <victor@suumit.com>
2018-02-18 21:54:47 +0000

    ac2b04a test(ivy): Add small_app spec for sprint #3 (#22018)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-4ec40c6
+ * @license Angular v6.0.0-beta.4-ac2b04a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6095,7 +6095,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-4ec40c6');
+const VERSION = new Version('6.0.0-beta.4-ac2b04a');
 
 /**
  * @fileoverview added by tsickle
