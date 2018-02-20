10046d4ac64f0a577bce8398413080735cc0dc3d
Victor Berchet <victor@suumit.com>
2018-02-20 20:03:04 +0000

    ee60bb5 fix(ivy): pureFunction8 should update the right bindings (#22313)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-f6120c0
+ * @license Angular v6.0.0-beta.4-ee60bb5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6108,7 +6108,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-f6120c0');
+const VERSION = new Version('6.0.0-beta.4-ee60bb5');
 
 /**
  * @fileoverview added by tsickle
