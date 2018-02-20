41f1fb77d246a4f01a8e49e8ff9f45333de0edba
Victor Berchet <victor@suumit.com>
2018-02-20 22:34:44 +0000

    99909bb feat(ivy): generate pipe references and definitions (#22034)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-ee60bb5
+ * @license Angular v6.0.0-beta.4-99909bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6108,7 +6108,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-ee60bb5');
+const VERSION = new Version('6.0.0-beta.4-99909bb');
 
 /**
  * @fileoverview added by tsickle
