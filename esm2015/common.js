1ec29ee46a42449479e91ead677ef734cdf21ac8
Victor Berchet <victor@suumit.com>
2018-02-21 00:31:55 +0000

    3b692a5 docs(aio): fix incorrect quote mark usage (#22335) PR Close #22335

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-69a0578
+ * @license Angular v6.0.0-beta.4-3b692a5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6108,7 +6108,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-69a0578');
+const VERSION = new Version('6.0.0-beta.4-3b692a5');
 
 /**
  * @fileoverview added by tsickle
