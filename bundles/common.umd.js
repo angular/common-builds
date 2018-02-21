a4d891727ad1dece2105a96e99437c3f1abb5010
Victor Berchet <victor@suumit.com>
2018-02-21 00:04:40 +0000

    519f022 docs(aio): update installed mobile tool list (#22331) PR Close #22331

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-236a932
+ * @license Angular v6.0.0-beta.4-519f022
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.4-236a932
+ * @license Angular v6.0.0-beta.4-519f022
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6695,7 +6695,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.4-236a932');
+var VERSION = new _angular_core.Version('6.0.0-beta.4-519f022');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
