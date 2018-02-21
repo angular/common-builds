ccf2583722a7f2859d54efb40443eadda0e38887
Victor Berchet <victor@suumit.com>
2018-02-21 00:18:31 +0000

    69a0578 docs(aio): fix the css of the heroes component's buttons (#22333)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-b5ca275
+ * @license Angular v6.0.0-beta.4-69a0578
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6108,7 +6108,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-b5ca275');
+const VERSION = new Version('6.0.0-beta.4-69a0578');
 
 /**
  * @fileoverview added by tsickle
