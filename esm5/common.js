cd9c6012dafa335712bfbb885b242562cc11cac4
Victor Berchet <victor@suumit.com>
2018-02-21 23:14:24 +0000

    612cfec docs(aio): updates directive event hooks real capabilities (#16654)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-dfdade2
+ * @license Angular v5.2.5-612cfec
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6525,7 +6525,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.5-dfdade2');
+var VERSION = new Version('5.2.5-612cfec');
 
 /**
  * @fileoverview added by tsickle
