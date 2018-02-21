67b7050ef8fdf0a8ceb9d2d3fe69de62437321f5
Victor Berchet <victor@suumit.com>
2018-02-21 00:02:19 +0000

    5bdb3ac build: update tsickle dep from compiler-cli (#22295) PR Close #22295

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-a346d28
+ * @license Angular v5.2.5-5bdb3ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.5-a346d28
+ * @license Angular v5.2.5-5bdb3ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6567,7 +6567,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.5-a346d28');
+var VERSION = new _angular_core.Version('5.2.5-5bdb3ac');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
