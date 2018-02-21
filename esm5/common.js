f4e0f68c701b23fc794a859d67615ca4913710b9
Victor Berchet <victor@suumit.com>
2018-02-21 00:41:28 +0000

    ee53577 docs: add ngStyle to cheat sheet (#22070)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-94756eb
+ * @license Angular v5.2.5-ee53577
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6525,7 +6525,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.5-94756eb');
+var VERSION = new Version('5.2.5-ee53577');
 
 /**
  * @fileoverview added by tsickle
