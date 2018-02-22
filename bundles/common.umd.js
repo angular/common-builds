e8dd7d0b72349770a99f94ef15b45029b5e4faa0
Victor Berchet <victor@suumit.com>
2018-02-22 22:20:42 +0000

    45eff4c fix(router): don't mutate route configs (#22358)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-b3ffeaa
+ * @license Angular v6.0.0-beta.5-45eff4c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.5-b3ffeaa
+ * @license Angular v6.0.0-beta.5-45eff4c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6695,7 +6695,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.5-b3ffeaa');
+var VERSION = new _angular_core.Version('6.0.0-beta.5-45eff4c');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
