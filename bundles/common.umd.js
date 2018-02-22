542f178ceb39e58640cb06e205559b8c6e69890a
Victor Berchet <victor@suumit.com>
2018-02-22 01:43:35 +0000

    6091a95 docs: add changelog for 6.0.0-beta.5

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-5c89d6b
+ * @license Angular v6.0.0-beta.5-6091a95
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.4-5c89d6b
+ * @license Angular v6.0.0-beta.5-6091a95
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6695,7 +6695,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.4-5c89d6b');
+var VERSION = new _angular_core.Version('6.0.0-beta.5-6091a95');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
