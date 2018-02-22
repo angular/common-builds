2583bee14722d5ce7dc8fdf27342db09b5357f7e
Victor Berchet <victor@suumit.com>
2018-02-22 19:41:04 +0000

    da1b4d5 docs: fix ngmodules-jsmodules pre-req (#22316)

--- bundles/common-http.umd.js
+++ bundles/common-http.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-aa100f6
+ * @license Angular v5.2.6-da1b4d5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,rxjs_observable_of,rxjs_operator_concatMap,rxjs_operator_filter,rxjs_operator_map,tslib,_angular_common,rxjs_Observable) { 'use strict';
 
 /**
- * @license Angular v5.2.6-aa100f6
+ * @license Angular v5.2.6-da1b4d5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
