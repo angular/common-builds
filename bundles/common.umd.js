deb45b5af38770afc9572b8932d1287178ee7526
Victor Berchet <victor@suumit.com>
2018-02-22 22:03:19 +0000

    1776239 build: disable bazel-out symlink (#22375)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-da1b4d5
+ * @license Angular v5.2.6-1776239
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.6-da1b4d5
+ * @license Angular v5.2.6-1776239
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6567,7 +6567,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.6-da1b4d5');
+var VERSION = new _angular_core.Version('5.2.6-1776239');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
