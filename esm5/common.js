0acc0e6cb7904fddb3e1ae3995602345d0ed9494
Victor Berchet <victor@suumit.com>
2018-02-23 02:08:02 +0000

    7effb00 fix(ivy): ngOnChanges to receive SimpleChanges with non minified property names as keys (#22352)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-f791862
+ * @license Angular v6.0.0-beta.5-7effb00
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6653,7 +6653,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-f791862');
+var VERSION = new Version('6.0.0-beta.5-7effb00');
 
 /**
  * @fileoverview added by tsickle
