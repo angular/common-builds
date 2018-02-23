b70c9e52c4a73bb77ba71852f5f928ec03e3b7d2
Victor Berchet <victor@suumit.com>
2018-02-23 21:43:13 +0000

    094666d fix(router): fix URL serialization so special characters are only encoded where needed (#22337)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-3a809cb
+ * @license Angular v6.0.0-beta.5-094666d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6108,7 +6108,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.5-3a809cb');
+const VERSION = new Version('6.0.0-beta.5-094666d');
 
 /**
  * @fileoverview added by tsickle
