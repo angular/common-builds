0ac73c3e5ce8a5056022eed77988ec691aab366c
Victor Berchet <victor@suumit.com>
2018-02-23 19:47:55 +0000

    1dcbc12 docs(aio): Essential JS 2 url updated (#19739) PR Close #19739

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-aad4316
+ * @license Angular v6.0.0-beta.5-1dcbc12
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6108,7 +6108,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.5-aad4316');
+const VERSION = new Version('6.0.0-beta.5-1dcbc12');
 
 /**
  * @fileoverview added by tsickle
