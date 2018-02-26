0e1367a140f7c3b50a1918a7a03633479e8f83d6
Alex Eagle <alexeagle@google.com>
2018-02-26 21:57:17 +0000

    5a32d7e build(aio): render default value for optional parameters (#22435)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-1f59981
+ * @license Angular v6.0.0-beta.5-5a32d7e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6728,7 +6728,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-1f59981');
+var VERSION = new Version('6.0.0-beta.5-5a32d7e');
 
 /**
  * @fileoverview added by tsickle
