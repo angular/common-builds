20dcf475ea56cead0ce29ff8ce8440e69227717c
Alex Eagle <alexeagle@google.com>
2018-02-27 23:42:50 +0000

    142117f fix(aio): do not show Properties/Methods sections if only internal members (#22471)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-79656e7
+ * @license Angular v6.0.0-beta.5-142117f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.5-79656e7
+ * @license Angular v6.0.0-beta.5-142117f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6770,7 +6770,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.5-79656e7');
+var VERSION = new _angular_core.Version('6.0.0-beta.5-142117f');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.formatDate = formatDate;
