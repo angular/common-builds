bbe401d3a6faa81f55197894577df94139b7cdb3
Alex Eagle <alexeagle@google.com>
2018-02-28 22:10:38 +0000

    9eaf1bb feat(ivy): support injecting ChangeDetectorRef (#22469)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-aabe16c
+ * @license Angular v6.0.0-beta.5-9eaf1bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6728,7 +6728,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-aabe16c');
+var VERSION = new Version('6.0.0-beta.5-9eaf1bb');
 
 /**
  * @fileoverview added by tsickle
