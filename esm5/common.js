cc34307159fd8fe1b0332d202b39925d08c99a67
Alex Eagle <alexeagle@google.com>
2018-03-01 16:33:50 +0000

    27962f8 build(aio): improve accuracy of code auto-linking (#22494)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-697d31a
+ * @license Angular v5.2.7-27962f8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6525,7 +6525,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.7-697d31a');
+var VERSION = new Version('5.2.7-27962f8');
 
 /**
  * @fileoverview added by tsickle
