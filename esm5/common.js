e64b907d02c8f2e20d8b9a57e12be35da3c8bfe8
Alex Eagle <alexeagle@google.com>
2018-03-05 21:49:01 +0000

    2c2b62f fix(ivy): preventDefault when listener returns false (#22529)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-58932c7
+ * @license Angular v6.0.0-beta.6-2c2b62f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6728,7 +6728,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.6-58932c7');
+var VERSION = new Version('6.0.0-beta.6-2c2b62f');
 
 /**
  * @fileoverview added by tsickle
