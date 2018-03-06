980bb478d244e960ca841218c972b169c4a3aca8
Alex Eagle <alexeagle@google.com>
2018-03-06 05:47:28 +0000

    f86d8ae docs(compiler): fix a line about ivy library (#22579) PR Close #22579

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-94bc277
+ * @license Angular v6.0.0-beta.6-f86d8ae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6183,7 +6183,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.6-94bc277');
+const VERSION = new Version('6.0.0-beta.6-f86d8ae');
 
 /**
  * @fileoverview added by tsickle
