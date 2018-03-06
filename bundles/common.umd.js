9d4299027d0e4df332e4b25a9a68cb52894bf7cb
Alex Eagle <alexeagle@google.com>
2018-03-06 05:44:50 +0000

    94bc277 fix(aio): tidy up embedded ToC styling (#22570)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-2a1e3d1
+ * @license Angular v6.0.0-beta.6-94bc277
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.6-2a1e3d1
+ * @license Angular v6.0.0-beta.6-94bc277
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6770,7 +6770,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.6-2a1e3d1');
+var VERSION = new _angular_core.Version('6.0.0-beta.6-94bc277');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.formatDate = formatDate;
--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-2a1e3d1
+ * @license Angular v6.0.0-beta.6-94bc277
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6183,7 +6183,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.6-2a1e3d1');
+const VERSION = new Version('6.0.0-beta.6-94bc277');
 
 /**
  * @fileoverview added by tsickle
