9fe28e038f7d4da0044085110493290d7f5b42a9
Alex Eagle <alexeagle@google.com>
2018-03-07 19:17:44 +0000

    4f744cc docs: update RELEASE_SCHEDULE.md by pushing out v6 rc by one week

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-363dfa5
+ * @license Angular v6.0.0-beta.6-4f744cc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.6-363dfa5
+ * @license Angular v6.0.0-beta.6-4f744cc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6770,7 +6770,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.6-363dfa5');
+var VERSION = new _angular_core.Version('6.0.0-beta.6-4f744cc');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.formatDate = formatDate;
