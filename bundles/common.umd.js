837e8a36fc7c482bd070748679f31859f8e3439c
Kara Erickson <karakara@google.com>
2018-03-09 07:02:28 +0000

    b0b9ca3 feat(ivy): produce Renderer2 back-patching and factories. (#22506)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-5412e10
+ * @license Angular v6.0.0-beta.7-b0b9ca3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.7-5412e10
+ * @license Angular v6.0.0-beta.7-b0b9ca3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6770,7 +6770,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-5412e10');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-b0b9ca3');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.formatDate = formatDate;
