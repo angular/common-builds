e41020cc9904d24f1ea84dc64fe6929ee0119d3a
Kara Erickson <karakara@google.com>
2018-03-09 01:06:07 +0000

    5412e10 docs(core): replace ancient live demos (#22665)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-489fec1
+ * @license Angular v6.0.0-beta.7-5412e10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6183,7 +6183,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.7-489fec1');
+const VERSION = new Version('6.0.0-beta.7-5412e10');
 
 /**
  * @fileoverview added by tsickle
