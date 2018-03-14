83394a90b5dbdc9c10398502047f1db275c42cd6
Kara Erickson <karakara@google.com>
2018-03-14 21:32:39 +0000

    fc6dfc2 fix(platform-server): add styles to elements correctly (#22527)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.8-c0670ef
+ * @license Angular v5.2.8-fc6dfc2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5985,7 +5985,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.8-c0670ef');
+const VERSION = new Version('5.2.8-fc6dfc2');
 
 /**
  * @fileoverview added by tsickle
