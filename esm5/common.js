46ff75ea96bb56dd6d747202a86095898bd908f7
Kara Erickson <karakara@google.com>
2018-03-14 17:43:19 +0000

    6f0dad1 build(aio): render doc-gen issues in overview dump (#22675)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-37fedd0
+ * @license Angular v6.0.0-beta.7-6f0dad1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6728,7 +6728,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-37fedd0');
+var VERSION = new Version('6.0.0-beta.7-6f0dad1');
 
 /**
  * @fileoverview added by tsickle
