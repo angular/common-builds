545a722fbcf507aced784ff51b9fc7dba4997f86
Miško Hevery <misko@hevery.com>
2018-03-15 19:11:11 +0000

    7d9de17 build: add release helper scripts (#22378) (#22781)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-db0afa9
+ * @license Angular v5.2.9-7d9de17
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.9-db0afa9
+ * @license Angular v5.2.9-7d9de17
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6567,7 +6567,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.9-db0afa9');
+var VERSION = new _angular_core.Version('5.2.9-7d9de17');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.NgLocaleLocalization = NgLocaleLocalization;
