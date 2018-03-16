21b19fa9d3a6e39d8b8f2cb87e88e19fdca60bad
Miško Hevery <misko@hevery.com>
2018-03-16 18:50:42 +0000

    cedc04c docs(ivy): document project status (#22751)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-269c3a1
+ * @license Angular v6.0.0-beta.7-cedc04c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6733,7 +6733,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-269c3a1');
+var VERSION = new Version('6.0.0-beta.7-cedc04c');
 
 /**
  * @fileoverview added by tsickle
