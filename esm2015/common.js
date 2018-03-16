a83b208a4e57b947149687baf6e47ac7110346ec
Miško Hevery <misko@hevery.com>
2018-03-16 20:04:01 +0000

    688096b feat(elements): remove attribute/input from config (#22413)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-bafdad9
+ * @license Angular v6.0.0-beta.7-688096b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6188,7 +6188,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.7-bafdad9');
+const VERSION = new Version('6.0.0-beta.7-688096b');
 
 /**
  * @fileoverview added by tsickle
