1d1ca872a81e5ff747f1c852cc63b42f58f0328f
Miško Hevery <misko@hevery.com>
2018-03-17 05:41:11 +0000

    f258ec6 docs(ivy): update status of ivy (#22834)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-5d82d8d
+ * @license Angular v6.0.0-beta.7-f258ec6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.7-5d82d8d
+ * @license Angular v6.0.0-beta.7-f258ec6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6775,7 +6775,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-5d82d8d');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-f258ec6');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.formatDate = formatDate;
