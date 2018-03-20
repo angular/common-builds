aec59abb324a99d8e434cdd2e001a48e16d1c6c5
Igor Minar <igor@angular.io>
2018-03-20 23:15:57 +0000

    17fb983 fix(ivy): fix type error in newer version of TS (#22897)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-3cc5c2e
+ * @license Angular v6.0.0-beta.7-17fb983
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.7-3cc5c2e
+ * @license Angular v6.0.0-beta.7-17fb983
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6775,7 +6775,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-3cc5c2e');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-17fb983');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.formatDate = formatDate;
