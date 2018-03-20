68d934870b90ad72c2c92f7cd8aceed1a8a7c900
Igor Minar <igor@angular.io>
2018-03-20 22:49:33 +0000

    3cc5c2e build: update to rxjs@6.0.0-beta.0 (#22887)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-e6c731f
+ * @license Angular v6.0.0-beta.7-3cc5c2e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.7-e6c731f
+ * @license Angular v6.0.0-beta.7-3cc5c2e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6775,7 +6775,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-e6c731f');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-3cc5c2e');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.formatDate = formatDate;
