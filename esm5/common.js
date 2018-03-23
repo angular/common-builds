e00dff5fb6693a8c5960aa629a3969db5a90d12f
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 17:28:20 +0000

    8d1e640 docs(aio): fix TS warning error  - filter expects a boolean function param (#22954)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-49f6d1d
+ * @license Angular v5.2.9-8d1e640
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6530,7 +6530,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.9-49f6d1d');
+var VERSION = new Version('5.2.9-8d1e640');
 
 /**
  * @fileoverview added by tsickle
