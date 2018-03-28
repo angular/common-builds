af6742af7441d9e9542343fea13b3a51cad7c547
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-28 17:22:57 +0000

    0d4fe38 fix(service-worker): ignore invalid `only-if-cached` requests (#22883)

--- esm2015/common.js
+++ esm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-776bb82
+ * @license Angular v5.2.9-0d4fe38
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5990,7 +5990,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.9-776bb82');
+const VERSION = new Version('5.2.9-0d4fe38');
 
 /**
  * @fileoverview added by tsickle
