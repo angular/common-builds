403b486415532edd2fa1c9800385c77cd59ae254
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 15:45:02 +0000

    dd61595 fix(forms): improve error message for invalid value accessors (#22731)

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-ae76eec
+ * @license Angular v5.2.9-dd61595
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6530,7 +6530,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.9-ae76eec');
+var VERSION = new Version('5.2.9-dd61595');
 
 /**
  * @fileoverview added by tsickle
