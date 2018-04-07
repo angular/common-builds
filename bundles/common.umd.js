0d569a29e16d7ac28366f6f3deb56c26a2dbcd5c
Igor Minar <igor@angular.io>
2018-04-07 05:16:16 +0000

    1fac5f4 docs: recommend best practice for providers (#22934)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-33f8120
+ * @license Angular v6.0.0-rc.3-1fac5f4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-rc.3-33f8120
+ * @license Angular v6.0.0-rc.3-1fac5f4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6889,7 +6889,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.3-33f8120');
+var VERSION = new _angular_core.Version('6.0.0-rc.3-1fac5f4');
 
 exports.ɵregisterLocaleData = registerLocaleData;
 exports.formatDate = formatDate;
