c25d2d2e4dc122ed84fd5cb58c731d3381142afb
Igor Minar <igor@angular.io>
2018-04-12 06:43:25 +0000

    af46d09 fix(elements): avoid exception when window is undefined (#23324)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-7e8cee6
+ * @license Angular v6.0.0-rc.3-af46d09
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6835,7 +6835,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.3-7e8cee6');
+var VERSION = new Version('6.0.0-rc.3-af46d09');
 
 /**
  * @fileoverview added by tsickle
