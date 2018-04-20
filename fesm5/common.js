399503a82ade836fb842dbad99d297d967d4d4b7
Victor Berchet <victor@suumit.com>
2018-04-20 02:22:43 +0000

    b9431e8 fix(compiler): handle undefined annotation metadata (#23349)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-7790cfa
+ * @license Angular v6.0.0-rc.5-b9431e8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6835,7 +6835,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-7790cfa');
+var VERSION = new Version('6.0.0-rc.5-b9431e8');
 
 /**
  * @fileoverview added by tsickle
