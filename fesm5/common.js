7176a697bb2db733942b92dc5962a3710f236df7
Victor Berchet <victor@suumit.com>
2018-04-22 19:56:27 +0000

    02424ff Revert "style(compiler): fix lint issues (#23480)"

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-b9431e8
+ * @license Angular v6.0.0-rc.5-02424ff
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6835,7 +6835,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-b9431e8');
+var VERSION = new Version('6.0.0-rc.5-02424ff');
 
 /**
  * @fileoverview added by tsickle
