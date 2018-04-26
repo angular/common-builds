8dfcf5fd9349959be7dbbc478bed106b8bbc280f
Igor Minar <igor@angular.io>
2018-04-26 02:22:09 +0000

    b1f040f fix(compiler-cli): don't rely on incompatible TS method (#23550)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+87.sha-eb031c6
+ * @license Angular v6.0.0-rc.5+88.sha-b1f040f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5906,7 +5906,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  *
  */
-var VERSION = new core.Version('6.0.0-rc.5+87.sha-eb031c6');
+var VERSION = new core.Version('6.0.0-rc.5+88.sha-b1f040f');
 
 /**
  * @license
