857ad5938408e5d10c09fa00ce20a897072791f3
Igor Minar <igor@angular.io>
2018-05-04 22:32:07 +0000

    65dba9d test: fix firebase deployment script test

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.10-402f452
+ * @license Angular v5.2.10-65dba9d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6537,7 +6537,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.10-402f452');
+var VERSION = new Version('5.2.10-65dba9d');
 
 /**
  * @fileoverview added by tsickle
