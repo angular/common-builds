3cdfcd621cc73d59a6e56c7f295aa169bad71cb2
Igor Minar <igor@angular.io>
2018-05-04 22:46:14 +0000

    e3e1577 build: update bazel to 0.13 (#23623)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+134.sha-b25e15c
+ * @license Angular v6.0.0-rc.5+135.sha-e3e1577
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6150,7 +6150,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5+134.sha-b25e15c');
+const VERSION = new Version('6.0.0-rc.5+135.sha-e3e1577');
 
 /**
  * @fileoverview added by tsickle
