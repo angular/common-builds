247bbaf0faf44bb2d9a5ad0b041ca6e98f9df0db
Igor Minar <igor@angular.io>
2018-05-08 21:18:55 +0000

    b0eca85 refactor(compiler): compile{Component,Directive} take only local information (#23545)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+141.sha-fc03427
+ * @license Angular v6.0.0-rc.5+144.sha-b0eca85
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5827,7 +5827,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5+141.sha-fc03427');
+var VERSION = new Version('6.0.0-rc.5+144.sha-b0eca85');
 
 /**
  * @license
