7f67afe8ca1f00d5d94a1742a81c60cc963b8621
Igor Minar <igor@angular.io>
2018-05-08 21:19:16 +0000

    fe7f48c docs(aio): Upgrade example dependencies to Angular V6 (#23660)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+28.sha-5581e97
+ * @license Angular v6.0.0+30.sha-fe7f48c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6150,7 +6150,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  *
  */
-const VERSION = new Version('6.0.0+28.sha-5581e97');
+const VERSION = new Version('6.0.0+30.sha-fe7f48c');
 
 /**
  * @fileoverview added by tsickle
