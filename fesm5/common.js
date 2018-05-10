de948f6ea315ba2116dadd511bf326ddd8a443f4
Igor Minar <igor@angular.io>
2018-05-10 15:22:23 +0000

    3d8799b fix(router): avoid freezing queryParams in-place (#22663)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+41.sha-8733843
+ * @license Angular v6.0.0+42.sha-3d8799b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5827,7 +5827,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  *
  */
-var VERSION = new Version('6.0.0+41.sha-8733843');
+var VERSION = new Version('6.0.0+42.sha-3d8799b');
 
 /**
  * @license
