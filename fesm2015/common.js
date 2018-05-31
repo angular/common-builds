11d194dfdd30466d4c9f98aa8d33036a18048ffb
Victor Berchet <victor@suumit.com>
2018-05-31 00:26:32 +0000

    5d6074e docs: fix typo (#24210)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+275.sha-b492b9e
+ * @license Angular v6.0.0-rc.5+282.sha-5d6074e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6127,7 +6127,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.0-rc.5+275.sha-b492b9e');
+const VERSION = new Version('6.0.0-rc.5+282.sha-5d6074e');
 
 /**
  * @fileoverview added by tsickle
