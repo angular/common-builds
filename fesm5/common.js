65b9c2c5123c8a7bd96c4a79b1c7789b7df6391c
Victor Berchet <victor@suumit.com>
2018-06-03 02:58:17 +0000

    44856bf refactor(ivy): move bindingStartIndex to TView (#24262)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+302.sha-cb65724
+ * @license Angular v6.0.0-rc.5+301.sha-44856bf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5353,7 +5353,7 @@ function isPlatformWorkerUi(platformId) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+302.sha-cb65724');
+var VERSION = new Version('6.0.0-rc.5+301.sha-44856bf');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+302.sha-cb65724
+ * @license Angular v6.0.0-rc.5+301.sha-44856bf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
