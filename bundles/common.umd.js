06fcc58f46712ff6653b3772f73a42a0514755ca
Victor Berchet <victor@suumit.com>
2018-06-04 17:55:22 +0000

    ccaa199 docs(aio): remove an extraneous apostrophe (#24293) PR Close #24293

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+304.sha-0690622
+ * @license Angular v6.0.0-rc.5+305.sha-ccaa199
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5432,7 +5432,7 @@ function isPlatformWorkerUi(platformId) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.0.0-rc.5+304.sha-0690622');
+var VERSION = new core.Version('6.0.0-rc.5+305.sha-ccaa199');
 
 /**
  * @license
