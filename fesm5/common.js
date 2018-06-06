3cbb491c7d008ba10f32ef0158bc092a0df0b96c
Victor Berchet <victor@suumit.com>
2018-06-06 19:26:58 +0000

    41698bf release: cut the v6.0.4 release

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+67.sha-23c50e2
+ * @license Angular v6.0.4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5352,7 +5352,7 @@ function isPlatformWorkerUi(platformId) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.3+67.sha-23c50e2');
+var VERSION = new Version('6.0.4');
 
 /**
  * @license
