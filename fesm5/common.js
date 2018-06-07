89ac441c479c4d23326b89daa8fc8bdfa2f1de34
Miško Hevery <misko@hevery.com>
2018-06-07 23:19:09 +0000

    d4c66d5 docs(ivy): update status of impl progress (#24323)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+16.sha-a6e797b
+ * @license Angular v6.1.0-beta.0+18.sha-d4c66d5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5361,7 +5361,7 @@ function isPlatformWorkerUi(platformId) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.0+16.sha-a6e797b');
+var VERSION = new Version('6.1.0-beta.0+18.sha-d4c66d5');
 
 /**
  * @license
