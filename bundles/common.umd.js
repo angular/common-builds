4506f82602a8bedd31d98b41bbcf17ecbf618e92
Miško Hevery <misko@hevery.com>
2018-06-07 23:10:19 +0000

    7031972 docs(aio): Added resource link to Amexio Canvas Web Based IDE (#24336)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+1.sha-cfa5b65
+ * @license Angular v6.0.4+2.sha-7031972
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5431,7 +5431,7 @@ function isPlatformWorkerUi(platformId) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.0.4+1.sha-cfa5b65');
+var VERSION = new core.Version('6.0.4+2.sha-7031972');
 
 /**
  * @license
