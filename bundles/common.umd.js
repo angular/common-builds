8843312eb5df6c2382ba6ee2aa0c4244cf968ab2
Miško Hevery <misko@hevery.com>
2018-06-08 21:27:14 +0000

    0d81151 docs(aio): add mix and connect to front page campaigns (#24357)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+2.sha-7031972
+ * @license Angular v6.0.4+3.sha-0d81151
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5431,7 +5431,7 @@ function isPlatformWorkerUi(platformId) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.0.4+2.sha-7031972');
+var VERSION = new core.Version('6.0.4+3.sha-0d81151');
 
 /**
  * @license
