cca13d6a894b3e2f4319886df8449fbf7eb981e6
Miško Hevery <misko@hevery.com>
2018-06-14 00:09:31 +0000

    3ccb449 release: cut the v6.1.0-beta.1 release

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+56.sha-503a524
+ * @license Angular v6.1.0-beta.1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5459,7 +5459,7 @@ function isPlatformWorkerUi(platformId) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.0+56.sha-503a524');
+var VERSION = new Version('6.1.0-beta.1');
 
 /**
  * @license
