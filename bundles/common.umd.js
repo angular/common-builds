ff77c83a801224632fbae5c8024dce84dec0774a
Miško Hevery <misko@hevery.com>
2018-06-14 00:14:36 +0000

    03f93b3 Revert "build: update to Bazel 0.14.0 (#24296)" (#24492)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1
+ * @license Angular v6.1.0-beta.1+1.sha-03f93b3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5547,7 +5547,7 @@ function isPlatformWorkerUi(platformId) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.1.0-beta.1');
+var VERSION = new core.Version('6.1.0-beta.1+1.sha-03f93b3');
 
 /**
  * @license
