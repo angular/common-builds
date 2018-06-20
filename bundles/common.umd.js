6c68af7952b29dfac70e5a33a9478377c2308228
Miško Hevery <misko@hevery.com>
2018-06-20 18:34:53 +0000

    e8354ed test(animations): properly reference body node for SSR environments (#23300)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+41.sha-5b76f04
+ * @license Angular v6.1.0-beta.1+42.sha-e8354ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5547,7 +5547,7 @@ function isPlatformWorkerUi(platformId) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.1.0-beta.1+41.sha-5b76f04');
+var VERSION = new core.Version('6.1.0-beta.1+42.sha-e8354ed');
 
 /**
  * @license
