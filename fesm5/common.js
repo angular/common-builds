098330b3349915b1c00829a8ac3dafc6a4323f27
Miško Hevery <misko@hevery.com>
2018-07-03 23:05:51 +0000

    7f3242a docs: fix documention for attributes directive (#24367)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+35.sha-b6af870
+ * @license Angular v6.1.0-beta.3+31.sha-7f3242a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5564,7 +5564,7 @@ function isPlatformWorkerUi(platformId) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.3+35.sha-b6af870');
+var VERSION = new Version('6.1.0-beta.3+31.sha-7f3242a');
 
 /**
  * @license
