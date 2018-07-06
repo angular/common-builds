c7e5b247bf6b808ed9fa301dd25f88dbfaac99c0
Miško Hevery <misko@hevery.com>
2018-07-06 18:14:41 +0000

    8fe8b8f docs: fix typos in 'Httpclient' docs (#19127)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+31.sha-7f3242a
+ * @license Angular v6.1.0-beta.3+36.sha-8fe8b8f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5377,7 +5377,7 @@ function isPlatformWorkerUi(platformId) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.3+31.sha-7f3242a');
+const VERSION = new Version('6.1.0-beta.3+36.sha-8fe8b8f');
 
 /**
  * @license
