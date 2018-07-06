c655473b8e101395096ef96e1727c31514bdcc93
Miško Hevery <misko@hevery.com>
2018-07-06 21:36:50 +0000

    250527c feat(service-worker): add support for `?` in SW config globbing (#24105)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.0-beta.3+55.sha-d571a51
+ * @license Angular v6.1.0-beta.3+58.sha-250527c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, Injector, NgModuleFactory, NgModuleRef, Type, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __values, __read, __extends, __decorate, __param, __metadata, __assign } from 'tslib';
+import { __decorate, __values, __read, __extends, __param, __metadata, __assign } from 'tslib';
 
 /**
  * @license
@@ -5562,7 +5562,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-beta.3+55.sha-d571a51');
+var VERSION = new Version('6.1.0-beta.3+58.sha-250527c');
 
 /**
  * @license
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+55.sha-d571a51
+ * @license Angular v6.1.0-beta.3+58.sha-250527c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __read, __extends, __spread } from 'tslib';
+import { __spread, __decorate, __read, __metadata, __param, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
