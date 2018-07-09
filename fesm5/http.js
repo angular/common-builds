e5d1a853b9905036aa20e2c0da91e47a9c519b22
Miško Hevery <misko@hevery.com>
2018-07-09 22:48:22 +0000

    6881404 fix(language-service): do not overwrite native `Reflect` (#24299)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+61.sha-3980640
+ * @license Angular v6.1.0-beta.3+62.sha-6881404
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __spread, __read } from 'tslib';
+import { __spread, __extends, __read } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
