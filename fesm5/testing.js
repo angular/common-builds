ec27174eb6817888df83d1ec732e6b9fc129996e
Miško Hevery <misko@hevery.com>
2018-07-09 22:56:39 +0000

    3d20c50 fix(ivy): correctly resolve Array  property access (#24664)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+63.sha-dcabb05
+ * @license Angular v6.1.0-beta.3+64.sha-3d20c50
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
