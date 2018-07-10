2bceda1c9d531f3a94ddd489cc77dc101c2e7905
Miško Hevery <misko@hevery.com>
2018-07-10 19:53:39 +0000

    03616bc docs: fix typo in Universal guide (#24812)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+75.sha-3a19f70
+ * @license Angular v6.1.0-beta.3+76.sha-03616bc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __spread, __param, __read, __extends } from 'tslib';
+import { __decorate, __metadata, __spread, __extends, __read, __param } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
