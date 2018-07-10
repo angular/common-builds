83cd28f4812affdcfad1101d985e93939947b8b8
Miško Hevery <misko@hevery.com>
2018-07-10 19:26:21 +0000

    197387d fix(platform-browser): mark Meta and Title services as tree shakable providers (#24815)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.0-beta.3+67.sha-1089261
+ * @license Angular v6.1.0-beta.3+68.sha-197387d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, NgModuleRef, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __extends, __read, __values, __assign } from 'tslib';
+import { __read, __extends, __values, __assign } from 'tslib';
 
 /**
  * @license
@@ -5556,7 +5556,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-beta.3+67.sha-1089261');
+var VERSION = new Version('6.1.0-beta.3+68.sha-197387d');
 
 /**
  * @license
