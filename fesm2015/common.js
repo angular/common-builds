f2650e404eda2afd137b3a0823fd012615a4e26f
Matias Niemelä <matias@yearofmoo.com>
2018-07-12 21:52:01 +0000

    e295206 docs: update component styles doc regarding relative URL (#24471)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.8+6.sha-6bf8aef
+ * @license Angular v6.0.8+8.sha-e295206
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5980,7 +5980,7 @@ function isPlatformWorkerUi(platformId) {
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
  */
-const /** @type {?} */ VERSION = new Version('6.0.8+6.sha-6bf8aef');
+const /** @type {?} */ VERSION = new Version('6.0.8+8.sha-e295206');
 
 /**
  * @fileoverview added by tsickle
