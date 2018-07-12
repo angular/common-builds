d129312d1b07ac8b3efe837133920dca83a593b9
Matias Niemelä <matias@yearofmoo.com>
2018-07-12 21:58:37 +0000

    a663565 build: fix windows scripts (#23121)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+109.sha-85d9c20
+ * @license Angular v6.1.0-beta.3+110.sha-a663565
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { InjectionToken, Version, defineInjectable, inject, ComponentFactoryResolver, NgModuleRef, ɵstringify, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, LOCALE_ID, KeyValueDiffers, IterableDiffers, Renderer2, ɵisListLikeIterable, EventEmitter, isDevMode, ɵdefineNgModule, defineInjector, ɵdefineDirective, ɵinjectViewContainerRef, ɵinjectTemplateRef, ɵdirectiveInject, ɵinjectElementRef, ɵdefinePipe, ɵinjectAttribute } from '@angular/core';
+import { InjectionToken, Version, defineInjectable, inject, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, NgModuleRef, isDevMode, LOCALE_ID, EventEmitter, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, ɵdefineNgModule, defineInjector, ɵdefineDirective, ɵdirectiveInject, ɵinjectElementRef, ɵinjectViewContainerRef, ɵinjectTemplateRef, ɵinjectAttribute, ɵdefinePipe } from '@angular/core';
 import { __values, __read, __extends, __assign } from 'tslib';
 
 /**
@@ -5124,7 +5124,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-beta.3+109.sha-85d9c20');
+var VERSION = new Version('6.1.0-beta.3+110.sha-a663565');
 
 /**
  * @license
