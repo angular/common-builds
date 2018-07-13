bc9fb4d577cd28658733eee96eb7f98e52c2eb96
Matias Niemelä <matias@yearofmoo.com>
2018-07-13 19:41:02 +0000

    97277bc build: update to Bazel 0.15 (#24841)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+112.sha-1821b75
+ * @license Angular v6.1.0-beta.3+113.sha-97277bc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
