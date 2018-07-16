8a20f71abf5d9b67f3018dab7cecb5b7786d4681
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-16 10:06:45 +0000

    31b4043 fixup! refactor(ivy): allow `ImportManager` to have configurable prefix

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+129.sha-acdb672
+ * @license Angular v6.1.0-beta.3+130.sha-31b4043
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6297,7 +6297,7 @@ function isPlatformWorkerUi(platformId) {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+129.sha-acdb672');
+const VERSION = new Version('6.1.0-beta.3+130.sha-31b4043');
 
 /**
  * @fileoverview added by tsickle
