ed627785f516cf2b845d329fd7b867cef667f294
Matias Niemelä <matias@yearofmoo.com>
2018-07-17 21:03:45 +0000

    ba3eb8b feat(ivy): properly apply class="", [class], [class.foo] and [attr.class] bindings (#24822)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+117.sha-c8ad965
+ * @license Angular v6.1.0-beta.3+118.sha-ba3eb8b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6297,7 +6297,7 @@ function isPlatformWorkerUi(platformId) {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+117.sha-c8ad965');
+const VERSION = new Version('6.1.0-beta.3+118.sha-ba3eb8b');
 
 /**
  * @fileoverview added by tsickle
