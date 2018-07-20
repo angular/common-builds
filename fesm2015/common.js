11e1ba6460b7cdfa07f763cec643c5e273e6962a
Victor Berchet <victor@suumit.com>
2018-07-20 18:38:28 +0000

    2aab1c9 ci: remove Tina from pullaprove (#25006)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3
+ * @license Angular v6.1.0-rc.3+1.sha-2aab1c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6297,7 +6297,7 @@ function isPlatformWorkerUi(platformId) {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-rc.3');
+const VERSION = new Version('6.1.0-rc.3+1.sha-2aab1c9');
 
 /**
  * @fileoverview added by tsickle
