eddd196655d0496bae6fbcf66b2fdf3444f32208
Victor Berchet <victor@suumit.com>
2018-07-20 19:25:43 +0000

    c438b5e build(bazel): turn on preserve-symlinks (#24881)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-rc.3+8.sha-1c533c9
+ * @license Angular v6.1.0-rc.3+11.sha-c438b5e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __decorate, __param, __metadata, __extends, __read } from 'tslib';
+import { __spread, __decorate, __metadata, __param, __read, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
