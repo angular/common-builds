d5049781fc12f8ccebd338d9287239c2bc03b22d
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-23 07:19:01 +0000

    0650f32 fixup! build: add dependencies to be used by ngcc

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-rc.3+70.sha-8a7b0e9
+ * @license Angular v6.1.0-rc.3+71.sha-0650f32
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __decorate, __metadata, __param, __read, __extends } from 'tslib';
+import { __decorate, __metadata, __spread, __read, __param, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
