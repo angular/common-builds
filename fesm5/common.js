43abeac91d9cddcecd0179a8df28b5a6730d663f
Victor Berchet <victor@suumit.com>
2018-07-23 17:36:57 +0000

    edef58f build(docs-infra): ensure all API headings are sentence cased (#24949)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.0-rc.3+50.sha-7c89af3
+ * @license Angular v6.1.0-rc.3+51.sha-edef58f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, Injector, NgModuleFactory, NgModuleRef, Type, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __read, __values, __decorate, __extends, __param, __metadata, __assign } from 'tslib';
+import { __read, __extends, __decorate, __param, __metadata, __values, __assign } from 'tslib';
 
 /**
  * @license
@@ -5571,7 +5571,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-rc.3+50.sha-7c89af3');
+var VERSION = new Version('6.1.0-rc.3+51.sha-edef58f');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-rc.3+50.sha-7c89af3
+ * @license Angular v6.1.0-rc.3+51.sha-edef58f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
