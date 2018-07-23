041b2402cd829171d990541538347ef5882fa0b4
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-23 07:03:21 +0000

    8a7b0e9 fixup! feat(ivy): implement esm2015 and esm5 reflection hosts

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+142.sha-082c994
+ * @license Angular v6.1.0-rc.3+70.sha-8a7b0e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5286,6 +5286,9 @@
          * Default is `0`.
          *   - `maxFractionDigits`: The maximum number of digits after the decimal point.
          * Default is `3`.
+         * If not provided, the number will be formatted with the proper amount of digits,
+         * depending on what the [ISO 4217](https://en.wikipedia.org/wiki/ISO_4217) specifies.
+         * For example, the Canadian dollar has 2 digits, whereas the Chilean peso has none.
          * @param locale A locale code for the locale format rules to use.
          * When not supplied, uses the value of `LOCALE_ID`, which is `en-US` by default.
          * See [Setting your app locale](guide/i18n#setting-up-the-locale-of-your-app).
@@ -5552,7 +5555,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.0-beta.3+142.sha-082c994');
+    var VERSION = new core.Version('6.1.0-rc.3+70.sha-8a7b0e9');
 
     /**
      * @license
