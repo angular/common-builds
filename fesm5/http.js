263a9cc71926c83834674ffb55ab66bd52bc53c7
Victor Berchet <victor@suumit.com>
2018-07-23 17:18:43 +0000

    7729bb2 docs: fix instructions for switching directories (#24439)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-rc.3+46.sha-4263245
+ * @license Angular v6.1.0-rc.3+47.sha-7729bb2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __spread, __read } from 'tslib';
+import { __spread, __extends, __read } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
