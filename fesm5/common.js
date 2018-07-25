ee1695e1edc134c9b356a100d1e19100a261309d
Victor Berchet <victor@suumit.com>
2018-07-25 00:21:30 +0000

    1ceddb6 fix(ivy): support re-order embedded templates (#24805)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.0-rc.3+53.sha-169e9dd
+ * @license Angular v6.1.0-rc.3+64.sha-1ceddb6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, NgModuleRef, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __extends, __read, __values, __assign } from 'tslib';
+import { __read, __extends, __values, __assign } from 'tslib';
 
 /**
  * @license
@@ -5565,7 +5565,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-rc.3+53.sha-169e9dd');
+var VERSION = new Version('6.1.0-rc.3+64.sha-1ceddb6');
 
 /**
  * @license
