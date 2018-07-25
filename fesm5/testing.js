569d565f82ceddf60255d73603dc5e5f62f6768b
Victor Berchet <victor@suumit.com>
2018-07-25 00:22:47 +0000

    1ceddb6 fix(ivy): support re-order embedded templates (#24805)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-rc.3+53.sha-169e9dd
+ * @license Angular v6.1.0-rc.3+64.sha-1ceddb6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
