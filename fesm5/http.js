b97b512d3e1bc6dbbe2071968ec1a53a605705a7
Victor Berchet <victor@suumit.com>
2018-07-25 18:19:30 +0000

    0560751 fix(core): stop reusing provider definitions across NgModuleRef instances (#25088)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.0.9+52.sha-e32c1b6
+ * @license Angular v6.0.9+53.sha-0560751
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __spread, __read } from 'tslib';
+import { __spread, __extends, __read } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
