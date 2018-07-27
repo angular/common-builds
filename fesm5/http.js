bad50fc7b5e12c29df0477a9817273cf435c2167
Igor Minar <igor@angular.io>
2018-07-27 17:59:21 +0000

    36a7705 feat(docs-infra): allow notification bar to show arbitrary content (#25020)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+7.sha-e86f3d9
+ * @license Angular v6.1.0+9.sha-36a7705
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __decorate, __param, __metadata, __read, __extends } from 'tslib';
+import { __decorate, __metadata, __spread, __read, __param, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
