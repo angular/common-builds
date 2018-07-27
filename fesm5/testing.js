af1e9714349caa9c27c40b47b3b035750444539a
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-27 07:01:39 +0000

    dad1d3d refactor(ivy): do not deep import from ngtsc into ngcc

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+15.sha-70174d1
+ * @license Angular v6.1.0+28.sha-dad1d3d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
