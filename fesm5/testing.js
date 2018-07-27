3d1fc287fa4423ec458fa183000da801dbe32db0
Igor Minar <igor@angular.io>
2018-07-27 19:29:40 +0000

    3426784 test: fix typings for `DoneFn` (#25163)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+17.sha-e8d4211
+ * @license Angular v6.1.0+18.sha-3426784
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
