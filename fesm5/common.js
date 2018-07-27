f83b1433afffb633a0141d62a0ee98888c6ca3d2
Igor Minar <igor@angular.io>
2018-07-27 17:41:31 +0000

    738f296 docs: Change unnecessary step in ToH-Tutorial (#25059)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.0+1.sha-514d03f
+ * @license Angular v6.1.0+6.sha-738f296
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, Version, defineInjectable, inject, EventEmitter, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, NgModuleRef, isDevMode, WrappedValue, ɵisObservable, ɵisPromise, LOCALE_ID, ɵdefineNgModule, defineInjector, ɵdefineDirective, ɵdirectiveInject, ɵinjectElementRef, ɵinjectViewContainerRef, ɵNgOnChangesFeature, ɵinjectTemplateRef, ɵinjectAttribute, ɵdefinePipe, ɵinjectChangeDetectorRef } from '@angular/core';
-import { __extends, __values, __read, __assign } from 'tslib';
+import { __read, __extends, __values, __assign } from 'tslib';
 
 /**
  * @license
@@ -5168,7 +5168,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0+1.sha-514d03f');
+var VERSION = new Version('6.1.0+6.sha-738f296');
 
 /**
  * @license
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+1.sha-514d03f
+ * @license Angular v6.1.0+6.sha-738f296
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __read, __extends } from 'tslib';
+import { __spread, __extends, __read } from 'tslib';
 import { InjectionToken, defineInjectable, inject, INJECTOR, ɵdefineNgModule, defineInjector, PLATFORM_ID } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
