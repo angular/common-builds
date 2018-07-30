9f2f9ee9ab7d83bf49ff37c2732715cf1062b3cc
Igor Minar <igor@angular.io>
2018-07-30 23:52:57 +0000

    22ebd53 docs: update bootstrapping and entry component guide to use httpclient (#25178)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+13.sha-a972c03
+ * @license Angular v6.1.0+14.sha-22ebd53
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __decorate, __metadata, __param, __read, __extends } from 'tslib';
+import { __spread, __decorate, __param, __metadata, __extends, __read } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
