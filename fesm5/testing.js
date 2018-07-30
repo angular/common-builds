e50fe30f545e2154ec0468216e0ab8497126aa8f
Igor Minar <igor@angular.io>
2018-07-30 23:44:53 +0000

    a972c03 docs: fix typo in dependency injection guide (#24972)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+12.sha-f5e1802
+ * @license Angular v6.1.0+13.sha-a972c03
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
