0eed80b5d5f911f72618a8e3e90cbddf9646e736
Igor Minar <igor@angular.io>
2018-07-30 22:26:51 +0000

    f5e1802 docs: refactor pipe example to use the HttpClient (#22741)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+11.sha-317c708
+ * @license Angular v6.1.0+12.sha-f5e1802
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
