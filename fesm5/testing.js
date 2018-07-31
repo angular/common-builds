8a4c5eaa504e36207a9ee8a48968918153b07fe4
Igor Minar <igor@angular.io>
2018-07-31 00:37:30 +0000

    24789e9 docs(aio): add StrongBrew to the trainer list (#24891)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+50.sha-f94f964
+ * @license Angular v6.1.0+51.sha-24789e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
