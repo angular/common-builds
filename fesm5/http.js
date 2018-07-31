113c9b21d7192ab82fe5e5ae67f2ff7ecfdafe83
Igor Minar <igor@angular.io>
2018-07-31 00:38:08 +0000

    785fb5c docs(aio): add StrongBrew to the trainer list (#24891)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+14.sha-22ebd53
+ * @license Angular v6.1.0+16.sha-785fb5c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __decorate, __param, __metadata, __extends, __read } from 'tslib';
+import { __decorate, __spread, __metadata, __param, __read, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
