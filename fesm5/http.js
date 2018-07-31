b484ace9c576639038c00168669e8073babb6256
Igor Minar <igor@angular.io>
2018-07-31 00:07:48 +0000

    efc6684 docs: fix typo in dependency injection guide (#24972)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+49.sha-4d5167e
+ * @license Angular v6.1.0+48.sha-efc6684
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __spread, __read, __extends } from 'tslib';
+import { __decorate, __metadata, __spread, __param, __read, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
