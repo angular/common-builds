d0cdb93278ebccff9b478fdf4ed3199fb0bc2328
Igor Minar <igor@angular.io>
2018-08-01 20:09:07 +0000

    2651604 refactor(animations): do not use short parameter names (#25198)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+24.sha-3ab31a4
+ * @license Angular v6.1.0+27.sha-2651604
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __extends, __read } from 'tslib';
+import { __extends, __spread, __read } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
