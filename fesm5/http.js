cf074d2629f618f4436b9d1acadc702e3e26cc84
Igor Minar <igor@angular.io>
2018-08-01 21:12:06 +0000

    cbc2ea1 build: update hello_world__closure to google-closure-compiler 20180716.0.0 (#25236)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+37.sha-fe5e8b7
+ * @license Angular v6.1.0+39.sha-cbc2ea1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __decorate, __metadata, __read, __extends, __param } from 'tslib';
+import { __decorate, __metadata, __spread, __param, __read, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
