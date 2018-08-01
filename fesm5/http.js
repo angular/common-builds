5b76dfe225471c16ae42d06d7ca13a1c306bd663
Igor Minar <igor@angular.io>
2018-08-01 20:08:31 +0000

    43dcf77 build(bazel): fix typo in protractor test target definition (#25235)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+26.sha-a83b9f7
+ * @license Angular v6.1.0+23.sha-43dcf77
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __decorate, __metadata, __read, __extends, __param } from 'tslib';
+import { __decorate, __metadata, __spread, __param, __read, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
