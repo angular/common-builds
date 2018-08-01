0ab0c6124db15a29eb0dffe7c9bc9a0d5f4b54b4
Igor Minar <igor@angular.io>
2018-08-01 20:27:57 +0000

    af07ffc docs(core): remove experimental tag (#24032)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+77.sha-7a4fb44
+ * @license Angular v6.1.0+79.sha-af07ffc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __spread, __read, __param, __extends } from 'tslib';
+import { __spread, __decorate, __metadata, __param, __extends, __read } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
