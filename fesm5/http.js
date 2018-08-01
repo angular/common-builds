3fa9ba56902a11887fc861b2583d50cdb6322612
Igor Minar <igor@angular.io>
2018-08-01 20:46:38 +0000

    5342aea docs(aio): update Kendo UI description in resource.json (#24845)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+80.sha-1dd2eaa
+ * @license Angular v6.1.0+81.sha-5342aea
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __extends, __read } from 'tslib';
+import { __extends, __spread, __read } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
