b749dd47f8a2f0c5f0afd5dc3458cd01853a22d5
Igor Minar <igor@angular.io>
2018-08-01 20:21:25 +0000

    01e6dab fix(compiler-cli): correct realPath to realpath. (#25023)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.0+72.sha-1e53278
+ * @license Angular v6.1.0+75.sha-01e6dab
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, NgModuleRef, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __read, __extends, __values, __assign } from 'tslib';
+import { __extends, __read, __values, __assign } from 'tslib';
 
 /**
  * @license
@@ -5565,7 +5565,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0+72.sha-1e53278');
+var VERSION = new Version('6.1.0+75.sha-01e6dab');
 
 /**
  * @license
