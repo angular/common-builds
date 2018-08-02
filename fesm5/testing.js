cfc117d88d5858b2a64227d88a99147cbf7a6d40
Igor Minar <igor@angular.io>
2018-08-02 16:13:21 +0000

    9ddf269 docs(elements): add section about custom element typings in `elements` guide (#25219)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+45.sha-1ef2eae
+ * @license Angular v6.1.0+48.sha-9ddf269
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
