c370c487cdc7bf56ee3a734ca7c0b9b778ecf51c
Igor Minar <igor@angular.io>
2018-08-02 22:41:16 +0000

    4f741e7 docs: release notes for the v6.1.1 release

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.0
+ * @license Angular v7.0.0-beta.0+1.sha-4f741e7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
