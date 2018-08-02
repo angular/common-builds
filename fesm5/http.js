896cfe167d49e54e5d8f0f74e2f5a80fa56d7947
Igor Minar <igor@angular.io>
2018-08-02 00:12:04 +0000

    3d7f555 ci: update pullapprove groups and add docs (#25257)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+85.sha-06af794
+ * @license Angular v6.1.0+86.sha-3d7f555
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __spread, __param, __read, __extends } from 'tslib';
+import { __spread, __read, __decorate, __param, __metadata, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
