8cccfe5f37157ad98f17def2042df9b796b639d7
Kara Erickson <karakara@google.com>
2018-08-03 23:55:45 +0000

    b0d86c1 refactor(bazel): dont rely on language target to downlevel for loop (#24534)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.0+15.sha-4e26478
+ * @license Angular v7.0.0-beta.0+17.sha-b0d86c1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __extends, __read } from 'tslib';
+import { __extends, __spread, __read } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
