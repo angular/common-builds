40f813b3f79253054d9a6c54a01e7e1296663289
Kara Erickson <karakara@google.com>
2018-08-06 22:28:30 +0000

    7d006c5 docs(core): fix tree-shakable spelling (#24057)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.0+28.sha-c158702
+ * @license Angular v7.0.0-beta.0+32.sha-7d006c5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __extends, __read } from 'tslib';
+import { __extends, __spread, __read } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
