331f628f30b6390ec03607b743467b2b1e5f2cb4
Kara Erickson <karakara@google.com>
2018-08-06 19:32:26 +0000

    a57f8a1 build: update Bazel to 0.16 (#25316)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.1+9.sha-e4acd83
+ * @license Angular v6.1.1+8.sha-a57f8a1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __spread, __param, __read, __extends } from 'tslib';
+import { __spread, __decorate, __metadata, __param, __read, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
