afb0a746400113c01f83731f3f6d6e9561367234
Kara Erickson <karakara@google.com>
2018-08-06 17:47:15 +0000

    97b5cb2 docs(aio): add Made with Angular (#21297)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+18.sha-26a15cc
+ * @license Angular v7.0.0-beta.0+22.sha-97b5cb2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6329,7 +6329,7 @@ function isPlatformWorkerUi(platformId) {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.0+18.sha-26a15cc');
+const VERSION = new Version('7.0.0-beta.0+22.sha-97b5cb2');
 
 /**
  * @fileoverview added by tsickle
