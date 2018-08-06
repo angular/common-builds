863e6501066e75c243ec09ac3b0302ded417c341
Kara Erickson <karakara@google.com>
2018-08-06 22:28:59 +0000

    2b906f6 docs: fix typo (#25331) PR Close #25331

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.0+29.sha-a9881bb
+ * @license Angular v7.0.0-beta.0+27.sha-2b906f6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __spread, __param, __read, __extends } from 'tslib';
+import { __decorate, __metadata, __param, __spread, __read, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
