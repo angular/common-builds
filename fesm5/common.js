715cb73f8befab2433206ae71c500e8fdd11be42
Kara Erickson <karakara@google.com>
2018-08-06 19:42:57 +0000

    4eb8ac6 build: update Bazel to 0.16 (#25316)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0-beta.0+23.sha-ba1e25f
+ * @license Angular v7.0.0-beta.0+24.sha-4eb8ac6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, Injector, NgModuleFactory, NgModuleRef, Type, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __read, __extends, __decorate, __param, __metadata, __values, __assign } from 'tslib';
+import { __values, __read, __extends, __decorate, __param, __metadata, __assign } from 'tslib';
 
 /**
  * @license
@@ -5599,7 +5599,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.0+23.sha-ba1e25f');
+var VERSION = new Version('7.0.0-beta.0+24.sha-4eb8ac6');
 
 /**
  * @license
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.0+23.sha-ba1e25f
+ * @license Angular v7.0.0-beta.0+24.sha-4eb8ac6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __spread, __param, __read, __extends } from 'tslib';
+import { __spread, __decorate, __metadata, __read, __param, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
