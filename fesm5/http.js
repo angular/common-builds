64b3b483c4aa65a9e8c45fbd2c177b574bd8770b
Kara Erickson <karakara@google.com>
2018-08-06 22:32:38 +0000

    8cbb836 docs: clarify heroes example (#21216)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.1+16.sha-6e7d5f0
+ * @license Angular v6.1.1+19.sha-8cbb836
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __spread, __param, __read, __extends } from 'tslib';
+import { __spread, __decorate, __param, __metadata, __read, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
