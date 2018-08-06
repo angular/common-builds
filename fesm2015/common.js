cdd1856af5d43e04c3bd1a86c7021014a8537ffe
Kara Erickson <karakara@google.com>
2018-08-06 19:41:39 +0000

    4eb8ac6 build: update Bazel to 0.16 (#25316)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+23.sha-ba1e25f
+ * @license Angular v7.0.0-beta.0+24.sha-4eb8ac6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6329,7 +6329,7 @@ function isPlatformWorkerUi(platformId) {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.0+23.sha-ba1e25f');
+const VERSION = new Version('7.0.0-beta.0+24.sha-4eb8ac6');
 
 /**
  * @fileoverview added by tsickle
