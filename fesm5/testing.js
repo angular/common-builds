1b9157c06db8df59e89611deb265827d3fd652cf
Kara Erickson <karakara@google.com>
2018-08-07 18:59:07 +0000

    054fbbe fix: add mappings for ngfactory & ngsummary files to their module names in aot summary resolver (#25335)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.1+24.sha-94a2ac7
+ * @license Angular v6.1.1+26.sha-054fbbe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
