ff202d0d6495215c37c9ecb89e3f25c8a0ef33fe
Kara Erickson <karakara@google.com>
2018-08-08 21:47:25 +0000

    f39551c docs: Clarify breaking change in minor release (#25393)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.2+4.sha-e76211a
+ * @license Angular v6.1.2+1.sha-f39551c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __spread, __param, __extends, __read } from 'tslib';
+import { __spread, __decorate, __metadata, __read, __extends, __param } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
