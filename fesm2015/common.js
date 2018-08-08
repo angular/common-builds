828703f1e3c514c8ac3810d1097c3ef07f2cdb76
Kara Erickson <karakara@google.com>
2018-08-08 19:06:01 +0000

    8434794 Revert "build: update Bazel to 0.16 (#25316)" (#25391)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+50.sha-732026c
+ * @license Angular v7.0.0-beta.0+51.sha-8434794
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6329,7 +6329,7 @@ function isPlatformWorkerUi(platformId) {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.0+50.sha-732026c');
+const VERSION = new Version('7.0.0-beta.0+51.sha-8434794');
 
 /**
  * @fileoverview added by tsickle
