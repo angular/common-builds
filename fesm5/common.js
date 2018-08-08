bda4ec1a3e314f79024b9b13947b8a84ca0beb38
Kara Erickson <karakara@google.com>
2018-08-08 21:54:17 +0000

    638ff76 docs: add ngrx book to the docs (#23389)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0-beta.1+7.sha-a3f1e2c
+ * @license Angular v7.0.0-beta.1+4.sha-638ff76
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, Injector, NgModuleFactory, NgModuleRef, Type, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __extends, __decorate, __param, __metadata, __values, __read, __assign } from 'tslib';
+import { __read, __values, __decorate, __extends, __param, __metadata, __assign } from 'tslib';
 
 /**
  * @license
@@ -5599,7 +5599,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.1+7.sha-a3f1e2c');
+var VERSION = new Version('7.0.0-beta.1+4.sha-638ff76');
 
 /**
  * @license
