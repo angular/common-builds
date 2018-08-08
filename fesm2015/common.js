74340ac6c0d577400656a5630cbc82396dfc8b63
Kara Erickson <karakara@google.com>
2018-08-08 21:50:36 +0000

    1039bea docs: Clarify breaking change in minor release (#25393)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1
+ * @license Angular v7.0.0-beta.1+1.sha-1039bea
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6329,7 +6329,7 @@ function isPlatformWorkerUi(platformId) {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.1');
+const VERSION = new Version('7.0.0-beta.1+1.sha-1039bea');
 
 /**
  * @fileoverview added by tsickle
