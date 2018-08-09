88fdbed362e9a48e1c1693b43e541d916fe45bd8
Ben Lesh <ben@benlesh.com>
2018-08-09 02:59:13 +0000

    fda30cb build: stop printing source-map-support warning (#25339)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.2+6.sha-3449f1e
+ * @license Angular v6.1.2+8.sha-fda30cb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __spread, __read } from 'tslib';
+import { __spread, __extends, __read } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
