5dff6fc2f6f078b57d47ef3ec2bbf6ba298cb2da
Ben Lesh <ben@benlesh.com>
2018-08-14 05:41:17 +0000

    3ac7070 docs: fix typo in Architecture overview page (#25438)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.1+28.sha-1bb3014
+ * @license Angular v7.0.0-beta.1+31.sha-3ac7070
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __spread, __param, __read, __extends } from 'tslib';
+import { __decorate, __param, __metadata, __spread, __extends, __read } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.1+28.sha-1bb3014
+ * @license Angular v7.0.0-beta.1+31.sha-3ac7070
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
