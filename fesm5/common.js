229bcc292637673a0cf5699a3f996bec82ded4dc
Ben Lesh <ben@benlesh.com>
2018-08-14 05:49:01 +0000

    5f08bdf ci: github robot should enforce that all requested reviews are submitted (#25336)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.2+11.sha-151e4b9
+ * @license Angular v6.1.2+13.sha-5f08bdf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, Injector, NgModuleFactory, NgModuleRef, Type, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __extends, __decorate, __param, __metadata, __read, __values, __assign } from 'tslib';
+import { __read, __values, __decorate, __extends, __param, __metadata, __assign } from 'tslib';
 
 /**
  * @license
@@ -5599,7 +5599,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.2+11.sha-151e4b9');
+var VERSION = new Version('6.1.2+13.sha-5f08bdf');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.2+11.sha-151e4b9
+ * @license Angular v6.1.2+13.sha-5f08bdf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
