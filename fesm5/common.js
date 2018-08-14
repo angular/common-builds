f292d4a97cd0e6ce7c25510a58c65e28b95227ab
Ben Lesh <ben@benlesh.com>
2018-08-14 20:10:54 +0000

    e906bf4 docs: add Accelebrate to resources (#23204)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+12.sha-f1ed022
+ * @license Angular v6.1.2+14.sha-e906bf4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5593,7 +5593,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.2+12.sha-f1ed022');
+var VERSION = new Version('6.1.2+14.sha-e906bf4');
 
 /**
  * @license
