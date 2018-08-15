bfe42812ab0d30e2a632e9b3b18d91078aa612e9
Ben Lesh <ben@benlesh.com>
2018-08-15 01:21:26 +0000

    01ec5fd fix(service-worker): `Cache-Control: no-cache` on assets breaks service worker (#25408)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.1+49.sha-eeb81b9
+ * @license Angular v7.0.0-beta.1+51.sha-01ec5fd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
