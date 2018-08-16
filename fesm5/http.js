ff9e384be87d090e723b5978fc805420a97e5d2e
Jason Aden <jasonaden@google.com>
2018-08-16 22:17:42 +0000

    620d140 docs: add HttpClientModule import code to services tutorial (#24854)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.3+1.sha-36fb4f4
+ * @license Angular v6.1.3+2.sha-620d140
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __decorate, __metadata, __param, __extends, __read } from 'tslib';
+import { __decorate, __metadata, __spread, __param, __extends, __read } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
