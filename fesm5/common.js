06b3a0b89f658dfc943b2923cfbba1b09c6986e1
Pete Bacon Darwin <pete@bacondarwin.com>
2018-08-16 17:45:36 +0000

    73146c1 fix(ivy): ensure factory statements are emitted correctly

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+43.sha-82c8052
+ * @license Angular v7.0.0-beta.2+33.sha-73146c1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5593,7 +5593,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.1+43.sha-82c8052');
+var VERSION = new Version('7.0.0-beta.2+33.sha-73146c1');
 
 /**
  * @license
