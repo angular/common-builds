444b1d8c86c3010ab239cfbe3f5394029d155edb
Jason Aden <jasonaden@google.com>
2018-08-16 22:09:58 +0000

    6482f6f refactor(ivy): separate container into 2 instructions (#25509)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+14.sha-abcc430
+ * @license Angular v7.0.0-beta.2+15.sha-6482f6f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6329,7 +6329,7 @@ function isPlatformWorkerUi(platformId) {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.2+14.sha-abcc430');
+const VERSION = new Version('7.0.0-beta.2+15.sha-6482f6f');
 
 /**
  * @fileoverview added by tsickle
