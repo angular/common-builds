d83cef114ab08d91b12243b0d5d06a5a2d219974
Jason Aden <jasonaden@google.com>
2018-08-16 22:14:21 +0000

    cc55d60 docs: add HttpClientModule import code to services tutorial (#24854)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+15.sha-6482f6f
+ * @license Angular v7.0.0-beta.2+17.sha-cc55d60
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6329,7 +6329,7 @@ function isPlatformWorkerUi(platformId) {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.2+15.sha-6482f6f');
+const VERSION = new Version('7.0.0-beta.2+17.sha-cc55d60');
 
 /**
  * @fileoverview added by tsickle
