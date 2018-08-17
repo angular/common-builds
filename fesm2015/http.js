a262d654aafa9749d520c810cc74a8fa6f74ddc9
Jason Aden <jasonaden@google.com>
2018-08-17 18:16:57 +0000

    31f0f5b feat(ivy): add support for local refs on ng-template (#25482)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.2+22.sha-f053a3f
+ * @license Angular v7.0.0-beta.2+21.sha-31f0f5b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { Injectable, InjectionToken, Injector, NgModule, defineInjectable, inject, INJECTOR, ɵdefineNgModule, defineInjector, Inject, PLATFORM_ID } from '@angular/core';
+import { Injectable, InjectionToken, Injector, NgModule, defineInjectable, inject, Inject, INJECTOR, ɵdefineNgModule, defineInjector, PLATFORM_ID } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
 import { DOCUMENT, ɵparseCookieValue } from '@angular/common';
