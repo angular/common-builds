e23e0d8d94adc6e2ed7c732c7edeca064cf7b872
Jason Aden <jasonaden@google.com>
2018-08-17 21:55:23 +0000

    2b810a4 docs(docs-infra): the `build.sh` script was renamed to `create-image.sh` (#25554)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.3+14.sha-a5cc9db
+ * @license Angular v6.1.3+13.sha-2b810a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, NgModuleRef, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __extends, __read, __values, __assign } from 'tslib';
+import { __read, __extends, __values, __assign } from 'tslib';
 
 /**
  * @license
@@ -5593,7 +5593,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.3+14.sha-a5cc9db');
+var VERSION = new Version('6.1.3+13.sha-2b810a4');
 
 /**
  * @license
