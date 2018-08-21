52f022d62587c63b3e2e152620f816a7f320b1de
Jason Aden <jasonaden@google.com>
2018-08-21 21:36:59 +0000

    f33dbf4 docs(changelog): add v6.1.3 release to main CHANGELOG.md (#25603)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.2+38.sha-6176974
+ * @license Angular v7.0.0-beta.2+39.sha-f33dbf4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
