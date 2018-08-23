60506c50a9058127f300b3a2093f6c811ced314c
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 01:55:11 +0000

    22d58fc build(bazel): remove workaround no longer needed for module names for ngfactory & ngsummary files (#25604)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+24.sha-61218f5
+ * @license Angular v7.0.0-beta.3+27.sha-22d58fc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5593,7 +5593,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.3+24.sha-61218f5');
+var VERSION = new Version('7.0.0-beta.3+27.sha-22d58fc');
 
 /**
  * @license
