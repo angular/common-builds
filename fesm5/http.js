860104dc612feb9e7f0ccfb65d61648dcdfdc78b
Matias Niemelä <matias@yearofmoo.com>
2018-08-24 17:05:24 +0000

    72ed2e9 build(bazel): esm5_outputs_aspect to work with targets such as ts_proto_library with no replay_params attribute (#25648)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.4+4.sha-4e82a76
+ * @license Angular v6.1.4+5.sha-72ed2e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __param, __spread, __read, __extends } from 'tslib';
+import { __decorate, __metadata, __spread, __param, __read, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
