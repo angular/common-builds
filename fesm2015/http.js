e1994336de1b6c65e5d6b620da6abc9c65cce9b7
Matias Niemelä <matias@yearofmoo.com>
2018-08-27 23:25:34 +0000

    6ed7993 fix(bazel): move bazel managed runtime deps for downstream usage (#25690)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.3+61.sha-b1902db
+ * @license Angular v7.0.0-beta.3+59.sha-6ed7993
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { Injectable, Injector, NgModule, InjectionToken, Inject, defineInjectable, inject, INJECTOR, ɵdefineNgModule, defineInjector, PLATFORM_ID } from '@angular/core';
+import { Injectable, InjectionToken, Injector, NgModule, defineInjectable, inject, Inject, INJECTOR, ɵdefineNgModule, defineInjector, PLATFORM_ID } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
 import { DOCUMENT, ɵparseCookieValue } from '@angular/common';
