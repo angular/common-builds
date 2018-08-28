15522d0ced9c814afd1edf9b04e62afad60b368e
Miško Hevery <misko@hevery.com>
2018-08-28 19:17:35 +0000

    0fe708f ci: remove `vicb` from `pullapprove.yml` (#25702)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.3+68.sha-b069514
+ * @license Angular v7.0.0-beta.3+69.sha-0fe708f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
