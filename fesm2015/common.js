732d4098d3d0187cdaff6686d62f7745d114d9c2
Matias Niemelä <matias@yearofmoo.com>
2018-08-28 19:43:24 +0000

    0386c44 fix(ivy): inject attributes for directives on ng-template / ng-container (#25697)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+69.sha-0fe708f
+ * @license Angular v7.0.0-beta.3+70.sha-0386c44
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6512,7 +6512,7 @@ function isPlatformWorkerUi(platformId) {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.3+69.sha-0fe708f');
+const VERSION = new Version('7.0.0-beta.3+70.sha-0386c44');
 
 /**
  * @fileoverview added by tsickle
