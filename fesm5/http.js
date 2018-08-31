a765396158a1793bca868f139fffab4698b9099b
Misko Hevery <misko@hevery.com>
2018-08-31 05:44:27 +0000

    ed6b68b fix(bazel): protractor rule should include *.e2e-spec.js (#25701)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.6+1.sha-2e09115
+ * @license Angular v6.1.6+2.sha-ed6b68b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __spread, __param, __extends, __read } from 'tslib';
+import { __decorate, __metadata, __spread, __extends, __param, __read } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
