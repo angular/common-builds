1e80a99d0ee202caef5eccd3386f133ee6964d03
Miško Hevery <misko@hevery.com>
2018-08-31 17:22:36 +0000

    a065dc2 fix(compiler): remove bogus lines

--- esm5/common.js
+++ esm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.10-bc27d4a
+ * @license Angular v5.2.11-a065dc2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6537,7 +6537,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.10-bc27d4a');
+var VERSION = new Version('5.2.11-a065dc2');
 
 /**
  * @fileoverview added by tsickle
