c48ffd74ef0b213c3d56907da689744b8c2433f0
Misko Hevery <misko@hevery.com>
2018-09-04 20:18:28 +0000

    51c0d9c style(ivy): remove unused ivy code (#25780)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.4+38.sha-cc29b9c
+ * @license Angular v7.0.0-beta.4+41.sha-51c0d9c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __read, __extends } from 'tslib';
+import { __spread, __extends, __read } from 'tslib';
 import { InjectionToken, defineInjectable, inject, INJECTOR, ɵdefineNgModule, defineInjector, PLATFORM_ID } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
