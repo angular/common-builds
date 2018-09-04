13059ed1cc4fa433443525d00e4e2f032e028984
Misko Hevery <misko@hevery.com>
2018-09-04 20:46:52 +0000

    d8930bb docs: correct misspellings and add missing punctuation in tutorial (#25676)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.6+9.sha-a4405d7
+ * @license Angular v6.1.6+11.sha-d8930bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __param, __spread, __read, __extends } from 'tslib';
+import { __decorate, __spread, __param, __metadata, __read, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
