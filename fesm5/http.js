5af820fa6bf1b15d1f3915726cfbf0922d875ea8
Misko Hevery <misko@hevery.com>
2018-09-04 20:55:46 +0000

    4fe369e docs: add pwa keyword to service worker page (#25725) PR Close #25725

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.6+11.sha-d8930bb
+ * @license Angular v6.1.6+12.sha-4fe369e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __spread, __param, __metadata, __read, __extends } from 'tslib';
+import { __decorate, __spread, __metadata, __param, __read, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
