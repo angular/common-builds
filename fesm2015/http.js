fcc78e809e458bfe8c350f3b11e4524fbcf60125
Misko Hevery <misko@hevery.com>
2018-09-05 19:21:00 +0000

    4596fc0 refactor(docs-infra): bump polyfills payload limit (#25806)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.6+14.sha-46de203
+ * @license Angular v6.1.6+15.sha-4596fc0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata } from 'tslib';
+import { __decorate, __metadata, __param } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.6+14.sha-46de203
+ * @license Angular v6.1.6+15.sha-4596fc0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __read, __extends, __spread } from 'tslib';
+import { __spread, __decorate, __metadata, __param, __read, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
