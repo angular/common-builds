6b37bff7db20dccd16a1ad4863bdab440706e41a
Misko Hevery <misko@hevery.com>
2018-09-05 19:21:21 +0000

    d752a89 build(docs-infra): ensure root node_modules exists (#25811)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.6+15.sha-4596fc0
+ * @license Angular v6.1.6+13.sha-d752a89
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
