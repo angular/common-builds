9bcb7f34b5f0d53f9f17f972fb4d3a2dc811e00e
Misko Hevery <misko@hevery.com>
2018-09-05 19:22:37 +0000

    234661b feat(docs-infra): disable "status" selector in API list when displaying only packages (#25718)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0-beta.4+46.sha-a417b2b
+ * @license Angular v7.0.0-beta.4+48.sha-234661b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, Injector, NgModuleFactory, NgModuleRef, Type, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __read, __extends, __decorate, __param, __metadata, __values, __assign } from 'tslib';
+import { __extends, __decorate, __param, __metadata, __read, __values, __assign } from 'tslib';
 
 /**
  * @license
@@ -5600,7 +5600,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.4+46.sha-a417b2b');
+var VERSION = new Version('7.0.0-beta.4+48.sha-234661b');
 
 /**
  * @license
