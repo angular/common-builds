588a5c769fd7ce7b269fa3ca04d84c32d31d9fe7
Misko Hevery <misko@hevery.com>
2018-09-05 18:47:34 +0000

    25c1f33 refactor(docs-infra): bump polyfills payload limit (#25806)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.4+42.sha-c1ae3c1
+ * @license Angular v7.0.0-beta.4+44.sha-25c1f33
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
