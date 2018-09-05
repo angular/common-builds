8dbf14942ac4fe5611d7ff46651abb2676cdc07a
Misko Hevery <misko@hevery.com>
2018-09-05 19:21:21 +0000

    a417b2b fix(ivy): detect frozen flyweight objects in definitions and unfreeze (#25755)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.4+44.sha-25c1f33
+ * @license Angular v7.0.0-beta.4+46.sha-a417b2b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { Injectable, InjectionToken, Injector, NgModule, defineInjectable, inject, Inject, INJECTOR, ɵdefineNgModule, defineInjector, PLATFORM_ID } from '@angular/core';
+import { Injectable, InjectionToken, Injector, NgModule, defineInjectable, inject, INJECTOR, ɵdefineNgModule, defineInjector, Inject, PLATFORM_ID } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
 import { DOCUMENT, ɵparseCookieValue } from '@angular/common';
