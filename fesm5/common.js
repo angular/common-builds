e857ea96f91e96ea7702aa6a3c0b821d6205a1dc
Misko Hevery <misko@hevery.com>
2018-09-05 20:12:56 +0000

    1bc6f64 docs: update event page (#25799) docs: change reactiveconf event location PR Close #25799

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0-beta.4+56.sha-d96e962
+ * @license Angular v7.0.0-beta.4+58.sha-1bc6f64
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { InjectionToken, EventEmitter, Version, defineInjectable, inject, IterableDiffers, KeyValueDiffers, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, NgModuleRef, isDevMode, WrappedValue, ɵisObservable, ɵisPromise, LOCALE_ID, ɵdefineNgModule, defineInjector, ɵdefineDirective, ɵdirectiveInject, ɵinjectElementRef, ɵinjectRenderer2, ɵPublicFeature, ɵinjectViewContainerRef, ɵNgOnChangesFeature, ɵinjectTemplateRef, ɵinjectAttribute, ɵdefinePipe, ɵinjectChangeDetectorRef } from '@angular/core';
-import { __read, __extends, __values, __assign } from 'tslib';
+import { InjectionToken, EventEmitter, defineInjectable, inject, Version, IterableDiffers, KeyValueDiffers, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, NgModuleRef, isDevMode, WrappedValue, ɵisObservable, ɵisPromise, LOCALE_ID, ɵdefineNgModule, defineInjector, ɵdefineDirective, ɵdirectiveInject, ɵinjectElementRef, ɵinjectRenderer2, ɵPublicFeature, ɵinjectViewContainerRef, ɵNgOnChangesFeature, ɵinjectTemplateRef, ɵinjectAttribute, ɵdefinePipe, ɵinjectChangeDetectorRef } from '@angular/core';
+import { __read, __values, __extends, __assign } from 'tslib';
 
 /**
  * @license
@@ -5322,7 +5322,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.4+56.sha-d96e962');
+var VERSION = new Version('7.0.0-beta.4+58.sha-1bc6f64');
 
 /**
  * @license
