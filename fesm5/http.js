e4907607ebcb78e52318a490aac77502911a3177
Misko Hevery <misko@hevery.com>
2018-09-05 20:37:01 +0000

    e72f741 docs: update event page (#25799) docs: change reactiveconf event location PR Close #25799

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.6+22.sha-82e0676
+ * @license Angular v6.1.6+24.sha-e72f741
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { __spread, __read, __extends } from 'tslib';
-import { InjectionToken, defineInjectable, inject, INJECTOR, ɵdefineNgModule, defineInjector, PLATFORM_ID } from '@angular/core';
+import { InjectionToken, defineInjectable, inject, PLATFORM_ID, INJECTOR, ɵdefineNgModule, defineInjector } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
 import { DOCUMENT, ɵparseCookieValue } from '@angular/common';
