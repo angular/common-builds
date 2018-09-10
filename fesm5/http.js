969ba2f139531eaae8d1a76e3d6677873110fe9d
Igor Minar <igor@angular.io>
2018-09-10 19:07:36 +0000

    8f9aeaa docs: move compiler options to last section of the page (#22353)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.7+10.sha-9f3da65
+ * @license Angular v6.1.7+9.sha-8f9aeaa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __spread, __read, __extends } from 'tslib';
+import { __decorate, __spread, __read, __extends, __param, __metadata } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
