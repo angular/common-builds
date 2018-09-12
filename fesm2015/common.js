548b12c90f9657ff6303520f53c55154bb5047f9
Igor Minar <igor@angular.io>
2018-09-12 00:02:20 +0000

    f2ba55f docs(router): change description of parameter usage for navigation functions (#21840)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0-beta.5+24.sha-f3b552f
+ * @license Angular v7.0.0-beta.5+28.sha-f2ba55f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, Injector, NgModuleFactory, NgModuleRef, Type, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __decorate, __param, __metadata } from 'tslib';
+import { __decorate, __metadata, __param } from 'tslib';
 
 /**
  * @license
@@ -5413,7 +5413,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-beta.5+24.sha-f3b552f');
+const VERSION = new Version('7.0.0-beta.5+28.sha-f2ba55f');
 
 /**
  * @license
