7a0581784cc6fb04166131dce1e8c3efed944d56
Igor Minar <igor@angular.io>
2018-09-12 00:00:53 +0000

    f2ba55f docs(router): change description of parameter usage for navigation functions (#21840)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.5+24.sha-f3b552f
+ * @license Angular v7.0.0-beta.5+28.sha-f2ba55f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
