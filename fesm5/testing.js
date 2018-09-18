4374d40d825c810645c955880cb5f69a4c6ff197
Ben Lesh <ben@benlesh.com>
2018-09-18 21:26:16 +0000

    cbbad1b refactor(ivy): pre-factor: set explicit type parameters for ModuleWithProviders (#25970)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.5+53.sha-e952c65
+ * @license Angular v7.0.0-beta.5+51.sha-cbbad1b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
