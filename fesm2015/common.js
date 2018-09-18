d77e736b88b26be03831c06d055418262df79459
Ben Lesh <ben@benlesh.com>
2018-09-18 21:38:31 +0000

    6c8791e docs(forms): change documentation of the FormGroup patchValue method (#25901)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.7+16.sha-d9bd860
+ * @license Angular v6.1.7+18.sha-6c8791e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { InjectionToken, Version, defineInjectable, inject, KeyValueDiffers, Renderer2, LOCALE_ID, ComponentFactoryResolver, NgModuleRef, WrappedValue, ɵisObservable, ɵisPromise, EventEmitter, IterableDiffers, ɵisListLikeIterable, ɵstringify, isDevMode, ɵdefineNgModule, defineInjector, ɵdefineDirective, ɵdirectiveInject, ɵinjectElementRef, ɵdefinePipe, ɵinjectViewContainerRef, ɵNgOnChangesFeature, ɵinjectChangeDetectorRef, ɵinjectAttribute, ɵinjectTemplateRef } from '@angular/core';
+import { InjectionToken, Version, defineInjectable, inject, KeyValueDiffers, Renderer2, LOCALE_ID, EventEmitter, ComponentFactoryResolver, NgModuleRef, WrappedValue, ɵisObservable, ɵisPromise, IterableDiffers, ɵisListLikeIterable, ɵstringify, isDevMode, ɵdefineNgModule, defineInjector, ɵdefineDirective, ɵdirectiveInject, ɵinjectElementRef, ɵdefinePipe, ɵinjectViewContainerRef, ɵNgOnChangesFeature, ɵinjectAttribute, ɵinjectTemplateRef, ɵinjectChangeDetectorRef } from '@angular/core';
 
 /**
  * @license
@@ -4991,7 +4991,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.7+16.sha-d9bd860');
+const VERSION = new Version('6.1.7+18.sha-6c8791e');
 
 /**
  * @license
--- fesm5/common.js
+++ fesm5/common.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.7+16.sha-d9bd860
+ * @license Angular v6.1.7+18.sha-6c8791e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { InjectionToken, Version, defineInjectable, inject, ComponentFactoryResolver, NgModuleRef, WrappedValue, ɵisObservable, ɵisPromise, KeyValueDiffers, EventEmitter, ɵstringify, IterableDiffers, Renderer2, ɵisListLikeIterable, isDevMode, LOCALE_ID, ɵdefineNgModule, defineInjector, ɵdefinePipe, ɵdirectiveInject, ɵdefineDirective, ɵinjectViewContainerRef, ɵNgOnChangesFeature, ɵinjectAttribute, ɵinjectTemplateRef, ɵinjectElementRef, ɵinjectChangeDetectorRef } from '@angular/core';
+import { InjectionToken, Version, defineInjectable, inject, ComponentFactoryResolver, NgModuleRef, WrappedValue, ɵisObservable, ɵisPromise, KeyValueDiffers, EventEmitter, IterableDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, isDevMode, LOCALE_ID, ɵdefineNgModule, defineInjector, ɵdefinePipe, ɵdirectiveInject, ɵdefineDirective, ɵinjectViewContainerRef, ɵNgOnChangesFeature, ɵinjectElementRef, ɵinjectAttribute, ɵinjectTemplateRef, ɵinjectChangeDetectorRef } from '@angular/core';
 import { __values, __read, __extends, __assign } from 'tslib';
 
 /**
@@ -5196,7 +5196,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.7+16.sha-d9bd860');
+var VERSION = new Version('6.1.7+18.sha-6c8791e');
 
 /**
  * @license
