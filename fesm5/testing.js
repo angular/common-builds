9c29bf5e08a371a071f96a9c38a796cd6ada2cfd
Ben Lesh <ben@benlesh.com>
2018-09-18 21:56:26 +0000

    110c81f build: ignore `aio`/`docs-infra` commits in `changelog` gulp task (#25838)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.7+19.sha-c693624
+ * @license Angular v6.1.7+21.sha-110c81f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
