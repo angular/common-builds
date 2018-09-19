6b414a720b8713905df56068204b45dbf6d45af9
Ben Lesh <ben@benlesh.com>
2018-09-19 17:21:24 +0000

    e8bab13 docs: delete extra sentence (#25984)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.5+51.sha-cbbad1b
+ * @license Angular v7.0.0-beta.5+54.sha-e8bab13
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
