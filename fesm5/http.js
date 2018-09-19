a8e9d0ad9f2693bd786a525312df82b87bfebe4e
Ben Lesh <ben@benlesh.com>
2018-09-19 22:36:31 +0000

    3988ebf release: cut the v6.1.8 release

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.7+39.sha-a41f331
+ * @license Angular v6.1.8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __spread, __read, __metadata, __extends, __param } from 'tslib';
+import { __decorate, __spread, __metadata, __read, __extends, __param } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
