650ed5c62a66008a0df0380ca2c8cc746d133f41
Ben Lesh <ben@benlesh.com>
2018-09-19 18:25:19 +0000

    dd5e35e docs: add ngmodule api doc (#25618)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+55.sha-f91b045
+ * @license Angular v7.0.0-beta.5+56.sha-dd5e35e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5232,8 +5232,15 @@ var COMMON_PIPES = [
 // Note: This does not contain the location providers,
 // as they need some platform specific implementations to work.
 /**
- * The module that includes all the basic Angular directives like {@link NgIf}, {@link NgForOf}, ...
+ * Exports all the basic Angular directives and pipes,
+ * such as `NgIf`, `NgForOf`, `DecimalPipe`, and so on.
+ * Re-exported by `BrowserModule`, which is included automatically in the root
+ * `AppModule` when you create a new app with the CLI `new` command.
  *
+ * * The `providers` options configure the NgModule's injector to provide
+ * localization dependencies to members.
+ * * The `exports` options make the declared directives and pipes available for import
+ * by other NgModules.
  *
  */
 var CommonModule = /** @class */ (function () {
@@ -5322,7 +5329,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.5+55.sha-f91b045');
+var VERSION = new Version('7.0.0-beta.5+56.sha-dd5e35e');
 
 /**
  * @license
