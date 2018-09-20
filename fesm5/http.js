fd04912602c49cd1f8b8136f79b7c34b0b6c1922
Kara Erickson <karakara@google.com>
2018-09-20 00:43:10 +0000

    c7a6adc docs(forms): update form apis based on review feedback (#25724)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.8
+ * @license Angular v6.1.8+3.sha-c7a6adc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __spread, __metadata, __read, __extends, __param } from 'tslib';
+import { __decorate, __param, __metadata, __spread, __read, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
