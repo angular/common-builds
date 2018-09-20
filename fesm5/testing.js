7f2de8326d1bb7ac4365873a71c1d73c0d118534
Kara Erickson <karakara@google.com>
2018-09-20 18:31:53 +0000

    549de1e fix(core): add missing `peerDependency ` to `@angular/compiler` (#26033)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.6+13.sha-48e73c1
+ * @license Angular v7.0.0-beta.6+14.sha-549de1e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
