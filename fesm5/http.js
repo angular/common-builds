16778a973b273ea2ae97c0da26774979825d7f2e
Kara Erickson <karakara@google.com>
2018-09-20 00:01:09 +0000

    d7e5bbf feat(compiler-cli): add support to extend `angularCompilerOptions` (#22717)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.6
+ * @license Angular v7.0.0-beta.6+6.sha-d7e5bbf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __spread, __metadata, __read, __extends, __param } from 'tslib';
+import { __decorate, __param, __metadata, __spread, __read, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.6
+ * @license Angular v7.0.0-beta.6+6.sha-d7e5bbf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
