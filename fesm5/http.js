0b265d044e660f9bf078198f6998884e0a6d5f67
Kara Erickson <karakara@google.com>
2018-09-21 18:05:23 +0000

    e42c1b0 ci: correct github robot size check configuration (#26057)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.6+27.sha-e7ade38
+ * @license Angular v7.0.0-beta.6+28.sha-e42c1b0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __extends, __read } from 'tslib';
+import { __spread, __read, __extends } from 'tslib';
 import { InjectionToken, defineInjectable, inject, PLATFORM_ID, INJECTOR, ɵdefineNgModule, defineInjector } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
