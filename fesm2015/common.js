aca6bb801b7a30c92c9c5b511a6cef14ecc66f5d
Kara Erickson <karakara@google.com>
2018-09-21 18:05:08 +0000

    026b60c build(docs-infra): expose deprecated status on items more clearly (#25750)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0-beta.6+28.sha-e42c1b0
+ * @license Angular v7.0.0-beta.6+33.sha-026b60c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, Injector, NgModuleFactory, NgModuleRef, Type, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __decorate, __param, __metadata } from 'tslib';
+import { __decorate, __metadata, __param } from 'tslib';
 
 /**
  * @license
@@ -5422,7 +5422,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-beta.6+28.sha-e42c1b0');
+const VERSION = new Version('7.0.0-beta.6+33.sha-026b60c');
 
 /**
  * @license
