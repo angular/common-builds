c67a2d7eb7eb4e973a8fa5a37eabaf2a4b4d6220
Kara Erickson <karakara@google.com>
2018-09-25 19:11:11 +0000

    42e2e7c build: remove obsolete comment in `env.sh` (#25819)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.8+39.sha-adad170
+ * @license Angular v6.1.8+40.sha-42e2e7c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __param, __extends, __read, __spread } from 'tslib';
+import { __decorate, __param, __metadata, __extends, __spread, __read } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
