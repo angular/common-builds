9416b07dd673156d6b139fb6859cb9609958e053
Kara Erickson <karakara@google.com>
2018-09-26 23:04:12 +0000

    a01acec fix(docs-infra): use correct parameters for paginated requests to GitHub (#25671)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.6+89.sha-4597582
+ * @license Angular v7.0.0-beta.7+18.sha-a01acec
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
