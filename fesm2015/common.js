3c63b068e0d9b671861ca7ae7316853c1d8d4844
Kara Erickson <karakara@google.com>
2018-09-26 19:04:15 +0000

    f29b218 feat(docs-infra): generate Angular CLI command reference (#25363)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+86.sha-bc88f31
+ * @license Angular v7.0.0-beta.6+88.sha-f29b218
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5423,7 +5423,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-beta.6+86.sha-bc88f31');
+const VERSION = new Version('7.0.0-beta.6+88.sha-f29b218');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.6+86.sha-bc88f31
+ * @license Angular v7.0.0-beta.6+88.sha-f29b218
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
