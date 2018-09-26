2db5d16ddccba290572e2f7d5e6107df5bd767b9
Kara Erickson <karakara@google.com>
2018-09-26 07:42:44 +0000

    15a2b8f fix(ivy): wrapper fns arent necessary anymore (#26108)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.6+79.sha-d191085
+ * @license Angular v7.0.0-beta.6+80.sha-15a2b8f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
