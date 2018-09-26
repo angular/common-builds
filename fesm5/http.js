91091383af4891b0aaabca514a8c73531d5675c8
Kara Erickson <karakara@google.com>
2018-09-26 18:51:26 +0000

    159e8b4 docs: update routing integration section based on feedback (#20023)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.8+41.sha-559c647
+ * @license Angular v6.1.8+65.sha-159e8b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __spread, __read, __extends } from 'tslib';
+import { __decorate, __spread, __metadata, __read, __param, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
