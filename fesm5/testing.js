38b43c564c484cec8a3f34d95630591825e1a061
Kara Erickson <karakara@google.com>
2018-09-26 02:29:03 +0000

    d191085 docs: cleanup minor changes for forms overview (#25663)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.6+61.sha-c7e2930
+ * @license Angular v7.0.0-beta.6+79.sha-d191085
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
