a5ce382259c8ea10c50eb1d01c4b85f628ac2474
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-27 23:13:58 +0000

    7cf5807 fix(ivy): ensure [style] and [class] bindings are placed in the same instruction (#26126)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0-beta.7+46.sha-9523991
+ * @license Angular v7.0.0-beta.7+47.sha-7cf5807
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, Injector, NgModuleFactory, NgModuleRef, Type, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __decorate, __metadata, __param } from 'tslib';
+import { __decorate, __param, __metadata } from 'tslib';
 
 /**
  * @license
@@ -5423,7 +5423,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-beta.7+46.sha-9523991');
+const VERSION = new Version('7.0.0-beta.7+47.sha-7cf5807');
 
 /**
  * @license
