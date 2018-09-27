2e0fc64eba1c262bb64dbd311703b85d0083f4a9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-27 22:40:12 +0000

    9523991 refactor(router): cleanup to navigation stream for readability and documentation (#25740)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.7+30.sha-63b795a
+ * @license Angular v7.0.0-beta.7+46.sha-9523991
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
