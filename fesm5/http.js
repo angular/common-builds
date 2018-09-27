b969db68a59b862430330b35afa3152f1a20c72d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-27 20:23:43 +0000

    04a023c build: pass `stripExportPattern` as an array of `RegExp` (#26012)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.9+18.sha-c12e553
+ * @license Angular v6.1.9+19.sha-04a023c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __read, __spread, __extends, __param } from 'tslib';
+import { __decorate, __spread, __metadata, __read, __param, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
