bc6cc87c4d6581a3eb3eee2c46193cc8dc21740b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-01 17:20:33 +0000

    a2878b0 fix(docs-infra): remove unnecessary margin on short descriptions (#25768)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0-rc.0+5.sha-ab379ab
+ * @license Angular v7.0.0-rc.0+23.sha-a2878b0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, Injector, NgModuleFactory, NgModuleRef, Type, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __values, __read, __decorate, __metadata, __extends, __param, __assign } from 'tslib';
+import { __values, __read, __decorate, __extends, __param, __metadata, __assign } from 'tslib';
 
 /**
  * @license
@@ -5609,7 +5609,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-rc.0+5.sha-ab379ab');
+var VERSION = new Version('7.0.0-rc.0+23.sha-a2878b0');
 
 /**
  * @license
