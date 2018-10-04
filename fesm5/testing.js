7ead4c5349ece090b2e11b741513fd37779f6cd2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-04 17:19:14 +0000

    aaaa340 fix(ivy): sanitize tag name while generating listener function name (#26237)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.0+5.sha-ab379ab
+ * @license Angular v7.0.0-rc.0+27.sha-aaaa340
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
