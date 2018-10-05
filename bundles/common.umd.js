6296e5a799363fd4e0e0e24966fac0d02062ec2a
Jason Aden <jasonaden@google.com>
2018-10-05 23:19:12 +0000

    2fe401d ci(docs-infra): show custom 404 page on preview server (for consistency) (#26199)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+67.sha-b35ab4f
+ * @license Angular v6.1.9+64.sha-2fe401d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5330,7 +5330,6 @@
          * For example, the Canadian dollar CAD has the symbol `CA$` and the symbol-narrow `$`. If the
          * locale has no narrow symbol, uses the standard symbol for the locale.
          *   - String: Use the given string value instead of a code or a symbol.
-         * For example, an empty string will suppress the currency & symbol.
          *   - Boolean (marked deprecated in v5): `true` for symbol and false for `code`.
          *
          * @param digitsInfo Decimal representation options, specified by a string
@@ -5621,7 +5620,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.9+67.sha-b35ab4f');
+    var VERSION = new core.Version('6.1.9+64.sha-2fe401d');
 
     /**
      * @license
