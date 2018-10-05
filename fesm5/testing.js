977d4448f6855d38621cd80ac9557bfba4680850
Jason Aden <jasonaden@google.com>
2018-10-05 21:13:58 +0000

    bfeed84 docs(service-worker): improve API docs for `SwPush`/`SwUpdate` (#23138)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.0+49.sha-3cce4af
+ * @license Angular v7.0.0-rc.0+53.sha-bfeed84
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
