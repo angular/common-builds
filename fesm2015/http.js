acad827d55f4c4c327730c058f35e44e90e2d007
Jason Aden <jasonaden@google.com>
2018-10-05 23:22:59 +0000

    3b4d9dc fix(platform-browser): fix #22155, destroy hammer manager when off (#22156)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.9+68.sha-3886bfa
+ * @license Angular v6.1.9+70.sha-3b4d9dc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __param } from 'tslib';
+import { __decorate, __param, __metadata } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.9+68.sha-3886bfa
+ * @license Angular v6.1.9+70.sha-3b4d9dc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __spread, __metadata, __read, __param, __extends } from 'tslib';
+import { __decorate, __spread, __metadata, __extends, __read, __param } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
