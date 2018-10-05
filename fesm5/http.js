d5841475cacc1adb6530b1b5497799192167dadb
Jason Aden <jasonaden@google.com>
2018-10-05 21:47:09 +0000

    5c5164b docs: typo fixes (#26247)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.9+60.sha-b8a081a
+ * @license Angular v6.1.9+61.sha-5c5164b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __spread, __metadata, __read, __param, __extends } from 'tslib';
+import { __decorate, __param, __metadata, __spread, __read, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
