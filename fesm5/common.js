aa4f99151b14e2557eedef7d3e63a15ad7d2b891
Jason Aden <jasonaden@google.com>
2018-10-05 21:09:43 +0000

    51dfdd5 fix(ivy): sync view with blueprint when necessary (#26263)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0-rc.0+45.sha-35bf952
+ * @license Angular v7.0.0-rc.0+47.sha-51dfdd5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, Injector, NgModuleFactory, NgModuleRef, Type, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __values, __read, __decorate, __metadata, __extends, __param, __assign } from 'tslib';
+import { __values, __read, __decorate, __extends, __param, __metadata, __assign } from 'tslib';
 
 /**
  * @license
@@ -5627,7 +5627,7 @@ function isPlatformWorkerUi(platformId) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-rc.0+45.sha-35bf952');
+var VERSION = new Version('7.0.0-rc.0+47.sha-51dfdd5');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.0+45.sha-35bf952
+ * @license Angular v7.0.0-rc.0+47.sha-51dfdd5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
