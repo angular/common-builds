7f3c773ac542f925e83171c83e830255db5effe7
Miško Hevery <misko@hevery.com>
2018-10-11 21:57:44 +0000

    50de03a docs: fix transpiles link in dependency injection (#26250)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.10+4.sha-f1e14a3
+ * @license Angular v6.1.10+3.sha-50de03a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __spread, __param, __metadata, __read, __extends } from 'tslib';
+import { __decorate, __spread, __metadata, __read, __param, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.10+4.sha-f1e14a3
+ * @license Angular v6.1.10+3.sha-50de03a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
