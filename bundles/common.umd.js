e61369aa4029b0d26283072d8441f3fa7244c7f5
Miško Hevery <misko@hevery.com>
2018-10-11 21:50:51 +0000

    bd186c7 docs: move angularjs and add performance to nav (#26375)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+17.sha-1657c99
+ * @license Angular v7.0.0-rc.1+18.sha-bd186c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5347,7 +5347,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('7.0.0-rc.1+17.sha-1657c99');
+    var VERSION = new i0.Version('7.0.0-rc.1+18.sha-bd186c7');
 
     /**
      * @license
