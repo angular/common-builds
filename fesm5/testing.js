26b6388a81c925aefdb36ec6725177455f58969f
Miško Hevery <misko@hevery.com>
2018-10-12 16:38:46 +0000

    ea20ae6 build(docs-infra): only show name in 'inherited from' section (#26387)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+32.sha-7777a99
+ * @license Angular v7.0.0-rc.1+33.sha-ea20ae6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
