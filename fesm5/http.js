4ceda9bd4a7da0ff4d7e31e4948c9d47a86abd66
Miško Hevery <misko@hevery.com>
2018-10-14 05:06:01 +0000

    1f06b6c build: upgrade `@types/jasminewd2` to 2.0.4 (#26432)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.10+10.sha-6790709
+ * @license Angular v6.1.10+11.sha-1f06b6c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __read, __spread, __param, __extends } from 'tslib';
+import { __decorate, __spread, __metadata, __read, __param, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
