c168ad4db29eae8bb4c7a163b5a7bb5c5cf0b319
Miško Hevery <misko@hevery.com>
2018-10-14 05:04:49 +0000

    1f06b6c build: upgrade `@types/jasminewd2` to 2.0.4 (#26432)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.10+10.sha-6790709
+ * @license Angular v6.1.10+11.sha-1f06b6c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __extends, __spread, __read } from 'tslib';
+import { __decorate, __param, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.10+10.sha-6790709
+ * @license Angular v6.1.10+11.sha-1f06b6c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
