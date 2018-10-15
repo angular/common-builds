9e475d120144b6a9e2df0e8f1fa25702c33b4c86
Miško Hevery <misko@hevery.com>
2018-10-15 18:58:26 +0000

    5183bbf docs: update process for cli tool and restructure doc (#25752)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+42.sha-9afc9a7
+ * @license Angular v7.0.0-rc.1+44.sha-5183bbf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
