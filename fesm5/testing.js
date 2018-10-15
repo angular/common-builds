4d88bb349c22ea145500cc23cfd7bebfc969fcce
Miško Hevery <misko@hevery.com>
2018-10-15 18:59:43 +0000

    2b00c17 docs: minor wording correction. "use" to "user". (#26452) PR Close #26452

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.10+12.sha-81724f5
+ * @license Angular v6.1.10+13.sha-2b00c17
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
