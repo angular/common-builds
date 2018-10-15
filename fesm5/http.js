1423dcd87ee736616c29b80bde830cc4596a13ee
Miško Hevery <misko@hevery.com>
2018-10-15 19:01:03 +0000

    2b00c17 docs: minor wording correction. "use" to "user". (#26452) PR Close #26452

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.10+12.sha-81724f5
+ * @license Angular v6.1.10+13.sha-2b00c17
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __spread, __metadata, __param, __read, __extends } from 'tslib';
+import { __decorate, __spread, __metadata, __read, __param, __extends } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
