54f98430dfed303f23e14e6087ef145d8f38c38c
Miško Hevery <misko@hevery.com>
2018-10-16 00:20:15 +0000

    eeebe62 docs: Indicate that PRs should have an associated issue (#25436) PR Close #25436

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.10+17.sha-05f279d
+ * @license Angular v6.1.10+18.sha-eeebe62
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
