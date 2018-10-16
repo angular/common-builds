2a276a3ac65cab23ced33c60ed0ea2cd39efec0d
Miško Hevery <misko@hevery.com>
2018-10-16 22:12:30 +0000

    2326b9c fix(service-worker): clean up caches from old SW versions (#26319)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+54.sha-41de0e0
+ * @license Angular v7.0.0-rc.1+55.sha-2326b9c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
