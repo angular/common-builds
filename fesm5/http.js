1029d358a0e3dcb05726d587d4c1f483deff5508
Misko Hevery <misko@hevery.com>
2018-10-17 19:20:00 +0000

    4b52257 fix(docs-infra): fix top menu logo position (#26473)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+58.sha-34aff0b
+ * @license Angular v7.0.0-rc.1+60.sha-4b52257
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __spread, __read, __extends } from 'tslib';
+import { __decorate, __spread, __read, __extends, __param, __metadata } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+58.sha-34aff0b
+ * @license Angular v7.0.0-rc.1+60.sha-4b52257
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
