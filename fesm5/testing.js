7aa7426e57aa3d9f8e366448987fd68c9ed88e25
Misko Hevery <misko@hevery.com>
2018-10-17 19:31:10 +0000

    a43b80a ci: add triage PRs config for the ngbot (#26477)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+84.sha-65b2093
+ * @license Angular v7.0.0-rc.1+76.sha-a43b80a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
