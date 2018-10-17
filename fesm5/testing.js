271b65d07b7305dc3bfe39c26f9c959b5e9f1689
Miško Hevery <misko@hevery.com>
2018-10-17 20:41:57 +0000

    b49b274 fix(aio): add relative to app level routes section (#26504)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+72.sha-9997ab5
+ * @license Angular v7.0.0-rc.1+74.sha-b49b274
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
