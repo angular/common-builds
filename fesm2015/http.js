85b7a4779c159f6618a1cfe44cef46f47ed8a5ab
Igor Minar <igor@angular.io>
2018-10-18 20:04:26 +0000

    bbf96db feat(docs-infra): add v6 to the aio version picker

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+99.sha-72c2578
+ * @license Angular v7.0.0-rc.1+100.sha-bbf96db
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __param } from 'tslib';
+import { __decorate, __param, __metadata } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
