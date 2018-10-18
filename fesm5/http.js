9006868f2433ad106f14218a6552a66f1edaf8f1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:42:11 +0000

    624433c docs: removing errant double-quote (#26483) PR Close #26483

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+93.sha-09cc458
+ * @license Angular v7.0.0-rc.1+94.sha-624433c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __extends, __spread, __read } from 'tslib';
+import { __decorate, __param, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+93.sha-09cc458
+ * @license Angular v7.0.0-rc.1+94.sha-624433c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
