812fe25b16c425998c975acb6b86d594730372e0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:44:24 +0000

    8d28fe9 docs: update http to https where possible (#26509)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0+1.sha-a403c4f
+ * @license Angular v7.0.0+4.sha-8d28fe9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
