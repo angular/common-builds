dc49892df2d0c7baa72f74355ff0f278bb3c682c
Miško Hevery <misko@hevery.com>
2018-10-18 19:46:49 +0000

    72c2578 docs: fix incomplete sentence in CHANGELOG.md

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+97.sha-7634c1c
+ * @license Angular v7.0.0-rc.1+99.sha-72c2578
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __read, __extends } from 'tslib';
+import { __spread, __extends, __read } from 'tslib';
 import { InjectionToken, defineInjectable, inject, PLATFORM_ID, INJECTOR, ɵdefineNgModule, defineInjector } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
