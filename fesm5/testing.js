c09a0529b92804f07d73e65b059d92057bef3637
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:45:17 +0000

    7634c1c docs: fix links to browserlist (#26531)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+98.sha-1a666de
+ * @license Angular v7.0.0-rc.1+97.sha-7634c1c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
