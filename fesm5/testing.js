654a0cc7b7c9762ab07b70ac72766ce8faa68a68
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:44:40 +0000

    32c018c docs: removing errant double-quote (#26483) PR Close #26483

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0+4.sha-8d28fe9
+ * @license Angular v7.0.0+2.sha-32c018c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
