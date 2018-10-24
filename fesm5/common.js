c02373029c3a9172cdaaeda9387b8f5e5ac78511
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-24 22:40:41 +0000

    67789f1 release: cut the v7.1.0-beta.0 release

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0-rc.1+194.sha-6e16a17
+ * @license Angular v7.1.0-beta.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, Injector, NgModuleFactory, NgModuleRef, Type, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __decorate, __metadata, __param, __read, __values, __extends, __assign } from 'tslib';
+import { __decorate, __metadata, __read, __param, __values, __extends, __assign } from 'tslib';
 
 /**
  * @license
@@ -5659,7 +5659,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.0-rc.1+194.sha-6e16a17');
+var VERSION = new Version('7.1.0-beta.0');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+194.sha-6e16a17
+ * @license Angular v7.1.0-beta.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
