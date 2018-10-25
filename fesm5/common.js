d860132275cfa98d627f98b03b97cb2fee11e7ee
Matias Niemelä <matias@yearofmoo.com>
2018-10-25 00:11:35 +0000

    0238744 test(upgrade): make e2e tests for upgrade docs examples less flaky (#26726)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0+87.sha-062af7a.with-local-changes
+ * @license Angular v7.0.1+4.sha-0238744.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, Injector, NgModuleFactory, NgModuleRef, Type, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __decorate, __metadata, __param, __read, __values, __extends, __assign } from 'tslib';
+import { __decorate, __metadata, __read, __param, __values, __extends, __assign } from 'tslib';
 
 /**
  * @license
@@ -5659,7 +5659,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.0+87.sha-062af7a.with-local-changes');
+var VERSION = new Version('7.0.1+4.sha-0238744.with-local-changes');
 
 /**
  * @license
