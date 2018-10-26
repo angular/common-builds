a1d19289cdeff644b9ea159bf5757798860c58c0
Matias Niemelä <matias@yearofmoo.com>
2018-10-26 21:42:17 +0000

    c048358 build: fix the golden files for hello_world_r2

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.0+30.sha-f385913
+ * @license Angular v7.1.0-beta.0+34.sha-c048358
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
