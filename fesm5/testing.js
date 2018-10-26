c5e2ed971827bed678475c7528c785290f5dfe0e
Matias Niemelä <matias@yearofmoo.com>
2018-10-26 19:08:20 +0000

    f385913 ci: configure chrome options for protractor (#26735)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.0+23.sha-9cd8327
+ * @license Angular v7.1.0-beta.0+30.sha-f385913
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
