e7d1beb84e4386628b4b0ed7d78ffdf284168784
Matias Niemelä <matias@yearofmoo.com>
2018-10-29 17:31:35 +0000

    c0bf222 ci: exclude ngcc from g3sync check (#26740)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.0+54.sha-d72d50d
+ * @license Angular v7.1.0-beta.0+55.sha-c0bf222
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
