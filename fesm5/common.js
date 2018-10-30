4a124ab24aaa5e47bdc1e7dce9384628361753da
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 22:59:26 +0000

    332394d build(bazel): generalize fix for AMD name for flat module out file (#26837)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.0+73.sha-62b4ff52
+ * @license Angular v7.1.0-beta.0+74.sha-332394d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { InjectionToken, EventEmitter, Version, defineInjectable, inject, ComponentFactoryResolver, NgModuleRef, ViewContainerRef, TemplateRef, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, KeyValueDiffers, ElementRef, IterableDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, LOCALE_ID, isDevMode, ɵdefineNgModule, defineInjector, ɵdefinePipe, ɵdirectiveInject, ɵdefineDirective, ɵNgOnChangesFeature, ɵinjectAttribute } from '@angular/core';
+import { InjectionToken, EventEmitter, Version, defineInjectable, inject, ComponentFactoryResolver, NgModuleRef, ViewContainerRef, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, TemplateRef, KeyValueDiffers, ElementRef, IterableDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, LOCALE_ID, isDevMode, ɵdefineNgModule, defineInjector, ɵdefinePipe, ɵdirectiveInject, ɵdefineDirective, ɵNgOnChangesFeature, ɵinjectAttribute } from '@angular/core';
 import { __values, __read, __extends, __assign } from 'tslib';
 
 /**
@@ -5381,7 +5381,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+73.sha-62b4ff52');
+var VERSION = new Version('7.1.0-beta.0+74.sha-332394d');
 
 /**
  * @license
