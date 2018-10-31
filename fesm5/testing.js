8cf390c8e4b6cd43dda3d424d2a8df30119468e5
Matias Niemelä <matias@yearofmoo.com>
2018-10-31 19:44:30 +0000

    d042c4a fix(core): Remove static dependency from @angular/core to @angular/compiler (#26734) PR Close #26734

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.0+74.sha-332394d
+ * @license Angular v7.1.0-beta.0+77.sha-d042c4a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
