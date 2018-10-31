8744541bb6c6a5a68f6d6bdc738ef708717248db
Matias Niemelä <matias@yearofmoo.com>
2018-10-31 23:50:43 +0000

    2c25d29 docs: release notes for the v7.0.2 release

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.0+78.sha-ec29fd3
+ * @license Angular v7.1.0-beta.0+79.sha-2c25d29
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
