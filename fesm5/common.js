76e8caebb4771f180fb129757e8530c5b84aa775
Kara Erickson <karakara@google.com>
2018-11-01 23:22:43 +0000

    dc05385 fix(router): remove type bludgeoning of context and outlet when running CanDeactivate (#26496)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.2+7.sha-5eb540c
+ * @license Angular v7.0.2+9.sha-dc05385
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5659,7 +5659,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.2+7.sha-5eb540c');
+var VERSION = new Version('7.0.2+9.sha-dc05385');
 
 /**
  * @license
