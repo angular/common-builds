126f978a2ba5aa5298befa4b4abe328c9996dfd5
Kara Erickson <karakara@google.com>
2018-11-01 21:04:50 +0000

    a2929df fix(ivy): dynamically created components should run init hooks (#26864)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.1.0-beta.1+15.sha-aadbc8a
+ * @license Angular v7.1.0-beta.1+17.sha-a2929df
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, Injector, NgModuleFactory, NgModuleRef, Type, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __decorate, __metadata, __read, __param, __values, __extends, __assign } from 'tslib';
+import { __decorate, __metadata, __param, __read, __values, __extends, __assign } from 'tslib';
 
 /**
  * @license
@@ -5659,7 +5659,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+15.sha-aadbc8a');
+var VERSION = new Version('7.1.0-beta.1+17.sha-a2929df');
 
 /**
  * @license
