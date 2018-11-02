fbad1e33e6ca64caa62b9304eb6128c95d7cfac2
Kara Erickson <karakara@google.com>
2018-11-02 18:46:39 +0000

    2d06693 docs: edit file structure page (#26552)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.2+17.sha-5c5b62c
+ * @license Angular v7.0.2+19.sha-2d06693
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5659,7 +5659,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.2+17.sha-5c5b62c');
+var VERSION = new Version('7.0.2+19.sha-2d06693');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.2+17.sha-5c5b62c
+ * @license Angular v7.0.2+19.sha-2d06693
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
