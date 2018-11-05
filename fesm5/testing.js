5c849514a574f060a0f67214b030a442ff1544ec
Kara Erickson <karakara@google.com>
2018-11-05 21:17:18 +0000

    6744b19 refactor(compiler): typo (#25496)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.1+72.sha-f9a4abb
+ * @license Angular v7.1.0-beta.1+73.sha-6744b19
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
