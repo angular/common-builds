7045ff202d43573145c832809e70f835c01cb20f
Kara Erickson <karakara@google.com>
2018-11-05 18:16:21 +0000

    86d41a1 docs: new GitHub Issue templates (#26918)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.1+67.sha-e6a0c45
+ * @license Angular v7.1.0-beta.1+68.sha-86d41a1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
