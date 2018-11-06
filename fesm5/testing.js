c1fae76d0920055239c7065085db65e0e0996a29
Kara Erickson <karakara@google.com>
2018-11-06 20:10:28 +0000

    297c54e fix(ivy): support env where requestAnimationFrame is not available (#26779)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.1+78.sha-dc2464e
+ * @license Angular v7.1.0-beta.1+79.sha-297c54e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
