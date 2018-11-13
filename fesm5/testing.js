82b0e46829416986fa8a9892f7aa11ef7f2dec04
Andrew Kushnir <akushnir@google.com>
2018-11-13 19:21:58 +0000

    efa443b docs: fix typo (#26878) PR Close #26878

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.2+33.sha-6b1780d
+ * @license Angular v7.1.0-beta.2+31.sha-efa443b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
