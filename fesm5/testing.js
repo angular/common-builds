686400d94b6fc5b340b8535b1e2812c454bafb1a
Andrew Kushnir <akushnir@google.com>
2018-11-14 22:43:50 +0000

    0e95a20 release: cut the v7.0.4 release

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.3+25.sha-9c7428e
+ * @license Angular v7.0.4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
