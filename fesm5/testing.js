6fcfdc2f98a3f828478d2be135423641404f4903
Andrew Kushnir <akushnir@google.com>
2018-11-14 20:56:47 +0000

    bf6ac6c feat(router): add pathParamsChange mode for runGuardsAndResolvers (#26861)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.2+51.sha-ce52424
+ * @license Angular v7.1.0-beta.2+48.sha-bf6ac6c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
