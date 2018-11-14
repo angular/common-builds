8e6a9de321c63c546e7b3bdd1b1558facf63e74d
Andrew Kushnir <akushnir@google.com>
2018-11-14 20:56:28 +0000

    3da8233 build(bazel): turn on --nolegacy-external-runfiles (#26770)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.2+46.sha-f8f1168
+ * @license Angular v7.1.0-beta.2+47.sha-3da8233
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
