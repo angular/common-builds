5e2335fade0e2f0ccc7d20e9eeab631e5274ad8e
Misko Hevery <misko@hevery.com>
2018-11-16 20:50:31 +0000

    a4934a7 docs: fix code examples style (#26573)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-rc.0+10.sha-91bffa9
+ * @license Angular v7.1.0-rc.0+11.sha-a4934a7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
