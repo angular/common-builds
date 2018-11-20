d5e70752b0cb312d47c82925111f98f29fa8884b
Misko Hevery <misko@hevery.com>
2018-11-20 20:07:50 +0000

    e56c8bf fix(ivy): align discovery methods for consistency (#27117)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-rc.0+28.sha-ca40565.with-local-changes
+ * @license Angular v7.1.0-rc.0+29.sha-e56c8bf.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __read, __extends } from 'tslib';
+import { __spread, __extends, __read } from 'tslib';
 import { Injectable, InjectionToken, Injector, NgModule, defineInjectable, ɵsetClassMetadata, inject, PLATFORM_ID, Inject, ɵdefineNgModule, defineInjector } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
