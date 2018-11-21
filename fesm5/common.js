19ecd5479fb9ef8258047ed52fbcd4c81697c6ef
Misko Hevery <misko@hevery.com>
2018-11-21 23:55:17 +0000

    dc300c5 feat(ivy): render flags support in host bindings function (FW-649) (#27204)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0
+ * @license Angular v7.1.0+1.sha-dc300c5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5713,7 +5713,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0');
+var VERSION = new Version('7.1.0+1.sha-dc300c5');
 
 /**
  * @license
