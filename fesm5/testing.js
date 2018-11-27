dea55aea9a15eb131a3c69af506cd97b97f25338
Jason Aden <jasonaden@google.com>
2018-11-27 22:10:41 +0000

    39e426c fix(ivy): TestBed.get(Compiler) throws "Error: Runtime compiler is not loaded" (#27223)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0+20.sha-d0e8020
+ * @license Angular v7.1.0+34.sha-39e426c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
