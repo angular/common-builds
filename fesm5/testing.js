2482b3f58bbe83320f1bad52435ef714ef1cab74
Jason Aden <jasonaden@google.com>
2018-11-29 23:06:55 +0000

    11a8bd8 fix(router): update URL after redirects when urlHandlingStrategy='eager' (#27356)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0+44.sha-57dae16
+ * @license Angular v7.1.0+45.sha-11a8bd8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
