64f30643d14de500122e6f0bda90e843f2b0f59c
Igor Minar <igor@angular.io>
2018-11-30 06:15:54 +0000

    a149c3a docs(forms): update API reference for common APIs and template-driven directives (#27033)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.1+8.sha-013c0ae
+ * @license Angular v7.1.1+9.sha-a149c3a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
