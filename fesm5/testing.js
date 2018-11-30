c33c68fb92cbaeae5f3248d40f44a2bffe089595
Igor Minar <igor@angular.io>
2018-11-30 06:08:28 +0000

    56254b5 build: add kara to approvers for packages needing ivy TestBed updates (#27337)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.1+5.sha-5d0e114
+ * @license Angular v7.1.1+6.sha-56254b5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
