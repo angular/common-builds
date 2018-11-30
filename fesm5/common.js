263ebeeba95eef27d15c75228321336126e09d9b
Igor Minar <igor@angular.io>
2018-11-30 22:03:49 +0000

    fc2c23e fix(bazel): ng_package should correctly map to source maps in secondary entry-points (#27313)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.1.1+12.sha-184d63d
+ * @license Angular v7.1.1+14.sha-fc2c23e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, Injector, NgModuleFactory, NgModuleRef, Type, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __decorate, __metadata, __param, __values, __read, __extends, __assign } from 'tslib';
+import { __decorate, __metadata, __param, __read, __values, __extends, __assign } from 'tslib';
 
 /**
  * @license
@@ -5659,7 +5659,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.1+12.sha-184d63d');
+var VERSION = new Version('7.1.1+14.sha-fc2c23e');
 
 /**
  * @license
--- fesm5/http/testing.js
+++ fesm5/http/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+12.sha-184d63d
+ * @license Angular v7.1.1+14.sha-fc2c23e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -398,4 +398,4 @@ var HttpClientTestingModule = /** @class */ (function () {
  */
 
 export { HttpClientTestingBackend as ɵangular_packages_common_http_testing_testing_a, HttpTestingController, HttpClientTestingModule, TestRequest };
-//# sourceMappingURL=http__testing.js.map
+//# sourceMappingURL=testing.js.map
