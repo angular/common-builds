0471e6eb410bddf5a9f7cf4f608ce67c7fabb8d4
Igor Minar <igor@angular.io>
2018-12-01 00:30:50 +0000

    f5f323d build: only stamp version info when releasing (#27362)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0+101.sha-35e02ad
+ * @license Angular v7.1.0+102.sha-f5f323d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __extends, __spread, __read } from 'tslib';
+import { __decorate, __param, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
