9ad803d416528d139e28e083e64d87ff7639450a
Igor Minar <igor@angular.io>
2018-12-03 16:51:35 +0000

    d847051 test(common): add PercentPipe round and trim tests (#27365)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0+103.sha-a1470c9
+ * @license Angular v7.1.0+105.sha-d847051
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
