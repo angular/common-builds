50f3b4eb5efe20798ddf35be06545198195a105b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 23:05:58 +0000

    7ec05b4 fix(ivy): run initialializers in TestBed on init (#27355)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0+110.sha-2a39425
+ * @license Angular v7.1.0+112.sha-7ec05b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
