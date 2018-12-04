74bfa69c0cb2ff466ebf02909ae046ac7ed90802
Igor Minar <igor@angular.io>
2018-12-04 22:38:42 +0000

    1597886 fix(ivy): resolve resources using TS module resolution semantics (#27357)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0+128.sha-8e644d9
+ * @license Angular v7.1.0+127.sha-1597886
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
