d6144f20a7619e52b36296ae74bde725cac38f0b
Igor Minar <igor@angular.io>
2018-12-05 19:08:13 +0000

    2bc3986 feat(ivy): support inputs & outputs with aliases in component decorators (#27350) PR Close #27350

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0+153.sha-20cef50
+ * @license Angular v7.1.0+155.sha-2bc3986
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __extends, __spread, __read } from 'tslib';
+import { __decorate, __param, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
