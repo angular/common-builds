0f3ea1b6ec082c7c2125871e13e8f75cf4817726
Igor Minar <igor@angular.io>
2018-12-05 21:47:33 +0000

    a97fbfd refactor(docs-infra): remove unused custom element (#27250)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.1+31.sha-a655d28
+ * @license Angular v7.1.1+43.sha-a97fbfd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __extends, __spread, __read } from 'tslib';
+import { __decorate, __param, __metadata, __extends, __read, __spread } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
