64b088bd932cdb8ad6703a878d9d34ac1827e9f8
Igor Minar <igor@angular.io>
2018-12-06 05:15:07 +0000

    d7c72fc build: remove obsolete gulp build task (#27386)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.1+44.sha-0508143
+ * @license Angular v7.1.1+53.sha-d7c72fc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
