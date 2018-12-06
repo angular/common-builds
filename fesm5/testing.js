6196fd0b63c1a6e19c3982ccc7a8f10f848579bb
Igor Minar <igor@angular.io>
2018-12-06 18:02:58 +0000

    d63d53f ci: test ts-api-guardian on windows (#27205)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0+199.sha-7c52d10
+ * @license Angular v7.1.0+200.sha-d63d53f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
