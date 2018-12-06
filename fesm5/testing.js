818ae289cbe6e3c903c3c95a36fe67e503a4fd08
Igor Minar <igor@angular.io>
2018-12-06 17:40:26 +0000

    091a504 Revert "fix(router): change processing url tree children condition (#26243)" (#27516)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0+194.sha-6740d0d
+ * @license Angular v7.1.0+196.sha-091a504
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
