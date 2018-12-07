e0ced02ca97b4559223e7cf7fa9bf5bcdbf62c85
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 17:46:40 +0000

    aa48810 feat(ivy): generate flat module index files (#27497)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.2.0-beta.1+1.sha-6bd28e6
+ * @license Angular v7.2.0-beta.1+3.sha-aa48810
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, Injector, NgModuleFactory, NgModuleRef, Type, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __decorate, __metadata, __read, __param, __values, __extends, __assign } from 'tslib';
+import { __decorate, __metadata, __param, __read, __values, __extends, __assign } from 'tslib';
 
 /**
  * @license
@@ -5659,7 +5659,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.1+1.sha-6bd28e6');
+var VERSION = new Version('7.2.0-beta.1+3.sha-aa48810');
 
 /**
  * @license
