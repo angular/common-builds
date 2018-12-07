3c2f16e14edd703592ebb6376a812610fae204e8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 18:02:41 +0000

    055dc2d docs: file new issue needs to have opener choose type from template (#27400)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-beta.1+11.sha-2a35471
+ * @license Angular v7.2.0-beta.1+10.sha-055dc2d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
