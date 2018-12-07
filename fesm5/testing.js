93baf595b60d7e317e5d82843946317aee006acb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 17:58:42 +0000

    6316051 build(docs-infra): upgrade cli command docs sources to 5eef7122c (#27539)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-beta.1+3.sha-aa48810
+ * @license Angular v7.2.0-beta.1+5.sha-6316051
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
