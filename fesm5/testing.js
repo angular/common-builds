b110e5e3a7b75bd9f90dd432a3e7c5885631004f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 19:15:37 +0000

    a9f39a4 fix(bazel): tsickle dependency not working with typescript 3.1.x (#27402)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.2+1.sha-90c273a
+ * @license Angular v7.1.2+5.sha-a9f39a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
