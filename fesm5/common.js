dd7a3dc07bc4899d33aeea561d5e700379c1c3b1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 20:19:02 +0000

    255e672 fix(ivy): define correct SVG namespace (FW-757) (#27529)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.2.0-beta.1+13.sha-f034114
+ * @license Angular v7.2.0-beta.1+15.sha-255e672
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, Injector, NgModuleFactory, NgModuleRef, Type, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __decorate, __metadata, __param, __values, __read, __extends, __assign } from 'tslib';
+import { __decorate, __metadata, __param, __read, __values, __extends, __assign } from 'tslib';
 
 /**
  * @license
@@ -5659,7 +5659,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.1+13.sha-f034114');
+var VERSION = new Version('7.2.0-beta.1+15.sha-255e672');
 
 /**
  * @license
