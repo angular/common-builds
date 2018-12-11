56d3751d272a3a752947a66da2a0cd875a9c03a2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-11 19:55:53 +0000

    ceb14de fix(ivy): proper slot allocation for pure functions in `hostBindings` function (#27587)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-beta.1+27.sha-fb6a31c
+ * @license Angular v7.2.0-beta.1+29.sha-ceb14de
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
