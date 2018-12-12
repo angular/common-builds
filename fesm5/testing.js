9840f5bcedd7523417fa7e9915b9fb6a20adab55
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-12 19:20:32 +0000

    348c949 fix(docs-infra): fix top menu item clickable area (#27620)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.3+7.sha-8081854
+ * @license Angular v7.1.3+6.sha-348c949
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
