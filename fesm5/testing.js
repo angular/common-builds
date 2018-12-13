0dc47ff73afe43ed2b8aefd2870099caa0920165
Miško Hevery <misko@hevery.com>
2018-12-13 19:39:42 +0000

    52fd4a2 build: disable postinstall-patch.js log output (#27619)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.3+14.sha-8c32a2e
+ * @license Angular v7.1.3+15.sha-52fd4a2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
