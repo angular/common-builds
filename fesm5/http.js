094587ecce7c868244d61d49aa5cd8e24444640d
Miško Hevery <misko@hevery.com>
2018-12-15 00:04:46 +0000

    023bd31 fix(ivy): ngcc should not emit TypeScript syntax (#27051)

--- fesm5/http.js
+++ fesm5/http.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-beta.2+46.sha-df123e0
+ * @license Angular v7.2.0-beta.2+45.sha-023bd31
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __param, __extends, __spread, __read } from 'tslib';
+import { __decorate, __param, __metadata, __extends, __spread, __read } from 'tslib';
 import { Injectable, InjectionToken, Inject, PLATFORM_ID, Injector, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { concatMap, filter, map } from 'rxjs/operators';
