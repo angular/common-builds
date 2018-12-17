7cd193d0ffad246e7fadfe58bde3748d5b4f2f21
Miško Hevery <misko@hevery.com>
2018-12-17 18:15:34 +0000

    94f17e9 docs: remove misplaced code in Universal guide (#26865)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-beta.2+53.sha-ea10a3a
+ * @license Angular v7.2.0-beta.2+62.sha-94f17e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
