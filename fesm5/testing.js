5a784571e90c67f8818f42da00d974dff6e9e2ea
Miško Hevery <misko@hevery.com>
2018-12-17 18:04:49 +0000

    ce4cff6 docs: Add Smart UI to UI Components section in resources (#27515)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.3+30.sha-78bd3c7
+ * @license Angular v7.1.3+31.sha-ce4cff6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
