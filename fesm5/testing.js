27aa8b02b33827976ad3041ed1d6bec945418f58
Miško Hevery <misko@hevery.com>
2018-12-18 18:02:04 +0000

    8d0a3ce build: use https link to editorconfig.org in .editorconfig (#27664)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-beta.2+76.sha-f57916c
+ * @license Angular v7.2.0-beta.2+80.sha-8d0a3ce
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
