72600db1c64850b60d688d25fb4c730e3c7fe52b
Miško Hevery <misko@hevery.com>
2018-12-18 22:21:30 +0000

    12c3176 feat(router): add predicate function  mode for runGuardsAndResolvers (#27682)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+90.sha-7901cd8
+ * @license Angular v7.2.0-beta.2+91.sha-12c3176
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5660,7 +5660,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+90.sha-7901cd8');
+var VERSION = new Version('7.2.0-beta.2+91.sha-12c3176');
 
 /**
  * @license
