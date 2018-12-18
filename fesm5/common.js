c1b108c5493985c60447130e61dfc3cb26a9b2bc
Miško Hevery <misko@hevery.com>
2018-12-18 18:41:42 +0000

    4774a1a docs(bazel): Update VSCode config (#27733)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.2.0-beta.2+80.sha-8d0a3ce
+ * @license Angular v7.2.0-beta.2+81.sha-4774a1a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, Injector, NgModuleFactory, NgModuleRef, Type, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __decorate, __metadata, __param, __read, __values, __extends, __assign } from 'tslib';
+import { __decorate, __metadata, __read, __param, __values, __extends, __assign } from 'tslib';
 
 /**
  * @license
@@ -5660,7 +5660,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+80.sha-8d0a3ce');
+var VERSION = new Version('7.2.0-beta.2+81.sha-4774a1a');
 
 /**
  * @license
