11a43e90d34aeb4759f25d201091b907c7e46964
Miško Hevery <misko@hevery.com>
2018-12-18 01:12:52 +0000

    be08611 docs(forms): update API reference for validator directives (#26926)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.3+33.sha-183f278
+ * @license Angular v7.1.3+35.sha-be08611
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
