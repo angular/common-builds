f4b4221061831c612b3ab5cb78d5281a62f9fb73
Miško Hevery <misko@hevery.com>
2018-12-18 21:47:31 +0000

    7901cd8 fix(ivy): add missing AttributeMarker export (#27485) PR Close #27485

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.2.0-beta.2+89.sha-69198ba
+ * @license Angular v7.2.0-beta.2+90.sha-7901cd8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, Injector, NgModuleFactory, NgModuleRef, Type, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __decorate, __metadata, __read, __param, __values, __extends, __assign } from 'tslib';
+import { __decorate, __metadata, __param, __read, __values, __extends, __assign } from 'tslib';
 
 /**
  * @license
@@ -5660,7 +5660,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+89.sha-69198ba');
+var VERSION = new Version('7.2.0-beta.2+90.sha-7901cd8');
 
 /**
  * @license
