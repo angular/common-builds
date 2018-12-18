017fd37e2c7893e776440cde89e81be5bf0cf925
Miško Hevery <misko@hevery.com>
2018-12-18 18:00:49 +0000

    8fdd702 docs: Fixed typo (#27669) PR Close #27669

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-beta.2+77.sha-d766ad0
+ * @license Angular v7.2.0-beta.2+79.sha-8fdd702
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
