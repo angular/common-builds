96ea9581fe69352c9eaf232eb7e63b6910f09c96
Miško Hevery <misko@hevery.com>
2018-12-18 05:16:03 +0000

    e98c57a refactor(core): rename spec file to match case of src file

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.2.0-beta.2+74.sha-4bf8d64
+ * @license Angular v7.2.0-beta.2+75.sha-e98c57a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, Injector, NgModuleFactory, NgModuleRef, Type, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __decorate, __metadata, __param, __read, __values, __extends, __assign } from 'tslib';
+import { __decorate, __metadata, __read, __param, __values, __extends, __assign } from 'tslib';
 
 /**
  * @license
@@ -5660,7 +5660,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+74.sha-4bf8d64');
+var VERSION = new Version('7.2.0-beta.2+75.sha-e98c57a');
 
 /**
  * @license
