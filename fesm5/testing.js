cd680a8a510cb43e2a0be11e90a21f8b9960b193
Matias Niemelä <matias@yearofmoo.com>
2018-12-19 20:43:09 +0000

    8f8572f fix(ivy): @Host should behave as in View Engine (#27646)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-rc.0+2.sha-e8f7241
+ * @license Angular v7.2.0-rc.0+3.sha-8f8572f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
