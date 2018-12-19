ea216f5183d3c95ac6e1fcf21aaef8cad147e655
Igor Minar <igor@angular.io>
2018-12-19 18:24:25 +0000

    01a5c74 turns out that github silently converts '/ in team names to '-'

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+77.sha-4d71c4c
+ * @license Angular v7.2.0-beta.2+78.sha-01a5c74
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5660,7 +5660,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+77.sha-4d71c4c');
+var VERSION = new Version('7.2.0-beta.2+78.sha-01a5c74');
 
 /**
  * @license
