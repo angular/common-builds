2b7a2d2dca1ec2bd45266f5ff61ff75c63fc4980
Matias Niemelä <matias@yearofmoo.com>
2018-12-20 21:52:52 +0000

    50a91ba docs(upgrade): add gotchas/tips/example for multiple downgraded modules (#27217)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-rc.0+14.sha-07ada7f
+ * @license Angular v7.2.0-rc.0+19.sha-50a91ba
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
