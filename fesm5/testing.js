7b1510eb0047c93b8dfddd35a05df79bc9bbbbfa
Matias Niemelä <matias@yearofmoo.com>
2018-12-20 17:25:13 +0000

    4b70a4e feat(ivy): support NgModule metadata from calls that do not return ModuleWithProviders types (#27326)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-rc.0+6.sha-b08f3ac
+ * @license Angular v7.2.0-rc.0+10.sha-4b70a4e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
