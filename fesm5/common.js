563831596023be4d598e7b903381c15bc7793133
Matias Niemelä <matias@yearofmoo.com>
2018-12-21 23:13:05 +0000

    4b67b0a fix(ivy): adding TestBed.overrideProvider support (#27693)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+91.sha-12c3176
+ * @license Angular v7.2.0-rc.0+25.sha-4b67b0a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5660,7 +5660,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+91.sha-12c3176');
+var VERSION = new Version('7.2.0-rc.0+25.sha-4b67b0a');
 
 /**
  * @license
