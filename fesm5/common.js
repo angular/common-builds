df17f91fa759db40003500917da94e71d50e2a80
Matias Niemelä <matias@yearofmoo.com>
2018-12-26 20:15:51 +0000

    eea2b0f revert: fix(router): ensure URL is updated after second redirect with UrlUpdateStrategy="eager" (#27523)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+25.sha-4b67b0a
+ * @license Angular v7.2.0-rc.0+28.sha-eea2b0f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5660,7 +5660,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+25.sha-4b67b0a');
+var VERSION = new Version('7.2.0-rc.0+28.sha-eea2b0f');
 
 /**
  * @license
