af5bfd1349e3ddcfa181f2dc27c3842f0df4780f
Ben Lesh <ben@benlesh.com>
2019-01-03 18:20:37 +0000

    880b4aa fix(docs-infra): upgrade to latest dgeni-packages to fix linking problem (#27864)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-rc.0+45.sha-a979001
+ * @license Angular v7.2.0-rc.0+48.sha-880b4aa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
