b571a13ad88236778f8417afaa260f824351e872
Ben Lesh <ben@benlesh.com>
2019-01-03 18:17:31 +0000

    5638c1d docs: fix npm description in architecture modules guide (#27818)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+43.sha-a0a3648
+ * @license Angular v7.2.0-rc.0+41.sha-5638c1d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5660,7 +5660,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+43.sha-a0a3648');
+var VERSION = new Version('7.2.0-rc.0+41.sha-5638c1d');
 
 /**
  * @license
