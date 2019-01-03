45538d8d3fb3cc9b6b6b3c07f9063db4ad76b3d4
Ben Lesh <ben@benlesh.com>
2019-01-03 18:08:55 +0000

    679235a docs: improve description of bundle budgets (#27833)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+21.sha-fdd8b04
+ * @license Angular v7.2.0-rc.0+19.sha-679235a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5660,7 +5660,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+21.sha-fdd8b04');
+var VERSION = new Version('7.2.0-rc.0+19.sha-679235a');
 
 /**
  * @license
