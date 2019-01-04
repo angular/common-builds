3e545d3edfb0ad3ea1f4390b8946b283c1912786
Kara Erickson <karakara@google.com>
2019-01-04 20:53:51 +0000

    176b3f1 ci: make `integration_test` job logs less verbose (#27934)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.2.0-rc.0+56.sha-3f2ebbd
+ * @license Angular v7.2.0-rc.0+54.sha-176b3f1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, EventEmitter, Injectable, Inject, Optional, LOCALE_ID, Directive, ElementRef, Input, IterableDiffers, KeyValueDiffers, Renderer2, ɵisListLikeIterable, ɵstringify, ComponentFactoryResolver, Injector, NgModuleFactory, NgModuleRef, Type, ViewContainerRef, TemplateRef, isDevMode, Host, Attribute, Pipe, ChangeDetectorRef, WrappedValue, ɵisObservable, ɵisPromise, NgModule, Version, defineInjectable, inject } from '@angular/core';
-import { __decorate, __metadata, __param, __read, __values, __extends, __assign } from 'tslib';
+import { __decorate, __metadata, __read, __param, __values, __extends, __assign } from 'tslib';
 
 /**
  * @license
@@ -5660,7 +5660,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+56.sha-3f2ebbd');
+var VERSION = new Version('7.2.0-rc.0+54.sha-176b3f1');
 
 /**
  * @license
