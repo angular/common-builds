fc8cec129023c4421eee0714b8fcbd719df9e8db
Kara Erickson <karakara@google.com>
2019-01-04 22:38:08 +0000

    5d3dcfc fix(ivy): ensure @animation host bindings/listeners work properly (#27896)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+61.sha-0bd9deb
+ * @license Angular v7.2.0-rc.0+62.sha-5d3dcfc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5660,7 +5660,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+61.sha-0bd9deb');
+var VERSION = new Version('7.2.0-rc.0+62.sha-5d3dcfc');
 
 /**
  * @license
