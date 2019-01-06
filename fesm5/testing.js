5f077f994489436a8acb0a1e653a6bd677e407ad
Filipe Silva <filipematossilva@gmail.com>
2019-01-06 15:17:08 +0000

    1dd11c9 ci: add linux buildkite tests

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-rc.0+54.sha-b68bc8b
+ * @license Angular v7.2.0-rc.0+54.sha-1dd11c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
