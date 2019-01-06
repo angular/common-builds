7137f0d66dfeff48248846030e6b90f94e7c7acf
Kara Erickson <karakara@google.com>
2019-01-06 02:09:11 +0000

    7242551 ci: fix typos in codeowners file (#27945)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+30.sha-1ad88e0
+ * @license Angular v7.2.0-rc.0+31.sha-7242551
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5660,7 +5660,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+30.sha-1ad88e0');
+var VERSION = new Version('7.2.0-rc.0+31.sha-7242551');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-rc.0+30.sha-1ad88e0
+ * @license Angular v7.2.0-rc.0+31.sha-7242551
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
