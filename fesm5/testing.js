0aa79c77a1716ceff22d3efe911f574e5ebbb4a3
Filipe Silva <filipematossilva@gmail.com>
2019-01-06 15:22:45 +0000

    8d5ab90 ci: add a change

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-rc.0+54.sha-1dd11c9
+ * @license Angular v7.2.0-rc.0+55.sha-8d5ab90
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
