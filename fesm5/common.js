38720c88a07a57914abeb96fc7d53aaca09067f5
Kara Erickson <karakara@google.com>
2019-01-07 23:53:56 +0000

    c1dacdd fix(ivy): QueryList not instance of exported QueryList (#27942)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+14.sha-ac5f5ed
+ * @license Angular v7.2.0+17.sha-c1dacdd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5714,7 +5714,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+14.sha-ac5f5ed');
+var VERSION = new Version('7.2.0+17.sha-c1dacdd');
 
 /**
  * @license
