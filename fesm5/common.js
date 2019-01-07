ea6de4e318b34142bab7d6d3765ec2ce4f5ea6ba
Kara Erickson <karakara@google.com>
2019-01-07 23:16:57 +0000

    9e04284 fix(bazel): protractor utils cannot start server on windows (#27915)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+37.sha-216bbd0
+ * @license Angular v7.2.0-rc.0+38.sha-9e04284
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5660,7 +5660,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+37.sha-216bbd0');
+var VERSION = new Version('7.2.0-rc.0+38.sha-9e04284');
 
 /**
  * @license
