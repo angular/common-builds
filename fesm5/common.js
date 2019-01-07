1e05ec870b0a59d0cc9e20404ae15040c85c00ab
Kara Erickson <karakara@google.com>
2019-01-07 23:10:27 +0000

    e775313 fix(ivy): ngOnDestroy hooks should be called on providers (#27955)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+7.sha-4588053
+ * @license Angular v7.2.0+9.sha-e775313
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5714,7 +5714,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+7.sha-4588053');
+var VERSION = new Version('7.2.0+9.sha-e775313');
 
 /**
  * @license
