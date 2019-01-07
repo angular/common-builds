dd863e672c5224b7461d230b72243d78687a0bcb
Kara Erickson <karakara@google.com>
2019-01-07 23:18:30 +0000

    4613864 test(ivy): add i18n integration tests via TestBed (#27852)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+10.sha-a75c734
+ * @license Angular v7.2.0+15.sha-4613864
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5714,7 +5714,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+10.sha-a75c734');
+var VERSION = new Version('7.2.0+15.sha-4613864');
 
 /**
  * @license
