c08de9d574e2afcd1f93e4f6595077b78b984efe
Andrew Kushnir <akushnir@google.com>
2019-01-09 23:54:40 +0000

    11325ba test(ivy): mark jit_summaries_integration_spec as obsolete in Ivy (#28027)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0+79.sha-d0c3e25
+ * @license Angular v7.2.0+80.sha-11325ba
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
