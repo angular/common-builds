97149823034e654b96849ecb3940bf345461cd3e
Andrew Kushnir <akushnir@google.com>
2019-01-09 23:54:14 +0000

    d0c3e25 docs: remove deprecated `fromPromise` from RxJS guide (#28015)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0+78.sha-e1e4887
+ * @license Angular v7.2.0+79.sha-d0c3e25
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __metadata } from 'tslib';
+import { __extends, __decorate, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
