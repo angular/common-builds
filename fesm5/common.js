a31d728f7b40025463ffa44e69a401cdd6d81bcb
Andrew Kushnir <akushnir@google.com>
2019-01-09 20:58:59 +0000

    a06469c docs(forms): remove duplicated link (#27884) PR Close #27884

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+70.sha-ecc304a
+ * @license Angular v7.2.0-rc.0+72.sha-a06469c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5660,7 +5660,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+70.sha-ecc304a');
+var VERSION = new Version('7.2.0-rc.0+72.sha-a06469c');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-rc.0+70.sha-ecc304a
+ * @license Angular v7.2.0-rc.0+72.sha-a06469c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
