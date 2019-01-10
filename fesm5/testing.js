064d10ac5524a0690b41771589a09f180692f924
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:21:32 +0000

    6003145 fix(ivy): properly rewrite imports in generated factory shims (#27998)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0+85.sha-5a0deb8
+ * @license Angular v7.2.0+87.sha-6003145
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata } from 'tslib';
+import { __decorate, __extends, __metadata } from 'tslib';
 import { EventEmitter, Injectable } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 
