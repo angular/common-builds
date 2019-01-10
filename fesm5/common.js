215f70bd55713e646e14ee5e4d081aec6309fd69
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:32:44 +0000

    dffcb9c ci: onlyInIvy tests should not be triggered for FIND_PASSING_TESTS mode (#28036)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+97.sha-3bafc00
+ * @license Angular v7.2.0+95.sha-dffcb9c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5660,7 +5660,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+97.sha-3bafc00');
+var VERSION = new Version('7.2.0+95.sha-dffcb9c');
 
 /**
  * @license
