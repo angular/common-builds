83cdd1e403d08fa42bdc35f19e88d50adcecfec7
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:00:20 +0000

    885f1af build: extract interface and util sub compilation from core (#28028)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+102.sha-b05baa5
+ * @license Angular v7.2.0+103.sha-885f1af
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5714,7 +5714,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+102.sha-b05baa5');
+var VERSION = new Version('7.2.0+103.sha-885f1af');
 
 /**
  * @license
