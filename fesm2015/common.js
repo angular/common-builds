8d780eb45c748cef1f7a4fe53fa991a6757fccec
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:29:20 +0000

    4577819 docs(router): fix misnamed path (#27879)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+83.sha-2530c9d
+ * @license Angular v7.2.0-rc.0+84.sha-4577819
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6489,7 +6489,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+83.sha-2530c9d');
+const VERSION = new Version('7.2.0-rc.0+84.sha-4577819');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+83.sha-2530c9d
+ * @license Angular v7.2.0-rc.0+84.sha-4577819
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5660,7 +5660,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+83.sha-2530c9d');
+var VERSION = new Version('7.2.0-rc.0+84.sha-4577819');
 
 /**
  * @license
