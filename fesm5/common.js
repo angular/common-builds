e37c7b889ab090d7f7f056ea421bd96377706784
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:32:09 +0000

    96f4969 docs: fix broken html for deployment.md (#27851)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+106.sha-aebec4b
+ * @license Angular v7.2.0+107.sha-96f4969
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5714,7 +5714,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+106.sha-aebec4b');
+var VERSION = new Version('7.2.0+107.sha-96f4969');
 
 /**
  * @license
