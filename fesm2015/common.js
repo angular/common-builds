eb351b0e4d9ea7339a3ad8148f762bc052cf6ab6
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:27:19 +0000

    7bafe18 docs(router): fixed a typo in CanLoad title text (#27894)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+81.sha-9447b85
+ * @license Angular v7.2.0-rc.0+85.sha-7bafe18
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6489,7 +6489,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+81.sha-9447b85');
+const VERSION = new Version('7.2.0-rc.0+85.sha-7bafe18');
 
 /**
  * @fileoverview added by tsickle
