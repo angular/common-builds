43bb3be35bf644d8656e7a9a185596a58c4588a2
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:25:18 +0000

    9447b85 refactor(core): improve type for EventEmitter (#27228)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+82.sha-c18c46a
+ * @license Angular v7.2.0-rc.0+81.sha-9447b85
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6489,7 +6489,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+82.sha-c18c46a');
+const VERSION = new Version('7.2.0-rc.0+81.sha-9447b85');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+82.sha-c18c46a
+ * @license Angular v7.2.0-rc.0+81.sha-9447b85
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5660,7 +5660,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+82.sha-c18c46a');
+var VERSION = new Version('7.2.0-rc.0+81.sha-9447b85');
 
 /**
  * @license
