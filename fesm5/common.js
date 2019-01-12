ed1da943b5f0fd64df75ac8961e5f36d99645266
Keen Yee Liau <kyliau@umich.edu>
2019-01-12 00:42:40 +0000

    ef5bdcb docs(bazel): Getting started with Bazel + CLI

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+139.sha-6eeb19c
+ * @license Angular v7.2.0+144.sha-ef5bdcb
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5672,7 +5672,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+139.sha-6eeb19c');
+var VERSION = new Version('7.2.0+144.sha-ef5bdcb');
 
 /**
  * @license
