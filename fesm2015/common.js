0f7c2c337c80f655c91a48f4a9f327c652a9ae6a
Andrew Kushnir <akushnir@google.com>
2019-01-15 18:09:57 +0000

    9faad4b docs(core): fix typo (#28042) PR Close #28042

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+123.sha-42ccea5
+ * @license Angular v7.2.0-rc.0+124.sha-9faad4b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6489,7 +6489,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+123.sha-42ccea5');
+const VERSION = new Version('7.2.0-rc.0+124.sha-9faad4b');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+123.sha-42ccea5
+ * @license Angular v7.2.0-rc.0+124.sha-9faad4b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5660,7 +5660,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+123.sha-42ccea5');
+var VERSION = new Version('7.2.0-rc.0+124.sha-9faad4b');
 
 /**
  * @license
