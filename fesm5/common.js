a874fadf21ce30fa358df6a49606c03abda5b14d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 01:57:26 +0000

    6d40ef2 fix(docs-infra): improve a11y (#28086)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+3.sha-4ea6c27
+ * @license Angular v7.2.1+4.sha-6d40ef2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5692,7 +5692,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.1+3.sha-4ea6c27');
+var VERSION = new Version('7.2.1+4.sha-6d40ef2');
 
 /**
  * @license
