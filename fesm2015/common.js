8e2c748b64b19b78201c9b80c879db0121e3a504
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 23:29:58 +0000

    32737a6 fix(router): `skipLocationChange` with named outlets (#28301)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+19.sha-7642308
+ * @license Angular v7.2.1+20.sha-32737a6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6542,7 +6542,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.1+19.sha-7642308');
+const VERSION = new Version('7.2.1+20.sha-32737a6');
 
 /**
  * @fileoverview added by tsickle
