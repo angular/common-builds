075ba587507da3b9ecdf7d4b503a5f56d68a5dfb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-23 19:25:14 +0000

    1fd6735 build: update and unify gitignore (#28316)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+6.sha-583061d
+ * @license Angular v8.0.0-beta.1+1.sha-1fd6735
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5836,7 +5836,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.1+6.sha-583061d');
+var VERSION = new Version('8.0.0-beta.1+1.sha-1fd6735');
 
 /**
  * @license
