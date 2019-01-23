3f7cae9f46152c2a5a210127321217a7a9feeb4d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-23 19:17:41 +0000

    5877b3f refactor: remove unused parameter in _main method invocation (#28203)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.2
+ * @license Angular v7.2.2+3.sha-5877b3f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6542,7 +6542,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.2');
+const VERSION = new Version('7.2.2+3.sha-5877b3f');
 
 /**
  * @fileoverview added by tsickle
