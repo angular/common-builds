67a826128f043ad03507c87fdee02923e047ee5a
Jason Aden <jasonaden@google.com>
2019-01-24 18:31:53 +0000

    beacbfc build(docs-infra): upgrade cli command docs sources to 699743282 (#28329)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.2+4.sha-f72319c
+ * @license Angular v7.2.2+5.sha-beacbfc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6542,7 +6542,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.2+4.sha-f72319c');
+const VERSION = new Version('7.2.2+5.sha-beacbfc');
 
 /**
  * @fileoverview added by tsickle
