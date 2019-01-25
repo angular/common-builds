54c78883ba8eeb2de2db4b0f1efd554bdcd2c861
Jason Aden <jasonaden@google.com>
2019-01-25 21:05:50 +0000

    a1b9995 refactor(router): initalize browserUrlTree to empty tree (#28376)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.2+15.sha-35f7ff0
+ * @license Angular v7.2.2+16.sha-a1b9995
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6612,7 +6612,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.2+15.sha-35f7ff0');
+const VERSION = new Version('7.2.2+16.sha-a1b9995');
 
 /**
  * @fileoverview added by tsickle
