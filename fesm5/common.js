bf472b1e9786bb93f2e278fb56bd9d969cc6479d
Matias Niemelä <matias@yearofmoo.com>
2019-01-31 21:51:21 +0000

    35e45dc fix(ivy): prevent errors from views being destroyed twice (#28413)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+109.sha-a227c52
+ * @license Angular v8.0.0-beta.2+3.sha-35e45dc
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5836,7 +5836,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.1+109.sha-a227c52');
+var VERSION = new Version('8.0.0-beta.2+3.sha-35e45dc');
 
 /**
  * @license
