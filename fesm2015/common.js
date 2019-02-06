16cf6c1b6bc70232d10ac38be37541963a36a17a
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 04:09:07 +0000

    74c202a release: cut the v7.2.4 release

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.3+26.sha-483e8d2
+ * @license Angular v7.2.4
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6612,7 +6612,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.3+26.sha-483e8d2');
+const VERSION = new Version('7.2.4');
 
 /**
  * @fileoverview added by tsickle
