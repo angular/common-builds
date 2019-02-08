991a45994f61cfde82404df84f30b18d0743c148
Miško Hevery <misko@hevery.com>
2019-02-08 17:47:10 +0000

    1ff3699 test(upgrade): change flaky test to not be affected by other tests (#28617)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+59.sha-0e4705a
+ * @license Angular v8.0.0-beta.3+60.sha-1ff3699
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5836,7 +5836,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+59.sha-0e4705a');
+var VERSION = new Version('8.0.0-beta.3+60.sha-1ff3699');
 
 /**
  * @license
