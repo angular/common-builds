7a3bc71a623732fae37967f86494fc56efb324b0
Miško Hevery <misko@hevery.com>
2019-02-08 01:10:21 +0000

    6f9dd1b refactor(common): Remove deprecated FileType (#28583)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+48.sha-c3fadad
+ * @license Angular v8.0.0-beta.3+49.sha-6f9dd1b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5836,7 +5836,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+48.sha-c3fadad');
+var VERSION = new Version('8.0.0-beta.3+49.sha-6f9dd1b');
 
 /**
  * @license
