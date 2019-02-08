5cd5668eade90c7a9fdacfc214296de1f33d342a
Miško Hevery <misko@hevery.com>
2019-02-08 17:34:07 +0000

    ceada77 ci: add all required statuses to the requiredStatuses list for angular-robot (#28613)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+56.sha-d91ea2c
+ * @license Angular v8.0.0-beta.3+57.sha-ceada77
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5836,7 +5836,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+56.sha-d91ea2c');
+var VERSION = new Version('8.0.0-beta.3+57.sha-ceada77');
 
 /**
  * @license
