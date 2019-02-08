0a9053d36f3ee4fed90ba791846a6a03e4141b26
Miško Hevery <misko@hevery.com>
2019-02-08 01:01:41 +0000

    c3fadad docs: update HttpHeader documentation (#26233)

--- http/src/headers.d.ts
+++ http/src/headers.d.ts
@@ -6,7 +6,8 @@
  * found in the LICENSE file at https://angular.io/license
  */
 /**
- * Immutable set of Http headers, with lazy parsing.
+ * `HttpHeaders` class represents the header configuration options for an HTTP request.
+ * Instances should be assumed immutable with lazy parsing.
  *
  * @publicApi
  */
@@ -28,27 +29,67 @@ export declare class HttpHeaders {
      * Queued updates to be materialized the next initialization.
      */
     private lazyUpdate;
+    /**  Constructs a new HTTP header object with the given values.*/
     constructor(headers?: string | {
         [name: string]: string | string[];
     });
     /**
-     * Checks for existence of header by given name.
+     * Checks for existence of a header by a given name.
+     *
+     * @param name The header name to check for existence.
+     *
+     * @returns Whether the header exits.
      */
     has(name: string): boolean;
     /**
-     * Returns first header that matches given name.
+     * Returns the first header value that matches a given name.
+     *
+     * @param name The header name to retrieve.
+     *
+     * @returns A string if the header exists, null otherwise
      */
     get(name: string): string | null;
     /**
-     * Returns the names of the headers
+     * Returns the names of the headers.
+     *
+     * @returns A list of header names.
      */
     keys(): string[];
     /**
-     * Returns list of header values for a given name.
+     * Returns a list of header values for a given header name.
+     *
+     * @param name The header name from which to retrieve the values.
+     *
+     * @returns A string of values if the header exists, null otherwise.
      */
     getAll(name: string): string[] | null;
+    /**
+     * Appends a new header value to the existing set of
+     * header values.
+     *
+     * @param name The header name for which to append the values.
+     *
+     * @returns A clone of the HTTP header object with the value appended.
+     */
     append(name: string, value: string | string[]): HttpHeaders;
+    /**
+     * Sets a header value for a given name. If the header name already exists,
+     * its value is replaced with the given value.
+     *
+     * @param name The header name.
+     * @param value Provides the value to set or overide for a given name.
+     *
+     * @returns A clone of the HTTP header object with the newly set header value.
+     */
     set(name: string, value: string | string[]): HttpHeaders;
+    /**
+     * Deletes all header values for a given name.
+     *
+     * @param name The header name.
+     * @param value The header values to delete for a given name.
+     *
+     * @returns A clone of the HTTP header object.
+     */
     delete(name: string, value?: string | string[]): HttpHeaders;
     private maybeSetNormalizedName;
     private init;
