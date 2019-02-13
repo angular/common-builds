6af7d32476603c492787e718fb467e1cc81f740a
Miško Hevery <misko@hevery.com>
2019-02-13 18:27:46 +0000

    f8b9e61 test(ivy): enable more docs examples e2e tests (#28688)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+132.sha-49fb8c3
+ * @license Angular v8.0.0-beta.3+134.sha-f8b9e61
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5836,7 +5836,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+132.sha-49fb8c3');
+var VERSION = new Version('8.0.0-beta.3+134.sha-f8b9e61');
 
 /**
  * @license
