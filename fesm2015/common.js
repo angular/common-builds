50fea508a0b76c0fea719da9dd88db2d0bc8d096
Misko Hevery <misko@hevery.com>
2019-02-17 05:22:18 +0000

    2e3cc45 build(bazel): specify tsconfig-test dependency (#28696)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+2.sha-c1094cf
+ * @license Angular v8.0.0-beta.4+1.sha-2e3cc45
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6646,7 +6646,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.4+2.sha-c1094cf');
+const VERSION = new Version('8.0.0-beta.4+1.sha-2e3cc45');
 
 /**
  * @fileoverview added by tsickle
