64787a3f8b0a93dc0ff37c649bbe00dbb790234f
Misko Hevery <misko@hevery.com>
2019-02-17 05:22:23 +0000

    911599d test(ivy): mark unknown binding test modifiedInIvy (#28740)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+2.sha-c1094cf
+ * @license Angular v8.0.0-beta.4+3.sha-911599d
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5835,7 +5835,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4+2.sha-c1094cf');
+var VERSION = new Version('8.0.0-beta.4+3.sha-911599d');
 
 /**
  * @license
