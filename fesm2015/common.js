39533fbfc17519d6e66fca8f8372b269ed7c521f
Igor Minar <igor@angular.io>
2019-02-19 21:11:58 +0000

    f6ca619 test: bazel integration test should use angular npm packages (#28762)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.5+12.sha-dd3cab6
+ * @license Angular v7.2.5+16.sha-f6ca619
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6611,7 +6611,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.5+12.sha-dd3cab6');
+const VERSION = new Version('7.2.5+16.sha-f6ca619');
 
 /**
  * @fileoverview added by tsickle
