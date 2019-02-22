2f7e3e98f0da8bac4dd1c28531f48812e8c82e5d
Igor Minar <igor@angular.io>
2019-02-22 02:09:08 +0000

    f4d6525 build: use uname instead of relying on sed --version (#28900)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+20.sha-3f2b51b.with-local-changes
+ * @license Angular v8.0.0-beta.5+22.sha-f4d6525.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6353,7 +6353,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.5+20.sha-3f2b51b.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.5+22.sha-f4d6525.with-local-changes');
 
     /**
      * @license
