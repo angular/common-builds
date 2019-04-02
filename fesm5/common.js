8b0d2f1b71fcadd2743bd08d6dc216ced334e65a
Jason Aden <jasonaden@google.com>
2019-04-02 17:55:42 +0000

    1c07061 refactor: change error message (#29594)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+99.sha-7c1f73a.with-local-changes
+ * @license Angular v8.0.0-beta.10+102.sha-1c07061.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6460,7 +6460,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+99.sha-7c1f73a.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+102.sha-1c07061.with-local-changes');
 
 /**
  * @license
