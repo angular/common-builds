85e667c74af8e6f6973abd9fe67d6a51b1f2e763
Jason Aden <jasonaden@google.com>
2019-04-03 22:35:22 +0000

    0a530e6 release: cut the v8.0.0-beta.11 release

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+124.sha-dc5577d.with-local-changes
+ * @license Angular v8.0.0-beta.11.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6508,7 +6508,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+124.sha-dc5577d.with-local-changes');
+var VERSION = new Version('8.0.0-beta.11.with-local-changes');
 
 /**
  * @license
