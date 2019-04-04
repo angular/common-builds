a8dbc9df896b74b4a87428e915c7b948fc621ec8
Igor Minar <igor@angular.io>
2019-04-04 23:45:46 +0000

    1423cc0 docs: correct route path description (#29669)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+11.sha-e483078.with-local-changes
+ * @license Angular v7.2.12+12.sha-1423cc0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6678,7 +6678,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.12+11.sha-e483078.with-local-changes');
+const VERSION = new Version('7.2.12+12.sha-1423cc0.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
