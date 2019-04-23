32ab5fb28395a9501a664aaf484da010b1c4dbcc
Brandon Roberts <robertsbt@gmail.com>
2019-04-23 17:01:05 +0000

    5c8d156 docs: explicitly state purpose of components

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+18.sha-1b7749d.with-local-changes
+ * @license Angular v8.0.0-beta.13+62.sha-5c8d156.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@
                 this._dispose();
                 return this.transform(obj);
             }
-            if (this._latestValue === this._latestReturnedValue) {
+            if (core.ɵlooseIdentical(this._latestValue, this._latestReturnedValue)) {
                 return this._latestReturnedValue;
             }
             this._latestReturnedValue = this._latestValue;
@@ -6544,7 +6544,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.12+18.sha-1b7749d.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.13+62.sha-5c8d156.with-local-changes');
 
     /**
      * @license
