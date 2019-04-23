f7d1ad3a35f748d5aebfb8a19758988a6bc50077
Ben Lesh <ben@benlesh.com>
2019-04-23 15:51:59 +0000

    cd48a53 build(docs-infra): pin versions of packages installed in preview server docker image (#29976)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.13+24.sha-4382cea.with-local-changes
+ * @license Angular v7.2.13+27.sha-cd48a53.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6678,7 +6678,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.13+24.sha-4382cea.with-local-changes');
+const VERSION = new Version('7.2.13+27.sha-cd48a53.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
