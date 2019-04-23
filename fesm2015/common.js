9d2e1bcf054037c3f4bfd7a3de9ae4b22bbe33e5
Ben Lesh <ben@benlesh.com>
2019-04-23 00:48:15 +0000

    4382cea docs: fix grammatical errors in the guides and API documentation (#29928)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.13+23.sha-9a81e52.with-local-changes
+ * @license Angular v7.2.13+24.sha-4382cea.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6678,7 +6678,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.13+23.sha-9a81e52.with-local-changes');
+const VERSION = new Version('7.2.13+24.sha-4382cea.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
