f59bf6b0679efc6d9108ed9b9686c9e6279bee7d
Andrew Kushnir <akushnir@google.com>
2019-04-25 19:31:49 +0000

    07f0385 test(upgrade): rename `angular.module()` to `angular.module_()` (#30107)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.14+10.sha-d025159.with-local-changes
+ * @license Angular v7.2.14+13.sha-07f0385.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6678,7 +6678,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.14+10.sha-d025159.with-local-changes');
+const VERSION = new Version('7.2.14+13.sha-07f0385.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
