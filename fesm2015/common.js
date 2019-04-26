7d3e46f5d19da3114aff4cad043df8c911a4a940
Andrew Kushnir <akushnir@google.com>
2019-04-26 23:37:16 +0000

    c99d379 refactor(ivy): move di tests for inject to acceptance (#29299)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+13.sha-b945bc3.with-local-changes
+ * @license Angular v8.0.0-rc.0+18.sha-c99d379.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7638,7 +7638,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0+13.sha-b945bc3.with-local-changes');
+const VERSION = new Version('8.0.0-rc.0+18.sha-c99d379.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
