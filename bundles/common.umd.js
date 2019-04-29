933b445036156e6ff4820309507da75988db249e
Andrew Kushnir <akushnir@google.com>
2019-04-29 20:45:39 +0000

    21418ea test: fix ngc-wrapped bazel tests in windows (#30111)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.2+3.sha-6748392.with-local-changes
+ * @license Angular v8.0.0-rc.2+5.sha-21418ea.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6569,7 +6569,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.2+3.sha-6748392.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.2+5.sha-21418ea.with-local-changes');
 
     /**
      * @license
