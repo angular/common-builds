204f902f3642eb89e7fbf5203a089035883a384a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-14 17:15:31 +0000

    34e0d62 build(docs-infra): update http-server to 0.11.1 (#30401)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+164.sha-090eac0.with-local-changes
+ * @license Angular v8.0.0-rc.0+167.sha-34e0d62.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6569,7 +6569,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.0+164.sha-090eac0.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.0+167.sha-34e0d62.with-local-changes');
 
     /**
      * @license
