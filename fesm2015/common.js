f12d1c028fb3aa09a4d51f8f412ec0faf5a8f563
Matias Niemelä <matias@yearofmoo.com>
2019-05-24 18:01:26 +0000

    0d23ced docs: add Michael Hladky to GDE resources (#30596)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+336.sha-6bf5ec2.with-local-changes
+ * @license Angular v8.0.0-rc.0+337.sha-0d23ced.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7615,7 +7615,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0+336.sha-6bf5ec2.with-local-changes');
+const VERSION = new Version('8.0.0-rc.0+337.sha-0d23ced.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
