ab528bc401819d6b6b9c7bbf92ad2548c4d008dd
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:12:02 +0000

    ae6fa92 docs: change wording for more clarity in TypeScript Configuration guide (#30748)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+5.sha-9a9ae60.with-local-changes
+ * @license Angular v8.0.0+4.sha-ae6fa92.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7628,7 +7628,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0+5.sha-9a9ae60.with-local-changes');
+const VERSION = new Version('8.0.0+4.sha-ae6fa92.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
