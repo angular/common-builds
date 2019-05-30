a36ad9543118a3e45c0132aa53d3ce977843d46a
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:19:34 +0000

    7757826 docs: code sample closing in ivy.md (#30711) PR Close #30711

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+368.sha-0b23adc.with-local-changes
+ * @license Angular v8.0.0-rc.0+360.sha-7757826.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -4263,7 +4263,7 @@
      * @publicApi
      * @see `NgSwitchCase`
      * @see `NgSwitchDefault`
-     * @see [Structural Directives](guide/structural-directives)
+     * @see [Stuctural Directives](guide/structural-directives)
      *
      */
     var NgSwitch = /** @class */ (function () {
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.0+368.sha-0b23adc.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.0+360.sha-7757826.with-local-changes');
 
     /**
      * @license
