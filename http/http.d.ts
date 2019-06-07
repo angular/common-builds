e131f915b8fa82f69cc3e9beb2a4d6e9cc9d14f4
Misko Hevery <misko@hevery.com>
2019-06-07 17:04:13 +0000

    c038675 refactor(ivy): inherently call ɵɵselect(0) (#30830)

--- http/http.d.ts
+++ http/http.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+10.sha-11a4454.with-local-changes
+ * @license Angular v8.1.0-next.1+18.sha-c038675.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3420,6 +3420,15 @@ export declare abstract class HttpResponseBase {
  */
 export declare interface HttpSentEvent {
     type: HttpEventType.Sent;
+}
+
+/**
+ * An upload progress event.
+ *
+ * @publicApi
+ */
+export declare interface HttpUploadProgressEvent extends HttpProgressEvent {
+    type: HttpEventType.UploadProgress;
 }
 
 /**
