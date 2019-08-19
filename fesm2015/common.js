94d86333bc9b41f2f66acadb1533e120e1c22a51
Andrew Kushnir <akushnir@google.com>
2019-08-19 17:29:28 +0000

    e5b18e810 build(docs-infra): upgrade cli command docs sources to e0055d293 (#32175)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+20.sha-b8f86ad.with-local-changes
+ * @license Angular v8.2.2+21.sha-e5b18e8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8412,7 +8412,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.2+20.sha-b8f86ad.with-local-changes');
+const VERSION = new Version('8.2.2+21.sha-e5b18e8.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
