2eec9970a87a2c28e671e2674e1ea08935ada55f
Matias Niemelä <matias@yearofmoo.com>
2019-08-30 18:15:14 +0000

    ba5e07efc perf(ivy): add a micro benchmark for map-based style and class bindings (#32401)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+48.sha-9bbe67f.with-local-changes
+ * @license Angular v9.0.0-next.4+46.sha-ba5e07e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9717,7 +9717,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.4+48.sha-9bbe67f.with-local-changes');
+const VERSION = new Version('9.0.0-next.4+46.sha-ba5e07e.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
