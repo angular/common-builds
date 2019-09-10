d27b4ebe4c9a7aa0aeeb8429c94c16fd141a6884
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 19:42:45 +0000

    8a7129e21 ci: disable hammerjs tests of components repository (#32485)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+63.sha-2124588.with-local-changes
+ * @license Angular v9.0.0-next.5+67.sha-8a7129e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9513,7 +9513,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.5+63.sha-2124588.with-local-changes');
+const VERSION = new Version('9.0.0-next.5+67.sha-8a7129e.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
