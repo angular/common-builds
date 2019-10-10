43ff3933a51652aaa8a8cfdad003fcc01e735bf6
Miško Hevery <misko@hevery.com>
2019-10-10 21:16:36 +0000

    be54c580b docs(common): Fixes typo (#33003) PR Close #33003

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10.with-local-changes
+ * @license Angular v9.0.0-next.10+4.sha-be54c58.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ var DomAdapter = /** @class */ (function () {
  * A DI Token representing the main rendering context. In a browser this is the DOM Document.
  *
  * Note: Document might not be available in the Application Context when Application and Rendering
- * Contexts are not the same (e.g. when running the application into a Web Worker).
+ * Contexts are not the same (e.g. when running the application in a Web Worker).
  *
  * @publicApi
  */
@@ -6608,7 +6608,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.10.with-local-changes');
+var VERSION = new Version('9.0.0-next.10+4.sha-be54c58.with-local-changes');
 
 /**
  * @license
