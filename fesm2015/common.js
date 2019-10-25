290c6f94d2a55308183836fe3e30c9e41122cf0b
Martin Probst <martin@probst.io>
2019-10-25 22:55:43 +0000

    585ea89ab2 fix(zone.js): work around TS3.7 issue

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+7.sha-ed391ad.with-local-changes
+ * @license Angular v9.0.0-next.14+2.sha-585ea89.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -805,7 +805,7 @@ function () { }, ɵ1 = /**
  */
 function (rf, ctx, elIndex) {
     if (rf & 1 /* Create */) {
-        ɵɵallocHostVars(1);
+        ɵɵallocHostVars(2);
     }
     if (rf & 2 /* Update */) {
         ɵɵclassMap(ctx.getValue());
@@ -1171,7 +1171,7 @@ function () { }, ɵ1$1 = /**
  */
 function (rf, ctx, elIndex) {
     if (rf & 1 /* Create */) {
-        ɵɵallocHostVars(1);
+        ɵɵallocHostVars(2);
     }
     if (rf & 2 /* Update */) {
         ɵɵstyleMap(ctx.getValue());
@@ -7741,7 +7741,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.12+7.sha-ed391ad.with-local-changes');
+const VERSION = new Version('9.0.0-next.14+2.sha-585ea89.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
