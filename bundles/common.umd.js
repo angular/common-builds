e846dd8d515510e0ce1b21c8371199dad69a84e1
Andrew Kushnir <akushnir@google.com>
2019-10-25 20:27:43 +0000

    14c4b1b205 refactor(ivy): remove ngBaseDef (#33264)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+53.sha-3505692.with-local-changes
+ * @license Angular v9.0.0-next.13+54.sha-14c4b1b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5837,7 +5837,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.13+53.sha-3505692.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.13+54.sha-14c4b1b.with-local-changes');
 
     /**
      * @license
