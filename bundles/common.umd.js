fd901a63608d8c13510908a8d8984435e2bdcc96
atscott <atscott01@gmail.com>
2019-11-04 20:29:34 +0000

    3992e7f05b docs: Update link to angular + bazel example app (#33409)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+35.sha-316b30b.with-local-changes
+ * @license Angular v9.0.0-rc.0+37.sha-3992e7f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5837,7 +5837,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.0+35.sha-316b30b.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-rc.0+37.sha-3992e7f.with-local-changes');
 
     /**
      * @license
--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+35.sha-316b30b.with-local-changes
+ * @license Angular v9.0.0-rc.0+37.sha-3992e7f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5701,7 +5701,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.0+35.sha-316b30b.with-local-changes');
+var VERSION = new Version('9.0.0-rc.0+37.sha-3992e7f.with-local-changes');
 
 /**
  * @license
