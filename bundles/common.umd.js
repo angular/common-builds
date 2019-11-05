789bc838c1f1722027476bf537ad3f3a742136ce
atscott <atscott01@gmail.com>
2019-11-05 00:35:38 +0000

    8de3fcfccd build: decrease payload size limit (#33587)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+37.sha-3992e7f.with-local-changes
+ * @license Angular v9.0.0-rc.0+40.sha-8de3fcf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5837,7 +5837,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.0+37.sha-3992e7f.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-rc.0+40.sha-8de3fcf.with-local-changes');
 
     /**
      * @license
