74f5a0e360e7a82d69bdb600b9561f769ec91e78
atscott <atscott01@gmail.com>
2019-11-07 00:16:41 +0000

    55b1b7745b release: cut the v9.0.0-rc.1 release

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+76.sha-814ed31.with-local-changes
+ * @license Angular v9.0.0-rc.1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7724,7 +7724,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.0+76.sha-814ed31.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
