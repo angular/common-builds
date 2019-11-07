5f8e5579db398ae058d6f25b7a986708d3ac70a5
Andrew Scott <atscott01@gmail.com>
2019-11-07 18:16:52 +0000

    8912b11f56 fix(ivy): recompile component when template changes in ngc watch mode (#33551)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+11.sha-8d72a37.with-local-changes
+ * @license Angular v9.0.0-rc.1+12.sha-8912b11.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5706,7 +5706,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+11.sha-8d72a37.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+12.sha-8912b11.with-local-changes');
 
 /**
  * @license
