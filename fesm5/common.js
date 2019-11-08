0c953705e74080e8ba916ab4353a123de79edf8b
Kara Erickson <karakara@google.com>
2019-11-08 19:14:11 +0000

    570936e39f docs: add ivy ssr opt-out (#33616)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+27.sha-1db8650.with-local-changes
+ * @license Angular v9.0.0-rc.1+26.sha-570936e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5696,7 +5696,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+27.sha-1db8650.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+26.sha-570936e.with-local-changes');
 
 /**
  * @license
