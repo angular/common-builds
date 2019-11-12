f30e9e27117b65635b73a2827901756f4a071406
Kara Erickson <karakara@google.com>
2019-11-12 18:30:31 +0000

    c315881d04 perf(core): Avoid unnecessary creating provider factory (#33742)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+58.sha-6615743.with-local-changes
+ * @license Angular v9.0.0-rc.1+65.sha-c315881.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5696,7 +5696,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+58.sha-6615743.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+65.sha-c315881.with-local-changes');
 
 /**
  * @license
