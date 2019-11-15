ea9b39147cc8d8e5cd1f9d51657037cdbdf37e2d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-15 21:33:18 +0000

    3074cdfea9 Revert "fix(router): make routerLinkActive work with query params which contain arrays (#22666)" (#33861)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.2+36.sha-c182dea.with-local-changes
+ * @license Angular v9.0.0-rc.2+37.sha-3074cdf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5832,7 +5832,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.2+36.sha-c182dea.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-rc.2+37.sha-3074cdf.with-local-changes');
 
     /**
      * @license
