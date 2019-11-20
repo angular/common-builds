f8c58738b6911043d17a11e95c4604828311bc60
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-20 19:26:42 +0000

    a7e2327212 test(docs-infra): more thoroughly clean up after `ScrollService` tests (#33937)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.2+61.sha-1218ce4.with-local-changes
+ * @license Angular v9.0.0-rc.2+92.sha-a7e2327.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5696,7 +5696,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.2+61.sha-1218ce4.with-local-changes');
+var VERSION = new Version('9.0.0-rc.2+92.sha-a7e2327.with-local-changes');
 
 /**
  * @license
