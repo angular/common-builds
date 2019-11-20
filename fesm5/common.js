7c56c9b608ca7c8d36254b6abe2b082a65329f0c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-20 23:09:01 +0000

    55748dbc55 fix(core): allow css custom variables/properties in the style sanitizer (#33841)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+190.sha-d78d29f.with-local-changes
+ * @license Angular v9.0.0-rc.1+197.sha-55748db.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5708,7 +5708,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+190.sha-d78d29f.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+197.sha-55748db.with-local-changes');
 
 /**
  * @license
