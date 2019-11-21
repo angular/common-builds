26402a0ceee759bc05af0cccb07ebc2fec8c28d0
Matias Niemelä <matias@yearofmoo.com>
2019-11-21 22:27:53 +0000

    3d69693692 fix(ivy): remove TNodeType assertion from `directiveInject` instruction (#33948)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+216.sha-fd5f101.with-local-changes
+ * @license Angular v9.0.0-rc.1+217.sha-3d69693.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5696,7 +5696,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+216.sha-fd5f101.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+217.sha-3d69693.with-local-changes');
 
 /**
  * @license
