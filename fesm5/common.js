89b38946e1d9c27f00778e13ca41137efe2e0445
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 17:08:34 +0000

    688b188484 build: remove compile_strategy from ng_module.bzl (#33999)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.3+55.sha-7b77ead.with-local-changes
+ * @license Angular v9.0.0-rc.3+63.sha-688b188.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5696,7 +5696,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.3+55.sha-7b77ead.with-local-changes');
+var VERSION = new Version('9.0.0-rc.3+63.sha-688b188.with-local-changes');
 
 /**
  * @license
