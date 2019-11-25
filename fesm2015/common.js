dea1d6f25f773596b1553a6ba210b0c18f5174b8
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 16:56:16 +0000

    1217c6b7cc docs: replace class hero to interface wherever class properties not used (#34022)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+244.sha-5129bcc.with-local-changes
+ * @license Angular v9.0.0-rc.1+246.sha-1217c6b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8215,7 +8215,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+244.sha-5129bcc.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+246.sha-1217c6b.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
