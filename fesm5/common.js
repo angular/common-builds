4b546a2fb4681dd28ba14538107897c90e96bf23
Miško Hevery <misko@hevery.com>
2019-12-02 19:57:39 +0000

    5a7f33496b refactor(compiler-cli): remove bogus packages/compiler-cli/integrationtest/benchmarks/ (#34142)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+317.sha-7eccbcd.with-local-changes
+ * @license Angular v9.0.0-rc.1+321.sha-5a7f334.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5696,7 +5696,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+317.sha-7eccbcd.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+321.sha-5a7f334.with-local-changes');
 
 /**
  * @license
