5f6b55b04c99467eb41bc06da6258ab4ad2ca45b
Miško Hevery <misko@hevery.com>
2019-12-02 19:49:01 +0000

    bbce2ad7d4 build: update to nodejs rules 0.42.1 (#34073)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+315.sha-39722df.with-local-changes
+ * @license Angular v9.0.0-rc.1+310.sha-bbce2ad.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5696,7 +5696,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+315.sha-39722df.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+310.sha-bbce2ad.with-local-changes');
 
 /**
  * @license
