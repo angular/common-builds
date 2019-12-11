322827f676192465987e78b176da392ce758baf4
Andrew Kushnir <akushnir@google.com>
2019-12-11 19:26:24 +0000

    184b83911c release: cut the v9.0.0-rc.6 release

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+61.sha-d7c459a.with-local-changes
+ * @license Angular v9.0.0-rc.6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5839,7 +5839,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.5+61.sha-d7c459a.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-rc.6.with-local-changes');
 
     /**
      * @license
