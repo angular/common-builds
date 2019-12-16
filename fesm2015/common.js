9bc0e80d2ad55aee74986be96c4f24795905b722
Kara Erickson <karakara@google.com>
2019-12-16 16:02:32 +0000

    a186dbc1d4 docs(upgrade): add example links to `downgradeComponent()` function docs (#34406)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+472.sha-f79110c.with-local-changes
+ * @license Angular v9.0.0-rc.1+479.sha-a186dbc
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7793,7 +7793,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+472.sha-f79110c.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+479.sha-a186dbc');
 
 /**
  * @fileoverview added by tsickle
