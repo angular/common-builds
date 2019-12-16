c1850a0e5e2b877767473eedcc9c4bfd0f67ec9c
Kara Erickson <karakara@google.com>
2019-12-16 23:51:50 +0000

    b1d4c58b81 feat(forms): expand NgModel disabled type to work with strict template type checking (#34438)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.6+60.sha-76e4870
+ * @license Angular v9.0.0-rc.6+61.sha-b1d4c58
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5841,7 +5841,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.6+60.sha-76e4870');
+    var VERSION = new i0.Version('9.0.0-rc.6+61.sha-b1d4c58');
 
     /**
      * @license
