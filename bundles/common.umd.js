496209a430b6eba4141b97cf6b75a7a6a24e1617
George Kalpakas <kalpakas.g@gmail.com>
2019-12-29 10:15:57 +0000

    61c805fa1b build(docs-infra): bump handlebars from 4.1.2 to 4.5.3 in /aio

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+522.sha-22ac0e9
+ * @license Angular v9.0.0-rc.1+522.sha-61c805f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5852,7 +5852,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+522.sha-22ac0e9');
+    var VERSION = new i0.Version('9.0.0-rc.1+522.sha-61c805f');
 
     /**
      * @license
