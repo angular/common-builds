1cb1cb4a716ebdfb2577b65cf323212a02dea2c8
atscott <atscott@google.com>
2020-01-11 00:23:47 +0000

    08c25818d8 fix(ivy): don't run decorator handlers against declaration files (#34557)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+75.sha-a7d8dcd
+ * @license Angular v9.0.0-rc.8+76.sha-08c2581
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5846,7 +5846,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.8+75.sha-a7d8dcd');
+    var VERSION = new i0.Version('9.0.0-rc.8+76.sha-08c2581');
 
     /**
      * @license
