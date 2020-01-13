eeb16c850b988888e3ff5d40149ce6ffc530e191
atscott <atscott@google.com>
2020-01-13 18:07:22 +0000

    ff02ddfd9b fix(ngcc): correctly detect dependencies in CommonJS (#34528)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+85.sha-a0eb57f
+ * @license Angular v9.0.0-rc.8+88.sha-ff02ddf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7788,7 +7788,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.8+85.sha-a0eb57f');
+const VERSION = new Version('9.0.0-rc.8+88.sha-ff02ddf');
 
 /**
  * @fileoverview added by tsickle
