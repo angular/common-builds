9bbd2ba819ae56868cd0441d21716b23f12d2391
GitHub <noreply@github.com>
2020-01-15 12:13:29 +0000

    50e52c8e1b fixup! docs: fix public api tool name

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+689.sha-ae9c061
+ * @license Angular v9.0.0-rc.1+691.sha-50e52c8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5881,7 +5881,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+689.sha-ae9c061');
+    var VERSION = new i0.Version('9.0.0-rc.1+691.sha-50e52c8');
 
     /**
      * @license
--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+689.sha-ae9c061
+ * @license Angular v9.0.0-rc.1+691.sha-50e52c8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7829,7 +7829,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+689.sha-ae9c061');
+const VERSION = new Version('9.0.0-rc.1+691.sha-50e52c8');
 
 /**
  * @fileoverview added by tsickle
