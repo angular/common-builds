f4d07b2c4ad9e173e17bb26ebb270ed91a3563fd
GitHub <noreply@github.com>
2020-01-15 21:12:28 +0000

    966cb355db fixup! docs: fix public api tool name

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+691.sha-50e52c8
+ * @license Angular v9.0.0-rc.1+692.sha-966cb35
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5881,7 +5881,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+691.sha-50e52c8');
+    var VERSION = new i0.Version('9.0.0-rc.1+692.sha-966cb35');
 
     /**
      * @license
