c9f92d45627c4473f4cca5fa0a9cb1177ea1dadb
Andrew Kushnir <akushnir@google.com>
2020-01-22 22:25:58 +0000

    34c30ae0aa build(docs-infra): always specify a landing file for StackBlitz projects (#34553)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+9.sha-20dc436
+ * @license Angular v9.0.0-rc.10+14.sha-34c30ae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7859,7 +7859,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.10+9.sha-20dc436');
+const VERSION = new Version('9.0.0-rc.10+14.sha-34c30ae');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+9.sha-20dc436
+ * @license Angular v9.0.0-rc.10+14.sha-34c30ae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5804,7 +5804,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.10+9.sha-20dc436');
+var VERSION = new Version('9.0.0-rc.10+14.sha-34c30ae');
 
 /**
  * @license
