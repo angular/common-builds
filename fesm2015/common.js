01f474d18a3aa4136ac9472fc678120547043313
Andrew Kushnir <akushnir@google.com>
2020-01-23 18:25:14 +0000

    48f8ca5483 build: upgrade to bazel 2.0.0 (#34877)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+761.sha-32b72f3
+ * @license Angular v9.0.0-rc.1+762.sha-48f8ca5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7871,7 +7871,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+761.sha-32b72f3');
+const VERSION = new Version('9.0.0-rc.1+762.sha-48f8ca5');
 
 /**
  * @fileoverview added by tsickle
