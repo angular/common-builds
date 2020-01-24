d05e63680eff7ea70343a192245d0786bdf12a2a
Andrew Kushnir <akushnir@google.com>
2020-01-24 19:54:33 +0000

    65354fb4ac ci(docs-infra): use the correct version of yarn in `aio_monitoring_stable` (#34954)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+51.sha-bca432c
+ * @license Angular v9.0.0-rc.10+52.sha-65354fb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7859,7 +7859,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.10+51.sha-bca432c');
+const VERSION = new Version('9.0.0-rc.10+52.sha-65354fb');
 
 /**
  * @fileoverview added by tsickle
