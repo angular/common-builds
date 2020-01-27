b0c5c06cacbfc8202ee96c2e68f1799a2d75cdd7
Andrew Kushnir <akushnir@google.com>
2020-01-27 19:25:33 +0000

    c0e73e0d67 fix(ivy): catch FatalDiagnosticError thrown from preanalysis phase (#34801)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+15.sha-efb17ae
+ * @license Angular v9.0.0-rc.11+23.sha-c0e73e0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5237,7 +5237,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.11+15.sha-efb17ae');
+var VERSION = new Version('9.0.0-rc.11+23.sha-c0e73e0');
 
 /**
  * @license
