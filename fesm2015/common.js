403a9ba231cf2fa3a2917b475a8567408fb20e5f
Andrew Kushnir <akushnir@google.com>
2020-01-27 21:34:10 +0000

    304584c291 perf(ivy): remove unused argument in hostBindings function (#34969)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+828.sha-7069a83
+ * @license Angular v9.0.0-rc.1+829.sha-304584c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7445,7 +7445,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+828.sha-7069a83');
+const VERSION = new Version('9.0.0-rc.1+829.sha-304584c');
 
 /**
  * @fileoverview added by tsickle
