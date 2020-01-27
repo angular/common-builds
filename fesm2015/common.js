139303cdac30eb111c30401206e5db7cb9cae426
Andrew Kushnir <akushnir@google.com>
2020-01-27 19:11:15 +0000

    549ab2ecc3 build(docs-infra): add words to ignore in snippets (#34915)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+823.sha-09869af
+ * @license Angular v9.0.0-rc.1+824.sha-549ab2e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7013,7 +7013,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+823.sha-09869af');
+const VERSION = new Version('9.0.0-rc.1+824.sha-549ab2e');
 
 /**
  * @fileoverview added by tsickle
