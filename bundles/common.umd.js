bd71efb54e5dbc730ccb3d1193a07762ea07ca13
Misko Hevery <misko@hevery.com>
2020-02-01 01:22:32 +0000

    58dbe662ff build: temporarily disable components-repo-unit-tests (#35079)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.12+17.sha-7f96fbb
+ * @license Angular v9.0.0-rc.12+24.sha-58dbe66
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5362,7 +5362,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.12+17.sha-7f96fbb');
+    var VERSION = new i0.Version('9.0.0-rc.12+24.sha-58dbe66');
 
     /**
      * @license
