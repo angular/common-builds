d2a024c2a22e5879ff14ca11d76d285c51afe595
Miško Hevery <misko@hevery.com>
2020-02-03 18:03:42 +0000

    6a771d9659 refactor(language-service): dedupe diagnostics using ts utility function (#35086)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.13
+ * @license Angular v9.0.0-rc.13+6.sha-6a771d9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6972,7 +6972,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.13');
+const VERSION = new Version('9.0.0-rc.13+6.sha-6a771d9');
 
 /**
  * @fileoverview added by tsickle
