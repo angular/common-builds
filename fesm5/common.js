1be8ca838b73929cfde7f72d8deb152f703c739e
Miško Hevery <misko@hevery.com>
2020-02-04 19:07:21 +0000

    6ddf5508db fix(ivy): support emitting a reference to interface declarations (#34849)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+918.sha-991e665
+ * @license Angular v9.0.0-rc.1+916.sha-6ddf550
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5237,7 +5237,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+918.sha-991e665');
+var VERSION = new Version('9.0.0-rc.1+916.sha-6ddf550');
 
 /**
  * @license
