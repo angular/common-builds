22d1aea924175d5f2b1cb44d557116ee01183a20
Misko Hevery <misko@hevery.com>
2020-02-06 17:54:07 +0000

    7526ef77b7 docs(ivy): add docs for styling priority order (#35066)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+930.sha-4fc2678
+ * @license Angular v9.0.0-rc.1+933.sha-7526ef7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7416,7 +7416,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+930.sha-4fc2678');
+const VERSION = new Version('9.0.0-rc.1+933.sha-7526ef7');
 
 /**
  * @fileoverview added by tsickle
