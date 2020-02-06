cdf446149989c25bb2767fbc66f79a8c9bd718e6
Misko Hevery <misko@hevery.com>
2020-02-06 17:57:20 +0000

    becab76c4a build: update scripts/release/post-check with the latest versions (#35151)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.14+23.sha-483ba6a
+ * @license Angular v9.0.0+5.sha-becab76
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6972,7 +6972,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.14+23.sha-483ba6a');
+const VERSION = new Version('9.0.0+5.sha-becab76');
 
 /**
  * @fileoverview added by tsickle
