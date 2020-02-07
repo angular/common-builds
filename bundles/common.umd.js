0303d3032a099806a8d34959602749f0c8e8f70c
Kara Erickson <karakara@google.com>
2020-02-07 21:27:29 +0000

    dea1b962c7 fix(ivy): repeat template guards to narrow types in event handlers (#35193)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+30.sha-dce230e
+ * @license Angular v9.0.0+31.sha-dea1b96
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5362,7 +5362,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0+30.sha-dce230e');
+    var VERSION = new i0.Version('9.0.0+31.sha-dea1b96');
 
     /**
      * @license
