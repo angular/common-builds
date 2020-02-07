6162f1eed5f66a266b3828a4450cc0d09eb3c040
Kara Erickson <karakara@google.com>
2020-02-07 19:56:54 +0000

    dce230eb00 docs: fix awkard space due to margin in sections with `alert` class (#35113)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+19.sha-14fc181
+ * @license Angular v9.0.0+30.sha-dce230e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5362,7 +5362,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0+19.sha-14fc181');
+    var VERSION = new i0.Version('9.0.0+30.sha-dce230e');
 
     /**
      * @license
--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+19.sha-14fc181
+ * @license Angular v9.0.0+30.sha-dce230e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6972,7 +6972,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0+19.sha-14fc181');
+const VERSION = new Version('9.0.0+30.sha-dce230e');
 
 /**
  * @fileoverview added by tsickle
