bb5ba41666fc836776023276ba5640abd7fd960e
Kara Erickson <karakara@google.com>
2020-02-11 21:49:37 +0000

    afdd405995 test: use puppeteer in aio build instead to remove CI_CHROMEDRIVER_VERSION_ARG (#35049)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+985.sha-7c9735a
+ * @license Angular v9.0.0-rc.1+988.sha-afdd405
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7416,7 +7416,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+985.sha-7c9735a');
+const VERSION = new Version('9.0.0-rc.1+988.sha-afdd405');
 
 /**
  * @fileoverview added by tsickle
