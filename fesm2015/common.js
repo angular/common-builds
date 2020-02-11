d9419ea1d1cd7b49e2e1d5c69910302813e193c1
Kara Erickson <karakara@google.com>
2020-02-11 20:06:14 +0000

    94d002b64e fix(elements): schematics fails with schema.json not found error (#35211)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+46.sha-3cc24a9
+ * @license Angular v9.0.0+47.sha-94d002b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6972,7 +6972,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0+46.sha-3cc24a9');
+const VERSION = new Version('9.0.0+47.sha-94d002b');
 
 /**
  * @fileoverview added by tsickle
