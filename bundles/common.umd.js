b255513aa18569bb4af4ab9fdda9030a4c9ac289
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-15 00:05:13 +0000

    c9d80b2fc8 docs(forms): remove outdated ngForm selector deprecation notice (#35435)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.1+24.sha-eee8c7f
+ * @license Angular v9.0.1+27.sha-c9d80b2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5362,7 +5362,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.1+24.sha-eee8c7f');
+    var VERSION = new i0.Version('9.0.1+27.sha-c9d80b2');
 
     /**
      * @license
