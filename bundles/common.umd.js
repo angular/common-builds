6b0fa45e1298ae0c05106396bcc5272ea4b41970
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-18 21:09:41 +0000

    7b56daffe6 fix(docs-infra): add fallback to web fonts so downloading does not block rendering (#35352)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+29.sha-d7c4f40
+ * @license Angular v9.1.0-next.0+33.sha-7b56daf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5373,7 +5373,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.0+29.sha-d7c4f40');
+    var VERSION = new i0.Version('9.1.0-next.0+33.sha-7b56daf');
 
     /**
      * @license
