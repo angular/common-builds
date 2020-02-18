03add2f308c1e2d0b628eef7f821dbf3122190d1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-18 21:19:37 +0000

    45c7b23cc8 fix(docs-infra): set `th`/`td` to proper width (#35437)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.1+27.sha-c9d80b2
+ * @license Angular v9.0.1+37.sha-45c7b23
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6972,7 +6972,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.1+27.sha-c9d80b2');
+const VERSION = new Version('9.0.1+37.sha-45c7b23');
 
 /**
  * @fileoverview added by tsickle
