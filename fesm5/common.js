dcce8ad91337bdef3ded0ed7d1428772a0d25db8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-19 21:23:30 +0000

    3a97972e58 fix(docs-infra): improve focus styles in topnav and footer (#33255)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.2
+ * @license Angular v9.0.2+3.sha-3a97972
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5226,7 +5226,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.2');
+var VERSION = new Version('9.0.2+3.sha-3a97972');
 
 /**
  * @license
