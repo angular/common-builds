6b3699d86b5000dd9adfcfab464b1fc1f952aead
Misko Hevery <misko@hevery.com>
2020-02-27 05:16:09 +0000

    c78df781f7 release: cut the v9.0.3 release

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.2+74.sha-dc800b2
+ * @license Angular v9.0.3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5362,7 +5362,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.2+74.sha-dc800b2');
+    var VERSION = new i0.Version('9.0.3');
 
     /**
      * @license
