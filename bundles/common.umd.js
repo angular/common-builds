42371b67d1258ea448ea8e2134e2d3f072826e6b
atscott <atscott@google.com>
2020-03-02 16:51:46 +0000

    48025eb3e8 fix(core): treat `[class]` and `[className]` as unrelated bindings (#35668)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.4+14.sha-e13fcba
+ * @license Angular v9.0.4+22.sha-48025eb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5364,7 +5364,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.4+14.sha-e13fcba');
+    var VERSION = new i0.Version('9.0.4+22.sha-48025eb');
 
     /**
      * @license
