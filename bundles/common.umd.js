3dc3ae32a3baa8872648eee3238573d5b08a11b8
atscott <atscott@google.com>
2020-03-03 17:14:32 +0000

    c195d22f68 fix(core): remove side effects from `ɵɵgetInheritedFactory()` (#35769)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+41.sha-47a1811
+ * @license Angular v9.1.0-next.2+46.sha-c195d22
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5375,7 +5375,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.2+41.sha-47a1811');
+    var VERSION = new i0.Version('9.1.0-next.2+46.sha-c195d22');
 
     /**
      * @license
