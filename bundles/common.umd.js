0b9ba00f53cbb806c7661321fe3148724b09cc30
atscott <atscott@google.com>
2020-03-04 00:29:27 +0000

    d2d4225ce6 Revert "fix(core): log error instead of warning for unknown properties and elements (#35798)" (#35845)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.4+44.sha-218e82e
+ * @license Angular v9.0.4+45.sha-d2d4225
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5364,7 +5364,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.4+44.sha-218e82e');
+    var VERSION = new i0.Version('9.0.4+45.sha-d2d4225');
 
     /**
      * @license
