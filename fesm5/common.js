0028a234064fd1fab5a593015d459d4cbadb16b0
Matias Niemelä <matias@yearofmoo.com>
2020-03-04 22:12:14 +0000

    de67978a7d fix(router): state data missing in routerLink (#33203)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+73.sha-b57be3b
+ * @license Angular v9.1.0-next.2+74.sha-de67978
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5239,7 +5239,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+73.sha-b57be3b');
+var VERSION = new Version('9.1.0-next.2+74.sha-de67978');
 
 /**
  * @license
