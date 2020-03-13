afe8f83ced84ad5191fa415854dccc467596c1e3
Andrew Kushnir <akushnir@google.com>
2020-03-13 15:21:39 +0000

    a8cc6a2ea2 build: enable forms tests on IE (#35971)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.6+28.sha-f699c96
+ * @license Angular v9.0.6+36.sha-a8cc6a2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5364,7 +5364,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.6+28.sha-f699c96');
+    var VERSION = new i0.Version('9.0.6+36.sha-a8cc6a2');
 
     /**
      * @license
