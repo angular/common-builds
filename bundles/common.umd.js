a3e6fa1a4b912b62b4b1a4c60c3caf7852eacf4d
Andrew Kushnir <akushnir@google.com>
2020-03-17 16:50:33 +0000

    8da4adc4dd feat(dev-infra): create common config file loading util (#36091)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.6+42.sha-ec750ca
+ * @license Angular v9.0.6+44.sha-8da4adc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5364,7 +5364,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.6+42.sha-ec750ca');
+    var VERSION = new i0.Version('9.0.6+44.sha-8da4adc');
 
     /**
      * @license
