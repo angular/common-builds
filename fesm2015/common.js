d406e3cfbb76b3c293157936b2a0785de13263ef
Misko Hevery <misko@hevery.com>
2020-03-24 17:40:19 +0000

    ae28d7c0b2 build(docs-infra): upgrade cli command docs sources to 56c648827 (#36224)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+22.sha-58f4254
+ * @license Angular v9.1.0-rc.0+27.sha-ae28d7c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -4308,6 +4308,7 @@ if (false) {
  * more complex then a property access, for example when using the async pipe (`userStreams |
  * async`).
  * - `index: number`: The index of the current item in the iterable.
+ * - `count: number`: The length of the iterable.
  * - `first: boolean`: True when the item is the first item in the iterable.
  * - `last: boolean`: True when the item is the last item in the iterable.
  * - `even: boolean`: True when the item has an even index in the iterable.
@@ -7418,7 +7419,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+22.sha-58f4254');
+const VERSION = new Version('9.1.0-rc.0+27.sha-ae28d7c');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+22.sha-58f4254
+ * @license Angular v9.1.0-rc.0+27.sha-ae28d7c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3055,6 +3055,7 @@ var NgForOfContext = /** @class */ (function () {
  * more complex then a property access, for example when using the async pipe (`userStreams |
  * async`).
  * - `index: number`: The index of the current item in the iterable.
+ * - `count: number`: The length of the iterable.
  * - `first: boolean`: True when the item is the first item in the iterable.
  * - `last: boolean`: True when the item is the last item in the iterable.
  * - `even: boolean`: True when the item has an even index in the iterable.
@@ -5256,7 +5257,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+22.sha-58f4254');
+var VERSION = new Version('9.1.0-rc.0+27.sha-ae28d7c');
 
 /**
  * @license
