abb032443ee263de5357bc0cbf621bcc39e85b57
Misko Hevery <misko@hevery.com>
2020-03-24 17:36:25 +0000

    380de1e7b4 fix(ngcc): use path-mappings from tsconfig in dependency resolution (#36180)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+21.sha-4419907
+ * @license Angular v9.1.0-rc.0+26.sha-380de1e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -4170,6 +4170,7 @@ if (false) {
  * more complex then a property access, for example when using the async pipe (`userStreams |
  * async`).
  * - `index: number`: The index of the current item in the iterable.
+ * - `count: number`: The length of the iterable.
  * - `first: boolean`: True when the item is the first item in the iterable.
  * - `last: boolean`: True when the item is the last item in the iterable.
  * - `even: boolean`: True when the item has an even index in the iterable.
@@ -6986,7 +6987,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+21.sha-4419907');
+const VERSION = new Version('9.1.0-rc.0+26.sha-380de1e');
 
 /**
  * @fileoverview added by tsickle
