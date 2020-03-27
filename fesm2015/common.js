2088aa69e94f24f14f133c59a83a71eeffdfd939
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-27 18:22:04 +0000

    8e0dec538e docs: clarify observables doc with new titles and tooltips (#36023)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+46.sha-bfa7b1a
+ * @license Angular v9.1.0-rc.0+45.sha-8e0dec5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6987,7 +6987,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+46.sha-bfa7b1a');
+const VERSION = new Version('9.1.0-rc.0+45.sha-8e0dec5');
 
 /**
  * @fileoverview added by tsickle
