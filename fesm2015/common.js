15e2d43b6f0327843dd5a1fd63b441c11a89d15c
Kara Erickson <karakara@google.com>
2020-04-01 23:04:54 +0000

    75afd80ae8 refactor(compiler): add `@nocollapse` annotation using a synthetic comment (#35932)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+90.sha-e9de281
+ * @license Angular v9.1.0-rc.0+89.sha-75afd80
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6987,7 +6987,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+90.sha-e9de281');
+const VERSION = new Version('9.1.0-rc.0+89.sha-75afd80');
 
 /**
  * @fileoverview added by tsickle
