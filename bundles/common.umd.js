68772fa3c016dc9f867638c95049d071ba671ac6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-04-01 20:55:33 +0000

    cb0a2a055d fix(ngcc): allow ngcc configuration to match pre-release versions of packages (#36370)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+27.sha-beb3f19
+ * @license Angular v9.1.0+43.sha-cb0a2a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5376,7 +5376,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0+27.sha-beb3f19');
+    var VERSION = new i0.Version('9.1.0+43.sha-cb0a2a0');
 
     /**
      * @license
