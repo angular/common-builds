02d919e6339c02a742632f2c317d1dc1fbf6a4b5
Kara Erickson <karakara@google.com>
2020-04-02 19:04:42 +0000

    9f486c31d2 build: enable `strictTemplates` in AIO (#36391)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+49.sha-c3ce190
+ * @license Angular v9.1.0+56.sha-9f486c3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6987,7 +6987,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0+49.sha-c3ce190');
+const VERSION = new Version('9.1.0+56.sha-9f486c3');
 
 /**
  * @fileoverview added by tsickle
