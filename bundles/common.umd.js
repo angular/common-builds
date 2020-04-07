79e99bf62f9292dbc8dc49abf28c64c56081bbdc
Kara Erickson <karakara@google.com>
2020-04-07 18:56:20 +0000

    b02c950aac docs(forms): Remove unnecessary repeating periods (#36474) PR Close #36474

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+95.sha-af42694
+ * @license Angular v9.1.0+99.sha-b02c950
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5393,7 +5393,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0+95.sha-af42694');
+    var VERSION = new i0.Version('9.1.0+99.sha-b02c950');
 
     /**
      * @license
