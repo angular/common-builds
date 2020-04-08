3cd6935898bc5a198885fc2c3ccfa7c0837c3190
Kara Erickson <karakara@google.com>
2020-04-08 18:25:36 +0000

    52ab9397a0 build: update matching regex for bazel stamping (#36523)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+106.sha-fd2ef74
+ * @license Angular v9.1.1+1.sha-52ab939
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5393,7 +5393,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0+106.sha-fd2ef74');
+    var VERSION = new i0.Version('9.1.1+1.sha-52ab939');
 
     /**
      * @license
--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+106.sha-fd2ef74
+ * @license Angular v9.1.1+1.sha-52ab939
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6987,7 +6987,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0+106.sha-fd2ef74');
+const VERSION = new Version('9.1.1+1.sha-52ab939');
 
 /**
  * @fileoverview added by tsickle
