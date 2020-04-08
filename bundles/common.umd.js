febeb5e75e496fadc53ce7eb76f385513ff2b0e8
Kara Erickson <karakara@google.com>
2020-04-08 19:32:14 +0000

    72053b0f27 build(bazel): fix runfiles resolve in karma-saucelabs.js after $location => $rootpath cleanup (#36511)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+1.sha-52ab939
+ * @license Angular v9.1.1+8.sha-72053b0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5393,7 +5393,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.1+1.sha-52ab939');
+    var VERSION = new i0.Version('9.1.1+8.sha-72053b0');
 
     /**
      * @license
