95567f723e29fdef1a761a23928749512a027ddd
atscott <atscott@google.com>
2020-04-09 20:56:48 +0000

    58d028178f fix(docs-infra): fix `elements` example when used with ES5 (#36536)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+25.sha-e06512b
+ * @license Angular v9.1.1+26.sha-58d0281
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5393,7 +5393,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.1+25.sha-e06512b');
+    var VERSION = new i0.Version('9.1.1+26.sha-58d0281');
 
     /**
      * @license
