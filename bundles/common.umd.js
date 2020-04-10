c2569767c57b89235e861c43e2c97244ac4dae97
atscott <atscott@google.com>
2020-04-10 21:14:11 +0000

    3992341d34 fix(core): undecorated-classes-with-decorated-fields migration should avoid error if base class has no value declaration (#36543)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+28.sha-8c559ef
+ * @license Angular v9.1.1+29.sha-3992341
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5393,7 +5393,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.1+28.sha-8c559ef');
+    var VERSION = new i0.Version('9.1.1+29.sha-3992341');
 
     /**
      * @license
--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+28.sha-8c559ef
+ * @license Angular v9.1.1+29.sha-3992341
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6987,7 +6987,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.1+28.sha-8c559ef');
+const VERSION = new Version('9.1.1+29.sha-3992341');
 
 /**
  * @fileoverview added by tsickle
