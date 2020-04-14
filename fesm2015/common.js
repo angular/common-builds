5e9b6d3eb91dbd2f4fadea2dae3423260dd60d6c
atscott <atscott@google.com>
2020-04-14 17:39:17 +0000

    5e80e7e216 refactor(language-service): clean up and exports and consolidate types (#36533)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+31.sha-d50cb30
+ * @license Angular v10.0.0-next.1+32.sha-5e80e7e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7419,7 +7419,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+31.sha-d50cb30');
+const VERSION = new Version('10.0.0-next.1+32.sha-5e80e7e');
 
 /**
  * @fileoverview added by tsickle
