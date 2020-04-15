01bd591e8acba8a41deaa1d58dbbbb2faf380fe9
atscott <atscott@google.com>
2020-04-15 16:43:39 +0000

    12266b2042 fix(ngcc): display output from the unlocker process on Windows (#36569)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+42.sha-c5e7251
+ * @license Angular v9.1.1+45.sha-12266b2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.1+42.sha-c5e7251');
+    var VERSION = new i0.Version('9.1.1+45.sha-12266b2');
 
     /**
      * @license
