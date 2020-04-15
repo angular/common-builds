208dca7c09bec27b81293a39eda4494f946c9e43
atscott <atscott@google.com>
2020-04-15 23:38:34 +0000

    5c3774cfe6 Revert "fix(common): `locales/global/*.js` are not ES5 compliant (#36342)" (#36648)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+45.sha-12266b2
+ * @license Angular v9.1.1+46.sha-5c3774c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.1+45.sha-12266b2');
+    var VERSION = new i0.Version('9.1.1+46.sha-5c3774c');
 
     /**
      * @license
