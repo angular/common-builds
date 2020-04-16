417eef737b4f99dc1708ebf8d91b25aaba11863c
atscott <atscott@google.com>
2020-04-16 17:02:22 +0000

    03ff380e13 docs: add " in architecture page (#36564)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+1.sha-c6e5fc4
+ * @license Angular v10.0.0-next.2+2.sha-03ff380
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5487,7 +5487,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.2+1.sha-c6e5fc4');
+    var VERSION = new i0.Version('10.0.0-next.2+2.sha-03ff380');
 
     /**
      * @license
