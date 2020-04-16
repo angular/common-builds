d0549ebc9ccca05760aafe995ed435ab4377fab2
atscott <atscott@google.com>
2020-04-16 17:03:04 +0000

    1e3b8a1354 docs: remove unneeded code from `universal` example/guide (#36483)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+2.sha-03ff380
+ * @license Angular v10.0.0-next.2+12.sha-1e3b8a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7081,7 +7081,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+2.sha-03ff380');
+const VERSION = new Version('10.0.0-next.2+12.sha-1e3b8a1');
 
 /**
  * @fileoverview added by tsickle
