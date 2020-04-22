0b0d8eb06b6cee8b136c1abbc2532a7b83f1ddd2
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 02:16:35 +0000

    a6e10ef869 build: update `REQUIRED_BASE_SHA` in merge script to `commit-message` script update commit (#36750)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+52.sha-c0ed57d
+ * @license Angular v9.1.2+54.sha-a6e10ef
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5487,7 +5487,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.2+52.sha-c0ed57d');
+    var VERSION = new i0.Version('9.1.2+54.sha-a6e10ef');
 
     /**
      * @license
--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+52.sha-c0ed57d
+ * @license Angular v9.1.2+54.sha-a6e10ef
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5334,7 +5334,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.2+52.sha-c0ed57d');
+var VERSION = new Version('9.1.2+54.sha-a6e10ef');
 
 /**
  * @license
