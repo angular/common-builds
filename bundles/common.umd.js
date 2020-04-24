1cd243e85019796a323b560055c023b60b25553c
Andrew Kushnir <akushnir@google.com>
2020-04-24 16:26:39 +0000

    b3fe9017f7 ci: migrate payload size tracking goldens to the golden directory (#36455)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+18.sha-d840503
+ * @license Angular v9.1.3+21.sha-b3fe901
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5471,7 +5471,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.3+18.sha-d840503');
+    var VERSION = new i0.Version('9.1.3+21.sha-b3fe901');
 
     /**
      * @license
