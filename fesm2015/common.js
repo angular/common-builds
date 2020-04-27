c1a9da8130cd6a89b16666f517bd97d5747fe931
Andrew Kushnir <akushnir@google.com>
2020-04-27 21:32:09 +0000

    2a27f69522 docs: fix typo (#36786)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+26.sha-180a328
+ * @license Angular v9.1.3+27.sha-2a27f69
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7065,7 +7065,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.3+26.sha-180a328');
+const VERSION = new Version('9.1.3+27.sha-2a27f69');
 
 /**
  * @fileoverview added by tsickle
