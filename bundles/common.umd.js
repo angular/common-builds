c7273cad87278f6b0c09dd55cdbd0d4436422a98
Andrew Kushnir <akushnir@google.com>
2020-04-28 23:19:08 +0000

    c440165384 fix(ngcc): recognize enum declarations emitted in JavaScript (#36550)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+34.sha-0ce96f1
+ * @license Angular v9.1.3+35.sha-c440165
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5471,7 +5471,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.3+34.sha-0ce96f1');
+    var VERSION = new i0.Version('9.1.3+35.sha-c440165');
 
     /**
      * @license
