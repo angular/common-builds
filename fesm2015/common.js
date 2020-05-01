35d735b2b8406bc41ce4ca664ad8ffd2df425f9e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-01 17:21:36 +0000

    a0342763b8 build(language-service): make test project a filegroup (#36865)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.4+14.sha-c8c2272
+ * @license Angular v9.1.4+17.sha-a034276
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7065,7 +7065,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.4+14.sha-c8c2272');
+const VERSION = new Version('9.1.4+17.sha-a034276');
 
 /**
  * @fileoverview added by tsickle
