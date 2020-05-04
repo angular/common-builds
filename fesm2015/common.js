fb6ac724338dd9a4e001928d4188a24f6b32b64b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:17:27 +0000

    75370107c2 docs(dev-infra): update format commands described in DEVELOPER.md (#36910)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.4+17.sha-a034276
+ * @license Angular v9.1.4+26.sha-7537010
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7065,7 +7065,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.4+17.sha-a034276');
+const VERSION = new Version('9.1.4+26.sha-7537010');
 
 /**
  * @fileoverview added by tsickle
