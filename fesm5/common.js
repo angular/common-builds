75b474b77f61d5c81cbf468cbe5fa43c8aaf78b6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-05 19:25:44 +0000

    b95a336f12 build: fix and re-enable elements tests on saucelabs (#36929)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+7.sha-306f46c
+ * @license Angular v10.0.0-next.5+18.sha-b95a336
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5342,7 +5342,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.5+7.sha-306f46c');
+var VERSION = new Version('10.0.0-next.5+18.sha-b95a336');
 
 /**
  * @license
