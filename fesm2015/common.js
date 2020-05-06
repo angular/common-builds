164a0f6ff45e269c305bfc7ab8dd6446d7140a35
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 21:13:41 +0000

    3c6c00d1d4 docs: remove the note about @angular/http from the deprecations guide (#36944)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+45.sha-fafa50d
+ * @license Angular v10.0.0-next.5+53.sha-3c6c00d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7067,7 +7067,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.5+45.sha-fafa50d');
+const VERSION = new Version('10.0.0-next.5+53.sha-3c6c00d');
 
 /**
  * @fileoverview added by tsickle
