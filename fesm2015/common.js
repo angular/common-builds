529eeb2f05af069520f2f510ac06eaa280fd7864
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 22:22:07 +0000

    420b9be1c1 refactor: disable sanitization for [style] and [style.prop] bindings (#35621)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+53.sha-3c6c00d
+ * @license Angular v10.0.0-next.5+54.sha-420b9be
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7499,7 +7499,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.5+53.sha-3c6c00d');
+const VERSION = new Version('10.0.0-next.5+54.sha-420b9be');
 
 /**
  * @fileoverview added by tsickle
