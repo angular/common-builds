08e017c1a8e5bf8258c1bfb943fedc60b7d444da
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 02:00:02 +0000

    ecffc3557f perf(compiler-cli): perform template type-checking incrementally (#36211)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+25.sha-388dc93
+ * @license Angular v10.0.0-next.5+42.sha-ecffc35
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5325,7 +5325,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.5+25.sha-388dc93');
+var VERSION = new Version('10.0.0-next.5+42.sha-ecffc35');
 
 /**
  * @license
