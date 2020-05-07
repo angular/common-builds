3041d3b49288a82f5581d46eb2ce00163961b316
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-07 22:05:58 +0000

    d0280a0335 fix(compiler): remove outdated and invalid warning for unresolved DI parameters (#36985)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+72.sha-97d6d90
+ * @license Angular v10.0.0-next.5+73.sha-d0280a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7499,7 +7499,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.5+72.sha-97d6d90');
+const VERSION = new Version('10.0.0-next.5+73.sha-d0280a0');
 
 /**
  * @fileoverview added by tsickle
