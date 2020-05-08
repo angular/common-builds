f0e813928754d21671d4645ff2a4aa4f04ed971c
Misko Hevery <misko@hevery.com>
2020-05-08 22:03:49 +0000

    bd30c37040 fix(docs-infra): add visual signal for external links (#36601)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+16.sha-eb34aa5
+ * @license Angular v10.0.0-next.6+17.sha-bd30c37
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7067,7 +7067,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+16.sha-eb34aa5');
+const VERSION = new Version('10.0.0-next.6+17.sha-bd30c37');
 
 /**
  * @fileoverview added by tsickle
