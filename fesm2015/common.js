58a242aad2c75f0dda71c42511696e445ac86daf
Misko Hevery <misko@hevery.com>
2020-05-08 22:06:13 +0000

    ed1b4a8f19 docs: add migration guides to sidenav in updating to Version 9 (#34979)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+17.sha-bd30c37
+ * @license Angular v10.0.0-next.6+18.sha-ed1b4a8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7067,7 +7067,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+17.sha-bd30c37');
+const VERSION = new Version('10.0.0-next.6+18.sha-ed1b4a8');
 
 /**
  * @fileoverview added by tsickle
