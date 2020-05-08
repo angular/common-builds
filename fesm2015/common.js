bf5c9fb23c1fceb6f49a353f6abe71ab9be8fbaf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 00:28:32 +0000

    d1b3af697c release: cut the v9.1.5 release

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.4+67.sha-dcf1dcb
+ * @license Angular v9.1.5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7065,7 +7065,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.4+67.sha-dcf1dcb');
+const VERSION = new Version('9.1.5');
 
 /**
  * @fileoverview added by tsickle
