6df423e23e58c7764774f73ebfa14de45d5df5b8
Misko Hevery <misko@hevery.com>
2020-05-11 18:30:06 +0000

    40025f55ad refactor(language-service): move TS utils to separate file (#36984)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+23.sha-286fbf4
+ * @license Angular v10.0.0-next.6+22.sha-40025f5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7499,7 +7499,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+23.sha-286fbf4');
+const VERSION = new Version('10.0.0-next.6+22.sha-40025f5');
 
 /**
  * @fileoverview added by tsickle
