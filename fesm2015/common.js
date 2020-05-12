501c4e814a20014b0725517e96cdc44c7bf16a03
Misko Hevery <misko@hevery.com>
2020-05-12 18:11:56 +0000

    8fe532e593 build(dev-infra): cleanup package dependencies for shared package (#36980)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+28.sha-681741c
+ * @license Angular v10.0.0-next.6+27.sha-8fe532e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7499,7 +7499,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+28.sha-681741c');
+const VERSION = new Version('10.0.0-next.6+27.sha-8fe532e');
 
 /**
  * @fileoverview added by tsickle
