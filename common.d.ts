0bc9d08a3d2780ce7dfbebf48a69288f2871729b
Misko Hevery <misko@hevery.com>
2020-05-13 23:36:14 +0000

    92b97f7d60 docs(common): English grammar (#36908)

--- common.d.ts
+++ common.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+19.sha-b91b3d7
+ * @license Angular v9.1.6+31.sha-92b97f7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2424,7 +2424,7 @@ export declare type Time = {
 
 /**
  * Transforms text to title case.
- * Capitalizes the first letter of each word, and transforms the
+ * Capitalizes the first letter of each word and transforms the
  * rest of the word to lower case.
  * Words are delimited by any whitespace character, such as a space, tab, or line-feed character.
  *
