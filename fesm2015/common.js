ce836ae746ea19e70013403d515f13552ccacdca
Misko Hevery <misko@hevery.com>
2020-05-13 23:26:19 +0000

    ce39755937 docs: Remove duplicate `and` (#37067)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+14.sha-2cacab9
+ * @license Angular v10.0.0-next.7+10.sha-ce39755
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7499,7 +7499,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.7+14.sha-2cacab9');
+const VERSION = new Version('10.0.0-next.7+10.sha-ce39755');
 
 /**
  * @fileoverview added by tsickle
