9d4d6a72ed9a56c91692d36fc477e9dfcf9f4820
Kara Erickson <karakara@google.com>
2020-05-18 22:16:24 +0000

    178a78f9ed docs(router): add docs for RouterLink inputs (#37018)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+67.sha-ba9e63a
+ * @license Angular v9.1.6+70.sha-178a78f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5318,7 +5318,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.6+67.sha-ba9e63a');
+var VERSION = new Version('9.1.6+70.sha-178a78f');
 
 /**
  * @license
