bd582d1b3f53cb10cc176bf8fe83e736c36cb8f0
Kara Erickson <karakara@google.com>
2020-05-19 18:07:33 +0000

    a63f634592 docs: remove https://angular.io from internal links (#37157)

--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+74.sha-c423457
+ * @license Angular v9.1.6+76.sha-a63f634
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5318,7 +5318,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.6+74.sha-c423457');
+var VERSION = new Version('9.1.6+76.sha-a63f634');
 
 /**
  * @license
