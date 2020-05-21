fdca7f34de392aaaeda87e29f003277f0f120371
Kara Erickson <karakara@google.com>
2020-05-21 17:57:14 +0000

    fdc5941fe7 feat(dev-infra): expose script for determining merge branches (#37217)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.9
+ * @license Angular v9.1.9+2.sha-fdc5941
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7065,7 +7065,7 @@ function isPlatformWorkerUi(platformId) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.9');
+const VERSION = new Version('9.1.9+2.sha-fdc5941');
 
 /**
  * @fileoverview added by tsickle
