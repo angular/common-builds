d1b37ed9f9c2cdfd3b8e745d2afe722067567d35
atscott <atscott@google.com>
2020-06-04 16:46:42 +0000

    57411c85b9 feat(ngcc): implement a program-based entry-point finder (#37075)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+63.sha-c61decf
+ * @license Angular v10.0.0-rc.0+67.sha-57411c8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5250,7 +5250,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+63.sha-c61decf');
+const VERSION = new Version('10.0.0-rc.0+67.sha-57411c8');
 
 /**
  * @license
