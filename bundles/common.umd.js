efb56b354ebf1bb274da17471187013b2b4215ec
atscott <atscott@google.com>
2020-06-05 17:58:31 +0000

    e9bff5fe9f fix(elements): fire custom element output events during component initialization (#36161)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+54.sha-53e1fb3
+ * @license Angular v10.0.0-rc.2+56.sha-e9bff5f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5504,7 +5504,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.2+54.sha-53e1fb3');
+    var VERSION = new i0.Version('10.0.0-rc.2+56.sha-e9bff5f');
 
     /**
      * @license
