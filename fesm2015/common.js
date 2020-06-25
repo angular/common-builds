0ce91e4cfaa37c47f2f1b2463b1118effe8aa731
Andrew Kushnir <akushnir@google.com>
2020-06-25 21:57:14 +0000

    64f2ffa166 fix(core): cleanup DOM elements when root view is removed (#37600)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0+17.sha-2cbe53a
+ * @license Angular v10.0.0+20.sha-64f2ffa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5106,7 +5106,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0+17.sha-2cbe53a');
+const VERSION = new Version('10.0.0+20.sha-64f2ffa');
 
 /**
  * @license
