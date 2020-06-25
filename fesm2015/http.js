a494b56c31baf634ae678caee6c803364694cd5f
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:32:58 +0000

    8a74508130 fix(http): avoid abort a request when fetch operation is completed (#37367)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+242.sha-77b62a5
+ * @license Angular v10.0.0-rc.0+245.sha-8a74508
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1774,7 +1774,9 @@ class HttpXhrBackend {
                     }
                 }
                 // Finally, abort the in-flight request.
-                xhr.abort();
+                if (xhr.readyState !== xhr.DONE) {
+                    xhr.abort();
+                }
             };
         });
     }
