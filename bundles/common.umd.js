474728d3e58c29f64796ec3f0eb6ebf82dd7ee80
Igor Minar <igor@angular.io>
2020-06-25 22:56:46 +0000

    20c44a6240 fix(docs-infra): fix typo in the deploy-to-firebase.sh script

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.11+3.sha-0feb560
+ * @license Angular v9.1.11+4.sha-20c44a6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5471,7 +5471,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.11+3.sha-0feb560');
+    var VERSION = new i0.Version('9.1.11+4.sha-20c44a6');
 
     /**
      * @license
--- fesm5/common.js
+++ fesm5/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.11+3.sha-0feb560
+ * @license Angular v9.1.11+4.sha-20c44a6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5318,7 +5318,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.11+3.sha-0feb560');
+var VERSION = new Version('9.1.11+4.sha-20c44a6');
 
 /**
  * @license
