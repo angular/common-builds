4115058ed59a047fbfea7e0d3d68e8891f3b944f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-06 21:17:27 +0000

    1801d0c650 fix(router): properly compare array queryParams for equality (#37709) (#37860)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+314.sha-d7dd295
+ * @license Angular v10.0.0-rc.0+316.sha-1801d0c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5158,7 +5158,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+314.sha-d7dd295');
+const VERSION = new Version('10.0.0-rc.0+316.sha-1801d0c');
 
 /**
  * @license
