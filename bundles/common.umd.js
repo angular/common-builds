820346b8b507ca4f1aff54be518d97d7d8876c50
atscott <atscott@google.com>
2020-07-08 23:31:08 +0000

    bbe3543c69 refactor(core): remove duplicated WrappedValue class (#37940)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+10.sha-e50c7dc
+ * @license Angular v10.1.0-next.0+4.sha-bbe3543
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5522,7 +5522,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.0+10.sha-e50c7dc');
+    var VERSION = new i0.Version('10.1.0-next.0+4.sha-bbe3543');
 
     /**
      * @license
--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+10.sha-e50c7dc
+ * @license Angular v10.1.0-next.0+4.sha-bbe3543
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5108,7 +5108,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.0+10.sha-e50c7dc');
+const VERSION = new Version('10.1.0-next.0+4.sha-bbe3543');
 
 /**
  * @license
