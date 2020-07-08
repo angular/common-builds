3f0d117df84868df34cb723381db74ce919db829
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-08 21:51:22 +0000

    f24972b1b1 release: cut the v10.0.3 release

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.2+12.sha-d2886b3
+ * @license Angular v10.0.3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5482,7 +5482,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.2+12.sha-d2886b3');
+    var VERSION = new i0.Version('10.0.3');
 
     /**
      * @license
