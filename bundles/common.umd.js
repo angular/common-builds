82805da39bce359d995c58448c0045fb0454bdc7
atscott <atscott01@gmail.com>
2020-07-09 22:57:23 +0000

    1550663b9e fix(bazel): ng_module rule does not expose flat module information in Ivy (#36971)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+18.sha-c40cbec
+ * @license Angular v10.1.0-next.0+19.sha-1550663
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5522,7 +5522,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.0+18.sha-c40cbec');
+    var VERSION = new i0.Version('10.1.0-next.0+19.sha-1550663');
 
     /**
      * @license
