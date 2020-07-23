99d208237db1e0cbdd506c61b97dc3e092781e75
Misko Hevery <misko@hevery.com>
2020-07-23 04:18:36 +0000

    3c1866b5bb docs: update api reference for router outlet directive (#38166)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+11.sha-062b8d9
+ * @license Angular v10.1.0-next.2+12.sha-3c1866b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5108,7 +5108,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+11.sha-062b8d9');
+const VERSION = new Version('10.1.0-next.2+12.sha-3c1866b');
 
 /**
  * @license
