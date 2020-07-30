98cb8128e29b3bdc177a744b2fb78c4427840554
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-30 22:43:40 +0000

    879ff08f0e fix(compiler): Metadata should not include methods on Object.prototype (#38292)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.6+20.sha-a15d7ac
+ * @license Angular v10.0.6+21.sha-879ff08f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5110,7 +5110,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.6+20.sha-a15d7ac');
+const VERSION = new Version('10.0.6+21.sha-879ff08f');
 
 /**
  * @license
