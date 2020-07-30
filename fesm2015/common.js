258c875380bcbc6510652cad0e67a56a61927c45
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-30 19:45:18 +0000

    a15d7ac1da Revert "fix(compiler): mark `NgModuleFactory` construction as not side effectful (#38147)" (#38303)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.6+19.sha-4a6abbd
+ * @license Angular v10.0.6+20.sha-a15d7ac
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5110,7 +5110,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.6+19.sha-4a6abbd');
+const VERSION = new Version('10.0.6+20.sha-a15d7ac');
 
 /**
  * @license
