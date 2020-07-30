a9d8f65350c49b6d73ed5f9235079a9ebaae5f10
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-30 01:11:06 +0000

    1eebb7f189 test(compiler-cli): disable one TypeChecker test on Windows due to path sensitivity issue (#38294)

--- common.d.ts
+++ common.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+16.sha-7f8c222
+ * @license Angular v10.1.0-next.3+18.sha-1eebb7f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2191,16 +2191,20 @@ export declare enum NumberSymbol {
  * browser's URL.
  *
  * If you're using `PathLocationStrategy`, you must provide a {@link APP_BASE_HREF}
- * or add a base element to the document. This URL prefix that will be preserved
- * when generating and recognizing URLs.
+ * or add a `<base href>` element to the document.
  *
- * For instance, if you provide an `APP_BASE_HREF` of `'/my/app'` and call
+ * For instance, if you provide an `APP_BASE_HREF` of `'/my/app/'` and call
+ * `location.go('/foo')`, the browser's URL will become
+ * `example.com/my/app/foo`. To ensure all relative URIs resolve correctly,
+ * the `<base href>` and/or `APP_BASE_HREF` should end with a `/`.
+ *
+ * Similarly, if you add `<base href='/my/app/'/>` to the document and call
  * `location.go('/foo')`, the browser's URL will become
  * `example.com/my/app/foo`.
  *
- * Similarly, if you add `<base href='/my/app'/>` to the document and call
- * `location.go('/foo')`, the browser's URL will become
- * `example.com/my/app/foo`.
+ * Note that when using `PathLocationStrategy`, neither the query nor
+ * the fragment in the `<base href>` will be preserved, as outlined
+ * by the [RFC](https://tools.ietf.org/html/rfc3986#section-5.2.2).
  *
  * @usageNotes
  *
