20e93806336ec1467d7d59b34a3f51a80dffce1c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-08-04 23:28:49 +0000

    2ab8e88924 release: cut the v10.0.8 release

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.7+20.sha-a91dd2e
+ * @license Angular v10.0.8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5524,7 +5524,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.7+20.sha-a91dd2e');
+    var VERSION = new i0.Version('10.0.8');
 
     /**
      * @license
--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.7+20.sha-a91dd2e
+ * @license Angular v10.0.8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5110,7 +5110,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.7+20.sha-a91dd2e');
+const VERSION = new Version('10.0.8');
 
 /**
  * @license
