0056d672913e7ed059be92d10f1facd7d660e7f9
Andrew Kushnir <akushnir@google.com>
2020-08-05 18:17:22 +0000

    f0766a4474 feat(dev-infra): provide organization-wide merge-tool label configuration (#38223)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+1.sha-585ef2b
+ * @license Angular v10.1.0-next.4+4.sha-f0766a4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5112,7 +5112,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+1.sha-585ef2b');
+const VERSION = new Version('10.1.0-next.4+4.sha-f0766a4');
 
 /**
  * @license
