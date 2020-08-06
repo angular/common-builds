0986203d85d3ae72da8363352380ae095397db33
Andrew Kushnir <akushnir@google.com>
2020-08-06 00:29:44 +0000

    0c2490368e refactor(platform-browser): specify return type of parseEventName (#38089)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+9.sha-bb88c9f
+ * @license Angular v10.1.0-next.4+10.sha-0c24903
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5162,7 +5162,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+9.sha-bb88c9f');
+const VERSION = new Version('10.1.0-next.4+10.sha-0c24903');
 
 /**
  * @license
