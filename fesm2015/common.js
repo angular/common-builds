5338cc8147d4ee8b9182269ac8e2c8404fc1fe14
Andrew Kushnir <akushnir@google.com>
2020-08-06 00:36:19 +0000

    db897f4f7a docs: add a page with the Angular roadmap (#38356)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+6.sha-4151314
+ * @license Angular v10.0.8+8.sha-db897f4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5110,7 +5110,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.8+6.sha-4151314');
+const VERSION = new Version('10.0.8+8.sha-db897f4');
 
 /**
  * @license
