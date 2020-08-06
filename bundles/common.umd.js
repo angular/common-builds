70616f0211689e0f9327880c931e5e8bbba28054
Andrew Kushnir <akushnir@google.com>
2020-08-06 22:42:51 +0000

    f5d5bac076 fix(service-worker): fix the chrome debugger syntax highlighter (#38332)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+16.sha-ad16e2f
+ * @license Angular v10.0.8+17.sha-f5d5bac
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5524,7 +5524,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.8+16.sha-ad16e2f');
+    var VERSION = new i0.Version('10.0.8+17.sha-f5d5bac');
 
     /**
      * @license
