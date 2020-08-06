7c8e7b49c338f65abf4a96fffde3170e1f42d65f
Andrew Kushnir <akushnir@google.com>
2020-08-06 19:01:51 +0000

    0551fbdf88 fix(docs-infra): correctly generate CLI commands docs when the overview page moves (#38365)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+17.sha-18cd1a9
+ * @license Angular v10.1.0-next.4+16.sha-0551fbd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5112,7 +5112,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+17.sha-18cd1a9');
+const VERSION = new Version('10.1.0-next.4+16.sha-0551fbd');
 
 /**
  * @license
