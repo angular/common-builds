65220748a502c133301fffcfdbdf5a429f256f7e
Andrew Kushnir <akushnir@google.com>
2020-08-07 19:31:14 +0000

    47fbfb37bf refactor(forms): get rid of duplicate functions (#38371) (#38380)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+22.sha-4107abb
+ * @license Angular v10.0.8+23.sha-47fbfb3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5110,7 +5110,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.8+22.sha-4107abb');
+const VERSION = new Version('10.0.8+23.sha-47fbfb3');
 
 /**
  * @license
