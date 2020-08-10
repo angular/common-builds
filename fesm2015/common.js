0fe1e294564fda06edcca9f7f1a125c8f9085073
Andrew Kushnir <akushnir@google.com>
2020-08-10 20:33:37 +0000

    7f111491a8 fix(platform-server): remove styles added by ServerStylesHost on destruction (#38367)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+27.sha-ee5123f
+ * @license Angular v10.0.8+28.sha-7f11149
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5110,7 +5110,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.8+27.sha-ee5123f');
+const VERSION = new Version('10.0.8+28.sha-7f11149');
 
 /**
  * @license
