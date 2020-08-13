8eaddf12e3b3616b58f15509e8a5291d84da25c6
Andrew Scott <atscott01@gmail.com>
2020-08-13 23:32:03 +0000

    ca798804b2 fix(router): export DefaultRouteReuseStrategy to Router public_api (#31575)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+18.sha-b071495
+ * @license Angular v10.1.0-next.5+19.sha-ca79880
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5162,7 +5162,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5+18.sha-b071495');
+const VERSION = new Version('10.1.0-next.5+19.sha-ca79880');
 
 /**
  * @license
