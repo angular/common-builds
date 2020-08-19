a2ccc39504dcbe8be7ec56696f55000895b5a97f
atscott <atscott01@gmail.com>
2020-08-19 16:37:42 +0000

    a3f3082bf0 release: cut the v10.0.11 release

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.10+9.sha-0570c24
+ * @license Angular v10.0.11
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5524,7 +5524,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.10+9.sha-0570c24');
+    var VERSION = new i0.Version('10.0.11');
 
     /**
      * @license
