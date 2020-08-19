c14e3912013f17068c28ad77ade1219048128214
Misko Hevery <misko@hevery.com>
2020-08-19 21:41:45 +0000

    552853648c fix(core): remove closing body tag from inert DOM builder (#38454)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.11
+ * @license Angular v10.0.11+5.sha-5528536
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5524,7 +5524,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.11');
+    var VERSION = new i0.Version('10.0.11+5.sha-5528536');
 
     /**
      * @license
