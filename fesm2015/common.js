375ad77e4171ec935af189d40c9b08cbb18f9f2d
Misko Hevery <misko@hevery.com>
2020-08-21 19:47:49 +0000

    e7da4040d6 fix(compiler-cli): adding references to const enums in runtime code (#38542)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+16.sha-2a643e1
+ * @license Angular v10.1.0-next.7+17.sha-e7da404
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5162,7 +5162,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.7+16.sha-2a643e1');
+const VERSION = new Version('10.1.0-next.7+17.sha-e7da404');
 
 /**
  * @license
