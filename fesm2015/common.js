3c7055eec5924175a2dff76d3fde3326900c9fff
Misko Hevery <misko@hevery.com>
2020-08-24 16:48:25 +0000

    8224e65e70 docs(localize): fix angular.json syntax (#38553)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.11+13.sha-814b436
+ * @license Angular v10.0.11+16.sha-8224e65
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5110,7 +5110,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.11+13.sha-814b436');
+const VERSION = new Version('10.0.11+16.sha-8224e65');
 
 /**
  * @license
