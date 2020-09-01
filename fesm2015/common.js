93b9e8c724fbf246ca55a4fffa60335b0d6e8d42
Joey Perrott <josephperrott@gmail.com>
2020-09-01 20:30:39 +0000

    59c234cfb4 build: add configuration for the caretaker command (#38601)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+35.sha-d9fea85
+ * @license Angular v10.1.0-rc.0+37.sha-59c234c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5112,7 +5112,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-rc.0+35.sha-d9fea85');
+const VERSION = new Version('10.1.0-rc.0+37.sha-59c234c');
 
 /**
  * @license
