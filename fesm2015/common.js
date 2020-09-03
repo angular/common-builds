1d2c0379d80a1a447e416066b68dbe5493356a45
atscott <atscott01@gmail.com>
2020-09-03 17:07:07 +0000

    50f4d8a1ce fix(localize): install `@angular/localize` in `devDependencies` by default (#38680)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+6.sha-fdea180
+ * @license Angular v11.0.0-next.0+7.sha-50f4d8a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5162,7 +5162,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.0+6.sha-fdea180');
+const VERSION = new Version('11.0.0-next.0+7.sha-50f4d8a');
 
 /**
  * @license
