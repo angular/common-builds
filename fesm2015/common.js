571c0dd1b1b70058e1723a12fc874fd6b3bb8894
atscott <atscott01@gmail.com>
2020-09-09 18:05:12 +0000

    f1b355b54f docs: Restructure table of contents to provide a more streamlined experience (#38763)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+43.sha-b1398d1
+ * @license Angular v10.1.0+46.sha-f1b355b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5112,7 +5112,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0+43.sha-b1398d1');
+const VERSION = new Version('10.1.0+46.sha-f1b355b');
 
 /**
  * @license
