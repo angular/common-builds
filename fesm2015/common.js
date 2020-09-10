5f44eca4a39f927853bdcb49a1b5814ff774312b
Andrew Kushnir <akushnir@google.com>
2020-09-10 21:54:21 +0000

    31d0ee4cbf docs: Move Displaying data topic and ToH tutorial (#38774)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+12.sha-a47383d
+ * @license Angular v10.1.1+13.sha-31d0ee4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5112,7 +5112,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.1+12.sha-a47383d');
+const VERSION = new Version('10.1.1+13.sha-31d0ee4');
 
 /**
  * @license
