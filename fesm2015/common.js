06c8852315ca1510d2bdab1d1e06dae37d652d70
Andrew Kushnir <akushnir@google.com>
2020-09-10 17:02:17 +0000

    9078187378 docs: add note about only one label target being allowed to be applied to a PR (#38793)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+9.sha-edb7f90
+ * @license Angular v10.1.1+11.sha-9078187
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5112,7 +5112,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.1+9.sha-edb7f90');
+const VERSION = new Version('10.1.1+11.sha-9078187');
 
 /**
  * @license
