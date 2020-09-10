0d7e9103dd5ea91cd16ebcb1785c8c9d3c5c554d
Andrew Kushnir <akushnir@google.com>
2020-09-10 16:32:26 +0000

    edb7f90363 fix(core): clear the `RefreshTransplantedView` when detached (#38768)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+5.sha-7669bd8
+ * @license Angular v10.1.1+9.sha-edb7f90
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5112,7 +5112,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.1+5.sha-7669bd8');
+const VERSION = new Version('10.1.1+9.sha-edb7f90');
 
 /**
  * @license
