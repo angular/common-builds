2fc0dd705d22b517c29d08aae5191f4361937dd3
Andrew Kushnir <akushnir@google.com>
2020-09-11 16:05:13 +0000

    8096c63c66 docs: clarify what fixes are merged to LTS versions (#38788)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+13.sha-31d0ee4
+ * @license Angular v10.1.1+15.sha-8096c63
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5112,7 +5112,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.1+13.sha-31d0ee4');
+const VERSION = new Version('10.1.1+15.sha-8096c63');
 
 /**
  * @license
