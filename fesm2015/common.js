7d4b0092cbb2decfb80f89b65c407e0d35757e1c
Andrew Kushnir <akushnir@google.com>
2020-09-15 23:29:40 +0000

    3d94919800 refactor(dev-infra): refactor commit-message files (#38845)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+32.sha-af3b401
+ * @license Angular v10.1.1+33.sha-3d94919
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5112,7 +5112,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.1+32.sha-af3b401');
+const VERSION = new Version('10.1.1+33.sha-3d94919');
 
 /**
  * @license
