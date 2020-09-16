37995db87887b2554916fa410a28c6c908e7ef1c
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:41:10 +0000

    e64c0c3730 fix(dev-infra): handle no caretaker config being defined (#38862)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.2
+ * @license Angular v10.1.2+9.sha-e64c0c3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5526,7 +5526,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.2');
+    var VERSION = new i0.Version('10.1.2+9.sha-e64c0c3');
 
     /**
      * @license
--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.2
+ * @license Angular v10.1.2+9.sha-e64c0c3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5112,7 +5112,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.2');
+const VERSION = new Version('10.1.2+9.sha-e64c0c3');
 
 /**
  * @license
