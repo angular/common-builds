b3b9bdd59527c6ac8853710ea55c94fbd31b08a9
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:53:36 +0000

    129107191c refactor(compiler): always return a mutable clone from `Scope#resolve` (#38857)

--- http/src/client.d.ts
+++ http/src/client.d.ts
@@ -2607,10 +2607,6 @@ export declare class HttpClient {
             [header: string]: string | string[];
         };
         observe: 'events';
-        params?: HttpParams | {
-            [param: string]: string | string[];
-        };
-        reportProgress?: boolean;
         responseType?: 'json';
         withCredentials?: boolean;
     }): Observable<HttpEvent<T>>;
