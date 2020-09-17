e2571e48278bcf67c1cf3dceb4bb81f6aec0fc01
Misko Hevery <misko@hevery.com>
2020-09-17 16:57:15 +0000

    a33d630a21 fix(zone.js): should have better backward compatibilities (#38797)

--- http/src/client.d.ts
+++ http/src/client.d.ts
@@ -2607,6 +2607,10 @@ export declare class HttpClient {
             [header: string]: string | string[];
         };
         observe: 'events';
+        params?: HttpParams | {
+            [param: string]: string | string[];
+        };
+        reportProgress?: boolean;
         responseType?: 'json';
         withCredentials?: boolean;
     }): Observable<HttpEvent<T>>;
