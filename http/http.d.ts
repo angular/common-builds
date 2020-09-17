fff882aad228bbc7f672c1bec64a45ef44ff8aac
Misko Hevery <misko@hevery.com>
2020-09-17 16:54:29 +0000

    a33d630a21 fix(zone.js): should have better backward compatibilities (#38797)

--- http/http.d.ts
+++ http/http.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+12.sha-1291071
+ * @license Angular v11.0.0-next.2+15.sha-a33d630
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2632,6 +2632,10 @@ export declare class HttpClient {
             [header: string]: string | string[];
         };
         observe: 'events';
+        params?: HttpParams | {
+            [param: string]: string | string[];
+        };
+        reportProgress?: boolean;
         responseType?: 'json';
         withCredentials?: boolean;
     }): Observable<HttpEvent<T>>;
