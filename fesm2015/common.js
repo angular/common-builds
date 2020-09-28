af141d5ef295b62f882931e16bcced3aa93a690d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:55:51 +0000

    3ba97ab391 ci: note Paul and Pawel as OOO in pullapprove (#39028)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+65.sha-c5e1789
+ * @license Angular v11.0.0-next.3+77.sha-3ba97ab
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5110,7 +5110,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+65.sha-c5e1789');
+const VERSION = new Version('11.0.0-next.3+77.sha-3ba97ab');
 
 /**
  * @license
