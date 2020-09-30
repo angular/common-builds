dea60d20ac2a450e1d8308aec065f5453031d6a2
Joey Perrott <josephperrott@gmail.com>
2020-09-30 20:18:56 +0000

    79620f5139 refactor(localize): avoid free-standing `FileSystem` functions (#39006)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+9.sha-bd5e425
+ * @license Angular v11.0.0-next.4+8.sha-79620f5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5110,7 +5110,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+9.sha-bd5e425');
+const VERSION = new Version('11.0.0-next.4+8.sha-79620f5');
 
 /**
  * @license
