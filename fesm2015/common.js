d5521e6b2f304de9db9a6b3482b1ca5b14a98b86
Joey Perrott <josephperrott@gmail.com>
2020-10-02 22:35:41 +0000

    6791cd79af refactor(compiler): iteratively parse interpolations (#38977)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+21.sha-f50313f
+ * @license Angular v10.1.4+22.sha-6791cd7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5112,7 +5112,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.4+21.sha-f50313f');
+const VERSION = new Version('10.1.4+22.sha-6791cd7');
 
 /**
  * @license
