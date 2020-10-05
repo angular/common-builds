bff9c3acd9464a9778ac2a20152fa9ec8431f8c0
Joey Perrott <josephperrott@gmail.com>
2020-10-05 15:30:32 +0000

    4c30f5135b docs(upgrade): expand upon change detection implications for downgraded components (#39100)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+22.sha-6791cd7
+ * @license Angular v10.1.4+23.sha-4c30f51
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5526,7 +5526,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.4+22.sha-6791cd7');
+    var VERSION = new i0.Version('10.1.4+23.sha-4c30f51');
 
     /**
      * @license
