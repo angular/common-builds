5478af9b71b619d700bc5e97eb579efccb435337
Joey Perrott <josephperrott@gmail.com>
2020-10-06 00:32:12 +0000

    0845d1148f build: update bazelversion (#39123)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+29.sha-f80c220
+ * @license Angular v10.1.4+31.sha-0845d11
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5112,7 +5112,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.4+29.sha-f80c220');
+const VERSION = new Version('10.1.4+31.sha-0845d11');
 
 /**
  * @license
