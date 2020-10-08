84d781f58f4273fc3b35304fac8bc860ee7a7759
Joey Perrott <josephperrott@gmail.com>
2020-10-08 18:03:15 +0000

    635543e6f6 build: upgrade angular build, integration/bazel and @angular/bazel package to rule_nodejs 2.2.0

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+3.sha-b635f25
+ * @license Angular v11.0.0-next.5+7.sha-635543e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5475,7 +5475,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.5+3.sha-b635f25');
+    var VERSION = new i0.Version('11.0.0-next.5+7.sha-635543e');
 
     /**
      * @license
