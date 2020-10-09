2203a98b5bb6dceef36fe5561e8858c9d29654eb
atscott <atscott01@gmail.com>
2020-10-09 17:02:01 +0000

    42be9047d8 feat(dev-infra): Add github links to caretaker checks (#39185)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+8.sha-52a0c6b
+ * @license Angular v10.1.5+9.sha-42be904
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5112,7 +5112,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.5+8.sha-52a0c6b');
+const VERSION = new Version('10.1.5+9.sha-42be904');
 
 /**
  * @license
