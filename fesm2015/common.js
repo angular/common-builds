cb9fbd01e506595b7a8190b0a9f7da53b7138963
atscott <atscott01@gmail.com>
2020-10-09 15:21:30 +0000

    8913aee527 build: bump Chromium to next stable version: 84.0.4147 (#39179)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+6.sha-7628c36
+ * @license Angular v10.1.5+7.sha-8913aee
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5112,7 +5112,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.5+6.sha-7628c36');
+const VERSION = new Version('10.1.5+7.sha-8913aee');
 
 /**
  * @license
