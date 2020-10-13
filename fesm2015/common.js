268874cae7ab8911c58f9cf32b603ffa297e2513
atscott <atscott01@gmail.com>
2020-10-13 22:21:17 +0000

    23fc2b43ac updated sajee's profile (#39019)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+18.sha-3487d5a
+ * @license Angular v10.1.5+20.sha-23fc2b4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5112,7 +5112,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.5+18.sha-3487d5a');
+const VERSION = new Version('10.1.5+20.sha-23fc2b4');
 
 /**
  * @license
