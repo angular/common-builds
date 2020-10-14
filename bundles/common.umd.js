d907ea5c0a6bc723cd5b3cc796065b02b3614229
atscott <atscott01@gmail.com>
2020-10-14 21:33:26 +0000

    aee2d3f994 docs: fix broken link (#39256)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6
+ * @license Angular v11.0.0-next.6+7.sha-aee2d3f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5611,7 +5611,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6');
+    var VERSION = new i0.Version('11.0.0-next.6+7.sha-aee2d3f');
 
     /**
      * @license
--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6
+ * @license Angular v11.0.0-next.6+7.sha-aee2d3f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5110,7 +5110,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6');
+const VERSION = new Version('11.0.0-next.6+7.sha-aee2d3f');
 
 /**
  * @license
