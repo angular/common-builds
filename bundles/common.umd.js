fc96a9f7939c5418a301278718c5e1fcc4f7c7ea
Andrew Kushnir <akushnir@google.com>
2020-10-16 00:49:05 +0000

    20b801e707 refactor(core): renames checkNoChangesMode to be clearer (#39277)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+22.sha-6acddb9
+ * @license Angular v11.0.0-next.6+24.sha-20b801e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5475,7 +5475,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+22.sha-6acddb9');
+    var VERSION = new i0.Version('11.0.0-next.6+24.sha-20b801e');
 
     /**
      * @license
