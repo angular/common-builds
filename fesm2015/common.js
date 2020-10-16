4f61f014df1de55e8081ccebb0870501c04b050c
Andrew Kushnir <akushnir@google.com>
2020-10-16 17:27:38 +0000

    4dc8d83d84 docs: edit attribute-binding doc and move colSpan note to property binding (#38860)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+34.sha-34b74ce
+ * @license Angular v11.0.0-next.6+36.sha-4dc8d83
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5060,7 +5060,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+34.sha-34b74ce');
+const VERSION = new Version('11.0.0-next.6+36.sha-4dc8d83');
 
 /**
  * @license
