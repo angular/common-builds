3ccd85ec2685ad1508c649e119181593b504d930
Andrew Kushnir <akushnir@google.com>
2020-10-16 15:40:50 +0000

    fdbc61111d build(docs-infra): add a tool to create new examples (#39283)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+24.sha-20b801e
+ * @license Angular v11.0.0-next.6+26.sha-fdbc611
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5475,7 +5475,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+24.sha-20b801e');
+    var VERSION = new i0.Version('11.0.0-next.6+26.sha-fdbc611');
 
     /**
      * @license
