929a63c58aaea6fa0959902da8a4fa49c7318caf
Andrew Kushnir <akushnir@google.com>
2020-10-19 22:14:57 +0000

    736e0644b0 refactor(compiler): remove support for TypeScript 3.9 (#39313)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+39.sha-d3087be
+ * @license Angular v11.0.0-next.6+42.sha-736e064
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5475,7 +5475,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+39.sha-d3087be');
+    var VERSION = new i0.Version('11.0.0-next.6+42.sha-736e064');
 
     /**
      * @license
