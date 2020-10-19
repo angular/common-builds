bb6ea437d785a47264bfc5aa653e6ebae9e8d3f7
Andrew Kushnir <akushnir@google.com>
2020-10-19 19:29:23 +0000

    32a8e72a14 feat(dev-infra): prompt caretaker to confirm the merge branches on merge (#39333)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+25.sha-7827d3e
+ * @license Angular v10.1.6+26.sha-32a8e72
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5112,7 +5112,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.6+25.sha-7827d3e');
+const VERSION = new Version('10.1.6+26.sha-32a8e72');
 
 /**
  * @license
