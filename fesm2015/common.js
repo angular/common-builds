c62deca11d1b3c9f024c38ecbd8e23d3d99adca0
Andrew Kushnir <akushnir@google.com>
2020-10-19 21:53:58 +0000

    02b87505ee docs: clarify grammatical error (#39279)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+26.sha-32a8e72
+ * @license Angular v10.1.6+27.sha-02b8750
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5112,7 +5112,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.6+26.sha-32a8e72');
+const VERSION = new Version('10.1.6+27.sha-02b8750');
 
 /**
  * @license
