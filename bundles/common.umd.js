482c96547900a5410465413a44150d0fd22e962d
Andrew Kushnir <akushnir@google.com>
2020-10-20 18:21:01 +0000

    595062e415 docs: Add Component Overview topic to angular.io (#39186)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+43.sha-5167eea
+ * @license Angular v11.0.0-next.6+46.sha-595062e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5475,7 +5475,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+43.sha-5167eea');
+    var VERSION = new i0.Version('11.0.0-next.6+46.sha-595062e');
 
     /**
      * @license
--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+43.sha-5167eea
+ * @license Angular v11.0.0-next.6+46.sha-595062e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5060,7 +5060,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+43.sha-5167eea');
+const VERSION = new Version('11.0.0-next.6+46.sha-595062e');
 
 /**
  * @license
