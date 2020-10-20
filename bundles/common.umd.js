0d4329ad09f1aab8b1910dd37d7197eef87d18aa
Andrew Kushnir <akushnir@google.com>
2020-10-20 21:00:33 +0000

    161b278b6f fix(router): incorrect signature for createUrlTree (#39347)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+46.sha-595062e
+ * @license Angular v11.0.0-next.6+49.sha-161b278
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5475,7 +5475,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+46.sha-595062e');
+    var VERSION = new i0.Version('11.0.0-next.6+49.sha-161b278');
 
     /**
      * @license
