2f6d3445847d2e03ff574522f9227c47f075794f
Andrew Kushnir <akushnir@google.com>
2020-10-20 18:31:56 +0000

    823200c6c1 docs: Add Component Overview topic to angular.io (#39186)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+28.sha-cd8ec3d
+ * @license Angular v10.1.6+29.sha-823200c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5112,7 +5112,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.6+28.sha-cd8ec3d');
+const VERSION = new Version('10.1.6+29.sha-823200c');
 
 /**
  * @license
