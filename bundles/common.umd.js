25656f887087bf0ac37ab0923f37eabf701e19a9
Andrew Kushnir <akushnir@google.com>
2020-10-21 21:14:39 +0000

    7f79e77c66 build: update bazelversion (#39351)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+55.sha-b4e8399
+ * @license Angular v11.0.0-rc.0+1.sha-7f79e77
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5475,7 +5475,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+55.sha-b4e8399');
+    var VERSION = new i0.Version('11.0.0-rc.0+1.sha-7f79e77');
 
     /**
      * @license
