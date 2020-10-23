e419bacfd0fa936a802b9faf45ea85449636b628
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-23 22:47:56 +0000

    6669571884 fix(zone.js): remove global declaration (#37861)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.0+1.sha-7f79e77
+ * @license Angular v11.0.0-rc.0+29.sha-6669571
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5475,7 +5475,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-rc.0+1.sha-7f79e77');
+    var VERSION = new i0.Version('11.0.0-rc.0+29.sha-6669571');
 
     /**
      * @license
