67865da1215d0a4f2098101c622f288ea44aabf2
Joey Perrott <josephperrott@gmail.com>
2020-10-28 22:02:42 +0000

    6b3db15080 docs: move template ref vars doc to concepts section clarify, add scope section, and update headers (#31195)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1
+ * @license Angular v10.2.1+2.sha-6b3db15
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5112,7 +5112,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.2.1');
+const VERSION = new Version('10.2.1+2.sha-6b3db15');
 
 /**
  * @license
