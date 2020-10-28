a6079597f50ccec14b21cbe097bc0914838f5d74
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 21:39:02 +0000

    bf05c979a7 release: cut the v11.0.0-rc.1 release

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.0+70.sha-ddc070e
+ * @license Angular v11.0.0-rc.1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5475,7 +5475,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-rc.0+70.sha-ddc070e');
+    var VERSION = new i0.Version('11.0.0-rc.1');
 
     /**
      * @license
