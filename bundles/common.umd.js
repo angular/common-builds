0330b8f825cef7fe6f2716d25f1cc065b41ee199
Joey Perrott <josephperrott@gmail.com>
2020-10-29 23:03:30 +0000

    0e60dc5597 release: cut the zone.js-0.11.3 release (#39440)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+12.sha-e45da28
+ * @license Angular v11.0.0-rc.1+13.sha-0e60dc5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5475,7 +5475,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-rc.1+12.sha-e45da28');
+    var VERSION = new i0.Version('11.0.0-rc.1+13.sha-0e60dc5');
 
     /**
      * @license
