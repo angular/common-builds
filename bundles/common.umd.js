acca23d717ad885b9eaf7b56ad61150012ca5a1d
Joey Perrott <josephperrott@gmail.com>
2020-10-29 17:19:07 +0000

    8ea0ee918d fix(dev-infra): update generated ng-dev compiled output (#39474)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+4.sha-eccf9d6
+ * @license Angular v11.0.0-rc.1+8.sha-8ea0ee9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5475,7 +5475,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-rc.1+4.sha-eccf9d6');
+    var VERSION = new i0.Version('11.0.0-rc.1+8.sha-8ea0ee9');
 
     /**
      * @license
