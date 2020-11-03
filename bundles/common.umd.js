5df7aad30b1aa7ac15bbbfc85a63c77204936c5b
Joey Perrott <josephperrott@gmail.com>
2020-11-03 23:19:08 +0000

    6817377590 feat(dev-infra): provide tooling to check what branches a pr targets (#39504)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+37.sha-b6064c2
+ * @license Angular v11.0.0-rc.1+40.sha-6817377
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5475,7 +5475,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-rc.1+37.sha-b6064c2');
+    var VERSION = new i0.Version('11.0.0-rc.1+40.sha-6817377');
 
     /**
      * @license
