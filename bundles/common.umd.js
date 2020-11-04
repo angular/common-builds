659838624260f403e961296ab4741ffb7047da02
Joey Perrott <josephperrott@gmail.com>
2020-11-04 20:02:13 +0000

    6959620002 docs: fix typos in the "Attribute Binding" guide (#39562)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+44.sha-43b4940
+ * @license Angular v11.0.0-rc.1+45.sha-6959620
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5475,7 +5475,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-rc.1+44.sha-43b4940');
+    var VERSION = new i0.Version('11.0.0-rc.1+45.sha-6959620');
 
     /**
      * @license
