6062618bceafbaebf26a3fcb38bd4bb5b7189d37
Misko Hevery <misko@hevery.com>
2020-11-06 23:57:48 +0000

    c68ca49d0b fix(compiler-cli): avoid duplicate diagnostics about unknown pipes (#39517)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.2+14.sha-dd28855
+ * @license Angular v11.0.0-rc.2+15.sha-c68ca49
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5475,7 +5475,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-rc.2+14.sha-dd28855');
+    var VERSION = new i0.Version('11.0.0-rc.2+15.sha-c68ca49');
 
     /**
      * @license
