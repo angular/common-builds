8ff797a4d3363b3416c7cbd4d2957900b333f3d1
Misko Hevery <misko@hevery.com>
2020-11-06 17:47:10 +0000

    1c6cf8a8c2 fix(compiler-cli): do not drop non-Angular decorators when downleveling (#39577)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.2+1.sha-08e077c
+ * @license Angular v10.2.2+7.sha-1c6cf8a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5526,7 +5526,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.2.2+1.sha-08e077c');
+    var VERSION = new i0.Version('10.2.2+7.sha-1c6cf8a');
 
     /**
      * @license
