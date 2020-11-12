ecfc02628baba9682ede0bbed544b480da9dbdbb
atscott <atscott01@gmail.com>
2020-11-12 17:38:16 +0000

    978f0810d9 fix(core): handle !important in style property value (#39603)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0+1.sha-92900d5
+ * @license Angular v11.0.0+5.sha-978f081
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5475,7 +5475,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0+1.sha-92900d5');
+    var VERSION = new i0.Version('11.0.0+5.sha-978f081');
 
     /**
      * @license
--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0+1.sha-92900d5
+ * @license Angular v11.0.0+5.sha-978f081
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5060,7 +5060,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0+1.sha-92900d5');
+const VERSION = new Version('11.0.0+5.sha-978f081');
 
 /**
  * @license
