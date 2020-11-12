f3b896fbb2e9c58f46d4529e99f040fa36af599f
Misko Hevery <misko@hevery.com>
2020-11-12 00:07:43 +0000

    92900d561d docs: update the roadmap with our priorities at Q4 (#39635)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.3
+ * @license Angular v11.0.0+1.sha-92900d5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5475,7 +5475,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-rc.3');
+    var VERSION = new i0.Version('11.0.0+1.sha-92900d5');
 
     /**
      * @license
