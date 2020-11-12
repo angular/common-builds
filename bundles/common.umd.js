72ad2ce53b9e74f94c09b24023a3e7f9f3c7d052
atscott <atscott01@gmail.com>
2020-11-12 18:00:26 +0000

    050cea9252 fix(forms): more precise control cleanup (#39623)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0+5.sha-978f081
+ * @license Angular v11.0.0+6.sha-050cea9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5475,7 +5475,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0+5.sha-978f081');
+    var VERSION = new i0.Version('11.0.0+6.sha-050cea9');
 
     /**
      * @license
--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0+5.sha-978f081
+ * @license Angular v11.0.0+6.sha-050cea9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5060,7 +5060,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0+5.sha-978f081');
+const VERSION = new Version('11.0.0+6.sha-050cea9');
 
 /**
  * @license
