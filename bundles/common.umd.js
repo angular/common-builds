f9bf4f5ca5f6fd570b728898eda5ac469bbd4186
atscott <atscott01@gmail.com>
2020-11-13 22:00:47 +0000

    7f3314a1e8 ci: update angular-robot config for L2 triage of PRs (#39655)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0+19.sha-83a6ab1
+ * @license Angular v11.0.0+20.sha-7f3314a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5475,7 +5475,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0+19.sha-83a6ab1');
+    var VERSION = new i0.Version('11.0.0+20.sha-7f3314a');
 
     /**
      * @license
