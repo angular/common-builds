402d93a29094b3412832e2c140367a2880c6b12c
atscott <atscott01@gmail.com>
2020-11-13 20:23:22 +0000

    83a6ab1edc refactor(core): document setComponentScope to provide context (#39662)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0+17.sha-f1d8717
+ * @license Angular v11.0.0+19.sha-83a6ab1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5475,7 +5475,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0+17.sha-f1d8717');
+    var VERSION = new i0.Version('11.0.0+19.sha-83a6ab1');
 
     /**
      * @license
