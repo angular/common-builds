b1f582085bb5d8148abd0868b0f68c8a7d74f22f
atscott <atscott01@gmail.com>
2020-11-17 21:36:37 +0000

    39266654e6 fix(http): queue jsonp <script> tag onLoad event handler in microtask (#39512)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+290.sha-49410f8
+ * @license Angular v11.0.0-next.6+289.sha-3926665
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5110,7 +5110,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+290.sha-49410f8');
+const VERSION = new Version('11.0.0-next.6+289.sha-3926665');
 
 /**
  * @license
