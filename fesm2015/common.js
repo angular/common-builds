3da7b4e489811074ee28ef67e923c7656fcba4a2
atscott <atscott01@gmail.com>
2020-11-18 18:44:47 +0000

    163fdc35f6 docs: add links to corresponding commits in v11.0.0 breaking changes in `CHANGELOG.md` (#39704)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0+46.sha-5d8b5eb
+ * @license Angular v11.0.0+53.sha-163fdc3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5060,7 +5060,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0+46.sha-5d8b5eb');
+const VERSION = new Version('11.0.0+53.sha-163fdc3');
 
 /**
  * @license
