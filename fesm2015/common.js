b0e66c15fc9d9c8e27a0d9ad1dd14e8343c57a98
atscott <atscott01@gmail.com>
2020-11-18 20:32:04 +0000

    3273856d33 docs: fixes on paragraph (#39664)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+3.sha-cc509d6
+ * @license Angular v11.1.0-next.0+4.sha-3273856
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5060,7 +5060,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+3.sha-cc509d6');
+const VERSION = new Version('11.1.0-next.0+4.sha-3273856');
 
 /**
  * @license
