36f5bf0e227794b8146132beece9fc20a0da6aa8
Andrew Kushnir <akushnir@google.com>
2020-11-19 20:06:34 +0000

    33639db911 release: cut the v11.0.2 release

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.1+17.sha-2fe2104
+ * @license Angular v11.0.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5475,7 +5475,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.1+17.sha-2fe2104');
+    var VERSION = new i0.Version('11.0.2');
 
     /**
      * @license
