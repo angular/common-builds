ba21d61e9282f320e0dc67657e241e1d1455e9b0
Andrew Kushnir <akushnir@google.com>
2020-11-19 20:34:38 +0000

    066126ae2f fix(core): remove duplicated noop function (#39761)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+18.sha-0e85938
+ * @license Angular v11.1.0-next.0+22.sha-066126a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5060,7 +5060,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+18.sha-0e85938');
+const VERSION = new Version('11.1.0-next.0+22.sha-066126a');
 
 /**
  * @license
