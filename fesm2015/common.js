05c65ea29e29766f079750938475d597ce277b1d
Andrew Kushnir <akushnir@google.com>
2020-11-19 20:45:38 +0000

    be998e830b refactor(core): move `injectAttributeImpl` to avoid cycles (#37085)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+31.sha-a965589
+ * @license Angular v11.1.0-next.0+33.sha-be998e8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5060,7 +5060,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+31.sha-a965589');
+const VERSION = new Version('11.1.0-next.0+33.sha-be998e8');
 
 /**
  * @license
