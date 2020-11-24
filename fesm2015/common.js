07db5f567c9f66f1396079674f01159cc7aa4b0a
Andrew Kushnir <akushnir@google.com>
2020-11-24 21:30:32 +0000

    266959e745 docs: added Srashti Jain to GDE resources (#39150)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+82.sha-e75244e
+ * @license Angular v11.1.0-next.0+73.sha-266959e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5060,7 +5060,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+82.sha-e75244e');
+const VERSION = new Version('11.1.0-next.0+73.sha-266959e');
 
 /**
  * @license
