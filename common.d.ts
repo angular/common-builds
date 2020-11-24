710d34e76c2e02353588919b5bd7b62309bddd60
Andrew Kushnir <akushnir@google.com>
2020-11-24 17:03:16 +0000

    2c275f6d7a docs: archive template expression operators doc, move pipes precedence section (#39170)

--- common.d.ts
+++ common.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+64.sha-6046419
+ * @license Angular v11.1.0-next.0+67.sha-2c275f6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1655,7 +1655,7 @@ export declare class NgForOfContext<T, U extends NgIterable<T> = NgIterable<T>> 
  *
  * The conditional displays the data only if `userStream` returns a value,
  * so you don't need to use the
- * [safe-navigation-operator](guide/template-expression-operators#safe-navigation-operator) (`?.`)
+ * safe-navigation-operator (`?.`)
  * to guard against null values when accessing properties.
  * You can display an alternative template while waiting for the data.
  *
