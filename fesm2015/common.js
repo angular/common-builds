75953ac4f578a59e0dddb0a295b0b4c9d7668729
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 19:29:35 +0000

    2786292780 test(compiler-cli): migrate i18n compliance tests (#39661)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+94.sha-6653028
+ * @license Angular v11.1.0-next.0+96.sha-2786292
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5060,7 +5060,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+94.sha-6653028');
+const VERSION = new Version('11.1.0-next.0+96.sha-2786292');
 
 /**
  * @license
