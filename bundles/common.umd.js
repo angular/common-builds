f81fa7a6df839a6ba4d4cdcfafa7f7c9a1538384
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 23:57:02 +0000

    01c1bfdbdd fix(core): Ensure OnPush ancestors are marked dirty when events occur (#39833)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+61.sha-4cfa209
+ * @license Angular v11.0.2+63.sha-01c1bfd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5475,7 +5475,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.2+61.sha-4cfa209');
+    var VERSION = new i0.Version('11.0.2+63.sha-01c1bfd');
 
     /**
      * @license
