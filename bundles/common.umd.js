0fedf700a3a24c1f20f68bfcc7a9f1f1f83b9ab1
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 20:25:13 +0000

    3d19645208 docs: edit binding-syntax doc (#38773)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+76.sha-518fbea
+ * @license Angular v11.0.2+77.sha-3d19645
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5475,7 +5475,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.2+76.sha-518fbea');
+    var VERSION = new i0.Version('11.0.2+77.sha-3d19645');
 
     /**
      * @license
