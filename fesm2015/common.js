be6f2b6a6fe50f1543c44f579c4c248a731dd55c
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 20:27:40 +0000

    b40aa0a5d3 docs: edit binding-syntax doc (#38773)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+119.sha-00d56e4
+ * @license Angular v11.1.0-next.0+120.sha-b40aa0a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5060,7 +5060,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+119.sha-00d56e4');
+const VERSION = new Version('11.1.0-next.0+120.sha-b40aa0a');
 
 /**
  * @license
