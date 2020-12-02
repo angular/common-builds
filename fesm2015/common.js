3e9531d0f5e916dbb0cb7f131592b60b3c1c8e00
Misko Hevery <misko@hevery.com>
2020-12-02 23:24:40 +0000

    790ca09e04 docs: remove superfluous "NOTE:" (#39933)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+25.sha-8d613c1
+ * @license Angular v11.1.0-next.1+21.sha-790ca09
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5060,7 +5060,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+25.sha-8d613c1');
+const VERSION = new Version('11.1.0-next.1+21.sha-790ca09');
 
 /**
  * @license
