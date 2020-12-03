03b7ca5583ac43b8ae327facf3a442638f27df76
Misko Hevery <misko@hevery.com>
2020-12-03 22:08:28 +0000

    4aeb65d002 docs: remove support for v8 LTS (#39915)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+19.sha-93564ca
+ * @license Angular v11.0.3+24.sha-4aeb65d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5475,7 +5475,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.3+19.sha-93564ca');
+    var VERSION = new i0.Version('11.0.3+24.sha-4aeb65d');
 
     /**
      * @license
--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+19.sha-93564ca
+ * @license Angular v11.0.3+24.sha-4aeb65d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5060,7 +5060,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.3+19.sha-93564ca');
+const VERSION = new Version('11.0.3+24.sha-4aeb65d');
 
 /**
  * @license
