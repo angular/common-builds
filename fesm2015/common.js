41d9266f42caeb4a8240c5643a8dc5ead8937664
Misko Hevery <misko@hevery.com>
2020-12-08 17:48:10 +0000

    8ab1231908 docs(animations): a small typo (#37164)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+42.sha-5631daa
+ * @license Angular v11.0.3+54.sha-8ab1231
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5060,7 +5060,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.3+42.sha-5631daa');
+const VERSION = new Version('11.0.3+54.sha-8ab1231');
 
 /**
  * @license
