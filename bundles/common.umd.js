9fa98b1d50a1be0fedf44e30c8503ce43c2fe484
Misko Hevery <misko@hevery.com>
2020-12-09 20:27:59 +0000

    eba7de6b22 Revert "ci: update pullapprove config to add comp labels" (#40053)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+73.sha-b2090fd
+ * @license Angular v11.0.4+1.sha-eba7de6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.3+73.sha-b2090fd');
+    var VERSION = new i0.Version('11.0.4+1.sha-eba7de6');
 
     /**
      * @license
