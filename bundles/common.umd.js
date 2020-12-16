431f13d143da59444d48c8efdd190415321d2477
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-16 18:04:06 +0000

    0aa220bc00 fix(core): fix possible XSS attack in development through SSR. (#40136)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.4+24.sha-a1c82e7
+ * @license Angular v11.0.4+26.sha-0aa220b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.4+24.sha-a1c82e7');
+    var VERSION = new i0.Version('11.0.4+26.sha-0aa220b');
 
     /**
      * @license
--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.4+24.sha-a1c82e7
+ * @license Angular v11.0.4+26.sha-0aa220b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5058,7 +5058,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.4+24.sha-a1c82e7');
+const VERSION = new Version('11.0.4+26.sha-0aa220b');
 
 /**
  * @license
