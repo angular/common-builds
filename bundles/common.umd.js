8daa9431ff475a1aca5d1dbb99d3017b21a03bd9
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:57:42 +0000

    7977509b18 fix(compiler): don't report parse error for interpolation inside string in property binding (#40267)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+33.sha-7e583ab
+ * @license Angular v11.0.5+43.sha-7977509
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.5+33.sha-7e583ab');
+    var VERSION = new i0.Version('11.0.5+43.sha-7977509');
 
     /**
      * @license
