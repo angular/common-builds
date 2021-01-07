c6e06b75c45fa8f1a47225a9738d7fd816577ead
atscott <atscott01@gmail.com>
2021-01-07 21:33:35 +0000

    40a03f0f28 docs: lifecycle-hooks example - extract things (components, directives) into their own files (#40212)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.6+7.sha-64d94e8
+ * @license Angular v11.0.7+4.sha-40a03f0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.6+7.sha-64d94e8');
+    var VERSION = new i0.Version('11.0.7+4.sha-40a03f0');
 
     /**
      * @license
