19a8c296c00132f2aadbcc670b0ba66d8001baae
atscott <atscott01@gmail.com>
2021-01-07 21:29:22 +0000

    aea265bfc5 docs: lifecycle-hooks example - extract things (components, directives) into their own files (#40212)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+9.sha-482a4c6
+ * @license Angular v11.1.0-next.4+13.sha-aea265b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5058,7 +5058,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+9.sha-482a4c6');
+const VERSION = new Version('11.1.0-next.4+13.sha-aea265b');
 
 /**
  * @license
