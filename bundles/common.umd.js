dd986e9d9fd5ea9d2f3ae7278d7a48606d78e385
Andrew Scott <atscott@google.com>
2021-01-10 01:01:18 +0000

    c44dd84a71 fix(router): Remove usage of `Object.values` to avoid the need for a polyfill (#40370)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.7+11.sha-6ccdb2d
+ * @license Angular v11.0.7+13.sha-c44dd84
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.7+11.sha-6ccdb2d');
+    var VERSION = new i0.Version('11.0.7+13.sha-c44dd84');
 
     /**
      * @license
