7e8d41f4e64171c74b4f1ac7de997b665ffddee1
atscott <atscott01@gmail.com>
2021-01-11 19:34:33 +0000

    adf42daed6 fix(router): better ngZone checking for warning (#25839)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.8+6.sha-6438a29
+ * @license Angular v11.0.8+19.sha-adf42da
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5058,7 +5058,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.8+6.sha-6438a29');
+const VERSION = new Version('11.0.8+19.sha-adf42da');
 
 /**
  * @license
