23e2e42093598297b85c7e0ab0cd9729c28ec8fe
atscott <atscott01@gmail.com>
2021-01-11 22:38:04 +0000

    f8495ca1c4 docs(router): clarify the description of legacy relativeLinkResolution (#40384)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.8+19.sha-adf42da
+ * @license Angular v11.0.8+20.sha-f8495ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.8+19.sha-adf42da');
+    var VERSION = new i0.Version('11.0.8+20.sha-f8495ca');
 
     /**
      * @license
