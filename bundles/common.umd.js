b0f39c1b0f40f48185c2189e31fb08edbeda2ad7
atscott <atscott01@gmail.com>
2021-01-12 00:07:47 +0000

    b48eabddb8 refactor(compiler-cli): include `template` source directly inside declaration object (#40383)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+79.sha-bd17610
+ * @license Angular v11.1.0-next.4+80.sha-b48eabd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+79.sha-bd17610');
+    var VERSION = new i0.Version('11.1.0-next.4+80.sha-b48eabd');
 
     /**
      * @license
