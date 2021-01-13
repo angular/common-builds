903f27e9c2f5c6954070acf1dab712bf2f8c9ac7
GitHub <noreply@github.com>
2021-01-13 19:13:23 +0000

    c6ca7b4a12 release: cut the v11.0.9 release (#40414)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.8+30.sha-642c45b
+ * @license Angular v11.0.8+31.sha-c6ca7b4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.8+30.sha-642c45b');
+    var VERSION = new i0.Version('11.0.8+31.sha-c6ca7b4');
 
     /**
      * @license
