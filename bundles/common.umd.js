4319efe7c4159da87a1e26e34a6513b928dde5e3
atscott <atscott01@gmail.com>
2021-01-13 22:35:18 +0000

    0664d750b6 perf(core): make DI decorators tree-shakable when used for `useFactory` deps config (#40145)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.5+1.sha-ebd94ef
+ * @license Angular v11.1.0-rc.0+7.sha-0664d75
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.5+1.sha-ebd94ef');
+    var VERSION = new i0.Version('11.1.0-rc.0+7.sha-0664d75');
 
     /**
      * @license
