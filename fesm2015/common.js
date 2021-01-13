a0e22cefc41510a6a68db4ed5bd2d4d6814c24b1
atscott <atscott01@gmail.com>
2021-01-13 22:34:19 +0000

    3f6279d394 ci: add mgechev to docs-marketing approvers (#40396)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.8+31.sha-c6ca7b4
+ * @license Angular v11.0.9+7.sha-3f6279d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5058,7 +5058,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.8+31.sha-c6ca7b4');
+const VERSION = new Version('11.0.9+7.sha-3f6279d');
 
 /**
  * @license
