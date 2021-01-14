03f63283eb08f97f7f26c62610274dae79cbcfd1
Andrew Kushnir <akushnir@google.com>
2021-01-14 19:59:14 +0000

    9b0b2dd688 docs: Fix typo in Trusted Types documentation (#40433)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+103.sha-b971bc6
+ * @license Angular v11.1.0-next.4+101.sha-9b0b2dd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5058,7 +5058,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+103.sha-b971bc6');
+const VERSION = new Version('11.1.0-next.4+101.sha-9b0b2dd');
 
 /**
  * @license
