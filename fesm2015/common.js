37568be2222a2a2548317984b8f80f3ff26a0d4b
Andrew Kushnir <akushnir@google.com>
2021-01-14 22:19:50 +0000

    d5f696c187 refactor(core): Remove the need for explicit static query instruction (#40091)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-rc.0+19.sha-63bf613
+ * @license Angular v11.1.0-rc.0+21.sha-d5f696c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5058,7 +5058,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-rc.0+19.sha-63bf613');
+const VERSION = new Version('11.1.0-rc.0+21.sha-d5f696c');
 
 /**
  * @license
