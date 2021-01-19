af79fc9b95bcb8c7b4771c4a8ab9ac174ca349b3
Andrew Kushnir <akushnir@google.com>
2021-01-19 17:51:08 +0000

    724e44a44a docs: add Angular Builders to community resources (#40475)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.9+22.sha-e7ed209
+ * @license Angular v11.0.9+25.sha-724e44a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.9+22.sha-e7ed209');
+    var VERSION = new i0.Version('11.0.9+25.sha-724e44a');
 
     /**
      * @license
