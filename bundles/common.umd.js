3fd6d3c783ff11da633a0522b4249dd86ddcea52
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-22 18:50:00 +0000

    b3b80d4e4a build: update to @bazel/bazelisk@1.7.3 (#40519)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+19.sha-566206b
+ * @license Angular v11.1.0+22.sha-b3b80d4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0+19.sha-566206b');
+    var VERSION = new i0.Version('11.1.0+22.sha-b3b80d4');
 
     /**
      * @license
