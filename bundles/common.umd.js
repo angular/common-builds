e5a2e1f0ded97cf6ce293e137162ae3c273d9f32
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-23 01:09:29 +0000

    dfe4200c2e Revert "fix(compiler): support multiple `:host-context()` selectors (#40494)" (#40531)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+24.sha-3bf6a99
+ * @license Angular v11.1.0+26.sha-dfe4200
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0+24.sha-3bf6a99');
+    var VERSION = new i0.Version('11.1.0+26.sha-dfe4200');
 
     /**
      * @license
