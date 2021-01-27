b1b48a042cbc4154eb00174b41b4beeaa980e532
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-27 23:45:02 +0000

    de124856f6 docs: update release notes for the 11.1.1 release (#40602)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+60.sha-63275ae
+ * @license Angular v11.1.1+1.sha-de12485
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0+60.sha-63275ae');
+    var VERSION = new i0.Version('11.1.1+1.sha-de12485');
 
     /**
      * @license
