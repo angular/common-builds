fd5b56e358b04670f2765c408b49748f1c76834e
Misko Hevery <misko@hevery.com>
2021-02-04 00:21:28 +0000

    58f8ba0a8c release: cut the v11.2.0-rc.0 release

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0-next.1
+ * @license Angular v11.2.0-rc.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.0-next.1');
+    var VERSION = new i0.Version('11.2.0-rc.0');
 
     /**
      * @license
