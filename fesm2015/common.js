87765b4cf3a5010fa413e0ac6ed68ea14622ce47
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 19:15:08 +0000

    4e4fcacb6d fix(docs-infra): correctly handle entry-points with no public exports (#40737)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+304.sha-e3213f6
+ * @license Angular v11.1.0-next.4+305.sha-4e4fcac
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5058,7 +5058,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+304.sha-e3213f6');
+const VERSION = new Version('11.1.0-next.4+305.sha-4e4fcac');
 
 /**
  * @license
