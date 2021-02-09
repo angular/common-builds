505a083a214083952032e3f5637180bcf58f1076
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 23:03:15 +0000

    820d95ba40 docs: add redirect from updating-v10 to v11 (#40108)

--- fesm2015/http.js
+++ fesm2015/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+308.sha-f76f2eb
+ * @license Angular v11.1.0-next.4+310.sha-820d95b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1778,6 +1778,7 @@ class HttpXhrBackend {
             // By default, register for load and error events.
             xhr.addEventListener('load', onLoad);
             xhr.addEventListener('error', onError);
+            xhr.addEventListener('timeout', onError);
             // Progress events are only enabled if requested.
             if (req.reportProgress) {
                 // Download progress is always enabled if requested.
@@ -1796,6 +1797,7 @@ class HttpXhrBackend {
                 // On a cancellation, remove all registered event listeners.
                 xhr.removeEventListener('error', onError);
                 xhr.removeEventListener('load', onLoad);
+                xhr.removeEventListener('timeout', onError);
                 if (req.reportProgress) {
                     xhr.removeEventListener('progress', onDownProgress);
                     if (reqBody !== null && xhr.upload) {
