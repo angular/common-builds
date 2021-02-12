c34a08e8956b3a6ec80e91a6067a8ac32a02d65f
Joey Perrott <josephperrott@gmail.com>
2021-02-12 00:18:37 +0000

    9482a4df4c docs: copy paste error (#40812)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0+16.sha-4c6d557
+ * @license Angular v11.2.0+18.sha-9482a4d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5500,7 +5500,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.0+16.sha-4c6d557');
+    var VERSION = new i0.Version('11.2.0+18.sha-9482a4d');
 
     /**
      * @license
--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0+16.sha-4c6d557
+ * @license Angular v11.2.0+18.sha-9482a4d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5085,7 +5085,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.0+16.sha-4c6d557');
+const VERSION = new Version('11.2.0+18.sha-9482a4d');
 
 /**
  * @license
