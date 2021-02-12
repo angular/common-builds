b8fe2109c72784a35b62471613ba642a35905453
Joey Perrott <josephperrott@gmail.com>
2021-02-12 17:49:44 +0000

    ebbcf0cc04 docs: Update start-deployment.md (#40817)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0+20.sha-8225bb5
+ * @license Angular v11.2.0+21.sha-ebbcf0c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5500,7 +5500,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.0+20.sha-8225bb5');
+    var VERSION = new i0.Version('11.2.0+21.sha-ebbcf0c');
 
     /**
      * @license
