389461b8e8b819933806c36bd628b3776c24125c
Joey Perrott <josephperrott@gmail.com>
2021-02-12 17:19:51 +0000

    8225bb5b57 refactor(compiler-cli): implement `ɵɵngDeclarePipe()` (#40803)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0+18.sha-9482a4d
+ * @license Angular v11.2.0+20.sha-8225bb5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5500,7 +5500,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.0+18.sha-9482a4d');
+    var VERSION = new i0.Version('11.2.0+20.sha-8225bb5');
 
     /**
      * @license
