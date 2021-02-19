d33a4a5aa4e34c344becd836af986614a132e1a1
atscott <atscott01@gmail.com>
2021-02-19 17:26:40 +0000

    228b5f73b1 fix(platform-browser): ensure that Hammer loader is called only once (#40911)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+12.sha-aed2782
+ * @license Angular v12.0.0-next.1+13.sha-228b5f7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5524,7 +5524,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.1+12.sha-aed2782');
+    var VERSION = new i0.Version('12.0.0-next.1+13.sha-228b5f7');
 
     /**
      * @license
--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+12.sha-aed2782
+ * @license Angular v12.0.0-next.1+13.sha-228b5f7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5109,7 +5109,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.1+12.sha-aed2782');
+const VERSION = new Version('12.0.0-next.1+13.sha-228b5f7');
 
 /**
  * @license
