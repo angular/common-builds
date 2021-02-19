fefbc3867d10233edd5503ffd0d8e5b6677de58e
atscott <atscott01@gmail.com>
2021-02-19 17:29:44 +0000

    b4301c3d88 docs: add missing '@usageNotes' tag to documentation (#40909)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+13.sha-228b5f7
+ * @license Angular v12.0.0-next.1+14.sha-b4301c3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5109,7 +5109,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.1+13.sha-228b5f7');
+const VERSION = new Version('12.0.0-next.1+14.sha-b4301c3');
 
 /**
  * @license
