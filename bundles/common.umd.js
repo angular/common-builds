ed815d3afb378d258ef54d9ac7b33de65291764d
Andrew Scott <atscott@google.com>
2021-02-19 20:28:52 +0000

    3b884ed1fc fix(http): ignore question mark when params are parsed (#40610)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.1+30.sha-dcbf600
+ * @license Angular v11.2.1+32.sha-3b884ed
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5500,7 +5500,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.1+30.sha-dcbf600');
+    var VERSION = new i0.Version('11.2.1+32.sha-3b884ed');
 
     /**
      * @license
