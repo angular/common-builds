94cfa11a0fbd37720118766eb2e488e2fcbcabf5
GitHub <noreply@github.com>
2021-02-22 22:06:01 +0000

    263bbd43c1 release: cut the v11.2.2 release (#40951)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.1+39.sha-73a3ff1
+ * @license Angular v11.2.1+40.sha-263bbd4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5500,7 +5500,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.1+39.sha-73a3ff1');
+    var VERSION = new i0.Version('11.2.1+40.sha-263bbd4');
 
     /**
      * @license
