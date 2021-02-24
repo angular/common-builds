88e787c48f0e766e77dd0dea3239b940b74d1621
atscott <atscott01@gmail.com>
2021-02-24 17:12:48 +0000

    3fe759eb6c test(language-service): remove circular dependency (#40966)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.2+15.sha-3551434
+ * @license Angular v11.2.2+25.sha-3fe759e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5500,7 +5500,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.2+15.sha-3551434');
+    var VERSION = new i0.Version('11.2.2+25.sha-3fe759e');
 
     /**
      * @license
