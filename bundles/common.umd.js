284a23df86c9c2a65118e1050bef18ecfa097ef5
Zach Arend <zachzach@google.com>
2021-02-24 23:26:19 +0000

    55eb7b5681 fix(compiler): ensure JIT compilation of ɵɵngDeclarePipe() works (#40929)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.3+3.sha-ca271b8
+ * @license Angular v11.2.3+4.sha-55eb7b5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5500,7 +5500,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.3+3.sha-ca271b8');
+    var VERSION = new i0.Version('11.2.3+4.sha-55eb7b5');
 
     /**
      * @license
