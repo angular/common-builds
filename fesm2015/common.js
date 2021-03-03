5c822456333bfb94afc499d1cbc420e7d9fe7d52
Zach Arend <zachzach@google.com>
2021-03-03 21:59:06 +0000

    bb6e5e2dd0 fix(language-service): don't show external template diagnostics in ts files (#41070)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+57.sha-0847a03
+ * @license Angular v12.0.0-next.2+58.sha-bb6e5e2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5161,7 +5161,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.2+57.sha-0847a03');
+const VERSION = new Version('12.0.0-next.2+58.sha-bb6e5e2');
 
 /**
  * @license
