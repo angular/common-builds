d5e13a9cf6a508033b6d632e5b90b5a0a3abcda8
Zach Arend <zachzach@google.com>
2021-03-03 21:33:13 +0000

    0847a0353b fix(language-service): Always attempt HTML AST to template AST conversion for LS (#41068)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+55.sha-7765b64
+ * @license Angular v12.0.0-next.2+57.sha-0847a03
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5161,7 +5161,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.2+55.sha-7765b64');
+const VERSION = new Version('12.0.0-next.2+57.sha-0847a03');
 
 /**
  * @license
