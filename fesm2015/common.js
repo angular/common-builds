5414ab2e296e9f9243e6d57545adee9c4bb8aec3
Zach Arend <zachzach@google.com>
2021-03-03 18:14:57 +0000

    ffbacbbc98 build(docs-infra): upgrade cli command docs sources to d62203a9f (#41046)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+50.sha-e9e7c33
+ * @license Angular v12.0.0-next.2+51.sha-ffbacbb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5161,7 +5161,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.2+50.sha-e9e7c33');
+const VERSION = new Version('12.0.0-next.2+51.sha-ffbacbb');
 
 /**
  * @license
