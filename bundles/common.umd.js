4047ac7aa78e89d73b38a1ae25ee6fea7ae76885
Zach Arend <zachzach@google.com>
2021-03-03 21:34:06 +0000

    6dd54972d4 fix(language-service): Always attempt HTML AST to template AST conversion for LS (#41068)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.3+46.sha-e4774da
+ * @license Angular v11.2.3+48.sha-6dd5497
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5535,7 +5535,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.3+46.sha-e4774da');
+    var VERSION = new i0.Version('11.2.3+48.sha-6dd5497');
 
     /**
      * @license
