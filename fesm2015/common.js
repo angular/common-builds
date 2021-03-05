d23ebdc686141398ac6caad3ea673af669b58e90
Andrew Kushnir <akushnir@google.com>
2021-03-05 18:04:50 +0000

    2de7eef92e docs: fix issue #40941 (#40942)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.4+12.sha-a05eb13
+ * @license Angular v11.2.4+14.sha-2de7eef
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5120,7 +5120,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.4+12.sha-a05eb13');
+const VERSION = new Version('11.2.4+14.sha-2de7eef');
 
 /**
  * @license
