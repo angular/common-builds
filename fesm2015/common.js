b0afb9c2f93009efb2be00386e5f62e8ea258365
Andrew Kushnir <akushnir@google.com>
2021-03-08 18:27:07 +0000

    925746b6d8 fix(bazel): fix incorrect rollup plugin method signature (#41101)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.4+24.sha-e35ecea
+ * @license Angular v11.2.4+26.sha-925746b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5120,7 +5120,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.4+24.sha-e35ecea');
+const VERSION = new Version('11.2.4+26.sha-925746b');
 
 /**
  * @license
