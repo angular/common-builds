cb3784f4a16fc259e15a97e19c7147789abc7aff
Andrew Kushnir <akushnir@google.com>
2021-03-11 20:50:00 +0000

    74aba139ed fix(docs-infra): fix angular.io on IE 11 (#41162) (#41184)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+7.sha-ed6c09a
+ * @license Angular v11.2.5+9.sha-74aba13
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5120,7 +5120,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.5+7.sha-ed6c09a');
+const VERSION = new Version('11.2.5+9.sha-74aba13');
 
 /**
  * @license
