f247b69cfa5325a71ac76c681cda1779696d9565
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-15 17:52:09 +0000

    b4484125d6 docs: document `help wanted` and `good first issue` labels (#41195)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+18.sha-5908eda
+ * @license Angular v11.2.5+19.sha-b448412
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5120,7 +5120,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.5+18.sha-5908eda');
+const VERSION = new Version('11.2.5+19.sha-b448412');
 
 /**
  * @license
