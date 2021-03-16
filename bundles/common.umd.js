4d9f35dcc5bf48df80923ac12ec1a9f10abaaf6a
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-16 16:55:45 +0000

    6414590f8d perf(forms): make `RadioControlRegistry` class tree-shakable (#41126)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+22.sha-bec919a
+ * @license Angular v11.2.5+25.sha-6414590
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5535,7 +5535,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.5+22.sha-bec919a');
+    var VERSION = new i0.Version('11.2.5+25.sha-6414590');
 
     /**
      * @license
--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+22.sha-bec919a
+ * @license Angular v11.2.5+25.sha-6414590
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5120,7 +5120,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.5+22.sha-bec919a');
+const VERSION = new Version('11.2.5+25.sha-6414590');
 
 /**
  * @license
