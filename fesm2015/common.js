cd6b1008378f3aec0ea196d8f32f2cda28184b10
Joey Perrott <josephperrott@gmail.com>
2021-03-23 18:41:17 +0000

    9cb11758c1 docs: add link to fork repo how-to (#41266)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.6+22.sha-18cd7a0
+ * @license Angular v11.2.6+24.sha-9cb1175
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5120,7 +5120,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.6+22.sha-18cd7a0');
+const VERSION = new Version('11.2.6+24.sha-9cb1175');
 
 /**
  * @license
