166e874de3ba8fe579d7e16d40167e5561852d65
Joey Perrott <josephperrott@gmail.com>
2021-03-23 21:39:04 +0000

    d8d77ed00d refactor(dev-infra): use conventional-commits-parser for commit parsing (#41329)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.6+25.sha-a68f6b0
+ * @license Angular v11.2.6+26.sha-d8d77ed
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5120,7 +5120,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.6+25.sha-a68f6b0');
+const VERSION = new Version('11.2.6+26.sha-d8d77ed');
 
 /**
  * @license
