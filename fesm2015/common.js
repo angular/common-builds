e8e9ff6876f6f0a1bd687a3634249fdc840ad603
Joey Perrott <josephperrott@gmail.com>
2021-03-24 15:51:20 +0000

    2d183f63c6 ci: fix `elements` test on older browsers (#41324)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.6+27.sha-cdbec67
+ * @license Angular v11.2.6+28.sha-2d183f6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5120,7 +5120,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.6+27.sha-cdbec67');
+const VERSION = new Version('11.2.6+28.sha-2d183f6');
 
 /**
  * @license
