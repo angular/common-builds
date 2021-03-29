183c93fecde2380e2e071f057fc8dab4b52ed18e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-29 22:37:25 +0000

    6dcea34c24 refactor(compiler): option to include html comments in `ParsedTemplate` (#41251)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.6+28.sha-2d183f6
+ * @license Angular v11.2.7+5.sha-6dcea34
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5120,7 +5120,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.6+28.sha-2d183f6');
+const VERSION = new Version('11.2.7+5.sha-6dcea34');
 
 /**
  * @license
