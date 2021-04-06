4712ee3242cc8a4b8ed721c59bee18196f8f8e5c
atscott <atscott01@gmail.com>
2021-04-06 17:29:17 +0000

    0eecbed69f docs(router): make getCurrentNavigation() jsdoc more explicit (#41417)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.8+11.sha-6eb3dc7
+ * @license Angular v11.2.8+17.sha-0eecbed
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5120,7 +5120,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.8+11.sha-6eb3dc7');
+const VERSION = new Version('11.2.8+17.sha-0eecbed');
 
 /**
  * @license
