a2c7c24208765f4a6f1dcf98d71b9a34fff46847
atscott <atscott01@gmail.com>
2021-04-06 00:00:21 +0000

    6eb3dc770d build: update the recommended `Dockerfile` for VSCode remote development (#41396)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.8+10.sha-754247f
+ * @license Angular v11.2.8+11.sha-6eb3dc7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5120,7 +5120,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.8+10.sha-754247f');
+const VERSION = new Version('11.2.8+11.sha-6eb3dc7');
 
 /**
  * @license
