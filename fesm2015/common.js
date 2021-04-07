2bb7c7b0320084581bec2b080b3f03dec6e40d6a
atscott <atscott01@gmail.com>
2021-04-07 17:40:54 +0000

    f8986e1a53 fix(dev-infra): make scopes for `test` and `refactor` commit types optional (#41486)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.8+26.sha-3f12a0d
+ * @license Angular v11.2.8+30.sha-f8986e1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5120,7 +5120,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.8+26.sha-3f12a0d');
+const VERSION = new Version('11.2.8+30.sha-f8986e1');
 
 /**
  * @license
