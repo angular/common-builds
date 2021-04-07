60b312c7363eaf0660f2e3df5277f2ad690ddc94
atscott <atscott01@gmail.com>
2021-04-07 18:12:33 +0000

    9408af6059 docs: fix link URL in NG0100 error page (#41484)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.8+30.sha-f8986e1
+ * @license Angular v11.2.8+32.sha-9408af6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5120,7 +5120,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.8+30.sha-f8986e1');
+const VERSION = new Version('11.2.8+32.sha-9408af6');
 
 /**
  * @license
