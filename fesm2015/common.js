e07be5d673eabfdcb1164f3d8e99aca49dee007d
Zach Arend <zachzach@google.com>
2021-04-08 17:44:10 +0000

    44a7fae00f fix(router): handle new navigations from a NavigationEnd event (#41262)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+16.sha-10a7c87
+ * @license Angular v12.0.0-next.8+18.sha-44a7fae
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5209,7 +5209,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+16.sha-10a7c87');
+const VERSION = new Version('12.0.0-next.8+18.sha-44a7fae');
 
 /**
  * @license
