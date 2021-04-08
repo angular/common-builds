2cc70f8c6d904aaf18acbb2fa3a8eef89545b317
Zach Arend <zachzach@google.com>
2021-04-08 17:43:01 +0000

    deacc741e0 fix(compiler-cli): ensure the compiler tracks `ts.Program`s correctly (#41291)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+18.sha-44a7fae
+ * @license Angular v12.0.0-next.8+17.sha-deacc74
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5158,7 +5158,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+18.sha-44a7fae');
+const VERSION = new Version('12.0.0-next.8+17.sha-deacc74');
 
 /**
  * @license
