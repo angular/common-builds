4538e53b0fee769b0f9660c32904245d4db7eae9
Zach Arend <zachzach@google.com>
2021-04-13 00:01:47 +0000

    99c9f7c709 ci: remove old compliance tests (#41556) (#41585)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.9+21.sha-419c189
+ * @license Angular v11.2.9+23.sha-99c9f7c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5120,7 +5120,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.9+21.sha-419c189');
+const VERSION = new Version('11.2.9+23.sha-99c9f7c');
 
 /**
  * @license
