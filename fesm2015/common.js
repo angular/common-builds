7bdc01954452ca8e3b91cccb77b8496b805b5cff
Zach Arend <zachzach@google.com>
2021-04-13 20:28:14 +0000

    04235fa079 docs: replace Codelyzer recommendation with Angular-ESLint in accessibility guide (#41294)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.9+36.sha-4847c14
+ * @license Angular v11.2.9+37.sha-04235fa
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5120,7 +5120,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.9+36.sha-4847c14');
+const VERSION = new Version('11.2.9+37.sha-04235fa');
 
 /**
  * @license
