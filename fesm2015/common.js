a2945297e974b4894d36aa93ecbcafa86645e6d8
Zach Arend <zachzach@google.com>
2021-04-14 16:44:30 +0000

    0bc539af29 Revert "fix(compiler-cli): autocomplete literal types in templates. (#41456)" (#41623)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+77.sha-de93a7a
+ * @license Angular v12.0.0-next.8+78.sha-0bc539a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5158,7 +5158,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+77.sha-de93a7a');
+const VERSION = new Version('12.0.0-next.8+78.sha-0bc539a');
 
 /**
  * @license
