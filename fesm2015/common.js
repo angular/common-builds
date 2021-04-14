e54316a60993aa0cf757db84caf845a518f8eae2
Zach Arend <zachzach@google.com>
2021-04-14 22:27:25 +0000

    3a2b5906ae release: cut the v11.2.10 release

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.9+41.sha-b8c4da9
+ * @license Angular v11.2.10
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5120,7 +5120,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.9+41.sha-b8c4da9');
+const VERSION = new Version('11.2.10');
 
 /**
  * @license
