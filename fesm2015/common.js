a9bd26e74d2c9baf6c7a4d465c3b61444b821e01
Andrew Kushnir <akushnir@google.com>
2021-04-16 20:30:06 +0000

    d99e8f1c85 feat(dev-infra): prevent pr merges of features or breaking changes on rc (#41660)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+10.sha-296f887
+ * @license Angular v11.2.10+11.sha-d99e8f1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5120,7 +5120,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.10+10.sha-296f887');
+const VERSION = new Version('11.2.10+11.sha-d99e8f1');
 
 /**
  * @license
