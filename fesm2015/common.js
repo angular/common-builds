c1f5ebc4e1af86c9ab22131ddc6befe82784b5c8
Andrew Kushnir <akushnir@google.com>
2021-04-17 00:44:09 +0000

    96175fdda2 docs: fix the property name in the example (#41410)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+135.sha-d5a8302
+ * @license Angular v12.0.0-next.8+136.sha-96175fd
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5209,7 +5209,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+135.sha-d5a8302');
+const VERSION = new Version('12.0.0-next.8+136.sha-96175fd');
 
 /**
  * @license
