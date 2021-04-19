b87841dceb1d2226ab046f1e532efd5d17cae7b1
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:23:38 +0000

    6a153a2a6f build: update diff to version 5.0.0 (#41684)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+147.sha-1391323
+ * @license Angular v12.0.0-next.8+149.sha-6a153a2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5209,7 +5209,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+147.sha-1391323');
+const VERSION = new Version('12.0.0-next.8+149.sha-6a153a2');
 
 /**
  * @license
