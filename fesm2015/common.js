887160518ff426d7d0a7e12e72d83b2b1a9c680f
Andrew Kushnir <akushnir@google.com>
2021-04-19 23:17:55 +0000

    a76f07bd13 docs: documentation of descendants property of @ContentChildren (#35927)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+31.sha-3631934
+ * @license Angular v11.2.10+35.sha-a76f07b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5120,7 +5120,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.10+31.sha-3631934');
+const VERSION = new Version('11.2.10+35.sha-a76f07b');
 
 /**
  * @license
