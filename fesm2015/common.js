3056e9ed3a262ce7e91e3583920753a0c7f0274a
Andrew Kushnir <akushnir@google.com>
2021-04-20 17:01:22 +0000

    7459f6d705 docs: rewrites animation query () method use (#41710)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+35.sha-a76f07b
+ * @license Angular v11.2.10+38.sha-7459f6d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5120,7 +5120,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.10+35.sha-a76f07b');
+const VERSION = new Version('11.2.10+38.sha-7459f6d');
 
 /**
  * @license
