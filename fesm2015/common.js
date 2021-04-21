faa181daf98e343eaef2b61c066843da455d7dc0
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-21 23:14:18 +0000

    0830885c95 test(zone.js): should invoke XHR task even onload handler throw error. (#41562)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.9+89.sha-897d5ad
+ * @license Angular v12.0.0-rc.0+2.sha-0830885
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5158,7 +5158,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.9+89.sha-897d5ad');
+const VERSION = new Version('12.0.0-rc.0+2.sha-0830885');
 
 /**
  * @license
