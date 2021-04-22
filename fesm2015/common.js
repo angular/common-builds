c3764468a16360e79d3f9d27d96391c2612cec9b
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 00:47:59 +0000

    6611d5c1c9 docs: add instructions for opting into latest Language Service (#41600)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+192.sha-c361466
+ * @license Angular v12.0.0-next.8+193.sha-6611d5c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5209,7 +5209,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+192.sha-c361466');
+const VERSION = new Version('12.0.0-next.8+193.sha-6611d5c');
 
 /**
  * @license
