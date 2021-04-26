0557746b015d051b1c60d2ca9f30231a8c67fe2d
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 22:41:43 +0000

    af12d8deaa fix(core): update supported range of node versions to only include LTS versions (#41822)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+233.sha-e47ea29
+ * @license Angular v12.0.0-next.8+251.sha-af12d8d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5209,7 +5209,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+233.sha-e47ea29');
+const VERSION = new Version('12.0.0-next.8+251.sha-af12d8d');
 
 /**
  * @license
