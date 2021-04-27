d419924083f6067ad6dd203e18ea306f687bbbe8
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 18:56:08 +0000

    e7586581af ci: add Zach Arend to pullapprove groups (#41839)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+23.sha-84a44a9
+ * @license Angular v11.2.11+24.sha-e758658
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5120,7 +5120,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.11+23.sha-84a44a9');
+const VERSION = new Version('11.2.11+24.sha-e758658');
 
 /**
  * @license
