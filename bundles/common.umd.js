96d16e62189c9742d8fca8979ff24686ca7002a7
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 17:37:07 +0000

    3783976543 docs: remove the section "Using components vs services from other modules" (#41835)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+252.sha-da6ed15
+ * @license Angular v12.0.0-next.8+255.sha-3783976
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5710,7 +5710,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+252.sha-da6ed15');
+    var VERSION = new i0.Version('12.0.0-next.8+255.sha-3783976');
 
     /**
      * @license
