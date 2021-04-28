40216f73364d4d1b5c7dad633c713fff39016224
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 22:30:54 +0000

    ecf9a12ce4 docs: Edits to remove jargon (#41536)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.12+3.sha-fca8d7a
+ * @license Angular v11.2.12+4.sha-ecf9a12
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5120,7 +5120,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.12+3.sha-fca8d7a');
+const VERSION = new Version('11.2.12+4.sha-ecf9a12');
 
 /**
  * @license
