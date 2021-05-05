17594a0dd18aa555d35032e9768d11786569235f
Misko Hevery <misko@hevery.com>
2021-05-05 21:05:11 +0000

    fb38175a40 docs: remove ts-node and tslint form dev deps (#41950)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+333.sha-753fb06
+ * @license Angular v12.0.0-next.8+334.sha-fb38175
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5209,7 +5209,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+333.sha-753fb06');
+const VERSION = new Version('12.0.0-next.8+334.sha-fb38175');
 
 /**
  * @license
