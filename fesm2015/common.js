3000c450eafb080df76399a9b5ab00ee9a2af76c
Misko Hevery <misko@hevery.com>
2021-05-05 04:38:43 +0000

    19b12d7286 refactor(dev-infra): switch away from deprecated platform execution properties (#41941)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+329.sha-5947239
+ * @license Angular v12.0.0-next.8+331.sha-19b12d7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1009,7 +1009,7 @@ var TranslationWidth;
  * Examples are given for `en-US`.
  *
  * @see `getLocaleDateFormat()`
- * @see `getLocaleTimeFormat()``
+ * @see `getLocaleTimeFormat()`
  * @see `getLocaleDateTimeFormat()`
  * @see [Internationalization (i18n) Guide](https://angular.io/guide/i18n)
  * @publicApi
@@ -5209,7 +5209,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+329.sha-5947239');
+const VERSION = new Version('12.0.0-next.8+331.sha-19b12d7');
 
 /**
  * @license
