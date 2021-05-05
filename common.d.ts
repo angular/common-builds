958dd10314f2233768847ca7f426a7e90ae0d056
Misko Hevery <misko@hevery.com>
2021-05-05 04:34:01 +0000

    5947239f89 fix(dev-infra): do not set lts dist tag on packages from release-candidate train (#41946)

--- common.d.ts
+++ common.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+330.sha-378bb04
+ * @license Angular v12.0.0-next.8+329.sha-5947239
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -538,7 +538,7 @@ export declare function formatPercent(value: number, locale: string, digitsInfo?
  * Examples are given for `en-US`.
  *
  * @see `getLocaleDateFormat()`
- * @see `getLocaleTimeFormat()`
+ * @see `getLocaleTimeFormat()``
  * @see `getLocaleDateTimeFormat()`
  * @see [Internationalization (i18n) Guide](https://angular.io/guide/i18n)
  * @publicApi
