671fa5b62ac2ea17300b76cb41d6249742fa4aad
Andrew Scott <atscott@google.com>
2021-05-14 00:19:53 +0000

    28ee9864a4 fix(benchpress): update the check for start and end events (#42085)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0+2.sha-7514917
+ * @license Angular v12.0.0+11.sha-28ee986
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5169,7 +5169,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0+2.sha-7514917');
+const VERSION = new Version('12.0.0+11.sha-28ee986');
 
 /**
  * @license
