d4bd0d2712d9026aaaf2e8894605a3aae268ee06
atscott <atscott01@gmail.com>
2021-05-17 18:18:09 +0000

    edfcbb43c5 docs: change `project` to `projected` for past tense (#42118)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0+13.sha-678bce2
+ * @license Angular v12.0.0+22.sha-edfcbb4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5584,7 +5584,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0+13.sha-678bce2');
+    var VERSION = new i0.Version('12.0.0+22.sha-edfcbb4');
 
     /**
      * @license
