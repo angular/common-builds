40921a391fee4b7a67da3d1d882ebf210216ecdc
atscott <atscott01@gmail.com>
2021-05-18 15:55:59 +0000

    68619112e9 docs: update bazel migration doc link (#42142) PR Close #42142

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0+22.sha-edfcbb4
+ * @license Angular v12.0.0+26.sha-6861911
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5169,7 +5169,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0+22.sha-edfcbb4');
+const VERSION = new Version('12.0.0+26.sha-6861911');
 
 /**
  * @license
