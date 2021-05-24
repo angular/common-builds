4fe931a9312c07f9bc59d5b4c0c6ed4a4bbb4ecb
Zach Arend <zachzach@google.com>
2021-05-24 19:44:47 +0000

    c820066d98 docs: align code in `product-example.component.*.ts` files (#42155)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+25.sha-bf505ad
+ * @license Angular v12.1.0-next.2+31.sha-c820066
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5224,7 +5224,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+25.sha-bf505ad');
+const VERSION = new Version('12.1.0-next.2+31.sha-c820066');
 
 /**
  * @license
