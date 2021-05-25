2ead00b28a081ee74b6851152d155449a83a4595
Zach Arend <zachzach@google.com>
2021-05-25 18:22:13 +0000

    594e63315e feat(dev-infra): add release notes generation to ng-dev (#42225)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+63.sha-c3cbfc2
+ * @license Angular v12.1.0-next.2+67.sha-594e633
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -883,7 +883,7 @@
      *
      * @usageNotes
      *
-     * It's better to use the `Router#navigate` service to trigger route changes. Use
+     * It's better to use the `Router.navigate()` service to trigger route changes. Use
      * `Location` only if you need to interact with or create normalized URLs outside of
      * routing.
      *
@@ -1062,8 +1062,13 @@
         /**
          * Subscribes to the platform's `popState` events.
          *
+         * Note: `Location.go()` does not trigger the `popState` event in the browser. Use
+         * `Location.onUrlChange()` to subscribe to URL changes instead.
+         *
          * @param value Event that is triggered when the state history changes.
          * @param exception The exception to throw.
+         *
+         * @see [onpopstate](https://developer.mozilla.org/en-US/docs/Web/API/WindowEventHandlers/onpopstate)
          *
          * @returns Subscribed events.
          */
@@ -5584,7 +5589,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.2+63.sha-c3cbfc2');
+    var VERSION = new i0.Version('12.1.0-next.2+67.sha-594e633');
 
     /**
      * @license
