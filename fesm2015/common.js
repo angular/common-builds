9fd25dab757f97aecffe24cbe03da67050d4975a
Andrew Kushnir <akushnir@google.com>
2021-05-26 21:44:46 +0000

    765854ed8f build: allow node v15 and above for aio (#42367)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.2+15.sha-12e7968
+ * @license Angular v12.0.2+21.sha-765854e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5183,7 +5183,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.2+15.sha-12e7968');
+const VERSION = new Version('12.0.2+21.sha-765854e');
 
 /**
  * @license
