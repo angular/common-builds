59e3e484dd19032a14dd66074a71493cc6018fd1
Andrew Kushnir <akushnir@google.com>
2021-06-02 17:08:08 +0000

    e937f77cd4 docs: specify correct interceptor class type (#42437)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.2+63.sha-8bdcca1
+ * @license Angular v12.0.2+65.sha-e937f77
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5183,7 +5183,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.2+63.sha-8bdcca1');
+const VERSION = new Version('12.0.2+65.sha-e937f77');
 
 /**
  * @license
