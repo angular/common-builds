7ea6edcfa025d41609487f5701c94730128935f7
Andrew Kushnir <akushnir@google.com>
2021-06-02 21:07:39 +0000

    0a1d8e5102 refactor(docs-infra): clean up AIO Sass files (#42442)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3
+ * @license Angular v12.0.3+11.sha-0a1d8e5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5183,7 +5183,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.3');
+const VERSION = new Version('12.0.3+11.sha-0a1d8e5');
 
 /**
  * @license
