4ef6c26665b91a8850b2122aef0c3d27e1317871
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 18:54:06 +0000

    50c87e86b6 fix(forms): the `min` and `max` validators should work correctly with `0` as a value (#42412)

--- bundles/common.umd.js
+++ bundles/common.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+17.sha-0dad375
+ * @license Angular v12.0.3+22.sha-50c87e8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5599,7 +5599,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.3+17.sha-0dad375');
+    var VERSION = new i0.Version('12.0.3+22.sha-50c87e8');
 
     /**
      * @license
