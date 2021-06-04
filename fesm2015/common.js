f42e8dd467bc6e35efc5513699447f8f628f2e5b
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-04 20:51:49 +0000

    6080db1839 build: suppress errors in git hooks (#42484)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+31.sha-beae425
+ * @license Angular v12.0.3+33.sha-6080db1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5183,7 +5183,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.3+31.sha-beae425');
+const VERSION = new Version('12.0.3+33.sha-6080db1');
 
 /**
  * @license
