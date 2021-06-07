58c8af828107ac50e9bc709251a1e17b53bc3653
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 18:12:15 +0000

    65d59305d5 build: remove unused no-strict tsconfig file (#42506)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+38.sha-f5d81d3
+ * @license Angular v12.0.3+41.sha-65d5930
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5183,7 +5183,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.3+38.sha-f5d81d3');
+const VERSION = new Version('12.0.3+41.sha-65d5930');
 
 /**
  * @license
