6287f8b221bdc4cc7d404dc17eca2cce3429794e
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 19:34:08 +0000

    5746afe93d docs: improve accessibility of attribute-binding example (#41432)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+53.sha-93de5a0
+ * @license Angular v12.0.3+55.sha-5746afe
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5183,7 +5183,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.3+53.sha-93de5a0');
+const VERSION = new Version('12.0.3+55.sha-5746afe');
 
 /**
  * @license
