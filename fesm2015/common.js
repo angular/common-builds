ca064306f9140029384092e1ec7d4189a37d4038
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-09 17:03:50 +0000

    6203f5e3a7 docs: clarify onSameUrlNavigation behavior (#42275)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+64.sha-3abf365
+ * @license Angular v12.0.3+65.sha-6203f5e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5183,7 +5183,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.3+64.sha-3abf365');
+const VERSION = new Version('12.0.3+65.sha-6203f5e');
 
 /**
  * @license
