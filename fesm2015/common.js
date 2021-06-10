51e47b8e47374110d1403e1695596f99c544b4c4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 16:42:14 +0000

    e0158a1515 fix(zone.js): only one listener should also re-throw an error correctly (#41868)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.4+4.sha-a75874c
+ * @license Angular v12.0.4+5.sha-e0158a1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5183,7 +5183,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.4+4.sha-a75874c');
+const VERSION = new Version('12.0.4+5.sha-e0158a1');
 
 /**
  * @license
