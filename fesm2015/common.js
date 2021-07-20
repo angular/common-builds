584ece3cdf790221431980714ffa4adc4235a643
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:51:38 +0000

    7eddd12bf0 feat(dev-infra): add option to setup global types in API golden test (#42876)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+34.sha-30165f9
+ * @license Angular v12.2.0-next.2+36.sha-7eddd12
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5098,7 +5098,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.2+34.sha-30165f9');
+const VERSION = new Version('12.2.0-next.2+36.sha-7eddd12');
 
 /**
  * @license
