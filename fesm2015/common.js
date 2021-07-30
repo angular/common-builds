db3fc43de9a73a6c7f42375e3fd406b300f2b879
Andrew Kushnir <akushnir@google.com>
2021-07-30 16:50:10 +0000

    0af354ce05 refactor(bazel): extract function to patch fileNameToModuleName on host (#42974)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+98.sha-c1c1cda.with-local-changes
+ * @license Angular v12.2.0-next.1+100.sha-81dce5c.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5098,7 +5098,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.1+98.sha-c1c1cda.with-local-changes');
+const VERSION = new Version('12.2.0-next.1+100.sha-81dce5c.with-local-changes');
 
 /**
  * @license
