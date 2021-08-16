fc86e0f072d2ff1e6fba920c5a528b74eaf7dd7b
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 20:30:29 +0000

    cd10a4cc13 test(compiler): add a test for parsing multiline expressions in attributes (#43129)

--- fesm2015/common.js
+++ fesm2015/common.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.1+12.sha-83fa3ee.with-local-changes
+ * @license Angular v12.2.1+19.sha-cd10a4c.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5098,7 +5098,7 @@ function isPlatformWorkerUi(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.1+12.sha-83fa3ee.with-local-changes');
+const VERSION = new Version('12.2.1+19.sha-cd10a4c.with-local-changes');
 
 /**
  * @license
