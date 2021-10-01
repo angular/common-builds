26b259edf76f063e1e2f30b965a7d41676758acf
Dylan Hunn <dylhunn@gmail.com>
2021-10-01 20:39:18 +0000

    c721135e37 release: cut the v13.0.0-next.10 release (#43672)

--- esm2020/src/pipes/date_pipe.mjs
+++ esm2020/src/pipes/date_pipe.mjs
@@ -178,9 +178,9 @@ export class DatePipe {
         }
     }
 }
-DatePipe.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.0.0-next.9+84.sha-c15b8c7.with-local-changes", ngImport: i0, type: DatePipe, deps: [{ token: LOCALE_ID }], target: i0.ɵɵFactoryTarget.Pipe });
-DatePipe.ɵpipe = i0.ɵɵngDeclarePipe({ minVersion: "12.0.0", version: "13.0.0-next.9+84.sha-c15b8c7.with-local-changes", ngImport: i0, type: DatePipe, name: "date" });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.0.0-next.9+84.sha-c15b8c7.with-local-changes", ngImport: i0, type: DatePipe, decorators: [{
+DatePipe.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.0.0-next.9+85.sha-c721135.with-local-changes", ngImport: i0, type: DatePipe, deps: [{ token: LOCALE_ID }], target: i0.ɵɵFactoryTarget.Pipe });
+DatePipe.ɵpipe = i0.ɵɵngDeclarePipe({ minVersion: "12.0.0", version: "13.0.0-next.9+85.sha-c721135.with-local-changes", ngImport: i0, type: DatePipe, name: "date" });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.0.0-next.9+85.sha-c721135.with-local-changes", ngImport: i0, type: DatePipe, decorators: [{
             type: Pipe,
             args: [{ name: 'date', pure: true }]
         }], ctorParameters: function () { return [{ type: undefined, decorators: [{
