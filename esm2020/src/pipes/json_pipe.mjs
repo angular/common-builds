00cb67c0412f27e0dbf08f0b0e6c36aa76058e35
Andrew Kushnir <akushnir@google.com>
2021-10-15 18:57:17 +0000

    e79e3cafec fix(router): fix Router's public API for canceledNavigationResolution (#43842)

--- esm2020/src/pipes/json_pipe.mjs
+++ esm2020/src/pipes/json_pipe.mjs
@@ -30,9 +30,9 @@ export class JsonPipe {
         return JSON.stringify(value, null, 2);
     }
 }
-JsonPipe.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.0.0-next.14+65.sha-9a6bd26.with-local-changes", ngImport: i0, type: JsonPipe, deps: [], target: i0.ɵɵFactoryTarget.Pipe });
-JsonPipe.ɵpipe = i0.ɵɵngDeclarePipe({ minVersion: "12.0.0", version: "13.0.0-next.14+65.sha-9a6bd26.with-local-changes", ngImport: i0, type: JsonPipe, name: "json", pure: false });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.0.0-next.14+65.sha-9a6bd26.with-local-changes", ngImport: i0, type: JsonPipe, decorators: [{
+JsonPipe.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.0.0-next.14+64.sha-e79e3ca.with-local-changes", ngImport: i0, type: JsonPipe, deps: [], target: i0.ɵɵFactoryTarget.Pipe });
+JsonPipe.ɵpipe = i0.ɵɵngDeclarePipe({ minVersion: "12.0.0", version: "13.0.0-next.14+64.sha-e79e3ca.with-local-changes", ngImport: i0, type: JsonPipe, name: "json", pure: false });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.0.0-next.14+64.sha-e79e3ca.with-local-changes", ngImport: i0, type: JsonPipe, decorators: [{
             type: Pipe,
             args: [{ name: 'json', pure: false }]
         }] });
