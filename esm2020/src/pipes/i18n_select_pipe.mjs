5f2cf67a9ebf0c5655607590137c40a134d82979
Andrew Scott <atscott01@gmail.com>
2021-11-09 21:13:53 +0000

    247c18dee0 fix(compiler-cli): handle pre-release versions when checking version (#44109)

--- esm2020/src/pipes/i18n_select_pipe.mjs
+++ esm2020/src/pipes/i18n_select_pipe.mjs
@@ -46,9 +46,9 @@ export class I18nSelectPipe {
         return '';
     }
 }
-I18nSelectPipe.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.1.0-next.0+62.sha-28ef5af.with-local-changes", ngImport: i0, type: I18nSelectPipe, deps: [], target: i0.ɵɵFactoryTarget.Pipe });
-I18nSelectPipe.ɵpipe = i0.ɵɵngDeclarePipe({ minVersion: "12.0.0", version: "13.1.0-next.0+62.sha-28ef5af.with-local-changes", ngImport: i0, type: I18nSelectPipe, name: "i18nSelect" });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.1.0-next.0+62.sha-28ef5af.with-local-changes", ngImport: i0, type: I18nSelectPipe, decorators: [{
+I18nSelectPipe.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.1.0-next.0+64.sha-247c18d.with-local-changes", ngImport: i0, type: I18nSelectPipe, deps: [], target: i0.ɵɵFactoryTarget.Pipe });
+I18nSelectPipe.ɵpipe = i0.ɵɵngDeclarePipe({ minVersion: "12.0.0", version: "13.1.0-next.0+64.sha-247c18d.with-local-changes", ngImport: i0, type: I18nSelectPipe, name: "i18nSelect" });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.1.0-next.0+64.sha-247c18d.with-local-changes", ngImport: i0, type: I18nSelectPipe, decorators: [{
             type: Pipe,
             args: [{ name: 'i18nSelect', pure: true }]
         }] });
