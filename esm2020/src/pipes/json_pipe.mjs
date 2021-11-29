2c1098e1dbfc2e9bd5dac831178a5a0c24d8b366
Dylan Hunn <dylhunn@gmail.com>
2021-11-29 22:39:38 +0000

    3aafa765ee fix(language-service): Correctly parse inputs and selectors with dollar signs (#44268)

--- esm2020/src/pipes/json_pipe.mjs
+++ esm2020/src/pipes/json_pipe.mjs
@@ -30,9 +30,9 @@ export class JsonPipe {
         return JSON.stringify(value, null, 2);
     }
 }
-JsonPipe.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.1.0-next.2+65.sha-ef140a7.with-local-changes", ngImport: i0, type: JsonPipe, deps: [], target: i0.ɵɵFactoryTarget.Pipe });
-JsonPipe.ɵpipe = i0.ɵɵngDeclarePipe({ minVersion: "12.0.0", version: "13.1.0-next.2+65.sha-ef140a7.with-local-changes", ngImport: i0, type: JsonPipe, name: "json", pure: false });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.1.0-next.2+65.sha-ef140a7.with-local-changes", ngImport: i0, type: JsonPipe, decorators: [{
+JsonPipe.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.1.0-next.2+67.sha-3aafa76.with-local-changes", ngImport: i0, type: JsonPipe, deps: [], target: i0.ɵɵFactoryTarget.Pipe });
+JsonPipe.ɵpipe = i0.ɵɵngDeclarePipe({ minVersion: "12.0.0", version: "13.1.0-next.2+67.sha-3aafa76.with-local-changes", ngImport: i0, type: JsonPipe, name: "json", pure: false });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.1.0-next.2+67.sha-3aafa76.with-local-changes", ngImport: i0, type: JsonPipe, decorators: [{
             type: Pipe,
             args: [{ name: 'json', pure: false }]
         }] });
