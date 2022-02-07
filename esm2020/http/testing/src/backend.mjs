7af77f348d5fa5220874a7b147d0b87ebd74e3b0
Dylan Hunn <dylhunn@gmail.com>
2022-02-07 19:04:22 +0000

    db6cf7e7c1 fix(compiler): allow banana-in-a-box bindings to end with non-null assertion (#37809)

--- esm2020/http/testing/src/backend.mjs
+++ esm2020/http/testing/src/backend.mjs
@@ -135,9 +135,9 @@ export class HttpClientTestingBackend {
         }
     }
 }
-HttpClientTestingBackend.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.0.0-next.1+39.sha-638a2d5.with-local-changes", ngImport: i0, type: HttpClientTestingBackend, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
-HttpClientTestingBackend.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "14.0.0-next.1+39.sha-638a2d5.with-local-changes", ngImport: i0, type: HttpClientTestingBackend });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.0.0-next.1+39.sha-638a2d5.with-local-changes", ngImport: i0, type: HttpClientTestingBackend, decorators: [{
+HttpClientTestingBackend.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.0.0-next.1+40.sha-db6cf7e.with-local-changes", ngImport: i0, type: HttpClientTestingBackend, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
+HttpClientTestingBackend.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "14.0.0-next.1+40.sha-db6cf7e.with-local-changes", ngImport: i0, type: HttpClientTestingBackend });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.0.0-next.1+40.sha-db6cf7e.with-local-changes", ngImport: i0, type: HttpClientTestingBackend, decorators: [{
             type: Injectable
         }] });
 function describeRequest(testRequest) {
