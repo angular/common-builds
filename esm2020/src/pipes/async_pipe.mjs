d1a0aaca798e0dc93219d878a3b198dda9ee29d2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-14 21:09:21 +0000

    f17e26f7c1 fix(docs-infra): fix broken link in Glossary (#45068)

--- esm2020/src/pipes/async_pipe.mjs
+++ esm2020/src/pipes/async_pipe.mjs
@@ -116,9 +116,9 @@ export class AsyncPipe {
         }
     }
 }
-AsyncPipe.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.0.0-next.2+26.sha-e872e9d.with-local-changes", ngImport: i0, type: AsyncPipe, deps: [{ token: i0.ChangeDetectorRef }], target: i0.ɵɵFactoryTarget.Pipe });
-AsyncPipe.ɵpipe = i0.ɵɵngDeclarePipe({ minVersion: "12.0.0", version: "14.0.0-next.2+26.sha-e872e9d.with-local-changes", ngImport: i0, type: AsyncPipe, name: "async", pure: false });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.0.0-next.2+26.sha-e872e9d.with-local-changes", ngImport: i0, type: AsyncPipe, decorators: [{
+AsyncPipe.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.0.0-next.2+28.sha-f17e26f.with-local-changes", ngImport: i0, type: AsyncPipe, deps: [{ token: i0.ChangeDetectorRef }], target: i0.ɵɵFactoryTarget.Pipe });
+AsyncPipe.ɵpipe = i0.ɵɵngDeclarePipe({ minVersion: "12.0.0", version: "14.0.0-next.2+28.sha-f17e26f.with-local-changes", ngImport: i0, type: AsyncPipe, name: "async", pure: false });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.0.0-next.2+28.sha-f17e26f.with-local-changes", ngImport: i0, type: AsyncPipe, decorators: [{
             type: Pipe,
             args: [{ name: 'async', pure: false }]
         }], ctorParameters: function () { return [{ type: i0.ChangeDetectorRef }]; } });
