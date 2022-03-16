be1c25e7b66c4819869b41bcd57559bae31367fe
GitHub <noreply@github.com>
2022-03-16 19:45:09 +0000

    34c48a3f9a docs: release notes for the v13.3.0 release (#45366)

--- esm2020/src/pipes/i18n_plural_pipe.mjs
+++ esm2020/src/pipes/i18n_plural_pipe.mjs
@@ -46,9 +46,9 @@ export class I18nPluralPipe {
         return pluralMap[key].replace(_INTERPOLATION_REGEXP, value.toString());
     }
 }
-I18nPluralPipe.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.0.0-next.6+20.sha-306fb5e", ngImport: i0, type: I18nPluralPipe, deps: [{ token: i1.NgLocalization }], target: i0.ɵɵFactoryTarget.Pipe });
-I18nPluralPipe.ɵpipe = i0.ɵɵngDeclarePipe({ minVersion: "12.0.0", version: "14.0.0-next.6+20.sha-306fb5e", ngImport: i0, type: I18nPluralPipe, name: "i18nPlural" });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.0.0-next.6+20.sha-306fb5e", ngImport: i0, type: I18nPluralPipe, decorators: [{
+I18nPluralPipe.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.0.0-next.6+21.sha-34c48a3", ngImport: i0, type: I18nPluralPipe, deps: [{ token: i1.NgLocalization }], target: i0.ɵɵFactoryTarget.Pipe });
+I18nPluralPipe.ɵpipe = i0.ɵɵngDeclarePipe({ minVersion: "12.0.0", version: "14.0.0-next.6+21.sha-34c48a3", ngImport: i0, type: I18nPluralPipe, name: "i18nPlural" });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.0.0-next.6+21.sha-34c48a3", ngImport: i0, type: I18nPluralPipe, decorators: [{
             type: Pipe,
             args: [{ name: 'i18nPlural', pure: true }]
         }], ctorParameters: function () { return [{ type: i1.NgLocalization }]; } });
