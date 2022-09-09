af311fdeaafb0fae5e7ca4360606f34af983663a
Dylan Hunn <dylhunn@gmail.com>
2022-09-09 21:22:52 +0000

    710d1da4ed refactor(common): create an `NgFor` alias for `NgForOf` directive (#47309)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.1+sha-7e0de5b
+ * @license Angular v15.0.0-next.1+sha-710d1da
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1049,7 +1049,8 @@ declare namespace i2 {
 declare namespace i3 {
     export {
         NgForOfContext,
-        NgForOf
+        NgForOf,
+        NgForOf as NgFor
     }
 }
 
@@ -1723,7 +1724,7 @@ export declare class NgComponentOutlet implements OnChanges, OnDestroy {
  * @ngModule CommonModule
  * @publicApi
  */
-export declare class NgForOf<T, U extends NgIterable<T> = NgIterable<T>> implements DoCheck {
+declare class NgForOf<T, U extends NgIterable<T> = NgIterable<T>> implements DoCheck {
     private _viewContainer;
     private _template;
     private _differs;
@@ -1778,6 +1779,8 @@ export declare class NgForOf<T, U extends NgIterable<T> = NgIterable<T>> impleme
     static ɵfac: i0.ɵɵFactoryDeclaration<NgForOf<any, any>, never>;
     static ɵdir: i0.ɵɵDirectiveDeclaration<NgForOf<any, any>, "[ngFor][ngForOf]", never, { "ngForOf": "ngForOf"; "ngForTrackBy": "ngForTrackBy"; "ngForTemplate": "ngForTemplate"; }, {}, never, never, true, never>;
 }
+export { NgForOf as NgFor }
+export { NgForOf }
 
 /**
  * @publicApi
