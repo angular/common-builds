8887c31decf24689d23fe08ed84fa8eeca46b013
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-11 22:25:16 +0000

    6c9b5a2557 docs: fix linter errors for security.md (#47688)

--- esm2020/src/pipes/keyvalue_pipe.mjs
+++ esm2020/src/pipes/keyvalue_pipe.mjs
@@ -59,9 +59,9 @@ export class KeyValuePipe {
         return this.keyValues;
     }
 }
-KeyValuePipe.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.2.5+sha-2fd893d", ngImport: i0, type: KeyValuePipe, deps: [{ token: i0.KeyValueDiffers }], target: i0.ɵɵFactoryTarget.Pipe });
-KeyValuePipe.ɵpipe = i0.ɵɵngDeclarePipe({ minVersion: "14.0.0", version: "14.2.5+sha-2fd893d", ngImport: i0, type: KeyValuePipe, isStandalone: true, name: "keyvalue", pure: false });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.2.5+sha-2fd893d", ngImport: i0, type: KeyValuePipe, decorators: [{
+KeyValuePipe.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.2.5+sha-6c9b5a2", ngImport: i0, type: KeyValuePipe, deps: [{ token: i0.KeyValueDiffers }], target: i0.ɵɵFactoryTarget.Pipe });
+KeyValuePipe.ɵpipe = i0.ɵɵngDeclarePipe({ minVersion: "14.0.0", version: "14.2.5+sha-6c9b5a2", ngImport: i0, type: KeyValuePipe, isStandalone: true, name: "keyvalue", pure: false });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.2.5+sha-6c9b5a2", ngImport: i0, type: KeyValuePipe, decorators: [{
             type: Pipe,
             args: [{
                     name: 'keyvalue',
