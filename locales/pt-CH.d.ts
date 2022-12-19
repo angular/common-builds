7496aa69255b183be49a46b60200e5ebe7381b5b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-19 20:37:13 +0000

    a3958a2a76 build: update google sync config to match internal changes (#48538)

--- locales/pt-CH.d.ts
+++ locales/pt-CH.d.ts
@@ -5,7 +5,6 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-/// <amd-module name="@angular/common/locales/pt-CH" />
 declare function plural(val: number): number;
 declare const _default: (string | number | number[] | (string | undefined)[] | typeof plural | (string[] | undefined)[] | {
     AUD: string[];
