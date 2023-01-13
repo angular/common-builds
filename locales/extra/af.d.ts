800ff7effa5efae74873dcda9997e06e6ce2f478
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-01-13 14:49:32 +0000

    5978eedec0 build: update tslint esm-cjs interop settings (#48732)

--- locales/extra/af.d.ts
+++ locales/extra/af.d.ts
@@ -5,6 +5,5 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-/// <amd-module name="@angular/common/locales/extra/af" />
 declare const _default: ((string[] | undefined)[] | (string | string[])[])[];
 export default _default;
