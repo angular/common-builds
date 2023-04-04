5ac9e3b452b36f219b6ed13a6d779a50a1ac34bf
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 02:51:52 +0000

    b2327f4df1 feat(core): Allow typeguards on QueryList.filter (#48042)

--- esm2022/src/directives/ng_optimized_image/preconnect_link_checker.mjs
+++ esm2022/src/directives/ng_optimized_image/preconnect_link_checker.mjs
@@ -117,11 +117,11 @@ class PreconnectLinkChecker {
         this.preconnectLinks?.clear();
         this.alreadySeen.clear();
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.0.0-next.6+sha-6ca1a53", ngImport: i0, type: PreconnectLinkChecker, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
-    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.0.0-next.6+sha-6ca1a53", ngImport: i0, type: PreconnectLinkChecker, providedIn: 'root' }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.0.0-next.6+sha-b2327f4", ngImport: i0, type: PreconnectLinkChecker, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
+    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.0.0-next.6+sha-b2327f4", ngImport: i0, type: PreconnectLinkChecker, providedIn: 'root' }); }
 }
 export { PreconnectLinkChecker };
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.0.0-next.6+sha-6ca1a53", ngImport: i0, type: PreconnectLinkChecker, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.0.0-next.6+sha-b2327f4", ngImport: i0, type: PreconnectLinkChecker, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'root' }]
         }], ctorParameters: function () { return []; } });
