392e000b8c19f0f1bfc44d6e224c05bb0ba34329
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 20:10:32 +0000

    9180f98f0e fix(core): ensure takeUntilDestroyed unregisters onDestroy listener on unsubscribe (#49901)

--- esm2022/testing/src/mock_location_strategy.mjs
+++ esm2022/testing/src/mock_location_strategy.mjs
@@ -76,11 +76,11 @@ class MockLocationStrategy extends LocationStrategy {
     getState() {
         return this.stateChanges[(this.stateChanges.length || 1) - 1];
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.0.0-rc.2+sha-2f4b61e", ngImport: i0, type: MockLocationStrategy, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
-    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.0.0-rc.2+sha-2f4b61e", ngImport: i0, type: MockLocationStrategy }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.0.0-rc.2+sha-9180f98", ngImport: i0, type: MockLocationStrategy, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
+    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.0.0-rc.2+sha-9180f98", ngImport: i0, type: MockLocationStrategy }); }
 }
 export { MockLocationStrategy };
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.0.0-rc.2+sha-2f4b61e", ngImport: i0, type: MockLocationStrategy, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.0.0-rc.2+sha-9180f98", ngImport: i0, type: MockLocationStrategy, decorators: [{
             type: Injectable
         }], ctorParameters: function () { return []; } });
 class _MockPopStateEvent {
