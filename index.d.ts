a4c9dbcef43f71a7af02b565a794783be6ecce5c
Dylan Hunn <dylhunn@gmail.com>
2023-07-11 15:47:09 +0000

    1837efb9da feat(common): Allow ngSrc to be changed post-init (#50683) Remove thrown error when ngSrc is modified after an NgOptimizedImage image is initialized

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.1+sha-a14bdfe
+ * @license Angular v16.2.0-next.1+sha-1837efb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2276,6 +2276,7 @@ export declare class NgOptimizedImage implements OnInit, OnChanges, OnDestroy {
     private getRewrittenSrcset;
     private getAutomaticSrcset;
     private getResponsiveSrcset;
+    private updateSrcAndSrcset;
     private getFixedSrcset;
     private shouldGenerateAutomaticSrcset;
     /** @nodoc */
