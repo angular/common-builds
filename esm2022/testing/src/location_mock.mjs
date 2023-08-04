8b1035bdba05986daaa03b44a5e6f15e00e907f4
Dylan Hunn <dylhunn@gmail.com>
2023-08-04 15:49:26 +0000

    0a38dc3c26 refactor(core): throw an error when hydration marker is missing from DOM (#51170)

--- esm2022/testing/src/location_mock.mjs
+++ esm2022/testing/src/location_mock.mjs
@@ -138,10 +138,10 @@ export class SpyLocation {
         this._history.push(new LocationState(path, query, state));
         this._historyIndex = this._history.length - 1;
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.3.0-next.0+sha-526e215", ngImport: i0, type: SpyLocation, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
-    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.3.0-next.0+sha-526e215", ngImport: i0, type: SpyLocation }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.3.0-next.0+sha-0a38dc3", ngImport: i0, type: SpyLocation, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
+    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.3.0-next.0+sha-0a38dc3", ngImport: i0, type: SpyLocation }); }
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.3.0-next.0+sha-526e215", ngImport: i0, type: SpyLocation, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.3.0-next.0+sha-0a38dc3", ngImport: i0, type: SpyLocation, decorators: [{
             type: Injectable
         }] });
 class LocationState {
