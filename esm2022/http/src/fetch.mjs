e8ca8bd6bd0d18e58b7357c005617dbed97857d2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-11 14:09:25 +0000

    5212b47bbf refactor(compiler): introduce defer trigger instructions (#51315)

--- esm2022/http/src/fetch.mjs
+++ esm2022/http/src/fetch.mjs
@@ -211,10 +211,10 @@ export class FetchBackend {
         }
         return chunksAll;
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "17.0.0-next.0+sha-f3f97f0", ngImport: i0, type: FetchBackend, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
-    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "17.0.0-next.0+sha-f3f97f0", ngImport: i0, type: FetchBackend }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "17.0.0-next.0+sha-5212b47", ngImport: i0, type: FetchBackend, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
+    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "17.0.0-next.0+sha-5212b47", ngImport: i0, type: FetchBackend }); }
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "17.0.0-next.0+sha-f3f97f0", ngImport: i0, type: FetchBackend, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "17.0.0-next.0+sha-5212b47", ngImport: i0, type: FetchBackend, decorators: [{
             type: Injectable
         }] });
 /**
