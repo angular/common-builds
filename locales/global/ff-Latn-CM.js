4c4eb076981e0457f07753a872575ca7be413a11
Dylan Hunn <dylhunn@gmail.com>
2023-10-26 16:45:00 +0000

    6a01d62 fix(migrations): fix broken migration when no control flow is present (#52399)

--- locales/global/ff-Latn-CM.js
+++ locales/global/ff-Latn-CM.js
@@ -7,10 +7,10 @@
  */
 
 // THIS CODE IS GENERATED - DO NOT MODIFY.
-  (function() {
-    globalThis.ng ??= {};
-    globalThis.ng.common ??= {};
-    globalThis.ng.common.locales ??= {};
+  (function(global) {
+    global.ng ??= {};
+    global.ng.common ??= {};
+    global.ng.common.locales ??= {};
     const u = undefined;
     function plural(val) {
 const n = val, i = Math.floor(Math.abs(val));
@@ -19,6 +19,6 @@ if (i === 0 || i === 1)
     return 1;
 return 5;
 }
-    globalThis.ng.common.locales['ff-latn-cm'] = ["ff-Latn-CM",[["subaka","kikiiɗe"],u,u],u,[["d","a","m","n","n","m","h"],["dew","aaɓ","maw","nje","naa","mwd","hbi"],["dewo","aaɓnde","mawbaare","njeslaare","naasaande","mawnde","hoore-biir"],["dew","aaɓ","maw","nje","naa","mwd","hbi"]],u,[["s","c","m","s","d","k","m","j","s","y","j","b"],["sii","col","mbo","see","duu","kor","mor","juk","slt","yar","jol","bow"],["siilo","colte","mbooy","seeɗto","duujal","korse","morso","juko","siilto","yarkomaa","jolal","bowte"]],u,[["H-I","C-I"],u,["Hade Iisa","Caggal Iisa"]],1,[6,0],["d/M/y","d MMM, y","d MMMM y","EEEE d MMMM y"],["HH:mm","HH:mm:ss","HH:mm:ss z","HH:mm:ss zzzz"],["{1} {0}",u,u,u],[","," ",";","%","+","-","E","×","‰","∞","NaN",":"],["#,##0.###","#,##0%","#,##0.00 ¤","#E0"],"XAF","FCFA","Mbuuɗi Seefaa BEAC",{"JPY":["JP¥","¥"],"USD":["US$","$"]},"ltr", plural, []];
-  })();
+    global.ng.common.locales['ff-latn-cm'] = ["ff-Latn-CM",[["subaka","kikiiɗe"],u,u],u,[["d","a","m","n","n","m","h"],["dew","aaɓ","maw","nje","naa","mwd","hbi"],["dewo","aaɓnde","mawbaare","njeslaare","naasaande","mawnde","hoore-biir"],["dew","aaɓ","maw","nje","naa","mwd","hbi"]],u,[["s","c","m","s","d","k","m","j","s","y","j","b"],["sii","col","mbo","see","duu","kor","mor","juk","slt","yar","jol","bow"],["siilo","colte","mbooy","seeɗto","duujal","korse","morso","juko","siilto","yarkomaa","jolal","bowte"]],u,[["H-I","C-I"],u,["Hade Iisa","Caggal Iisa"]],1,[6,0],["d/M/y","d MMM, y","d MMMM y","EEEE d MMMM y"],["HH:mm","HH:mm:ss","HH:mm:ss z","HH:mm:ss zzzz"],["{1} {0}",u,u,u],[","," ",";","%","+","-","E","×","‰","∞","NaN",":"],["#,##0.###","#,##0%","#,##0.00 ¤","#E0"],"XAF","FCFA","Mbuuɗi Seefaa BEAC",{"JPY":["JP¥","¥"],"USD":["US$","$"]},"ltr", plural, []];
+  })(globalThis);
     
--- locales/global/fur.js
+++ locales/global/fur.js
@@ -7,10 +7,10 @@
  */
 
 // THIS CODE IS GENERATED - DO NOT MODIFY.
-  (function() {
-    globalThis.ng ??= {};
-    globalThis.ng.common ??= {};
-    globalThis.ng.common.locales ??= {};
+  (function(global) {
+    global.ng ??= {};
+    global.ng.common ??= {};
+    global.ng.common.locales ??= {};
     const u = undefined;
     function plural(val) {
 const n = val;
@@ -19,6 +19,6 @@ if (n === 1)
     return 1;
 return 5;
 }
-    globalThis.ng.common.locales['fur'] = ["fur",[["a.","p."],u,u],u,[["D","L","M","M","J","V","S"],["dom","lun","mar","mie","joi","vin","sab"],["domenie","lunis","martars","miercus","joibe","vinars","sabide"],["dom","lun","mar","mie","joi","vin","sab"]],u,[["Z","F","M","A","M","J","L","A","S","O","N","D"],["Zen","Fev","Mar","Avr","Mai","Jug","Lui","Avo","Set","Otu","Nov","Dic"],["Zenâr","Fevrâr","Març","Avrîl","Mai","Jugn","Lui","Avost","Setembar","Otubar","Novembar","Dicembar"]],u,[["pdC","ddC"],u,u],1,[6,0],["dd/MM/yy","dd/MM/y","d 'di' MMMM 'dal' y","EEEE d 'di' MMMM 'dal' y"],["HH:mm","HH:mm:ss","HH:mm:ss z","HH:mm:ss zzzz"],["{1} {0}",u,u,u],[",",".",";","%","+","-","E","×","‰","∞","NaN",":"],["#,##0.###","#,##0%","¤ #,##0.00","#E0"],"EUR","€","euro",{"JPY":["JP¥","¥"],"USD":["US$","$"]},"ltr", plural, []];
-  })();
+    global.ng.common.locales['fur'] = ["fur",[["a.","p."],u,u],u,[["D","L","M","M","J","V","S"],["dom","lun","mar","mie","joi","vin","sab"],["domenie","lunis","martars","miercus","joibe","vinars","sabide"],["dom","lun","mar","mie","joi","vin","sab"]],u,[["Z","F","M","A","M","J","L","A","S","O","N","D"],["Zen","Fev","Mar","Avr","Mai","Jug","Lui","Avo","Set","Otu","Nov","Dic"],["Zenâr","Fevrâr","Març","Avrîl","Mai","Jugn","Lui","Avost","Setembar","Otubar","Novembar","Dicembar"]],u,[["pdC","ddC"],u,u],1,[6,0],["dd/MM/yy","dd/MM/y","d 'di' MMMM 'dal' y","EEEE d 'di' MMMM 'dal' y"],["HH:mm","HH:mm:ss","HH:mm:ss z","HH:mm:ss zzzz"],["{1} {0}",u,u,u],[",",".",";","%","+","-","E","×","‰","∞","NaN",":"],["#,##0.###","#,##0%","¤ #,##0.00","#E0"],"EUR","€","euro",{"JPY":["JP¥","¥"],"USD":["US$","$"]},"ltr", plural, []];
+  })(globalThis);
     
