55d0c8747241aacb8ff38ebcbceb89454d22cadb
Andrew Scott <atscott@google.com>
2024-01-03 18:39:29 +0000

    fed831f docs: fix incorrect glossary entry (#53609)

--- esm2022/http/src/fetch.mjs
+++ esm2022/http/src/fetch.mjs
@@ -211,10 +211,10 @@ export class FetchBackend {
         }
         return chunksAll;
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "17.1.0-next.5+sha-33b5707", ngImport: i0, type: FetchBackend, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
-    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "17.1.0-next.5+sha-33b5707", ngImport: i0, type: FetchBackend }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "17.1.0-next.5+sha-fed831f", ngImport: i0, type: FetchBackend, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
+    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "17.1.0-next.5+sha-fed831f", ngImport: i0, type: FetchBackend }); }
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "17.1.0-next.5+sha-33b5707", ngImport: i0, type: FetchBackend, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "17.1.0-next.5+sha-fed831f", ngImport: i0, type: FetchBackend, decorators: [{
             type: Injectable
         }] });
 /**
