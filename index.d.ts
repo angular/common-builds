6b863ffb138ca22b052a816bc278910c4a9f7889
Dylan Hunn <dylhunn@gmail.com>
2024-05-21 14:45:48 +0000

    87c5f3c docs(common): fix FormatWidth.Short description (#55821)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.0+sha-1360110
+ * @license Angular v18.1.0-next.0+sha-87c5f3c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -618,7 +618,7 @@ export declare function formatPercent(value: number, locale: string, digitsInfo?
  */
 export declare enum FormatWidth {
     /**
-     * For `en-US`, 'M/d/yy, h:mm a'`
+     * For `en-US`, `'M/d/yy, h:mm a'`
      * (Example: `6/15/15, 9:03 AM`)
      */
     Short = 0,
