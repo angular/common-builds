1e38edc58a5878876d67b3ca8ec1c68841c28f05
Andrew Scott <atscott@google.com>
2024-07-19 18:55:14 +0000

    c76b440 fix(compiler-cli): add warning for unused let declarations (#57033)

--- esm2022/http/src/fetch.mjs
+++ esm2022/http/src/fetch.mjs
@@ -217,10 +217,10 @@ export class FetchBackend {
         }
         return chunksAll;
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "18.2.0-next.1+sha-244c50d", ngImport: i0, type: FetchBackend, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
-    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "18.2.0-next.1+sha-244c50d", ngImport: i0, type: FetchBackend }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "18.2.0-next.1+sha-c76b440", ngImport: i0, type: FetchBackend, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
+    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "18.2.0-next.1+sha-c76b440", ngImport: i0, type: FetchBackend }); }
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "18.2.0-next.1+sha-244c50d", ngImport: i0, type: FetchBackend, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "18.2.0-next.1+sha-c76b440", ngImport: i0, type: FetchBackend, decorators: [{
             type: Injectable
         }] });
 /**
