77a093c5392a12169b250a30cbcbe3893e66fc26
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-02 15:15:45 +0000

    6c76c91 feat(router): Add defaultQueryParamsHandling to router configuration (#57198)

--- esm2022/http/src/fetch.mjs
+++ esm2022/http/src/fetch.mjs
@@ -217,10 +217,10 @@ export class FetchBackend {
         }
         return chunksAll;
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "18.2.0-next.3+sha-2a915d1", ngImport: i0, type: FetchBackend, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
-    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "18.2.0-next.3+sha-2a915d1", ngImport: i0, type: FetchBackend }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "18.2.0-next.3+sha-6c76c91", ngImport: i0, type: FetchBackend, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
+    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "18.2.0-next.3+sha-6c76c91", ngImport: i0, type: FetchBackend }); }
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "18.2.0-next.3+sha-2a915d1", ngImport: i0, type: FetchBackend, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "18.2.0-next.3+sha-6c76c91", ngImport: i0, type: FetchBackend, decorators: [{
             type: Injectable
         }] });
 /**
