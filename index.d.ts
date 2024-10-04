b67a4c1622f36153d81f85130007496246905521
Andrew Kushnir <akushnir@google.com>
2024-10-04 21:43:52 +0000

    0eb1c5c docs(common): Minor typo in code example (#58085)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.8+sha-06600d2
+ * @license Angular v19.0.0-next.8+sha-0eb1c5c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2403,7 +2403,7 @@ export declare abstract class NgLocalization {
  *   {
  *      provide: IMAGE_LOADER,
  *      useValue: (config: ImageLoaderConfig) => {
- *        return `https://example.com/${config.src}-${config.width}.jpg}`;
+ *        return `https://example.com/${config.src}-${config.width}.jpg`;
  *      }
  *   },
  * ],
