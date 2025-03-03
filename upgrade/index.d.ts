e7249b735e79bd4d513438a484d3771e4ae7c7f0
Miles Malerba <mmalerba@google.com>
2025-03-03 17:20:19 +0000

    36b60a9 fix(common): clean up `onUrlChange` listener when root scope is destroyed (#60004)

--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.0+sha-51b8ff2
+ * @license Angular v20.0.0-next.0+sha-36b60a9
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -42,6 +42,7 @@ export declare class $locationShim {
     private $$changeListeners;
     private cachedState;
     private urlChanges;
+    private readonly removeOnUrlChangeFn;
     constructor($injector: any, location: Location_2, platformLocation: PlatformLocation, urlCodec: UrlCodec, locationStrategy: LocationStrategy);
     private initialize;
     private resetBrowserUpdate;
