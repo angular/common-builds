76c316300de95e06d0ed531d9b71166c8723d893
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 09:09:03 +0000

    1071802 fix(compiler-cli): Always retain prior results for all files (#61487)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-9ce79be
+ * @license Angular v19.2.11+sha-1071802
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function registerLocaleData(data, localeId, extraData) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-9ce79be');
+const VERSION = new Version('19.2.11+sha-1071802');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
