b10ed7cda694f62437c30328cbae5fad38cf7229
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 11:35:04 +0000

    7bf4906 fix(compiler-cli): account for expression with type arguments during HMR extraction (#63261)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-3d7b31c
+ * @license Angular v21.0.0-next.0+sha-7bf4906
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-3d7b31c');
+const VERSION = new Version('21.0.0-next.0+sha-7bf4906');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
