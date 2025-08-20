102efcfb607399af471c876bcd3376802e9e3af8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 10:10:23 +0000

    d76a911 build: update all non-major dependencies (#63266)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-591c7e2
+ * @license Angular v20.2.0-rc.1+sha-d76a911
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.1+sha-591c7e2');
+const VERSION = new Version('20.2.0-rc.1+sha-d76a911');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
