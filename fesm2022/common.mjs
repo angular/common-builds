4d525b3ce096c9f3ba10497e3fe4a19390ecf5b7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 16:12:14 +0000

    009b068 build: reenable firefox tests for devtools (#63288)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-1c5fb1e
+ * @license Angular v21.0.0-next.0+sha-009b068
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-1c5fb1e');
+const VERSION = new Version('21.0.0-next.0+sha-009b068');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
