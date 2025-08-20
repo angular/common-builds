176b2500484c2e9a507d17f356a8395848289c81
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 09:47:53 +0000

    eeeaadc fix(language-service): Support to resolve the re-export component. (#62585)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-6a6cb01
+ * @license Angular v21.0.0-next.0+sha-eeeaadc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-6a6cb01');
+const VERSION = new Version('21.0.0-next.0+sha-eeeaadc');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
