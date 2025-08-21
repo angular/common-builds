b4a0d6fcfd6b4e7999123784d665b034def30fee
Andrew Kushnir <akushnir@google.com>
2025-08-21 18:46:45 +0000

    69df2c0 build: update dependency cypress to v15 (#63303)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-7d6ae95
+ * @license Angular v21.0.0-next.0+sha-69df2c0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-7d6ae95');
+const VERSION = new Version('21.0.0-next.0+sha-69df2c0');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
