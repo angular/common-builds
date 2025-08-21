73b694939c08be48eddd874c325155f4ffb047ae
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-08-21 10:57:34 +0000

    1b0c84a docs: release notes for the v20.2.1 release

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-827c3c1
+ * @license Angular v21.0.0-next.0+sha-1b0c84a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-827c3c1');
+const VERSION = new Version('21.0.0-next.0+sha-1b0c84a');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
