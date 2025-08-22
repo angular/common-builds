2bc92753e1996ff70f81efd18392e0f49823a93d
Andrew Kushnir <akushnir@google.com>
2025-08-22 18:34:02 +0000

    56a8408 build: update github/codeql-action action to v3.29.11 (#63325)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-6f541b8
+ * @license Angular v21.0.0-next.0+sha-56a8408
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-6f541b8');
+const VERSION = new Version('21.0.0-next.0+sha-56a8408');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
