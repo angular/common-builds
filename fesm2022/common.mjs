4c42389c5e1762a5163310b2ba97503161d5624e
Andrew Kushnir <akushnir@google.com>
2025-08-22 19:31:00 +0000

    e1481d8 ci: add myself to review fw-general (#63337)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.1+sha-e8aa7d3
+ * @license Angular v20.2.1+sha-e1481d8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.1+sha-e8aa7d3');
+const VERSION = new Version('20.2.1+sha-e1481d8');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
