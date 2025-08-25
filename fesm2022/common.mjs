d29e6d74b0547c210809a03db33d36de4c9e163c
Andrew Kushnir <akushnir@google.com>
2025-08-25 15:41:02 +0000

    f881082 build: update bazel dependencies (#63345)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.1+sha-650ad1f
+ * @license Angular v20.2.1+sha-f881082
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.1+sha-650ad1f');
+const VERSION = new Version('20.2.1+sha-f881082');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
