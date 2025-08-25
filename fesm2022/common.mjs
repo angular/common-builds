46b5e4d805ecd5aa72cf3c686555a05fd2de011f
Andrew Kushnir <akushnir@google.com>
2025-08-25 17:53:02 +0000

    5a11106 build: update cross-repo angular dependencies (#63338)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-1ebba54
+ * @license Angular v21.0.0-next.0+sha-5a11106
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-1ebba54');
+const VERSION = new Version('21.0.0-next.0+sha-5a11106');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
