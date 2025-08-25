c5e59c2a5938073ddb4aac4c7be8a903d8d53428
Andrew Kushnir <akushnir@google.com>
2025-08-25 15:21:41 +0000

    1e3196b docs: add important service worker note (#63364)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-3df1dcc
+ * @license Angular v21.0.0-next.0+sha-1e3196b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-3df1dcc');
+const VERSION = new Version('21.0.0-next.0+sha-1e3196b');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
