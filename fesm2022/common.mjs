e82b2e94a28b147d7871e013184db5cb243fd38b
Andrew Kushnir <akushnir@google.com>
2025-08-25 22:41:28 +0000

    a26d94d build: enable strict deps enforcement for ts_project (#63376)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.1+sha-454a269
+ * @license Angular v20.2.1+sha-a26d94d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.1+sha-454a269');
+const VERSION = new Version('20.2.1+sha-a26d94d');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
