f51a72b205cd4b1b61c54d867386f367f4effd86
Andrew Kushnir <akushnir@google.com>
2025-08-26 18:49:25 +0000

    dfa2044 build: fix strict deps failure (#63403)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-a038840
+ * @license Angular v21.0.0-next.0+sha-dfa2044
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-a038840');
+const VERSION = new Version('21.0.0-next.0+sha-dfa2044');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
