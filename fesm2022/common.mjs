918ee523968130660990c78fc59e628f6081b20c
Andrew Kushnir <akushnir@google.com>
2025-08-26 16:53:36 +0000

    e220a61 docs(docs-infra): patch port changes from `main` (#63394)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.1+sha-c9b0f45
+ * @license Angular v20.2.1+sha-e220a61
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.1+sha-c9b0f45');
+const VERSION = new Version('20.2.1+sha-e220a61');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
