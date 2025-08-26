e735eaa5177197a559628c19704cf50883a67aca
Andrew Kushnir <akushnir@google.com>
2025-08-26 21:56:44 +0000

    23d1ace refactor(core): Create a base effect interface and prototype to be used by both angular and wiz. (#62931)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.1+sha-e220a61
+ * @license Angular v20.2.1+sha-23d1ace
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.1+sha-e220a61');
+const VERSION = new Version('20.2.1+sha-23d1ace');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
