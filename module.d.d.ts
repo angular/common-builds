445c6ce3efb5a7ac0e1ed93979160c7e360a88cd
Andrew Kushnir <akushnir@google.com>
2025-08-27 18:33:43 +0000

    3a3bd36 docs: update to new standalone default behavior (#63329)

--- module.d.d.ts
+++ module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-f868465
+ * @license Angular v21.0.0-next.1+sha-3a3bd36
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -746,7 +746,7 @@ interface HttpUserEvent<T> {
  *
  * @publicApi
  */
-type HttpEvent<T> = HttpSentEvent | HttpHeaderResponse | HttpResponse<T> | HttpProgressEvent | HttpUserEvent<T>;
+type HttpEvent<T> = HttpSentEvent | HttpHeaderResponse | HttpResponse<T> | HttpDownloadProgressEvent | HttpUploadProgressEvent | HttpUserEvent<T>;
 /**
  * Base class for both `HttpResponse` and `HttpHeaderResponse`.
  *
