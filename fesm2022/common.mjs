1eebd13ae223e5f53378c890fc557cfc0bdf37d1
Miles Malerba <mmalerba@google.com>
2025-08-27 23:13:32 +0000

    cd748aa docs: remove the ng-class migration entry (#63430)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.2+sha-fce5602
+ * @license Angular v20.2.2+sha-cd748aa
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.2+sha-fce5602');
+const VERSION = new Version('20.2.2+sha-cd748aa');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
