edc2e1b7c5a3587844a0c7f0b4aedda7aa2e86be
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-08-27 17:57:49 +0000

    01f44ee docs: release notes for the v20.2.2 release

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-f4f5a8a
+ * @license Angular v21.0.0-next.0+sha-01f44ee
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-f4f5a8a');
+const VERSION = new Version('21.0.0-next.0+sha-01f44ee');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
