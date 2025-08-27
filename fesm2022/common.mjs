1a0cb102c1156bd77d25f44ca7eccdb16c88367f
Andrew Kushnir <akushnir@google.com>
2025-08-27 18:43:31 +0000

    fce5602 refactor(core): mark `VERSION` as `@__PURE__` for better tree-shaking (#63400)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.2+sha-06c5367
+ * @license Angular v20.2.2+sha-fce5602
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.2+sha-06c5367');
+const VERSION = new Version('20.2.2+sha-fce5602');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
