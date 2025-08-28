32585a5ca8801e256653122ee477694b588c038b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-28 10:23:32 +0000

    690405b docs: Add docs service worker to new options (#63436)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.2+sha-fe533cd
+ * @license Angular v20.2.2+sha-690405b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.2+sha-fe533cd');
+const VERSION = new Version('20.2.2+sha-690405b');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
