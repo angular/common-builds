0bb9752057f96c96350babc6e3f07e64b9a973ac
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-28 14:14:22 +0000

    ed3d1f2 fix(core): Fix cancellation of animation enter classes (#63442)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-99cf028
+ * @license Angular v21.0.0-next.1+sha-ed3d1f2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-99cf028');
+const VERSION = new Version('21.0.0-next.1+sha-ed3d1f2');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
