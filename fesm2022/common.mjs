14bbd11e6a53efc6ffc19c561519f728c23b9bb7
Doug Parker <dgp1130@users.noreply.github.com>
2025-08-28 17:39:41 +0000

    147c743 release: bump Angular DevTools to 1.0.44 (#63452)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-c9b215b
+ * @license Angular v21.0.0-next.1+sha-147c743
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-c9b215b');
+const VERSION = new Version('21.0.0-next.1+sha-147c743');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
