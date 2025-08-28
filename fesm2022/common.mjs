d900a751f839af37d2d0c7efd5943b923bd36562
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-28 10:05:36 +0000

    421edfe build: update @bazel/ibazel to `0.26.10` (#63435)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.2+sha-87bfe16
+ * @license Angular v20.2.2+sha-421edfe
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.2+sha-87bfe16');
+const VERSION = new Version('20.2.2+sha-421edfe');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
