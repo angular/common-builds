9897c395a42cf2007583d57cf73020767a70e0ee
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-28 10:12:14 +0000

    fe533cd docs: update Angular CLI help (#63420)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.2+sha-421edfe
+ * @license Angular v20.2.2+sha-fe533cd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.2+sha-421edfe');
+const VERSION = new Version('20.2.2+sha-fe533cd');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
