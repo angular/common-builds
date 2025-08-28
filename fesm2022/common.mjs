8c0146b0f1af1d66d677b1b9b1b3825b9be8eceb
Miles Malerba <mmalerba@google.com>
2025-08-28 16:05:59 +0000

    b8314bd feat(forms): add experimental signal-based forms (#63408)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-a1d1cdf
+ * @license Angular v21.0.0-next.1+sha-b8314bd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-a1d1cdf');
+const VERSION = new Version('21.0.0-next.1+sha-b8314bd');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
