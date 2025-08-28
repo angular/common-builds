77f3802a43c820a7962ad8364f0ac0cadaa83e02
Miles Malerba <mmalerba@google.com>
2025-08-28 15:52:45 +0000

    05a9322 build: remove `rulesJsInteropMode` (#63448)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.2+sha-114906d
+ * @license Angular v20.2.2+sha-05a9322
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.2+sha-114906d');
+const VERSION = new Version('20.2.2+sha-05a9322');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
