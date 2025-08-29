cffa7e55a7c7c90e304a210bec14bda8b3ab21e0
Miles Malerba <mmalerba@google.com>
2025-08-29 21:09:46 +0000

    e75b04f docs(docs-infra): properties should be readonly (#63492)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-bbb46e5
+ * @license Angular v21.0.0-next.1+sha-e75b04f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-bbb46e5');
+const VERSION = new Version('21.0.0-next.1+sha-e75b04f');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
