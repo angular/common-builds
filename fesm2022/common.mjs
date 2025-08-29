11800c77afbc7285e43c892ef51c611197ffb293
Miles Malerba <mmalerba@google.com>
2025-08-29 21:34:44 +0000

    862495c build: configure signal forms for release (#63458)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-e75b04f
+ * @license Angular v21.0.0-next.1+sha-862495c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-e75b04f');
+const VERSION = new Version('21.0.0-next.1+sha-862495c');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
