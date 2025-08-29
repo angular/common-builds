dba1ebd24df3d740874d12cf75c2e0e304cd6971
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-08-29 12:37:59 +0000

    1875d5c docs: release notes for the v20.2.3 release

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-0a82138
+ * @license Angular v21.0.0-next.1+sha-1875d5c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-0a82138');
+const VERSION = new Version('21.0.0-next.1+sha-1875d5c');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
