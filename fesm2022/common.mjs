608ee9c5d69f31ceaa7c87724e6d2f6b846c6a74
Miles Malerba <mmalerba@google.com>
2025-08-29 22:14:48 +0000

    16fa983 ci: add more team members as approvers (#63495)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-db95a5c
+ * @license Angular v21.0.0-next.1+sha-16fa983
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-db95a5c');
+const VERSION = new Version('21.0.0-next.1+sha-16fa983');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
