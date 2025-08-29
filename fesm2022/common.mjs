3b93b9a71631736b3ac25f67c198c14f743d01bb
Miles Malerba <mmalerba@google.com>
2025-08-29 15:59:01 +0000

    8dbeac1 build: update cross-repo angular dependencies (#63478)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-1abf6d5
+ * @license Angular v20.2.3+sha-8dbeac1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.3+sha-1abf6d5');
+const VERSION = new Version('20.2.3+sha-8dbeac1');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
