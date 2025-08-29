30f6e1ca871a78b47534eab6ef25609c292bc578
Miles Malerba <mmalerba@google.com>
2025-08-29 21:47:21 +0000

    db95a5c refactor(forms): loosen the error and disabled type on FormUiControl (#63455)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-862495c
+ * @license Angular v21.0.0-next.1+sha-db95a5c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-862495c');
+const VERSION = new Version('21.0.0-next.1+sha-db95a5c');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
