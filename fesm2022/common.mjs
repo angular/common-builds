12b1d73e499bd19dbeb463b6c2b206861ec2a30b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-29 11:56:43 +0000

    479a919 fix(compiler): fixes regression with event parsing and animate prefix (#63470)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.2+sha-98468c0
+ * @license Angular v20.2.2+sha-479a919
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.2+sha-98468c0');
+const VERSION = new Version('20.2.2+sha-479a919');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
