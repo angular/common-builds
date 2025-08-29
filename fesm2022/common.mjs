e75c578221568c22e0911796570c996220adb1d3
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-08-29 12:36:21 +0000

    64ddde2 release: cut the v20.2.3 release

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.2+sha-479a919
+ * @license Angular v20.2.3+sha-64ddde2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.2+sha-479a919');
+const VERSION = new Version('20.2.3+sha-64ddde2');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
