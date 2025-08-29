29cd09400da6be0e577915982fad91ac6148d6c8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-29 13:48:28 +0000

    dcd81d9 build: update cross-repo angular dependencies to 06d3af5 (#63475)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-3fb02ed
+ * @license Angular v21.0.0-next.1+sha-dcd81d9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-3fb02ed');
+const VERSION = new Version('21.0.0-next.1+sha-dcd81d9');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
