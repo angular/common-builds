d07c3b8e77e615e71332ff162ae57bbd5170bdd8
Miles Malerba <mmalerba@google.com>
2025-09-02 16:47:01 +0000

    e63608d docs(docs-infra): signal property should be readonly (#63166)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-668ced9
+ * @license Angular v21.0.0-next.1+sha-e63608d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-668ced9');
+const VERSION = new Version('21.0.0-next.1+sha-e63608d');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
