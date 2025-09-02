05f68c49a0d5749987f8286e2e02d5e9c2173c26
Miles Malerba <mmalerba@google.com>
2025-09-02 18:56:05 +0000

    4c36659 refactor(router): Add handling for empty observables in guards (#63546)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-92e09ad
+ * @license Angular v21.0.0-next.1+sha-4c36659
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-92e09ad');
+const VERSION = new Version('21.0.0-next.1+sha-4c36659');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
