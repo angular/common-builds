edf4fea5569a7dc6864d63a2c4f7cb6e762b4a73
Miles Malerba <mmalerba@google.com>
2025-09-02 19:42:32 +0000

    4fb8c46 docs: update Angular CLI help (#63552)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-4c36659
+ * @license Angular v21.0.0-next.1+sha-4fb8c46
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-4c36659');
+const VERSION = new Version('21.0.0-next.1+sha-4fb8c46');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
