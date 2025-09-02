4ee93ba4fca9bd90f8793c1f87bce7b45ec669a4
Miles Malerba <mmalerba@google.com>
2025-09-02 16:35:24 +0000

    1541606 docs(docs-infra): apply migration script changes (#63514)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-16fa983
+ * @license Angular v21.0.0-next.1+sha-1541606
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-16fa983');
+const VERSION = new Version('21.0.0-next.1+sha-1541606');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
