dcdbf24c8088ad30563e3937b407462e4d665dc5
Miles Malerba <mmalerba@google.com>
2025-09-03 03:55:44 +0000

    475ed59 refactor(core): export zoneless provider token privately (#63558)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-6148a40
+ * @license Angular v21.0.0-next.1+sha-475ed59
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-6148a40');
+const VERSION = new Version('21.0.0-next.1+sha-475ed59');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
