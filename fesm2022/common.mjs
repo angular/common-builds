ccc0c43fb0538b6ff33ac27b38faf3f804d50e3c
Miles Malerba <mmalerba@google.com>
2025-09-03 16:04:25 +0000

    e6949ad docs: fix #63555 (#63560)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-d006721
+ * @license Angular v21.0.0-next.1+sha-e6949ad
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-d006721');
+const VERSION = new Version('21.0.0-next.1+sha-e6949ad');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
