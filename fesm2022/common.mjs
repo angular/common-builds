be0a857b262999a092aae8be7fad6f2733afd81f
Andrew Scott <atscott@google.com>
2025-09-04 03:59:32 +0000

    951701d build: update cross-repo angular dependencies (#63581)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-08d363e
+ * @license Angular v20.2.4+sha-951701d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.4+sha-08d363e');
+const VERSION = new Version('20.2.4+sha-951701d');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
