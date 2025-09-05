af17581c451260489e1ba35c5fb9d8f0e3574725
Andrew Scott <atscott@google.com>
2025-09-05 21:03:52 +0000

    bacda4f refactor(forms): add support for binding number fields to date inputs (#63585)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-c0791e1
+ * @license Angular v21.0.0-next.2+sha-bacda4f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-c0791e1');
+const VERSION = new Version('21.0.0-next.2+sha-bacda4f');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
