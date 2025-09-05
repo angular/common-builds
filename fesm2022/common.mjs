d23a145fc0ea76c8469d5926aa3a9ae624f99386
Andrew Scott <atscott@google.com>
2025-09-05 20:11:30 +0000

    f18980c docs(docs-infra): fix mini share icon URL. (#63586)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-c3303a5
+ * @license Angular v20.2.4+sha-f18980c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.4+sha-c3303a5');
+const VERSION = new Version('20.2.4+sha-f18980c');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
