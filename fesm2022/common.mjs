6e651bc0ed458d3b5c5f9af6fa6571bbdd3dd6f1
Andrew Scott <atscott@google.com>
2025-09-05 19:56:38 +0000

    c3303a5 test: increase mermaid spec timeout to reduce flakes (#63610)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-f697995
+ * @license Angular v20.2.4+sha-c3303a5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.4+sha-f697995');
+const VERSION = new Version('20.2.4+sha-c3303a5');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
