b885f891a332b1c13d2f2fad51bd150c9777a257
Andrew Scott <atscott@google.com>
2025-09-08 18:18:25 +0000

    a6640e3 build: add additional comments explaining deep imports in adev (#63644)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-f2ec6fb
+ * @license Angular v20.2.4+sha-a6640e3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.4+sha-f2ec6fb');
+const VERSION = new Version('20.2.4+sha-a6640e3');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
