18bd7158c4ea602dc7ac69c6dfbc6032268de309
Andrew Scott <atscott@google.com>
2025-09-08 17:17:54 +0000

    f2ec6fb docs(docs-infra): quote title attributes in links (#63578)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-f18980c
+ * @license Angular v20.2.4+sha-f2ec6fb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.4+sha-f18980c');
+const VERSION = new Version('20.2.4+sha-f2ec6fb');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
