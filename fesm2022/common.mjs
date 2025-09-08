8379b337aa6071836dd751f5877b23a2d08fe7c2
Andrew Scott <atscott@google.com>
2025-09-08 17:14:36 +0000

    4f256d3 test(devtools): write some e2e tests for the transfer state tab (#62843)

--- fesm2022/common.mjs
+++ fesm2022/common.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-bacda4f
+ * @license Angular v21.0.0-next.2+sha-4f256d3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@ function isPlatformServer(platformId) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-bacda4f');
+const VERSION = new Version('21.0.0-next.2+sha-4f256d3');
 
 /**
  * Defines a scroll position manager. Implemented by `BrowserViewportScroller`.
