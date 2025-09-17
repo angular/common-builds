c1ee72d81448f5488aec4194e63c90cebf3f1aad
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 20:15:36 +0000

    669c7c0 build: update cross-repo angular dependencies (#63880)

--- fesm2022/platform_navigation.mjs
+++ fesm2022/platform_navigation.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-79a818b
+ * @license Angular v20.3.0+sha-669c7c0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -12,10 +12,10 @@ import { Injectable } from '@angular/core';
  * implementations.
  */
 class PlatformNavigation {
-    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.3.0+sha-79a818b", ngImport: i0, type: PlatformNavigation, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
-    static ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "20.3.0+sha-79a818b", ngImport: i0, type: PlatformNavigation, providedIn: 'platform', useFactory: () => window.navigation });
+    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.3.0+sha-669c7c0", ngImport: i0, type: PlatformNavigation, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
+    static ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "20.3.0+sha-669c7c0", ngImport: i0, type: PlatformNavigation, providedIn: 'platform', useFactory: () => window.navigation });
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.3.0+sha-79a818b", ngImport: i0, type: PlatformNavigation, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.3.0+sha-669c7c0", ngImport: i0, type: PlatformNavigation, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'platform', useFactory: () => window.navigation }]
         }] });
