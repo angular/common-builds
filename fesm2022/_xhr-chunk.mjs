f49789e16abdb3d2f2db60fc86a5daff598c9355
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-27 16:23:43 +0000

    b8c8bc6 build: update rules_angular digest to 1f1aa3e

--- fesm2022/_xhr-chunk.mjs
+++ fesm2022/_xhr-chunk.mjs
@@ -1,28 +1,22 @@
 /**
- * @license Angular v21.1.0-next.0+sha-a5678f6
+ * @license Angular v21.1.0-next.0+sha-b8c8bc6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 function parseCookieValue(cookieStr, name) {
-    name = encodeURIComponent(name);
-    for (const cookie of cookieStr.split(';')) {
-        const eqIndex = cookie.indexOf('=');
-        const [cookieName, cookieValue] = eqIndex == -1 ? [cookie, ''] : [cookie.slice(0, eqIndex), cookie.slice(eqIndex + 1)];
-        if (cookieName.trim() === name) {
-            return decodeURIComponent(cookieValue);
-        }
+  name = encodeURIComponent(name);
+  for (const cookie of cookieStr.split(';')) {
+    const eqIndex = cookie.indexOf('=');
+    const [cookieName, cookieValue] = eqIndex == -1 ? [cookie, ''] : [cookie.slice(0, eqIndex), cookie.slice(eqIndex + 1)];
+    if (cookieName.trim() === name) {
+      return decodeURIComponent(cookieValue);
     }
-    return null;
+  }
+  return null;
 }
 
-/**
- * A wrapper around the `XMLHttpRequest` constructor.
- *
- * @publicApi
- */
-class XhrFactory {
-}
+class XhrFactory {}
 
 export { XhrFactory, parseCookieValue };
 //# sourceMappingURL=_xhr-chunk.mjs.map
