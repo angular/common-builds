fade14170b53c544b8a58953f6cb54e4c61abf8b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-28 20:01:55 +0000

    6e004ca refactor(docs-infra): switch to unit test builder for unit tests

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.9+sha-b41a070
+ * @license Angular v21.0.0-next.9+sha-6e004ca
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -600,7 +600,7 @@ class LocationUpgradeModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-next.9+sha-b41a070",
+    version: "21.0.0-next.9+sha-6e004ca",
     ngImport: i0,
     type: LocationUpgradeModule,
     deps: [],
@@ -608,14 +608,14 @@ class LocationUpgradeModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.0.0-next.9+sha-b41a070",
+    version: "21.0.0-next.9+sha-6e004ca",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.0.0-next.9+sha-b41a070",
+    version: "21.0.0-next.9+sha-6e004ca",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
@@ -623,7 +623,7 @@ class LocationUpgradeModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-next.9+sha-b41a070",
+  version: "21.0.0-next.9+sha-6e004ca",
   ngImport: i0,
   type: LocationUpgradeModule,
   decorators: [{
