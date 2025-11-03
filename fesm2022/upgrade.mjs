a904efea7b833450e82c066102425fee70797539
Andrew Scott <atscott@google.com>
2025-11-03 20:11:11 +0000

    5d75d0e refactor(core): Replace usages of the `Function` type for animations

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-303bff6
+ * @license Angular v21.0.0-rc.0+sha-5d75d0e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -600,7 +600,7 @@ class LocationUpgradeModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.0+sha-303bff6",
+    version: "21.0.0-rc.0+sha-5d75d0e",
     ngImport: i0,
     type: LocationUpgradeModule,
     deps: [],
@@ -608,14 +608,14 @@ class LocationUpgradeModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.0.0-rc.0+sha-303bff6",
+    version: "21.0.0-rc.0+sha-5d75d0e",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.0+sha-303bff6",
+    version: "21.0.0-rc.0+sha-5d75d0e",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
@@ -623,7 +623,7 @@ class LocationUpgradeModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.0+sha-303bff6",
+  version: "21.0.0-rc.0+sha-5d75d0e",
   ngImport: i0,
   type: LocationUpgradeModule,
   decorators: [{
