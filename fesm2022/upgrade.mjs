9c686b312e09ee1059879b8ae82e545d2a761c8b
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-11 16:54:24 +0000

    75dd600 build: lock file maintenance

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-0770387
+ * @license Angular v21.1.0-next.0+sha-75dd600
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -600,7 +600,7 @@ class LocationUpgradeModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-0770387",
+    version: "21.1.0-next.0+sha-75dd600",
     ngImport: i0,
     type: LocationUpgradeModule,
     deps: [],
@@ -608,14 +608,14 @@ class LocationUpgradeModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.1.0-next.0+sha-0770387",
+    version: "21.1.0-next.0+sha-75dd600",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-0770387",
+    version: "21.1.0-next.0+sha-75dd600",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
@@ -623,7 +623,7 @@ class LocationUpgradeModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-0770387",
+  version: "21.1.0-next.0+sha-75dd600",
   ngImport: i0,
   type: LocationUpgradeModule,
   decorators: [{
