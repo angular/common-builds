6c082d1997323bc24854953cbf28e0381dbb9e8b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-12 21:23:24 +0000

    be37129 refactor(compiler-cli): rework type checking for signal forms

--- fesm2022/_platform_navigation-chunk.mjs
+++ fesm2022/_platform_navigation-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-eb9034d
+ * @license Angular v21.0.0-rc.2+sha-be37129
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -10,7 +10,7 @@ import { Injectable } from '@angular/core';
 class PlatformNavigation {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.2+sha-eb9034d",
+    version: "21.0.0-rc.2+sha-be37129",
     ngImport: i0,
     type: PlatformNavigation,
     deps: [],
@@ -18,7 +18,7 @@ class PlatformNavigation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.2+sha-eb9034d",
+    version: "21.0.0-rc.2+sha-be37129",
     ngImport: i0,
     type: PlatformNavigation,
     providedIn: 'platform',
@@ -27,7 +27,7 @@ class PlatformNavigation {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.2+sha-eb9034d",
+  version: "21.0.0-rc.2+sha-be37129",
   ngImport: i0,
   type: PlatformNavigation,
   decorators: [{
