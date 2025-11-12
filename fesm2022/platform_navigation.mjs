148b7e82dd7c90a8baaf072f36d35e9a0d95267a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-12 23:06:15 +0000

    3c065f7 docs: Fixed mermaid diagram node label contrast in dark mode

--- fesm2022/platform_navigation.mjs
+++ fesm2022/platform_navigation.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.11+sha-1c4fb90
+ * @license Angular v20.3.11+sha-3c065f7
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -12,10 +12,10 @@ import { Injectable } from '@angular/core';
  * implementations.
  */
 class PlatformNavigation {
-    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.3.11+sha-1c4fb90", ngImport: i0, type: PlatformNavigation, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
-    static ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "20.3.11+sha-1c4fb90", ngImport: i0, type: PlatformNavigation, providedIn: 'platform', useFactory: () => window.navigation });
+    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.3.11+sha-3c065f7", ngImport: i0, type: PlatformNavigation, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
+    static ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "20.3.11+sha-3c065f7", ngImport: i0, type: PlatformNavigation, providedIn: 'platform', useFactory: () => window.navigation });
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.3.11+sha-1c4fb90", ngImport: i0, type: PlatformNavigation, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.3.11+sha-3c065f7", ngImport: i0, type: PlatformNavigation, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'platform', useFactory: () => window.navigation }]
         }] });
