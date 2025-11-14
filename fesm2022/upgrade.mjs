237d8a83f78d4413de9d9b47ac40a5b5ccfaf185
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 16:32:20 +0000

    4fb2958 fix(docs-infra): update firebase caching regex for generated files

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-c2a52fd
+ * @license Angular v21.0.0-rc.2+sha-4fb2958
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -600,7 +600,7 @@ class LocationUpgradeModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.2+sha-c2a52fd",
+    version: "21.0.0-rc.2+sha-4fb2958",
     ngImport: i0,
     type: LocationUpgradeModule,
     deps: [],
@@ -608,14 +608,14 @@ class LocationUpgradeModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.0.0-rc.2+sha-c2a52fd",
+    version: "21.0.0-rc.2+sha-4fb2958",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.2+sha-c2a52fd",
+    version: "21.0.0-rc.2+sha-4fb2958",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
@@ -623,7 +623,7 @@ class LocationUpgradeModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.2+sha-c2a52fd",
+  version: "21.0.0-rc.2+sha-4fb2958",
   ngImport: i0,
   type: LocationUpgradeModule,
   decorators: [{
