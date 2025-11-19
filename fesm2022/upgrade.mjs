93513d914a683a9880dba99c73643ac94a10553b
Joey Perrott <josephperrott@gmail.com>
2025-11-19 21:38:29 +0000

    4e14a53 docs: add release blog to v21 page

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-6854776
+ * @license Angular v21.0.0+sha-4e14a53
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -600,7 +600,7 @@ class LocationUpgradeModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0+sha-6854776",
+    version: "21.0.0+sha-4e14a53",
     ngImport: i0,
     type: LocationUpgradeModule,
     deps: [],
@@ -608,14 +608,14 @@ class LocationUpgradeModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.0.0+sha-6854776",
+    version: "21.0.0+sha-4e14a53",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.0.0+sha-6854776",
+    version: "21.0.0+sha-4e14a53",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
@@ -623,7 +623,7 @@ class LocationUpgradeModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0+sha-6854776",
+  version: "21.0.0+sha-4e14a53",
   ngImport: i0,
   type: LocationUpgradeModule,
   decorators: [{
