5768cdecf231bda650db0f36a367a18da795ef5b
kirjs <kirts@google.com>
2025-11-20 16:50:36 +0000

    eadc8de docs: add 20 to 21 update guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-f349a91
+ * @license Angular v21.0.0+sha-eadc8de
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -600,7 +600,7 @@ class LocationUpgradeModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0+sha-f349a91",
+    version: "21.0.0+sha-eadc8de",
     ngImport: i0,
     type: LocationUpgradeModule,
     deps: [],
@@ -608,14 +608,14 @@ class LocationUpgradeModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.0.0+sha-f349a91",
+    version: "21.0.0+sha-eadc8de",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.0.0+sha-f349a91",
+    version: "21.0.0+sha-eadc8de",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
@@ -623,7 +623,7 @@ class LocationUpgradeModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0+sha-f349a91",
+  version: "21.0.0+sha-eadc8de",
   ngImport: i0,
   type: LocationUpgradeModule,
   decorators: [{
