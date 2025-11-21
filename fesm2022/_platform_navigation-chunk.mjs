df53c589db2fa2bf20d5f6012a5da414f7e63dbf
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-21 18:29:49 +0000

    88546ab docs: consistent casing for onpush zoneless migration MCP tool name

--- fesm2022/_platform_navigation-chunk.mjs
+++ fesm2022/_platform_navigation-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-8f346ad
+ * @license Angular v21.1.0-next.0+sha-88546ab
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -10,7 +10,7 @@ import { Injectable } from '@angular/core';
 class PlatformNavigation {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-8f346ad",
+    version: "21.1.0-next.0+sha-88546ab",
     ngImport: i0,
     type: PlatformNavigation,
     deps: [],
@@ -18,7 +18,7 @@ class PlatformNavigation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-8f346ad",
+    version: "21.1.0-next.0+sha-88546ab",
     ngImport: i0,
     type: PlatformNavigation,
     providedIn: 'platform',
@@ -27,7 +27,7 @@ class PlatformNavigation {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-8f346ad",
+  version: "21.1.0-next.0+sha-88546ab",
   ngImport: i0,
   type: PlatformNavigation,
   decorators: [{
