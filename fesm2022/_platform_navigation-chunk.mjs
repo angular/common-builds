546fd68d18fc598c244208f7962bed50a03606e9
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 14:37:35 +0000

    c7ae6f3 build: update all github actions

--- fesm2022/_platform_navigation-chunk.mjs
+++ fesm2022/_platform_navigation-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-92f0b66
+ * @license Angular v21.0.0+sha-c7ae6f3
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -10,7 +10,7 @@ import { Injectable } from '@angular/core';
 class PlatformNavigation {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0+sha-92f0b66",
+    version: "21.0.0+sha-c7ae6f3",
     ngImport: i0,
     type: PlatformNavigation,
     deps: [],
@@ -18,7 +18,7 @@ class PlatformNavigation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.0+sha-92f0b66",
+    version: "21.0.0+sha-c7ae6f3",
     ngImport: i0,
     type: PlatformNavigation,
     providedIn: 'platform',
@@ -27,7 +27,7 @@ class PlatformNavigation {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0+sha-92f0b66",
+  version: "21.0.0+sha-c7ae6f3",
   ngImport: i0,
   type: PlatformNavigation,
   decorators: [{
