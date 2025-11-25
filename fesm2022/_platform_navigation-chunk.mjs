d69334e936984629c3b3fa9d623b7dbf5cfebe21
GitHub <noreply@github.com>
2025-11-25 15:38:47 +0000

    c70e246 feat(forms): add DI option for classes on `Field` directive

--- fesm2022/_platform_navigation-chunk.mjs
+++ fesm2022/_platform_navigation-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-67d41e9
+ * @license Angular v21.1.0-next.0+sha-c70e246
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -10,7 +10,7 @@ import { Injectable } from '@angular/core';
 class PlatformNavigation {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-67d41e9",
+    version: "21.1.0-next.0+sha-c70e246",
     ngImport: i0,
     type: PlatformNavigation,
     deps: [],
@@ -18,7 +18,7 @@ class PlatformNavigation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-67d41e9",
+    version: "21.1.0-next.0+sha-c70e246",
     ngImport: i0,
     type: PlatformNavigation,
     providedIn: 'platform',
@@ -27,7 +27,7 @@ class PlatformNavigation {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-67d41e9",
+  version: "21.1.0-next.0+sha-c70e246",
   ngImport: i0,
   type: PlatformNavigation,
   decorators: [{
