a265f279656490332048a8e7e73bbeb70d4f0bfc
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 14:15:52 +0000

    e30e61b fix(compiler-cli): avoid allocating an object for signals in production mode

--- fesm2022/http.mjs
+++ fesm2022/http.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-f35b2ef
+ * @license Angular v21.1.0-next.0+sha-e30e61b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -83,11 +83,9 @@ class HttpResourceImpl extends _ResourceImpl {
     source: this.extRequest,
     computation: () => undefined
   });
-  headers = computed(() => this.status() === 'resolved' || this.status() === 'error' ? this._headers() : undefined, {
-    ...(ngDevMode ? {
-      debugName: "headers"
-    } : {})
-  });
+  headers = computed(() => this.status() === 'resolved' || this.status() === 'error' ? this._headers() : undefined, ...(ngDevMode ? [{
+    debugName: "headers"
+  }] : []));
   progress = this._progress.asReadonly();
   statusCode = this._statusCode.asReadonly();
   constructor(injector, request, defaultValue, debugName, parse, equal) {
@@ -100,11 +98,9 @@ class HttpResourceImpl extends _ResourceImpl {
       abortSignal.addEventListener('abort', onAbort);
       const stream = signal({
         value: undefined
-      }, {
-        ...(ngDevMode ? {
-          debugName: "stream"
-        } : {})
-      });
+      }, ...(ngDevMode ? [{
+        debugName: "stream"
+      }] : []));
       let resolve;
       const promise = new Promise(r => resolve = r);
       const send = value => {
