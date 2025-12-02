dd06643e8ed2a38f10a990c4ce6eea1655d57879
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 12:08:37 +0000

    f35b2ef refactor(compiler): Generate the `controlCreate` instruction after the native element has been created

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-a784995
+ * @license Angular v21.1.0-next.0+sha-f35b2ef
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -600,7 +600,7 @@ class LocationUpgradeModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-a784995",
+    version: "21.1.0-next.0+sha-f35b2ef",
     ngImport: i0,
     type: LocationUpgradeModule,
     deps: [],
@@ -608,14 +608,14 @@ class LocationUpgradeModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.1.0-next.0+sha-a784995",
+    version: "21.1.0-next.0+sha-f35b2ef",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-a784995",
+    version: "21.1.0-next.0+sha-f35b2ef",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
@@ -623,7 +623,7 @@ class LocationUpgradeModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-a784995",
+  version: "21.1.0-next.0+sha-f35b2ef",
   ngImport: i0,
   type: LocationUpgradeModule,
   decorators: [{
