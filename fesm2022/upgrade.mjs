a6ece403286ed3edf70d5f1295adab6221ef917e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-08 18:09:21 +0000

    253dc95 feat(docs-infra): add complexity level badge

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.1+sha-dd47c57
+ * @license Angular v21.1.0-next.1+sha-253dc95
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -601,7 +601,7 @@ class LocationUpgradeModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.1+sha-dd47c57",
+    version: "21.1.0-next.1+sha-253dc95",
     ngImport: i0,
     type: LocationUpgradeModule,
     deps: [],
@@ -609,14 +609,14 @@ class LocationUpgradeModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.1.0-next.1+sha-dd47c57",
+    version: "21.1.0-next.1+sha-253dc95",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.1.0-next.1+sha-dd47c57",
+    version: "21.1.0-next.1+sha-253dc95",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
@@ -624,7 +624,7 @@ class LocationUpgradeModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.1+sha-dd47c57",
+  version: "21.1.0-next.1+sha-253dc95",
   ngImport: i0,
   type: LocationUpgradeModule,
   decorators: [{
