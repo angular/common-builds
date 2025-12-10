fa359833e1aee98cb8dba690ef25be07290d5051
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-10 16:28:31 +0000

    01ab257 refactor(core): match spec updates for FakeNavigation

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.3+sha-96bb4c6
+ * @license Angular v21.0.3+sha-01ab257
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -601,7 +601,7 @@ class LocationUpgradeModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.3+sha-96bb4c6",
+    version: "21.0.3+sha-01ab257",
     ngImport: i0,
     type: LocationUpgradeModule,
     deps: [],
@@ -609,14 +609,14 @@ class LocationUpgradeModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.0.3+sha-96bb4c6",
+    version: "21.0.3+sha-01ab257",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.0.3+sha-96bb4c6",
+    version: "21.0.3+sha-01ab257",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
@@ -624,7 +624,7 @@ class LocationUpgradeModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.3+sha-96bb4c6",
+  version: "21.0.3+sha-01ab257",
   ngImport: i0,
   type: LocationUpgradeModule,
   decorators: [{
