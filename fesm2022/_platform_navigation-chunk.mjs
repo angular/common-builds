68e67cb051160ab4f19bc825bc8ce3c25fe7bd09
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 00:52:55 +0000

    fa4e6e6 release: cut the v21.0.4 release

--- fesm2022/_platform_navigation-chunk.mjs
+++ fesm2022/_platform_navigation-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.3+sha-c937da4
+ * @license Angular v21.0.4+sha-fa4e6e6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -10,7 +10,7 @@ import { Injectable } from '@angular/core';
 class PlatformNavigation {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.3+sha-c937da4",
+    version: "21.0.4+sha-fa4e6e6",
     ngImport: i0,
     type: PlatformNavigation,
     deps: [],
@@ -18,7 +18,7 @@ class PlatformNavigation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.3+sha-c937da4",
+    version: "21.0.4+sha-fa4e6e6",
     ngImport: i0,
     type: PlatformNavigation,
     providedIn: 'platform',
@@ -27,7 +27,7 @@ class PlatformNavigation {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.3+sha-c937da4",
+  version: "21.0.4+sha-fa4e6e6",
   ngImport: i0,
   type: PlatformNavigation,
   decorators: [{
