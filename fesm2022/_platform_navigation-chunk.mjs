b54e13cc6d40d4cf6887e5d73f726354ccfa1217
Andrew Kushnir <akushnir@google.com>
2025-12-12 21:04:11 +0000

    534b1b5 Revert "refactor(compiler): remove unnecessary sanitization for safe attributes"

--- fesm2022/_platform_navigation-chunk.mjs
+++ fesm2022/_platform_navigation-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-88e1829
+ * @license Angular v21.0.5+sha-534b1b5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -10,7 +10,7 @@ import { Injectable } from '@angular/core';
 class PlatformNavigation {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.5+sha-88e1829",
+    version: "21.0.5+sha-534b1b5",
     ngImport: i0,
     type: PlatformNavigation,
     deps: [],
@@ -18,7 +18,7 @@ class PlatformNavigation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.5+sha-88e1829",
+    version: "21.0.5+sha-534b1b5",
     ngImport: i0,
     type: PlatformNavigation,
     providedIn: 'platform',
@@ -27,7 +27,7 @@ class PlatformNavigation {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.5+sha-88e1829",
+  version: "21.0.5+sha-534b1b5",
   ngImport: i0,
   type: PlatformNavigation,
   decorators: [{
