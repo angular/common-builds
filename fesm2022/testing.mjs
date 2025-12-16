510c7a140ddb9a2a5a87e88b57b75f8b2e30f651
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-16 22:48:48 +0000

    6270bba ci: reformat files

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.3+sha-06be803
+ * @license Angular v21.1.0-next.3+sha-6270bba
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -202,7 +202,7 @@ class MockPlatformLocation {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.3+sha-06be803",
+    version: "21.1.0-next.3+sha-6270bba",
     ngImport: i0,
     type: MockPlatformLocation,
     deps: [{
@@ -213,14 +213,14 @@ class MockPlatformLocation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0-next.3+sha-06be803",
+    version: "21.1.0-next.3+sha-6270bba",
     ngImport: i0,
     type: MockPlatformLocation
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.3+sha-06be803",
+  version: "21.1.0-next.3+sha-6270bba",
   ngImport: i0,
   type: MockPlatformLocation,
   decorators: [{
@@ -300,7 +300,7 @@ class FakeNavigationPlatformLocation {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.3+sha-06be803",
+    version: "21.1.0-next.3+sha-6270bba",
     ngImport: i0,
     type: FakeNavigationPlatformLocation,
     deps: [],
@@ -308,14 +308,14 @@ class FakeNavigationPlatformLocation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0-next.3+sha-06be803",
+    version: "21.1.0-next.3+sha-6270bba",
     ngImport: i0,
     type: FakeNavigationPlatformLocation
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.3+sha-06be803",
+  version: "21.1.0-next.3+sha-6270bba",
   ngImport: i0,
   type: FakeNavigationPlatformLocation,
   decorators: [{
@@ -498,7 +498,7 @@ class SpyLocation {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.3+sha-06be803",
+    version: "21.1.0-next.3+sha-6270bba",
     ngImport: i0,
     type: SpyLocation,
     deps: [],
@@ -506,14 +506,14 @@ class SpyLocation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0-next.3+sha-06be803",
+    version: "21.1.0-next.3+sha-6270bba",
     ngImport: i0,
     type: SpyLocation
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.3+sha-06be803",
+  version: "21.1.0-next.3+sha-6270bba",
   ngImport: i0,
   type: SpyLocation,
   decorators: [{
@@ -594,7 +594,7 @@ class MockLocationStrategy extends LocationStrategy {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.3+sha-06be803",
+    version: "21.1.0-next.3+sha-6270bba",
     ngImport: i0,
     type: MockLocationStrategy,
     deps: [],
@@ -602,14 +602,14 @@ class MockLocationStrategy extends LocationStrategy {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0-next.3+sha-06be803",
+    version: "21.1.0-next.3+sha-6270bba",
     ngImport: i0,
     type: MockLocationStrategy
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.3+sha-06be803",
+  version: "21.1.0-next.3+sha-6270bba",
   ngImport: i0,
   type: MockLocationStrategy,
   decorators: [{
