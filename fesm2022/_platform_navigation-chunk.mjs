a76f847c11d584599169c7599929bc9d9e263d85
kirjs <kirts@google.com>
2026-01-06 22:29:35 +0000

    e3fba18 feat(forms): add `[formField]` directive

--- fesm2022/_platform_navigation-chunk.mjs
+++ fesm2022/_platform_navigation-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-defc924
+ * @license Angular v21.0.6+sha-e3fba18
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -10,7 +10,7 @@ import { Injectable } from '@angular/core';
 class PlatformNavigation {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.6+sha-defc924",
+    version: "21.0.6+sha-e3fba18",
     ngImport: i0,
     type: PlatformNavigation,
     deps: [],
@@ -18,7 +18,7 @@ class PlatformNavigation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.6+sha-defc924",
+    version: "21.0.6+sha-e3fba18",
     ngImport: i0,
     type: PlatformNavigation,
     providedIn: 'platform',
@@ -27,7 +27,7 @@ class PlatformNavigation {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.6+sha-defc924",
+  version: "21.0.6+sha-e3fba18",
   ngImport: i0,
   type: PlatformNavigation,
   decorators: [{
