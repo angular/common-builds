0cc8d422ea795a5381d7314655987a1e83e850be
kirjs <kirts@google.com>
2026-01-07 19:16:36 +0000

    35fe58f docs(vscode-extension): update angular.io links to angular.dev in messages

--- fesm2022/_platform_navigation-chunk.mjs
+++ fesm2022/_platform_navigation-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-c3f3ebe
+ * @license Angular v21.0.6+sha-35fe58f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -10,7 +10,7 @@ import { Injectable } from '@angular/core';
 class PlatformNavigation {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.6+sha-c3f3ebe",
+    version: "21.0.6+sha-35fe58f",
     ngImport: i0,
     type: PlatformNavigation,
     deps: [],
@@ -18,7 +18,7 @@ class PlatformNavigation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.6+sha-c3f3ebe",
+    version: "21.0.6+sha-35fe58f",
     ngImport: i0,
     type: PlatformNavigation,
     providedIn: 'platform',
@@ -27,7 +27,7 @@ class PlatformNavigation {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.6+sha-c3f3ebe",
+  version: "21.0.6+sha-35fe58f",
   ngImport: i0,
   type: PlatformNavigation,
   decorators: [{
