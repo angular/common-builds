84b6e0653ceb0b80310e9bc29e83bc44e049bd64
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-12 18:10:59 +0000

    511d9ca docs: remove Component suffix in animations, HTTP, and SSR guides

--- fesm2022/_platform_navigation-chunk.mjs
+++ fesm2022/_platform_navigation-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-b228355
+ * @license Angular v21.2.0-next.0+sha-511d9ca
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ const PRECOMMIT_HANDLER_SUPPORTED = new InjectionToken('', {
 class PlatformNavigation {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.0+sha-b228355",
+    version: "21.2.0-next.0+sha-511d9ca",
     ngImport: i0,
     type: PlatformNavigation,
     deps: [],
@@ -23,7 +23,7 @@ class PlatformNavigation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.0-next.0+sha-b228355",
+    version: "21.2.0-next.0+sha-511d9ca",
     ngImport: i0,
     type: PlatformNavigation,
     providedIn: 'platform',
@@ -32,7 +32,7 @@ class PlatformNavigation {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.0+sha-b228355",
+  version: "21.2.0-next.0+sha-511d9ca",
   ngImport: i0,
   type: PlatformNavigation,
   decorators: [{
