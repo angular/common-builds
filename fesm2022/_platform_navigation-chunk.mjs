350f0517e83eac117703c75fadc4381499ac442e
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 16:54:16 +0000

    e4dcd72 fix(docs-infra): improve symbolic green contrast for WCAG compliance

--- fesm2022/_platform_navigation-chunk.mjs
+++ fesm2022/_platform_navigation-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-204aaca
+ * @license Angular v21.1.0-rc.0+sha-e4dcd72
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ const PRECOMMIT_HANDLER_SUPPORTED = new InjectionToken('', {
 class PlatformNavigation {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-rc.0+sha-204aaca",
+    version: "21.1.0-rc.0+sha-e4dcd72",
     ngImport: i0,
     type: PlatformNavigation,
     deps: [],
@@ -23,7 +23,7 @@ class PlatformNavigation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0-rc.0+sha-204aaca",
+    version: "21.1.0-rc.0+sha-e4dcd72",
     ngImport: i0,
     type: PlatformNavigation,
     providedIn: 'platform',
@@ -32,7 +32,7 @@ class PlatformNavigation {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-rc.0+sha-204aaca",
+  version: "21.1.0-rc.0+sha-e4dcd72",
   ngImport: i0,
   type: PlatformNavigation,
   decorators: [{
