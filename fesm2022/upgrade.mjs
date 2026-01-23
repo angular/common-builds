5508384e1f7849e791c6daebc357e4ebe235ad1d
Andrew Scott <atscott01@gmail.com>
2026-01-23 18:47:52 +0000

    b203ab0 build: update bazel dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-f7eb996
+ * @license Angular v21.1.1+sha-b203ab0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -601,7 +601,7 @@ class LocationUpgradeModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.1+sha-f7eb996",
+    version: "21.1.1+sha-b203ab0",
     ngImport: i0,
     type: LocationUpgradeModule,
     deps: [],
@@ -609,14 +609,14 @@ class LocationUpgradeModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.1.1+sha-f7eb996",
+    version: "21.1.1+sha-b203ab0",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.1.1+sha-f7eb996",
+    version: "21.1.1+sha-b203ab0",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
@@ -624,7 +624,7 @@ class LocationUpgradeModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.1+sha-f7eb996",
+  version: "21.1.1+sha-b203ab0",
   ngImport: i0,
   type: LocationUpgradeModule,
   decorators: [{
