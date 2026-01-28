fcf00322a89bc03f5812f12d7ec6320a43a4d0ac
Andrew Scott <atscott01@gmail.com>
2026-01-28 20:53:52 +0000

    5a6fefb release: cut the v21.2.0-next.1 release

--- fesm2022/_platform_navigation-chunk.mjs
+++ fesm2022/_platform_navigation-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-571747d
+ * @license Angular v21.2.0-next.1+sha-5a6fefb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ const PRECOMMIT_HANDLER_SUPPORTED = new InjectionToken('', {
 class PlatformNavigation {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.0+sha-571747d",
+    version: "21.2.0-next.1+sha-5a6fefb",
     ngImport: i0,
     type: PlatformNavigation,
     deps: [],
@@ -23,7 +23,7 @@ class PlatformNavigation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.0-next.0+sha-571747d",
+    version: "21.2.0-next.1+sha-5a6fefb",
     ngImport: i0,
     type: PlatformNavigation,
     providedIn: 'platform',
@@ -32,7 +32,7 @@ class PlatformNavigation {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.0+sha-571747d",
+  version: "21.2.0-next.1+sha-5a6fefb",
   ngImport: i0,
   type: PlatformNavigation,
   decorators: [{
