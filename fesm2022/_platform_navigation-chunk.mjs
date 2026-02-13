6ad8fbd9bbcaf50789af0f492221f686a1972055
Matthew Beck (Berry) <me@mattrb.com>
2026-02-13 17:48:49 +0000

    e10a634 refactor(compiler-cli): use phaseModifier for type-only import detection

--- fesm2022/_platform_navigation-chunk.mjs
+++ fesm2022/_platform_navigation-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-dfd5e3a
+ * @license Angular v21.2.0-next.3+sha-e10a634
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ const PRECOMMIT_HANDLER_SUPPORTED = new InjectionToken('', {
 class PlatformNavigation {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.3+sha-dfd5e3a",
+    version: "21.2.0-next.3+sha-e10a634",
     ngImport: i0,
     type: PlatformNavigation,
     deps: [],
@@ -23,7 +23,7 @@ class PlatformNavigation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.0-next.3+sha-dfd5e3a",
+    version: "21.2.0-next.3+sha-e10a634",
     ngImport: i0,
     type: PlatformNavigation,
     providedIn: 'platform',
@@ -32,7 +32,7 @@ class PlatformNavigation {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.3+sha-dfd5e3a",
+  version: "21.2.0-next.3+sha-e10a634",
   ngImport: i0,
   type: PlatformNavigation,
   decorators: [{
