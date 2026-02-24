d4ddbbf3d4c565f57a0ae4848ca193e1476546f0
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-24 22:16:57 +0000

    63cb3f4 build: update bazel dependencies

--- fesm2022/_platform_navigation-chunk.mjs
+++ fesm2022/_platform_navigation-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-787cd87
+ * @license Angular v21.2.0-rc.0+sha-63cb3f4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ const PRECOMMIT_HANDLER_SUPPORTED = new InjectionToken('', {
 class PlatformNavigation {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-rc.0+sha-787cd87",
+    version: "21.2.0-rc.0+sha-63cb3f4",
     ngImport: i0,
     type: PlatformNavigation,
     deps: [],
@@ -23,7 +23,7 @@ class PlatformNavigation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.0-rc.0+sha-787cd87",
+    version: "21.2.0-rc.0+sha-63cb3f4",
     ngImport: i0,
     type: PlatformNavigation,
     providedIn: 'platform',
@@ -32,7 +32,7 @@ class PlatformNavigation {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-rc.0+sha-787cd87",
+  version: "21.2.0-rc.0+sha-63cb3f4",
   ngImport: i0,
   type: PlatformNavigation,
   decorators: [{
