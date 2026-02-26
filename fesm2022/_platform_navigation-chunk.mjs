dc139931d4f07e0b5f81b9613ac6c57823b9c43d
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-26 22:38:24 +0000

    4e9648d build: update cross-repo angular dependencies

--- fesm2022/_platform_navigation-chunk.mjs
+++ fesm2022/_platform_navigation-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-54a0541
+ * @license Angular v21.2.0+sha-4e9648d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ const PRECOMMIT_HANDLER_SUPPORTED = new InjectionToken('', {
 class PlatformNavigation {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0+sha-54a0541",
+    version: "21.2.0+sha-4e9648d",
     ngImport: i0,
     type: PlatformNavigation,
     deps: [],
@@ -23,7 +23,7 @@ class PlatformNavigation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.0+sha-54a0541",
+    version: "21.2.0+sha-4e9648d",
     ngImport: i0,
     type: PlatformNavigation,
     providedIn: 'platform',
@@ -32,7 +32,7 @@ class PlatformNavigation {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0+sha-54a0541",
+  version: "21.2.0+sha-4e9648d",
   ngImport: i0,
   type: PlatformNavigation,
   decorators: [{
