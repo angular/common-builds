a7169b150f5255c578171d5a561f12efe3c2c311
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-03-03 22:52:06 +0000

    d9e40cb build: update dependency jasmine to v6

--- fesm2022/_platform_location-chunk.mjs
+++ fesm2022/_platform_location-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-0e9d58e
+ * @license Angular v22.0.0-next.0+sha-d9e40cb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -22,7 +22,7 @@ class PlatformLocation {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.0+sha-0e9d58e",
+    version: "22.0.0-next.0+sha-d9e40cb",
     ngImport: i0,
     type: PlatformLocation,
     deps: [],
@@ -30,7 +30,7 @@ class PlatformLocation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-next.0+sha-0e9d58e",
+    version: "22.0.0-next.0+sha-d9e40cb",
     ngImport: i0,
     type: PlatformLocation,
     providedIn: 'platform',
@@ -39,7 +39,7 @@ class PlatformLocation {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.0+sha-0e9d58e",
+  version: "22.0.0-next.0+sha-d9e40cb",
   ngImport: i0,
   type: PlatformLocation,
   decorators: [{
@@ -117,7 +117,7 @@ class BrowserPlatformLocation extends PlatformLocation {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.0+sha-0e9d58e",
+    version: "22.0.0-next.0+sha-d9e40cb",
     ngImport: i0,
     type: BrowserPlatformLocation,
     deps: [],
@@ -125,7 +125,7 @@ class BrowserPlatformLocation extends PlatformLocation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-next.0+sha-0e9d58e",
+    version: "22.0.0-next.0+sha-d9e40cb",
     ngImport: i0,
     type: BrowserPlatformLocation,
     providedIn: 'platform',
@@ -134,7 +134,7 @@ class BrowserPlatformLocation extends PlatformLocation {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.0+sha-0e9d58e",
+  version: "22.0.0-next.0+sha-d9e40cb",
   ngImport: i0,
   type: BrowserPlatformLocation,
   decorators: [{
