fdec4ab31cacccd6473ffddf0ea418e069359d18
Andrew Scott <atscott01@gmail.com>
2026-03-10 17:08:32 +0000

    9794e75 docs: fix first-app code getHousingLocationById housing.service.ts

--- types/common.d.ts
+++ types/common.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.1+sha-2049698
+ * @license Angular v22.0.0-next.1+sha-9794e75
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -845,7 +845,7 @@ declare const VERSION: Version;
  */
 declare abstract class ViewportScroller {
     /** @nocollapse */
-    static ɵprov: unknown;
+    static ɵprov: i0.ɵɵInjectableDeclaration<NullViewportScroller | BrowserViewportScroller>;
     /**
      * Configures the top offset used when scrolling to an anchor.
      * @param offset A position in screen coordinates (a tuple with x and y values)
@@ -874,6 +874,55 @@ declare abstract class ViewportScroller {
      * info](https://developers.google.com/web/updates/2015/09/history-api-scroll-restoration).
      */
     abstract setHistoryScrollRestoration(scrollRestoration: 'auto' | 'manual'): void;
+}
+/**
+ * Manages the scroll position for a browser window.
+ */
+declare class BrowserViewportScroller implements ViewportScroller {
+    private document;
+    private window;
+    private offset;
+    constructor(document: Document, window: Window);
+    /**
+     * Configures the top offset used when scrolling to an anchor.
+     * @param offset A position in screen coordinates (a tuple with x and y values)
+     * or a function that returns the top offset position.
+     *
+     */
+    setOffset(offset: [number, number] | (() => [number, number])): void;
+    /**
+     * Retrieves the current scroll position.
+     * @returns The position in screen coordinates.
+     */
+    getScrollPosition(): [number, number];
+    /**
+     * Sets the scroll position.
+     * @param position The new position in screen coordinates.
+     */
+    scrollToPosition(position: [number, number], options?: ScrollOptions): void;
+    /**
+     * Scrolls to an element and attempts to focus the element.
+     *
+     * Note that the function name here is misleading in that the target string may be an ID for a
+     * non-anchor element.
+     *
+     * @param target The ID of an element or name of the anchor.
+     *
+     * @see https://html.spec.whatwg.org/#the-indicated-part-of-the-document
+     * @see https://html.spec.whatwg.org/#scroll-to-fragid
+     */
+    scrollToAnchor(target: string, options?: ScrollOptions): void;
+    /**
+     * Disables automatic scroll restoration provided by the browser.
+     */
+    setHistoryScrollRestoration(scrollRestoration: 'auto' | 'manual'): void;
+    /**
+     * Scrolls to an element using the native offset and the specified offset set on this scroller.
+     *
+     * The offset can be used when we know that there is a floating header and scrolling naively to an
+     * element (ex: `scrollIntoView`) leaves the element hidden behind the floating header.
+     */
+    private scrollToElement;
 }
 /**
  * Provides an empty implementation of the viewport scroller.
