58b44bb2568f2885d0076f8ed4df0255550ef287
Matthew Beck (Berry) <me@mattrb.com>
2026-03-12 23:42:00 +0000

    17d8a88 refactor(core): Update determineLongestAnimation to account for playback rate

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.3+sha-89c9a4d
+ * @license Angular v22.0.0-next.3+sha-17d8a88
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -601,7 +601,7 @@ class LocationUpgradeModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.3+sha-89c9a4d",
+    version: "22.0.0-next.3+sha-17d8a88",
     ngImport: i0,
     type: LocationUpgradeModule,
     deps: [],
@@ -609,14 +609,14 @@ class LocationUpgradeModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "22.0.0-next.3+sha-89c9a4d",
+    version: "22.0.0-next.3+sha-17d8a88",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "22.0.0-next.3+sha-89c9a4d",
+    version: "22.0.0-next.3+sha-17d8a88",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
@@ -624,7 +624,7 @@ class LocationUpgradeModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.3+sha-89c9a4d",
+  version: "22.0.0-next.3+sha-17d8a88",
   ngImport: i0,
   type: LocationUpgradeModule,
   decorators: [{
