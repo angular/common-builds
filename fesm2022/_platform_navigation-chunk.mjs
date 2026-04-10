14d16b282aec9e6f293fe978a9d2ee27248816e3
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-10 19:06:28 +0000

    a89b565 refactor(language-service): Avoid deep import of comments helper

--- fesm2022/_platform_navigation-chunk.mjs
+++ fesm2022/_platform_navigation-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.7+sha-e453848
+ * @license Angular v22.0.0-next.7+sha-a89b565
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ const PRECOMMIT_HANDLER_SUPPORTED = new InjectionToken('', {
 class PlatformNavigation {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.7+sha-e453848",
+    version: "22.0.0-next.7+sha-a89b565",
     ngImport: i0,
     type: PlatformNavigation,
     deps: [],
@@ -23,7 +23,7 @@ class PlatformNavigation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-next.7+sha-e453848",
+    version: "22.0.0-next.7+sha-a89b565",
     ngImport: i0,
     type: PlatformNavigation,
     providedIn: 'platform',
@@ -32,7 +32,7 @@ class PlatformNavigation {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.7+sha-e453848",
+  version: "22.0.0-next.7+sha-a89b565",
   ngImport: i0,
   type: PlatformNavigation,
   decorators: [{
