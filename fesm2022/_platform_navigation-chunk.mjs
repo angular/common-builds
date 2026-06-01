d3d52b3b402084544d8c47c7fe21d0715acdc4c4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 10:24:20 +0000

    15f7f8a docs: fix esbuild and TensorFlow branding on overview page

--- fesm2022/_platform_navigation-chunk.mjs
+++ fesm2022/_platform_navigation-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.2+sha-d82c364
+ * @license Angular v22.0.0-rc.2+sha-15f7f8a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ const PRECOMMIT_HANDLER_SUPPORTED = new InjectionToken('', {
 class PlatformNavigation {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-rc.2+sha-d82c364",
+    version: "22.0.0-rc.2+sha-15f7f8a",
     ngImport: i0,
     type: PlatformNavigation,
     deps: [],
@@ -23,7 +23,7 @@ class PlatformNavigation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-rc.2+sha-d82c364",
+    version: "22.0.0-rc.2+sha-15f7f8a",
     ngImport: i0,
     type: PlatformNavigation,
     providedIn: 'platform',
@@ -32,7 +32,7 @@ class PlatformNavigation {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-rc.2+sha-d82c364",
+  version: "22.0.0-rc.2+sha-15f7f8a",
   ngImport: i0,
   type: PlatformNavigation,
   decorators: [{
