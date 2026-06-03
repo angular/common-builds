b9eeef6cd2bc4595acafe29fa50ff6da5ac2f483
Andrew Scott <atscott01@gmail.com>
2026-06-03 19:15:52 +0000

    3d9ca2f fix(compiler-cli): bind switch exhaustive check expressions

--- fesm2022/_platform_navigation-chunk.mjs
+++ fesm2022/_platform_navigation-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-32b348a
+ * @license Angular v22.0.0+sha-3d9ca2f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ const PRECOMMIT_HANDLER_SUPPORTED = new InjectionToken('', {
 class PlatformNavigation {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0+sha-32b348a",
+    version: "22.0.0+sha-3d9ca2f",
     ngImport: i0,
     type: PlatformNavigation,
     deps: [],
@@ -23,7 +23,7 @@ class PlatformNavigation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0+sha-32b348a",
+    version: "22.0.0+sha-3d9ca2f",
     ngImport: i0,
     type: PlatformNavigation,
     providedIn: 'platform',
@@ -32,7 +32,7 @@ class PlatformNavigation {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0+sha-32b348a",
+  version: "22.0.0+sha-3d9ca2f",
   ngImport: i0,
   type: PlatformNavigation,
   decorators: [{
