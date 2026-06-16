4cded9d43b0101dc1e40d4e0fe2abab0cc90b1b8
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-16 15:24:26 +0000

    383a42a build: update cross-repo angular dependencies

--- fesm2022/_xhr-chunk.mjs
+++ fesm2022/_xhr-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-83622ee
+ * @license Angular v22.1.0-next.0+sha-383a42a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -25,7 +25,7 @@ class BrowserXhr {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.1.0-next.0+sha-83622ee",
+    version: "22.1.0-next.0+sha-383a42a",
     ngImport: i0,
     type: BrowserXhr,
     deps: [],
@@ -33,14 +33,14 @@ class BrowserXhr {
   });
   static ɵprov = i0.ɵɵngDeclareService({
     minVersion: "22.0.0",
-    version: "22.1.0-next.0+sha-83622ee",
+    version: "22.1.0-next.0+sha-383a42a",
     ngImport: i0,
     type: BrowserXhr
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.1.0-next.0+sha-83622ee",
+  version: "22.1.0-next.0+sha-383a42a",
   ngImport: i0,
   type: BrowserXhr,
   decorators: [{
@@ -50,7 +50,7 @@ i0.ɵɵngDeclareClassMetadata({
 class XhrFactory {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.1.0-next.0+sha-83622ee",
+    version: "22.1.0-next.0+sha-383a42a",
     ngImport: i0,
     type: XhrFactory,
     deps: [],
@@ -58,7 +58,7 @@ class XhrFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.1.0-next.0+sha-83622ee",
+    version: "22.1.0-next.0+sha-383a42a",
     ngImport: i0,
     type: XhrFactory,
     providedIn: 'root',
@@ -67,7 +67,7 @@ class XhrFactory {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.1.0-next.0+sha-83622ee",
+  version: "22.1.0-next.0+sha-383a42a",
   ngImport: i0,
   type: XhrFactory,
   decorators: [{
