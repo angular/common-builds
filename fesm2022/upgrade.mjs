99b03dac1a5482fd58c49acdbf6c164652429c9c
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-16 15:54:12 +0000

    f2c5045 docs: use the @Service decorator in the learn-angular DI tutorial

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-93d0a5f
+ * @license Angular v22.0.1+sha-f2c5045
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -601,7 +601,7 @@ class LocationUpgradeModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.1+sha-93d0a5f",
+    version: "22.0.1+sha-f2c5045",
     ngImport: i0,
     type: LocationUpgradeModule,
     deps: [],
@@ -609,14 +609,14 @@ class LocationUpgradeModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "22.0.1+sha-93d0a5f",
+    version: "22.0.1+sha-f2c5045",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "22.0.1+sha-93d0a5f",
+    version: "22.0.1+sha-f2c5045",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
@@ -624,7 +624,7 @@ class LocationUpgradeModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.1+sha-93d0a5f",
+  version: "22.0.1+sha-f2c5045",
   ngImport: i0,
   type: LocationUpgradeModule,
   decorators: [{
