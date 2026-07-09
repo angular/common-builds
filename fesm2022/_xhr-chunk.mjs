8635efe03d6bfceffa1d6f175e36e4a3f7464e25
Matthew Beck <me@mattrb.com>
2026-07-09 19:08:48 +0000

    cd6b903 feat(docs-infra): add Angie to the embedded editor loading steps

--- fesm2022/_xhr-chunk.mjs
+++ fesm2022/_xhr-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.6+sha-aa03cb9
+ * @license Angular v22.0.6+sha-cd6b903
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -25,7 +25,7 @@ class BrowserXhr {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.6+sha-aa03cb9",
+    version: "22.0.6+sha-cd6b903",
     ngImport: i0,
     type: BrowserXhr,
     deps: [],
@@ -33,14 +33,14 @@ class BrowserXhr {
   });
   static ɵprov = i0.ɵɵngDeclareService({
     minVersion: "22.0.0",
-    version: "22.0.6+sha-aa03cb9",
+    version: "22.0.6+sha-cd6b903",
     ngImport: i0,
     type: BrowserXhr
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.6+sha-aa03cb9",
+  version: "22.0.6+sha-cd6b903",
   ngImport: i0,
   type: BrowserXhr,
   decorators: [{
@@ -50,7 +50,7 @@ i0.ɵɵngDeclareClassMetadata({
 class XhrFactory {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.6+sha-aa03cb9",
+    version: "22.0.6+sha-cd6b903",
     ngImport: i0,
     type: XhrFactory,
     deps: [],
@@ -58,7 +58,7 @@ class XhrFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.6+sha-aa03cb9",
+    version: "22.0.6+sha-cd6b903",
     ngImport: i0,
     type: XhrFactory,
     providedIn: 'root',
@@ -67,7 +67,7 @@ class XhrFactory {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.6+sha-aa03cb9",
+  version: "22.0.6+sha-cd6b903",
   ngImport: i0,
   type: XhrFactory,
   decorators: [{
