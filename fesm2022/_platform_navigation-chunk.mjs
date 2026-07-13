f022f84509987d95bfc92c68c067cb2c0e5ab072
Matthew Beck <me@mattrb.com>
2026-07-13 20:05:19 +0000

    d55f5c4 docs: Add mention that about signal narrowing on `@switch` blocks

--- fesm2022/_platform_navigation-chunk.mjs
+++ fesm2022/_platform_navigation-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.5+sha-f6a00ff
+ * @license Angular v22.1.0-next.5+sha-d55f5c4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ const PRECOMMIT_HANDLER_SUPPORTED = new InjectionToken('', {
 class PlatformNavigation {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.1.0-next.5+sha-f6a00ff",
+    version: "22.1.0-next.5+sha-d55f5c4",
     ngImport: i0,
     type: PlatformNavigation,
     deps: [],
@@ -23,7 +23,7 @@ class PlatformNavigation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.1.0-next.5+sha-f6a00ff",
+    version: "22.1.0-next.5+sha-d55f5c4",
     ngImport: i0,
     type: PlatformNavigation,
     providedIn: 'platform',
@@ -32,7 +32,7 @@ class PlatformNavigation {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.1.0-next.5+sha-f6a00ff",
+  version: "22.1.0-next.5+sha-d55f5c4",
   ngImport: i0,
   type: PlatformNavigation,
   decorators: [{
