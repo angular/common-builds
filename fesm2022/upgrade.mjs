0d2c1d382210567761756b0e8f27dbca5d7796fe
Matthew Beck <me@mattrb.com>
2026-07-16 04:27:46 +0000

    f8df06d release: cut the v22.0.7 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.6+sha-3b82aea
+ * @license Angular v22.0.7+sha-f8df06d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -601,7 +601,7 @@ class LocationUpgradeModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.6+sha-3b82aea",
+    version: "22.0.7+sha-f8df06d",
     ngImport: i0,
     type: LocationUpgradeModule,
     deps: [],
@@ -609,14 +609,14 @@ class LocationUpgradeModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "22.0.6+sha-3b82aea",
+    version: "22.0.7+sha-f8df06d",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "22.0.6+sha-3b82aea",
+    version: "22.0.7+sha-f8df06d",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
@@ -624,7 +624,7 @@ class LocationUpgradeModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.6+sha-3b82aea",
+  version: "22.0.7+sha-f8df06d",
   ngImport: i0,
   type: LocationUpgradeModule,
   decorators: [{
