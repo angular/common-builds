6eb47aaa6d078d2a8401b82cffa61fe40700be07
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-18 16:08:19 +0000

    85f12a5 fix(core): stop running further effects once one destroys the view mid-flush

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.2+sha-b3c78a5
+ * @license Angular v22.1.2+sha-85f12a5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -601,7 +601,7 @@ class LocationUpgradeModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.1.2+sha-b3c78a5",
+    version: "22.1.2+sha-85f12a5",
     ngImport: i0,
     type: LocationUpgradeModule,
     deps: [],
@@ -609,14 +609,14 @@ class LocationUpgradeModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "22.1.2+sha-b3c78a5",
+    version: "22.1.2+sha-85f12a5",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "22.1.2+sha-b3c78a5",
+    version: "22.1.2+sha-85f12a5",
     ngImport: i0,
     type: LocationUpgradeModule,
     imports: [CommonModule]
@@ -624,7 +624,7 @@ class LocationUpgradeModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.1.2+sha-b3c78a5",
+  version: "22.1.2+sha-85f12a5",
   ngImport: i0,
   type: LocationUpgradeModule,
   decorators: [{
