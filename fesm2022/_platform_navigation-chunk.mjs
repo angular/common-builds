ef68b8ac1b33dec707b64d15e98bd26695da73f7
Andrew Scott <atscott01@gmail.com>
2026-09-11 23:07:57 +0000

    fc2f6fd docs: stop the disabled aria toolbar radio group from changing selection

--- fesm2022/_platform_navigation-chunk.mjs
+++ fesm2022/_platform_navigation-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.7+sha-f2cf65a
+ * @license Angular v22.2.0-next.7+sha-fc2f6fd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ const PRECOMMIT_HANDLER_SUPPORTED = new InjectionToken('', {
 class PlatformNavigation {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.2.0-next.7+sha-f2cf65a",
+    version: "22.2.0-next.7+sha-fc2f6fd",
     ngImport: i0,
     type: PlatformNavigation,
     deps: [],
@@ -23,7 +23,7 @@ class PlatformNavigation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.2.0-next.7+sha-f2cf65a",
+    version: "22.2.0-next.7+sha-fc2f6fd",
     ngImport: i0,
     type: PlatformNavigation,
     providedIn: 'platform',
@@ -32,7 +32,7 @@ class PlatformNavigation {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.2.0-next.7+sha-f2cf65a",
+  version: "22.2.0-next.7+sha-fc2f6fd",
   ngImport: i0,
   type: PlatformNavigation,
   decorators: [{
