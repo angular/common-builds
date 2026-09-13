3481a8716dd455ed5ec8f25da7fa6913d83c83fd
Andrew Scott <atscott01@gmail.com>
2026-09-13 21:25:29 +0000

    16c8064 test(core): reduce fakeAsync usage in animation tests

--- fesm2022/_platform_navigation-chunk.mjs
+++ fesm2022/_platform_navigation-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.6+sha-8509045
+ * @license Angular v22.1.6+sha-16c8064
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ const PRECOMMIT_HANDLER_SUPPORTED = new InjectionToken('', {
 class PlatformNavigation {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.1.6+sha-8509045",
+    version: "22.1.6+sha-16c8064",
     ngImport: i0,
     type: PlatformNavigation,
     deps: [],
@@ -23,7 +23,7 @@ class PlatformNavigation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.1.6+sha-8509045",
+    version: "22.1.6+sha-16c8064",
     ngImport: i0,
     type: PlatformNavigation,
     providedIn: 'platform',
@@ -32,7 +32,7 @@ class PlatformNavigation {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.1.6+sha-8509045",
+  version: "22.1.6+sha-16c8064",
   ngImport: i0,
   type: PlatformNavigation,
   decorators: [{
